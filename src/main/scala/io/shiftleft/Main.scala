3de41a8e95087053c34cc0df14450b0117aeb51d
Behrad Garmany <behrad.garmany@rub.de>
2019-03-20 19:26:34 +0100

    Documentation I (#2)
    * documentation act I started
    
    * new version
    
    * added imports  for dataflow and queries

--- src/main/scala/io/shiftleft/Main.scala
+++ src/main/scala/io/shiftleft/Main.scala
@@ -1,6 +1,8 @@
 package io.shiftleft
 
 import io.shiftleft.cpgloading.tinkergraph.CpgLoader
+import io.shiftleft.queryprimitives.steps.Implicits._
+import io.shiftleft.passes.dataflows._
 
 object Main extends App {
   val cpg = CpgLoader.loadCodePropertyGraph(args(0), runEnhancements = true)
