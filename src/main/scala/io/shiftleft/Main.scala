61715027c5de3be59bd27ca569cbab941155821a
behrad <behrad@shiftleft.io>
2019-03-26 15:44:24 +0100

    made barebone Main.scala consistent with readme

--- src/main/scala/io/shiftleft/Main.scala
+++ src/main/scala/io/shiftleft/Main.scala
@@ -3,11 +3,18 @@ package io.shiftleft
 import io.shiftleft.cpgloading.tinkergraph.CpgLoader
 import io.shiftleft.queryprimitives.steps.Implicits._
 import io.shiftleft.passes.dataflows._
+import io.shiftleft.codepropertygraph.generated.nodes
+import io.shiftleft.passes.dataflows.steps._
 
 object Main extends App {
   val cpg = CpgLoader.loadCodePropertyGraph(args(0), runEnhancements = true)
+  implicit val graph = cpg.graph
 
-  // Print all methods starting with "<operator>"
+  // Print all methods
   cpg.method.p
 
+  // Print all method names
+  cpg.method.name.p
+
 }
+
