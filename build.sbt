12631608a774e6b0633b2554e52114374dcc19fe
Markus Lottmann <markus@shiftleft.io>
2019-04-04 11:32:05 +0200

    Make compile with new version.

--- build.sbt
+++ build.sbt
@@ -2,11 +2,10 @@ name := "joern"
 organization := "io.shiftleft"
 scalaVersion := "2.12.7"
 
-val cpgVersion = "0.9.109"
+val cpgVersion = "0.9.120"
 
 libraryDependencies ++= Seq(
   "io.shiftleft" % "codepropertygraph" % cpgVersion,
-  "io.shiftleft" % "cpgloader-tinkergraph" % cpgVersion,
   "io.shiftleft" % "query-primitives" % cpgVersion,
   "io.shiftleft" % "enhancements" % cpgVersion,
   "io.shiftleft" % "semanticcpg" % cpgVersion,
--- src/main/scala/io/shiftleft/Main.scala
+++ src/main/scala/io/shiftleft/Main.scala
@@ -1,13 +1,12 @@
 package io.shiftleft
 
-import io.shiftleft.cpgloading.tinkergraph.CpgLoader
 import io.shiftleft.queryprimitives.steps.Implicits._
-import io.shiftleft.passes.dataflows._
+import io.shiftleft.passes.dataflows.Implicits._
 import io.shiftleft.codepropertygraph.generated.nodes
 import io.shiftleft.passes.dataflows.steps._
 
 object Main extends App {
-  val cpg = CpgLoader.loadCodePropertyGraph(args(0), runEnhancements = true)
+  val cpg = CpgLoader.loadCodePropertyGraph(args(0))
   implicit val graph = cpg.graph
 
   // Print all methods
