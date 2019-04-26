4cf3578d3441fe5b09ff5d02c264297e8b5ecb3e
Markus Lottmann <markus@shiftleft.io>
2019-04-26 11:13:20 +0200

    Bump codepropertygraph version and adjust imports. (#13)

--- src/main/scala/io/shiftleft/Main.scala
+++ src/main/scala/io/shiftleft/Main.scala
@@ -1,9 +1,7 @@
 package io.shiftleft
 
-import io.shiftleft.queryprimitives.steps.Implicits._
-import io.shiftleft.passes.dataflows.Implicits._
-import io.shiftleft.codepropertygraph.generated.nodes
-import io.shiftleft.passes.dataflows.steps._
+import io.shiftleft.cpgloading.CpgLoader
+
 
 object Main extends App {
   val cpg = CpgLoader.loadCodePropertyGraph(args(0))
