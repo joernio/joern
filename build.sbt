9b291080cdb3e10e764a380c29b97222aae783a1
Fabian Yamaguchi <fabs@shiftleft.io>
2019-05-07 15:36:08 +0200

    Tune CPG loading.

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "joern"
 organization := "io.shiftleft"
 scalaVersion := "2.12.8"
 
-val cpgVersion = "0.9.147"
+val cpgVersion = "0.9.157"
 
 libraryDependencies ++= Seq(
   "io.shiftleft" % "codepropertygraph" % cpgVersion,
--- src/main/scala/io/shiftleft/Main.scala
+++ src/main/scala/io/shiftleft/Main.scala
@@ -1,16 +1,8 @@
 package io.shiftleft
 
-import io.shiftleft.cpgloading.CpgLoader
-
+import io.shiftleft.joern.CpgLoader
 
 object Main extends App {
-  val cpg = CpgLoader.loadCodePropertyGraph(args(0), Some("src/main/resources/default.argdef"))
-  implicit val graph = cpg.graph
-
-  println("------------------")
-
-  // Print all method names
+ val cpg = CpgLoader.load(args(0))
   println(cpg.method.name.p)
-
 }
-
