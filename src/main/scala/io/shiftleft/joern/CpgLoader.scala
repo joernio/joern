f9e5486950140dd965c1879d6c5833f9b57899f4
Fabian Yamaguchi <fabs@shiftleft.io>
2019-05-10 18:55:01 +0200

    WIP: apply enhancements at generation time.

--- src/main/scala/io/shiftleft/joern/CpgLoader.scala
+++ src/main/scala/io/shiftleft/joern/CpgLoader.scala
@@ -1,10 +1,7 @@
 package io.shiftleft.joern
 
-import io.shiftleft.SerializedCpg
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.codepropertygraph.cpgloading.CpgLoaderConfig
-import io.shiftleft.layers.EnhancementRunner
-import io.shiftleft.semanticsloader.SemanticsLoader
 
 /**
   * Thin wrapper around `codepropertygraph`'s CpgLoader
@@ -14,18 +11,11 @@ object CpgLoader {
   /**
     * Load code property graph
     * @param filename name of the file that stores the cpg
-    * @param semanticsFilenameOption file from which semantics are loaded. If not provided defaults are loaded.
+    * @param semanticsFilename file from which semantics are loaded. If not provided defaults are loaded.
     * */
-  def load(filename: String, semanticsFilenameOption: Option[String] = None): Cpg = {
+  def load(filename: String, semanticsFilename: Option[String] = None): Cpg = {
     val config = CpgLoaderConfig.default
-    val cpg = io.shiftleft.codepropertygraph.cpgloading.CpgLoader.load(filename, config)
-
-
-    val semanticsFilename = semanticsFilenameOption.getOrElse("src/main/resources/default.semantics")
-    val semantics = new SemanticsLoader(semanticsFilename).load
-    new EnhancementRunner(semantics).run(cpg, new SerializedCpg())
-
-    cpg
+    io.shiftleft.codepropertygraph.cpgloading.CpgLoader.load(filename, config)
   }
 
 }
