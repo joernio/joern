c7cab2a83e7c47541272ab2447cb486bd20ebabb
Fabian Yamaguchi <fabs@shiftleft.io>
2019-05-16 15:22:38 +0200

    Clean exit if port is in use. (#53)

--- joern-server/src/main/scala/io/shiftleft/joern/server/JettyLauncher.scala
+++ joern-server/src/main/scala/io/shiftleft/joern/server/JettyLauncher.scala
@@ -4,6 +4,7 @@ import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.servlet.DefaultServlet
 import org.eclipse.jetty.webapp.WebAppContext
 import org.scalatra.servlet.ScalatraListener
+import org.slf4j.LoggerFactory
 
 /**
   * Launcher to enable running of cpg-server as a
@@ -11,7 +12,9 @@ import org.scalatra.servlet.ScalatraListener
   * */
 object JettyLauncher extends App {
 
-  val port = if (System.getenv("PORT") != null) System.getenv("PORT").toInt else 8080
+  val logger = LoggerFactory.getLogger(getClass)
+
+  val port = if (System.getenv("JOERN_PORT") != null) System.getenv("PORT").toInt else 8080
   val server = new Server(port)
   val context = new WebAppContext()
   context setContextPath "/"
@@ -20,7 +23,16 @@ object JettyLauncher extends App {
   context.addServlet(classOf[DefaultServlet], "/")
 
   server.setHandler(context)
-  server.start
+
+  try {
+    server.start
+  } catch {
+    case exception: java.net.BindException => {
+      logger.warn(s"Not starting server - port $port is occupied")
+      System.exit(1)
+    }
+  }
+
   server.join
 
 }
