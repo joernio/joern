422d132bfd8ae0f339cd0a25001b3be15fd7794b
Jacob Taylor-Hindle <GlassAndOneHalf@users.noreply.github.com>
2019-10-01 16:08:36 +0200

    Ensure CPG creation is fully asynchronous. (#106)
    * Ensure CPG creation is fully asynchronous.
    
    * Run scalafmt.

--- joern-server/src/test/scala/io/shiftleft/joern/server/cpg/JoernCpgProviderSpec.scala
+++ joern-server/src/test/scala/io/shiftleft/joern/server/cpg/JoernCpgProviderSpec.scala
@@ -1,12 +1,20 @@
 package io.shiftleft.joern.server.cpg
 
+import scala.concurrent.ExecutionContext
+
+import cats.effect.{ContextShift, IO}
 import javax.script.ScriptEngineManager
-import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.concurrent.Eventually
+import org.scalatest.{Inside, Matchers, WordSpec}
 
 import io.shiftleft.cpgserver.model.CpgOperationSuccess
 import io.shiftleft.cpgserver.query.DefaultCpgQueryExecutor
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
-class JoernCpgProviderSpec extends WordSpec with Matchers {
+class JoernCpgProviderSpec extends WordSpec with Matchers with Eventually with Inside {
+
+  private implicit val cs: ContextShift[IO] = IO.contextShift(ExecutionContext.global)
 
   private def withJoernProvider[T](f: JoernCpgProvider => T): T = {
     f(new JoernCpgProvider)
@@ -15,8 +23,11 @@ class JoernCpgProviderSpec extends WordSpec with Matchers {
   "createCpg" should {
     "successfully create a CPG from a set of input files" in withJoernProvider { provider =>
       val uuid = provider.createCpg(Set("joern-cli/src/test/resources/testcode/free")).unsafeRunSync()
-      provider.retrieveCpg(uuid).value.unsafeRunSync() should matchPattern {
-        case Some(CpgOperationSuccess(_)) =>
+
+      eventually(timeout(5 seconds), interval(1 second)) {
+        provider.retrieveCpg(uuid).value.unsafeRunSync() should matchPattern {
+          case Some(CpgOperationSuccess(_)) =>
+        }
       }
     }
 
@@ -25,10 +36,20 @@ class JoernCpgProviderSpec extends WordSpec with Matchers {
       val queryExecutor = new DefaultCpgQueryExecutor(new ScriptEngineManager())
 
       val cpgId = provider.createCpg(Set("joern-cli/src/test/resources/testcode/free")).unsafeRunSync()
-      val Some(CpgOperationSuccess(cpg)) = provider.retrieveCpg(cpgId).value.unsafeRunSync()
+
+      val cpg = eventually(timeout(5 seconds), interval(1 second)) {
+        inside(provider.retrieveCpg(cpgId).value.unsafeRunSync()) {
+          case Some(CpgOperationSuccess(cpg)) => cpg
+        }
+      }
 
       val queryId = queryExecutor.executeQuery(cpg, "cpg.method.toJsonPretty").unsafeRunSync()
-      val Some(CpgOperationSuccess(queryResult)) = queryExecutor.retrieveQueryResult(queryId).value.unsafeRunSync()
+
+      val queryResult = eventually(timeout(5 seconds), interval(1 second)) {
+        inside(queryExecutor.retrieveQueryResult(queryId).value.unsafeRunSync()) {
+          case Some(CpgOperationSuccess(queryResult)) => queryResult
+        }
+      }
       queryResult should not be empty
     }
   }
