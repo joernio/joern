98077fc04a394d424ce3e7baa20bd7577320079c
Max Leuthäuser <max.leuthaeuser@gmail.com>
2019-10-24 17:01:48 +0200

    Added test for cfgToDot script.

--- scripts/cfgToDot/cfgToDot.scala
+++ scripts/cfgToDot/cfgToDot.scala
@@ -1,10 +1,11 @@
+import javax.script.ScriptEngineManager
+import java.nio.file.Paths
+
 import gremlin.scala._
 import io.shiftleft.codepropertygraph.generated._
-import java.nio.file.Paths
 import io.shiftleft.semanticcpg.utils.ExpandTo
 import org.apache.tinkerpop.gremlin.structure.Direction
 import io.shiftleft.Implicits.JavaIteratorDeco
-import javax.script.ScriptEngineManager
 
 /** Some helper functions: adapted from ReachingDefPass.scala in codeproperty graph repo */
 def vertexToStr(vertex: Vertex): String = {
