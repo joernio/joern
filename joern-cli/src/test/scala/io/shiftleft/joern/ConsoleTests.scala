f837a777f654f7d785a5a03f35b3690a105fc784
Max Leuthäuser <max.leuthaeuser@gmail.com>
2019-10-24 15:38:13 +0200

    Adapted ConsoleTests to changes from master and last commit.

--- joern-cli/src/test/scala/io/shiftleft/joern/ConsoleTests.scala
+++ joern-cli/src/test/scala/io/shiftleft/joern/ConsoleTests.scala
@@ -1,21 +1,20 @@
 package io.shiftleft.joern
 
 import better.files.File
+import better.files.Dsl._
 import io.shiftleft.semanticcpg.language._
 import org.scalatest.{Matchers, WordSpec}
 
 class ConsoleTests extends WordSpec with Matchers with AbstractJoernCliTest {
 
   "should execute the list-funcs correctly for example code" in withTestCpg(
-    File("joern-cli") / "src" / "test" / "resources" / "testcode" / "free") {
+    File(getClass.getClassLoader.getResource("testcode/free"))) {
     case (cpg, outputFilename) =>
       val expected = cpg.method.name.l
-
-      val actual = new TestConsole(outputFilename).runScript("list-funcs")
+      cp(File(outputFilename), File(".") / "cpg.bin.zip")
+      val actual = console.Console.runScript("list-funcs")
       actual shouldBe expected
-
-      val actualT: List[String] = new TestConsole(outputFilename).runScriptT("list-funcs")
-      actualT shouldBe expected
+      rm(File(".") / "cpg.bin.zip")
   }
 
 }
