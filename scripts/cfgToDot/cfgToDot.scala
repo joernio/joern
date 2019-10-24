9e57a67489710c1f9f3f7eff57f30c551f79d94d
Max Leuthäuser <max.leuthaeuser@gmail.com>
2019-10-24 16:37:50 +0200

    Fixed broken imports in cfgToDot.scala.

--- scripts/cfgToDot/cfgToDot.scala
+++ scripts/cfgToDot/cfgToDot.scala
@@ -1,9 +1,9 @@
 import gremlin.scala._
 import io.shiftleft.codepropertygraph.generated._
 import java.nio.file.Paths
-import io.shiftleft.queryprimitives.utils.ExpandTo
+import io.shiftleft.semanticcpg.utils.ExpandTo
 import org.apache.tinkerpop.gremlin.structure.Direction
-import io.shiftleft.queryprimitives.steps.Implicits.JavaIteratorDeco
+import io.shiftleft.Implicits.JavaIteratorDeco
 import javax.script.ScriptEngineManager
 
 /** Some helper functions: adapted from ReachingDefPass.scala in codeproperty graph repo */
