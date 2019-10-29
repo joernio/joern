fd5e4a84a25451fa97caa7027c642d3f32d4b90c
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2019-10-29 17:19:14 +0100

    Added pdg-for-funcs script and its test. (#147)
    Also: added some documentation for existing scripts.

--- scripts/ast-for-funcs/ast-for-funcs.scala
+++ scripts/ast-for-funcs/ast-for-funcs.scala
@@ -1,3 +1,78 @@
+/* ast-for-funcs.scala
+
+   This script returns a Json representation of the AST for each method contained in the currently loaded CPG.
+
+   Input: A valid CPG
+   Output: Json
+
+   Running the Script
+   ------------------
+   see: README.md
+
+   The JSON generated has the following keys:
+
+    "file": The file (as full path) the CPG was generated from
+    "functions": Array of all methods contained in the currently loaded CPG
+      |_ "function": Method name as String
+      |_ "id": Method id as String (String representation of the underlying Method node)
+      |_ "AST": Array of all nodes connected via AST edges
+          |_ "id": Node id as String (String representation of the underlying AST node)
+          |_ "properties": Array of properties of the current node as key-value pair
+          |_ "edges": Array of all AST edges where the current node is referenced as inVertex or outVertex
+              |_ "id": Edge id as String (String representation of the AST edge)
+              |_ "in": Node id as String of the inVertex node (String representation of the inVertex node)
+              |_ "out": Node id as String of the outVertex node (String representation of the outVertex node)
+
+   Sample Output
+   -------------
+   {
+    "file" : "/path/to/free/free.c",
+    "functions" : [
+      {
+        "function" : "free_list",
+        "id" : "io.shiftleft.codepropertygraph.generated.nodes.Method@b",
+        "AST" : [
+          {
+            "id" : "io.shiftleft.codepropertygraph.generated.nodes.MethodParameterOut@46",
+            "edges" : [
+              {
+                "id" : "io.shiftleft.codepropertygraph.generated.edges.Ast@27adc",
+                "in" : "io.shiftleft.codepropertygraph.generated.nodes.MethodParameterOut@46",
+                "out" : "io.shiftleft.codepropertygraph.generated.nodes.Method@b"
+              }
+            ],
+            "properties" : [
+              {
+                "key" : "NAME",
+                "value" : "head"
+              },
+              {
+                "key" : "CODE",
+                "value" : "struct node *head"
+              },
+              {
+                "key" : "LINE_NUMBER",
+                "value" : "7"
+              },
+              {
+                "key" : "EVALUATION_STRATEGY",
+                "value" : "BY_VALUE"
+              },
+              {
+                "key" : "TYPE_FULL_NAME",
+                "value" : "struct node *"
+              },
+              {
+                "key" : "COLUMN_NUMBER",
+                "value" : "15"
+              },
+              {
+                "key" : "ORDER",
+                "value" : "1"
+              },
+              // ...
+ */
+
 import scala.collection.JavaConverters._
 
 import io.circe.syntax._
