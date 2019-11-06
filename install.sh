19eba88d2a1a35265dac8e89e9bce9825c084436
Jacob Taylor-Hindle <GlassAndOneHalf@users.noreply.github.com>
2019-11-06 17:51:03 +0100

    Add Joern install script. (#161)
    * Add Joern install script.
    
    * Remove TODO comment from install script.
    
    * Remove unneeded install check.

---
+++ install.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env sh
+set -eu
+
+SCRIPT_ABS_PATH=$(readlink -f "$0")
+SCRIPT_ABS_DIR=$(dirname "$SCRIPT_ABS_PATH")
+
+# Check required tools are installed.
+check_installed() {
+  if ! type "$1" > /dev/null; then
+    echo "Please ensure you have $1 installed."
+    exit 1
+  fi
+}
+check_installed "sbt"
+
+# Confirm install with user.
+JOERN_DEFAULT_INSTALL_DIR=~/bin/joern
+JOERN_DEFAULT_LINK_DIR="/usr/local/bin"
+
+echo -n "This script will build Joern and download the FuzzyC2CPG preprocessor. Proceed? [Y/n]: "
+read -r JOERN_PROMPT_ANSWER
+
+if [ "$JOERN_PROMPT_ANSWER" != "Y" ] && [ "$JOERN_PROMPT_ANSWER" != "y" ]; then
+  exit 0
+fi
+
+echo -n "Enter an install location or press enter for the default ($JOERN_DEFAULT_INSTALL_DIR): "
+read -r JOERN_INSTALL_DIR
+
+if [ "$JOERN_INSTALL_DIR" = "" ]; then
+  JOERN_INSTALL_DIR=$JOERN_DEFAULT_INSTALL_DIR
+fi
+
+mkdir -p $JOERN_INSTALL_DIR
+
+echo -n "Would you like to create a symlink to the Joern tools? (default n) [Y/n]: "
+read -r JOERN_LINK_ANSWER
+
+if [ "$JOERN_LINK_ANSWER" = "Y" ] || [ "$JOERN_LINK_ANSWER" = "y" ]; then
+  echo -n "Where would you like to link the Joern tools? (default $JOERN_DEFAULT_LINK_DIR): "
+  read -r JOERN_LINK_DIR
+  if [ "$JOERN_LINK_DIR" = "" ]; then
+    JOERN_LINK_DIR=$JOERN_DEFAULT_LINK_DIR
+  fi
+fi
+
+# Build and extract the Joern CLI & Server
+cd "$SCRIPT_ABS_DIR"; sbt clean createDistribution; cd - > /dev/null;
+unzip -qo -d "$JOERN_INSTALL_DIR" "$SCRIPT_ABS_DIR"/joern-cli.zip
+unzip -qo -d "$JOERN_INSTALL_DIR" "$SCRIPT_ABS_DIR"/joern-server.zip
+
+# Link to JOERN_LINK_DIR if desired by the user
+if [ -n "${JOERN_LINK_DIR+dummy}" ]; then
+  echo "Creating symlinks to Joern tools in $JOERN_LINK_DIR"
+  ln -sf "$JOERN_INSTALL_DIR"/joern-cli/joern "$JOERN_LINK_DIR"
+  ln -sf "$JOERN_INSTALL_DIR"/joern-cli/joern-cpg2scpg "$JOERN_LINK_DIR"
+  ln -sf "$JOERN_INSTALL_DIR"/joern-cli/joern-parse "$JOERN_LINK_DIR"
+  ln -sf "$JOERN_INSTALL_DIR"/joern-server/joernd "$JOERN_LINK_DIR"
+fi
+
+echo "Install complete!"
