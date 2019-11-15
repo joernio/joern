44c0da898be3a57d838d72f1725c29a8dd8f3e09
Jacob Taylor-Hindle <GlassAndOneHalf@users.noreply.github.com>
2019-11-15 12:33:28 +0100

    Add extension to Cpg for running scripts. (#180)

--- joern-cli/src/main/scala/io/shiftleft/joern/JoernScriptManager.scala
+++ joern-cli/src/main/scala/io/shiftleft/joern/JoernScriptManager.scala
@@ -1,5 +1,19 @@
 package io.shiftleft.joern
 
+import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.console.ScriptManager
 
-class JoernScriptManager(executor: JoernScriptExecutor = new JoernScriptExecutor()) extends ScriptManager(executor)
+class JoernScriptManager(executor: JoernScriptExecutor = new JoernScriptExecutor()) extends ScriptManager(executor) {
+
+  implicit class CpgScriptRunner(cpg: Cpg) {
+
+    /**
+      * Run an arbitrary script over this CPG.
+      *
+      * @param name The name of the script to run.
+      * @return The result of running the script against this CPG.
+      */
+    def runScript(name: String): AnyRef =
+      JoernScriptManager.this.runScript(name, cpg)
+  }
+}
