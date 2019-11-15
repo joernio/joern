573a7d9126f81fe7356e1e7ddfd00a101520ff67
Jacob Taylor-Hindle <jacob.hindle@babylonhealth.com>
2019-11-15 10:46:59 +0100

    Add comments to test script.

--- tests/scripts-test.sh
+++ tests/scripts-test.sh
@@ -9,8 +9,10 @@ JOERN="$SCRIPT_ABS_DIR"/../joern
 JOERN_PARSER="$SCRIPT_ABS_DIR"/../joern-parse
 JOERN_SCRIPTS_DIR="$SCRIPT_ABS_DIR/../scripts"
 
+# Generate a CPG for use in the script tests.
 $JOERN_PARSER "$SCRIPT_ABS_DIR"/code
 
+# Run each script.
 for script in "$JOERN_SCRIPTS_DIR"/*.sc; do
   echo "Testing script [$script]..."
   $JOERN --import "$SCRIPT_ABS_DIR"/loadcpg.sc --script "$script"
