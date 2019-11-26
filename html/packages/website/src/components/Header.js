52b75a42866e9fc7d1ec80856b54f72dac29e432
Chaitanya K Kamatham <projects+github@kchaitanya.com>
2019-11-25 16:57:48 -0800

    feat: added typed

--- html/packages/website/src/components/Header.js
+++ html/packages/website/src/components/Header.js
@@ -1,19 +1,32 @@
 import React from 'react';
-
+import Typed from 'typed.js';
 import logo from '../assets/images/logo.svg';
 
-const Header = props => (
-  <header id="header" className="alt">
-    <span className="logo">
-      <img src={logo} alt="" />
-    </span>
-    {/* <h1>Joern</h1> */}
-    <p>
-     Next Generation <b><em> Code Analysis</em></b>
-      <br />
-      engine for <a href="#">C/C++</a>.
-    </p>
-  </header>
-);
+const Header = props => {
+  const pref = React.useRef();
+  React.useEffect(() => {
+    const typed = new Typed(pref.current, {
+      strings: ['First sentence.', 'Second sentence.'],
+      typeSpeed: 10,
+    });
+    return typed.destroy;
+  }, []);
+  return (
+    <header id="header" className="alt">
+      <span className="logo">
+        <img src={logo} alt="" />
+      </span>
+      <div ref={pref}></div>
+      <p>
+        Next Generation{' '}
+        <b>
+          <em> Code Analysis</em>
+        </b>
+        <br />
+        engine for <a href="#">C/C++</a>.
+      </p>
+    </header>
+  );
+};
 
 export default Header;
