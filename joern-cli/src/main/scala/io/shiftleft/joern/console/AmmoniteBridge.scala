d492bc62a885bd9bb47b03a3e4706be4168a5ad2
Michael Pollmeier <michael@michaelpollmeier.com>
2019-11-28 10:16:07 +1300

    apply tab completion workaround
    ammonite tab completion is partly broken for scala > 2.12.8
    applying workaround for package wildcard imports from https://github.com/lihaoyi/Ammonite/issues/1009

--- joern-cli/src/main/scala/io/shiftleft/joern/console/AmmoniteBridge.scala
+++ joern-cli/src/main/scala/io/shiftleft/joern/console/AmmoniteBridge.scala
@@ -10,10 +10,12 @@ object AmmoniteBridge extends App with BridgeBase {
     * Code that is executed when starting the shell
     * */
   override def predefPlus(lines: List[String]): String = {
+    /* ammonite tab completion is partly broken for scala > 2.12.8
+     * applying workaround for package wildcard imports from https://github.com/lihaoyi/Ammonite/issues/1009 */
     val default =
       """
-        |import gremlin.scala._
-        |import io.shiftleft.console._
+        |import gremlin.scala.{`package` => _, _}
+        |import io.shiftleft.console.{`package` => _, _}
         |import io.shiftleft.joern.console._
         |import io.shiftleft.joern.console.Console._
         |import io.shiftleft.codepropertygraph.Cpg
@@ -21,8 +23,8 @@ object AmmoniteBridge extends App with BridgeBase {
         |import io.shiftleft.codepropertygraph.generated._
         |import io.shiftleft.codepropertygraph.generated.nodes._
         |import io.shiftleft.codepropertygraph.generated.edges._
-        |import io.shiftleft.dataflowengine.language._
-        |import io.shiftleft.semanticcpg.language._
+        |import io.shiftleft.dataflowengine.language.{`package` => _, _}
+        |import io.shiftleft.semanticcpg.language.{`package` => _, _}
         |import scala.jdk.CollectionConverters._
         |implicit val resolver: ICallResolver = NoResolve
         |
