ccc657acb46871b1e3b01bdc8048f78cc8a4527e
Fabian Yamaguchi <fabs@shiftleft.io>
2020-02-21 15:48:16 +0100

    Use FuzzyC2Cpg's overflowdb mode by default. (#219)

--- joern-cli/src/main/scala/io/shiftleft/joern/Cpg2Scpg.scala
+++ joern-cli/src/main/scala/io/shiftleft/joern/Cpg2Scpg.scala
@@ -1,10 +1,10 @@
 package io.shiftleft.joern
 
-import better.files.File
 import io.shiftleft.dataflowengine.layers.dataflows.DataFlowRunner
 import io.shiftleft.dataflowengine.semanticsloader.SemanticsLoader
 import io.shiftleft.semanticcpg.layers.EnhancementRunner
 import io.shiftleft.SerializedCpg
+import io.shiftleft.codepropertygraph.Cpg
 import org.slf4j.LoggerFactory
 
 object Cpg2Scpg extends App {
@@ -15,7 +15,7 @@ object Cpg2Scpg extends App {
 
   parseConfig.foreach { config =>
     try {
-      run(config.inputPath, "store.bin", config.dataFlow, config.semanticsFile)
+      run(config.inputPath, config.dataFlow, config.semanticsFile).close()
     } catch {
       case exception: Exception =>
         logger.error("Failed to generate CPG.", exception)
@@ -40,21 +40,19 @@ object Cpg2Scpg extends App {
     }.parse(args, Config(DEFAULT_CPG_IN_FILE, true, CpgLoader.defaultSemanticsFile))
 
   /**
-    * Load the CPG at `cpgFilename` and add enhancements,
+    * Load the CPG at `storeFilename` and add enhancements,
     * turning the CPG into an SCPG.
     * @param storeFilename the filename of the cpg
     * */
-  def run(inputFilename: String, storeFilename: String, dataFlow: Boolean, semanticsFilename: String): Unit = {
-    val storeFile = File(storeFilename)
-    if (storeFilename != "" && storeFile.exists) { storeFile.delete() }
-    val cpg = CpgLoader.load(inputFilename, storeFilename)
+  def run(storeFilename: String, dataFlow: Boolean, semanticsFilename: String): Cpg = {
+    val cpg = CpgLoader.loadFromOdb(storeFilename)
     new EnhancementRunner().run(cpg, new SerializedCpg())
     if (dataFlow) {
       val semantics = new SemanticsLoader(semanticsFilename).load()
       new DataFlowRunner(semantics).run(cpg, new SerializedCpg())
     }
     io.shiftleft.codepropertygraph.cpgloading.CpgLoader.createIndexes(cpg)
-    cpg.graph.close()
+    cpg
   }
 
 }
--- joern-cli/src/test/scala/io/shiftleft/joern/AbstractJoernCliTest.scala
+++ joern-cli/src/test/scala/io/shiftleft/joern/AbstractJoernCliTest.scala
@@ -1,8 +1,11 @@
 package io.shiftleft.joern
 
+import java.util.concurrent.LinkedBlockingQueue
+
 import better.files.File
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.fuzzyc2cpg.FuzzyC2Cpg
+import io.shiftleft.proto.cpg.Cpg.CpgStruct
 
 trait AbstractJoernCliTest {
 
@@ -12,22 +15,18 @@ trait AbstractJoernCliTest {
 
   private def loadTestCpg(file: File): (Cpg, String) = {
     val inputFilenames = Set(file.pathAsString)
-    val tmpFile = File.newTemporaryFile("cpg", "bin.zip")
+    val tmpFile = File.newTemporaryFile("cpg", "bin")
     val fuzzycOutFilename = tmpFile.pathAsString
     tmpFile.delete()
 
     // Create a CPG using the C/C++ fuzzy parser
-    val fuzzyc2Cpg = new FuzzyC2Cpg(fuzzycOutFilename)
+    val queue = new LinkedBlockingQueue[CpgStruct.Builder]()
+    val factory = new io.shiftleft.fuzzyc2cpg.output.overflowdb.OutputModuleFactory(fuzzycOutFilename, queue)
+    val fuzzyc2Cpg = new FuzzyC2Cpg(factory)
     fuzzyc2Cpg.runAndOutput(inputFilenames, Set(".c"))
     // Link CPG fragments and enhance to create semantic CPG
-
-    val storeTmpFile = File.newTemporaryFile("store", "bin")
-    val storeFilename = storeTmpFile.pathAsString
-    storeTmpFile.delete()
-    Cpg2Scpg.run(fuzzycOutFilename, storeFilename, dataFlow = true, CpgLoader.defaultSemanticsFile)
-
-    // Load the CPG
-    (CpgLoader.loadFromOdb(storeFilename), fuzzycOutFilename)
+    val cpg = Cpg2Scpg.run(fuzzycOutFilename, dataFlow = true, CpgLoader.defaultSemanticsFile)
+    (cpg, fuzzycOutFilename)
   }
 
 }
