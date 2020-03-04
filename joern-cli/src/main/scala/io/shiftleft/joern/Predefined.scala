4f83f38b6cf23e8e19653551411a783db138183d
Fabian Yamaguchi <fabs@shiftleft.io>
2020-03-04 11:43:20 +0100

    Reduce duplication for predefs.

---
+++ joern-cli/src/main/scala/io/shiftleft/joern/Predefined.scala
@@ -0,0 +1,33 @@
+package io.shiftleft.joern
+
+object Predefined {
+
+  /* ammonite tab completion is partly broken for scala > 2.12.8
+   * applying workaround for package wildcard imports from https://github.com/lihaoyi/Ammonite/issues/1009 */
+  val shared: String = """
+        |import gremlin.scala.{`package` => _, _}
+        |import io.shiftleft.console.{`package` => _, _}
+        |import io.shiftleft.joern.console._
+        |import io.shiftleft.codepropertygraph.Cpg
+        |import io.shiftleft.codepropertygraph.cpgloading._
+        |import io.shiftleft.codepropertygraph.generated._
+        |import io.shiftleft.codepropertygraph.generated.nodes._
+        |import io.shiftleft.codepropertygraph.generated.edges._
+        |import io.shiftleft.dataflowengine.language.{`package` => _, _}
+        |import io.shiftleft.semanticcpg.language.{`package` => _, _}
+        |import scala.jdk.CollectionConverters._
+        |implicit val resolver: ICallResolver = NoResolve
+        |
+      """.stripMargin
+
+  val forInteractiveShell: String = shared +
+    """
+      | import io.shiftleft.joern.console.Console._
+    """.stripMargin
+
+  val forScripts: String = shared +
+    """
+    | import io.shiftleft.joern.console.Console.{cpg => _, _}
+  """.stripMargin
+
+}
--- joern-cli/src/main/scala/io/shiftleft/joern/console/AmmoniteBridge.scala
+++ joern-cli/src/main/scala/io/shiftleft/joern/console/AmmoniteBridge.scala
@@ -1,6 +1,7 @@
 package io.shiftleft.joern.console
 
 import io.shiftleft.console.{BridgeBase, JoernProduct}
+import io.shiftleft.joern.Predefined
 
 object AmmoniteBridge extends App with BridgeBase {
 
@@ -10,26 +11,8 @@ object AmmoniteBridge extends App with BridgeBase {
     * Code that is executed when starting the shell
     * */
   override def predefPlus(lines: List[String]): String = {
-    /* ammonite tab completion is partly broken for scala > 2.12.8
-     * applying workaround for package wildcard imports from https://github.com/lihaoyi/Ammonite/issues/1009 */
-    val default =
-      """
-        |import gremlin.scala.{`package` => _, _}
-        |import io.shiftleft.console.{`package` => _, _}
-        |import io.shiftleft.joern.console._
-        |import io.shiftleft.joern.console.Console._
-        |import io.shiftleft.codepropertygraph.Cpg
-        |import io.shiftleft.codepropertygraph.cpgloading._
-        |import io.shiftleft.codepropertygraph.generated._
-        |import io.shiftleft.codepropertygraph.generated.nodes._
-        |import io.shiftleft.codepropertygraph.generated.edges._
-        |import io.shiftleft.dataflowengine.language.{`package` => _, _}
-        |import io.shiftleft.semanticcpg.language.{`package` => _, _}
-        |import scala.jdk.CollectionConverters._
-        |implicit val resolver: ICallResolver = NoResolve
-        |
-      """.stripMargin
-    lines.foldLeft(default) { case (res, line) => res + s"\n$line" }
+
+    lines.foldLeft(Predefined.forInteractiveShell) { case (res, line) => res + s"\n$line" }
   }
 
   override def promptStr(): String = "joern> "
