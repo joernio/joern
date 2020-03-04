3c3ca4fffc330b05cb4a8d04f03ef1724760bbb3
Fabian Yamaguchi <fabs@shiftleft.io>
2020-03-04 11:54:10 +0100

    Reduce duplication for predefs. (#228)

--- joern-cli/src/main/scala/io/shiftleft/joern/scripting/JoernAmmoniteExecutor.scala
+++ joern-cli/src/main/scala/io/shiftleft/joern/scripting/JoernAmmoniteExecutor.scala
@@ -1,22 +1,9 @@
 package io.shiftleft.joern.scripting
 
 import io.shiftleft.console.scripting.AmmoniteExecutor
+import io.shiftleft.joern.Predefined
 
 object JoernAmmoniteExecutor extends AmmoniteExecutor {
   override lazy val predef: String =
-    """
-      |import gremlin.scala.{`package` => _, _}
-      |import io.shiftleft.console.{`package` => _, _}
-      |import io.shiftleft.joern.console._
-      |import io.shiftleft.joern.console.Console.{cpg => _, _}
-      |import io.shiftleft.codepropertygraph.Cpg
-      |import io.shiftleft.codepropertygraph.cpgloading._
-      |import io.shiftleft.codepropertygraph.generated._
-      |import io.shiftleft.codepropertygraph.generated.nodes._
-      |import io.shiftleft.codepropertygraph.generated.edges._
-      |import io.shiftleft.dataflowengine.language.{`package` => _, _}
-      |import io.shiftleft.semanticcpg.language.{`package` => _, _}
-      |import scala.jdk.CollectionConverters._
-      |implicit val resolver: ICallResolver = NoResolve
-      |""".stripMargin
+    Predefined.forScripts
 }
