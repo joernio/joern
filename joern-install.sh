be1425c419346b266b208d08436431b537fe3002
Carlos Macasaet <l0s@users.noreply.github.com>
2020-03-07 00:33:08 -0800

    Support installation via script on macOS (#230)
    * Support installation via script on macOS
    
    The readlink command on macOS does not support the -f / --canonicalize
    parameter available in the GNU version. This adds a check for the Darwin
    kernel and provides an alternative approach to finding the script's
    directory.
    
    Addresses: #229
    
    * Improve zsh/macOS support for installation script
    
    This fixes an issue that was identified when explicitly running
    `joern-install.sh` using zsh.
    
    Addresses: #229

--- joern-install.sh
+++ joern-install.sh
@@ -1,8 +1,26 @@
 #!/usr/bin/env sh
 set -eu
 
-SCRIPT_ABS_PATH=$(readlink -f "$0")
-SCRIPT_ABS_DIR=$(dirname "$SCRIPT_ABS_PATH")
+if [ "$(uname)" = 'Darwin' ]; then
+  # get script location
+  # https://unix.stackexchange.com/a/96238
+  if [ "${BASH_SOURCE:-x}" != 'x' ]; then
+    this_script=$BASH_SOURCE
+  elif [ "${ZSH_VERSION:-x}" != 'x' ]; then
+    setopt function_argzero
+    this_script=$0
+  elif eval '[[ -n ${.sh.file} ]]' 2>/dev/null; then
+    eval 'this_script=${.sh.file}'
+  else
+    echo 1>&2 "Unsupported shell. Please use bash, ksh93 or zsh."
+    exit 2
+  fi
+  relative_directory=$(dirname "$this_script")
+  SCRIPT_ABS_DIR=$(cd "$relative_directory" && pwd)
+else
+  SCRIPT_ABS_PATH=$(readlink -f "$0")
+  SCRIPT_ABS_DIR=$(dirname "$SCRIPT_ABS_PATH")
+fi
 
 # Check required tools are installed.
 check_installed() {
