6c4b765e1719b42392c4e2341fd9f55b5289e5d0
Fabian Yamaguchi <fabs@shiftleft.io>
2020-05-14 19:47:10 +0200

    Fix passing of semantics to OssDataFlow pass (#254)
    * Fix passing of semantics to OssDataFlow pass.
    
    * Set correct CPG version.

--- joern-cli/src/main/scala/io/shiftleft/joern/console/JoernAmmoniteExecutor.scala
+++ joern-cli/src/main/scala/io/shiftleft/joern/console/JoernAmmoniteExecutor.scala
@@ -3,6 +3,7 @@ package io.shiftleft.joern.console
 import io.shiftleft.console.scripting.AmmoniteExecutor
 
 object JoernAmmoniteExecutor extends AmmoniteExecutor {
+
   override lazy val predef: String =
     Predefined.forScripts
 }
