6d76da12babe0f05e7f162391b6342256fb7f533
Fabian Yamaguchi <fabs@shiftleft.io>
2020-06-22 13:28:57 +0200

    Update docs2/src/theme/DocSidebar/index.js
    Co-authored-by: Claudiu-Vlad Ursache <claudiu@cvursache.com>

--- docs2/src/theme/DocSidebar/index.js
+++ docs2/src/theme/DocSidebar/index.js
@@ -1,10 +1,3 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 import React, {useState, useCallback} from 'react';
 import classnames from 'classnames';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
