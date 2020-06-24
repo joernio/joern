99c256713b22e8d09d7351de1b28e05599c5a538
Fabian Yamaguchi <fabs@shiftleft.io>
2020-06-24 16:10:04 +0200

    Make github happy

--- html/packages/website/gatsby-config.js
+++ html/packages/website/gatsby-config.js
@@ -12,8 +12,8 @@ module.exports = {
         name: 'shiftleft-joern-website',
         short_name: 'website',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#000000',
+        theme_color: '#000000',
         display: 'minimal-ui',
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
