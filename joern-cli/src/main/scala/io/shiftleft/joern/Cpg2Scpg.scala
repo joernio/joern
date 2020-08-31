ec8d42e91ede844dbec00646d10206aed01d69c5
Fabian Yamaguchi <fabs@shiftleft.io>
2020-08-31 18:51:41 +0200

    Remove more dead code

--- joern-cli/src/main/scala/io/shiftleft/joern/Cpg2Scpg.scala
+++ joern-cli/src/main/scala/io/shiftleft/joern/Cpg2Scpg.scala
@@ -4,42 +4,11 @@ import io.shiftleft.dataflowengineoss.layers.dataflows.{OssDataFlow, OssDataFlow
 import io.shiftleft.semanticcpg.layers.{LayerCreatorContext, Scpg}
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.codepropertygraph.cpgloading.CpgLoaderConfig
-import org.slf4j.LoggerFactory
 import overflowdb.OdbConfig
 
-object Cpg2Scpg extends App {
+object Cpg2Scpg {
 
   val DEFAULT_CPG_IN_FILE = "cpg.bin"
-
-  private val logger = LoggerFactory.getLogger(getClass)
-
-  parseConfig.foreach { config =>
-    try {
-      val cpg = run(config.inputPath, config.dataFlow)
-      logger.info(s"Closing Cpg...")
-      val startTime = System.currentTimeMillis()
-      cpg.close()
-      logger.info(s"Done in : ${System.currentTimeMillis() - startTime} ms")
-
-    } catch {
-      case exception: Exception =>
-        logger.error("Failed to generate CPG.", exception)
-        System.exit(1)
-    }
-    System.exit(0)
-  }
-
-  case class Config(inputPath: String, dataFlow: Boolean)
-  def parseConfig: Option[Config] =
-    new scopt.OptionParser[Config](getClass.getSimpleName) {
-      arg[String]("<cpg>")
-        .text("CPG to enhance")
-        .action((x, c) => c.copy(inputPath = x))
-      opt[Unit]("nodataflow")
-        .text("do not perform data flow analysis")
-        .action((x, c) => c.copy(dataFlow = false))
-
-    }.parse(args, Config(DEFAULT_CPG_IN_FILE, true))
 
   /**
     * Load the CPG at `storeFilename` and add enhancements,
