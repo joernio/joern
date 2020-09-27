1409acd9810e5b9909a233c9b5dc1b20d9c1b701
Fabian Yamaguchi <fabs@shiftleft.io>
2020-09-27 22:56:18 +0200

    `joern-export` support for different graphs

--- joern-cli/src/main/scala/io/shiftleft/joern/JoernExport.scala
+++ joern-cli/src/main/scala/io/shiftleft/joern/JoernExport.scala
@@ -3,15 +3,41 @@ package io.shiftleft.joern
 import better.files.File
 import better.files.Dsl._
 import io.shiftleft.codepropertygraph.Cpg
-import io.shiftleft.dataflowengineoss.layers.dataflows.{Cpg14DumpOptions, DumpCpg14, OssDataFlow, OssDataFlowOptions}
+import io.shiftleft.dataflowengineoss.layers.dataflows.{
+  Cpg14DumpOptions,
+  DdgDumpOptions,
+  DumpCpg14,
+  DumpDdg,
+  DumpPdg,
+  OssDataFlow,
+  OssDataFlowOptions,
+  PdgDumpOptions
+}
 import io.shiftleft.dataflowengineoss.semanticsloader.Semantics
 import io.shiftleft.joern.console.JoernWorkspaceLoader
 import io.shiftleft.semanticcpg.language._
-import io.shiftleft.semanticcpg.layers.LayerCreatorContext
+import io.shiftleft.semanticcpg.layers.{
+  AstDumpOptions,
+  CdgDumpOptions,
+  CfgDumpOptions,
+  DumpAst,
+  DumpCdg,
+  DumpCfg,
+  LayerCreatorContext
+}
 
-case class ExporterConfig(cpgFileName: String = "cpg.bin", outDir: String = "out")
+case class ExporterConfig(cpgFileName: String = "cpg.bin", outDir: String = "out", repr: String = "cpg14")
 
 object JoernExport extends App {
+
+  object Representations {
+    val ast = "ast"
+    val cfg = "cfg"
+    val ddg = "ddg"
+    val cdg = "cdg"
+    val pdg = "pdg"
+    val cpg14 = "cpg14"
+  }
 
   private def parseConfig: Option[ExporterConfig] =
     new scopt.OptionParser[ExporterConfig]("joern-export") {
@@ -24,6 +50,9 @@ object JoernExport extends App {
       opt[String]("out")
         .text("output directory")
         .action((x, c) => c.copy(outDir = x))
+      opt[String]("repr")
+        .text("representation to extract: [ast|cfg|ddg|cdg|pdg|cpg14]")
+        .action((x, c) => c.copy(repr = x))
     }.parse(args, ExporterConfig())
 
   parseConfig.foreach { config =>
@@ -42,8 +71,23 @@ object JoernExport extends App {
         if (semantics.elements.isEmpty) {
           System.err.println("Warning: semantics are empty.")
         }
-        val opts = Cpg14DumpOptions(config.outDir)
-        new DumpCpg14(opts).create(context)
+
+        config.repr match {
+          case Representations.ast =>
+            new DumpAst(AstDumpOptions(config.outDir)).create(context)
+          case Representations.cfg =>
+            new DumpCfg(CfgDumpOptions(config.outDir)).create(context)
+          case Representations.ddg =>
+            new DumpDdg(DdgDumpOptions(config.outDir)).create(context)
+          case Representations.cdg =>
+            new DumpCdg(CdgDumpOptions(config.outDir)).create(context)
+          case Representations.pdg =>
+            new DumpPdg(PdgDumpOptions(config.outDir)).create(context)
+          case Representations.cpg14 =>
+            new DumpCpg14(Cpg14DumpOptions(config.outDir)).create(context)
+          case repr =>
+            System.err.println(s"unknown representation: $repr. Baling out.")
+        }
       }
     }
   }
