3200f76e292984f36d8450c759ee7d03aa5484bc
Fabian Yamaguchi <fabs@shiftleft.io>
2020-10-12 16:39:55 +0200

    First working version

--- joern-cli/src/main/scala/io/shiftleft/joern/JoernFlow.scala
+++ joern-cli/src/main/scala/io/shiftleft/joern/JoernFlow.scala
@@ -1,4 +1,9 @@
 package io.shiftleft.joern
+import io.shiftleft.semanticcpg.language._
+import io.shiftleft.dataflowengineoss.language._
+import io.shiftleft.dataflowengineoss.queryengine.EngineContext
+import io.shiftleft.dataflowengineoss.semanticsloader.Semantics
+import io.shiftleft.joern.console.JoernWorkspaceLoader
 
 case class FlowConfig(cpgFileName: String = "cpg.bin",
                       srcRegex: String = ".*",
@@ -21,20 +26,21 @@ object JoernFlow extends App {
         .text("destination regex")
         .action((x, c) => c.copy(dstRegex = x))
 
-      arg[Int]("srcParam")
-        .text("Source parameter")
-        .optional()
-        .action((x, c) => c.copy(dstParam = Some(x)))
-
-      arg[Int]("dstParam")
-        .text("Destination parameter")
-        .optional()
-        .action((x, c) => c.copy(dstParam = Some(x)))
-
       arg[String]("cpg")
         .text("CPG file name ('cpg.bin' by default)")
         .optional()
         .action((x, c) => c.copy(srcRegex = x))
+
+      opt[Int]("src-param")
+        .text("Source parameter")
+        .optional()
+        .action((x, c) => c.copy(dstParam = Some(x)))
+
+      opt[Int]("dst-param")
+        .text("Destination parameter")
+        .optional()
+        .action((x, c) => c.copy(dstParam = Some(x)))
+
     }
   }.parse(args, FlowConfig())
 
@@ -43,7 +49,23 @@ object JoernFlow extends App {
     val cpg = CpgBasedTool.loadFromOdb(config.cpgFileName)
     println("[DONE]")
 
+    val source = cpg.method(config.srcRegex).parameter.filter { p =>
+      config.srcParam.isEmpty || config.srcParam.contains(p.order)
+    }
+    val sink = cpg.method(config.dstRegex).parameter.filter { p =>
+      config.dstParam.isEmpty || config.dstParam.contains(p.order)
+    }
+
+    implicit val semantics: Semantics = JoernWorkspaceLoader.defaultSemantics
+    implicit val context: EngineContext = EngineContext(semantics)
+
+    println("Determining flows...")
+    sink.reachableByFlows(source).p.foreach(println)
+    println("[DONE]")
+
+    print("Closing graph... ")
     cpg.close()
+    println("[DONE]")
   }
 
 }
