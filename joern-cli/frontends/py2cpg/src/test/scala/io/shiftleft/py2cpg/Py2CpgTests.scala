11d9eedb5f47cae76181a5aea29cc802de385da1
Markus Lottmann <markus@shiftleft.io>
2021-01-15 12:46:28 +0100

    Rename test class.

--- joern-cli/frontends/py2cpg/src/test/scala/io/shiftleft/py2cpg/Py2CpgTests.scala
+++ joern-cli/frontends/py2cpg/src/test/scala/io/shiftleft/py2cpg/Py2CpgTests.scala
@@ -4,7 +4,7 @@ import io.shiftleft.semanticcpg.language._
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-class CodeToCpgTests extends AnyWordSpec with Matchers {
+class Py2CpgTests extends AnyWordSpec with Matchers {
   "foo" in {
     val cpg = Py2CpgTest.newContext.buildCpg("""print("Foo")""")
 
