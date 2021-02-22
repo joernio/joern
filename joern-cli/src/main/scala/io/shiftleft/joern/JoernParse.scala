3b120487d9053b2e6a775a6b5e7c62094cb1cdca
Fabian Yamaguchi <fabs@shiftleft.io>
2021-02-22 19:33:15 +0100

    Call Plume in `codepropertygraph - console` (#472)
    * Call plume from codepropertygraph
    
    * Set correct CPG version

--- joern-cli/src/main/scala/io/shiftleft/joern/JoernParse.scala
+++ joern-cli/src/main/scala/io/shiftleft/joern/JoernParse.scala
@@ -1,7 +1,8 @@
 package io.shiftleft.joern
 
+import io.joern.plume.PlumeCpgGenerator
 import io.shiftleft.fuzzyc2cpg.FuzzyC2Cpg
-import io.shiftleft.joern.plume.PlumeCpgGenerator
+
 import scala.util.control.NonFatal
 
 object JoernParse extends App {
@@ -22,7 +23,7 @@ object JoernParse extends App {
         case "c" =>
           createCpgFromCSourceCode(config)
         case "java" =>
-          PlumeCpgGenerator.createCpgForJava(config)
+          PlumeCpgGenerator.createCpgForJava(config.inputPaths.toList.sorted, config.outputCpgFile)
         case _ =>
           println(s"Error: Language ${config.language} not recognized")
           return
