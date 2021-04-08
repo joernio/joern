61da982ce0169a30da033a327bc43b5bd85055f1
Michael Pollmeier <michael@michaelpollmeier.com>
2021-04-08 22:01:01 +1200

    fix test script for CI
    (hopefully this works on release as well, cannot test...)

--- joern-cli/src/universal/schema-extender/test.sh
+++ joern-cli/src/universal/schema-extender/test.sh
@@ -5,7 +5,9 @@ echo "testing schema extender"
 set -x #verbose on
 
 ./schema-extender.sh
+
 # we should now be able to use our new `EXAMPLE_NODE` node
+mkdir -p scripts
 echo 'assert(nodes.ExampleNode.Label == "EXAMPLE_NODE")
 assert(nodes.ExampleNode.PropertyNames.all.contains("EXAMPLE_PROPERTY"))' > scripts/SchemaExtenderTest.sc
 ./joern --script scripts/SchemaExtenderTest.sc
