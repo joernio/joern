0e853dc67fac79524775d5212557cf9a0440707e
Markus Lottmann <markus@shiftleft.io>
2021-06-29 13:25:57 +0200

    Improve logging in case of errors.

--- joern-cli/frontends/py2cpg/src/main/scala/io/shiftleft/py2cpg/CodeToCpg.scala
+++ joern-cli/frontends/py2cpg/src/main/scala/io/shiftleft/py2cpg/CodeToCpg.scala
@@ -4,22 +4,34 @@ import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.passes.{DiffGraph, IntervalKeyPool, ParallelCpgPass}
 import io.shiftleft.py2cpg.Py2Cpg.InputProvider
 import io.shiftleft.pythonparser.PyParser
+import org.slf4j.LoggerFactory
 
 class CodeToCpg(cpg: Cpg, inputProvider: Iterable[InputProvider], keyPool: IntervalKeyPool)
     extends ParallelCpgPass[InputProvider](
       cpg,
       keyPools = Some(keyPool.split(inputProvider.size))
     ) {
+  import CodeToCpg.logger
 
   override def partIterator: Iterator[InputProvider] = inputProvider.iterator
 
   override def runOnPart(inputProvider: InputProvider): Iterator[DiffGraph] = {
     val inputPair = inputProvider()
-    val parser = new PyParser()
-    val astRoot = parser.parse(inputPair.content)
-    val astVisitor = new PythonAstVisitor(inputPair.file, PythonV2AndV3)
-    astVisitor.convert(astRoot)
+    try {
+      val parser = new PyParser()
+      val astRoot = parser.parse(inputPair.content)
+      val astVisitor = new PythonAstVisitor(inputPair.file, PythonV2AndV3)
+      astVisitor.convert(astRoot)
 
-    Iterator.single(astVisitor.getDiffGraph)
+      Iterator.single(astVisitor.getDiffGraph)
+    } catch {
+      case exception: Throwable =>
+        logger.warn(s"Failed to convert file ${inputPair.file}", exception)
+        Iterator.empty
+    }
   }
+}
+
+object CodeToCpg {
+  private val logger = LoggerFactory.getLogger(getClass)
 }
