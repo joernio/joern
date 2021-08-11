18057ce20af8efd33d21048fe71ee981de5c9bdc
Markus Lottmann <markus@shiftleft.io>
2021-08-11 12:32:36 +0200

    Use node and language definitions only from CPG repo.

--- joern-cli/frontends/py2cpg/src/test/scala/io/shiftleft/py2cpg/cpg/FunctionDefCpgTests.scala
+++ joern-cli/frontends/py2cpg/src/test/scala/io/shiftleft/py2cpg/cpg/FunctionDefCpgTests.scala
@@ -3,7 +3,6 @@ package io.shiftleft.py2cpg.cpg
 import io.shiftleft.py2cpg.Constants
 import io.shiftleft.py2cpg.Py2CpgTestContext
 import io.shiftleft.semanticcpg.language._
-import io.shiftleft.semanticcpgext.language._
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
