9775bbea5f41fdf87a3db73d6b4df82260ed37c1
Michael Pollmeier <michael@michaelpollmeier.com>
2021-08-20 17:54:54 +0200

    remove fuzzyc/c2cpg as direct dependencies - will download binaries

--- build.sbt
+++ build.sbt
@@ -7,7 +7,7 @@ organization := "io.shiftleft"
 ThisBuild / scalaVersion := "2.13.5"
 // don't upgrade to 2.13.6 until https://github.com/com-lihaoyi/Ammonite/issues/1182 is resolved
 ThisBuild /Test /fork := true
-val cpgVersion = "1.3.293"
+val cpgVersion = "1.3.298"
 val ghidra2cpgVersion = "0.0.24"
 val js2cpgVersion = "0.2.2"
 
---
+++ project/SimpleCache.scala
@@ -0,0 +1,25 @@
+import sbt.{IO,URL}
+import java.io.File
+import java.net.URLEncoder
+
+object SimpleCache {
+  val LocalCacheDir = ".local"
+
+  /** download given url if not yet present in cache, then add to cache
+    * warning: only use for immutable artifacts, i.e. do not use for snapshot artifacts that may change 
+    */
+  def downloadMaybe(url: String): File = {
+    val urlEncoded = URLEncoder.encode(url, "UTF-8")
+    val localFile = new File(s"$LocalCacheDir/$urlEncoded")
+
+    if (!localFile.exists) {
+      println(s"downloading $url")
+      sbt.io.Using.urlInputStream(new URL(url)) { inputStream =>
+        IO.transfer(inputStream, localFile)
+      }
+    }
+
+    localFile
+  }
+
+}
