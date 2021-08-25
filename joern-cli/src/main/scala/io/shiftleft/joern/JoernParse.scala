b2fa35a83571a5c3b771286cbc6fca21b71e4aa2
Johannes Coetzee <jcoetzee@shiftleft.io>
2021-08-25 16:30:37 +0200

    Rewrite JoernParse to use mechanisms in codepropertygraph to generate CPGs

--- joern-cli/src/main/scala/io/shiftleft/joern/JoernParse.scala
+++ joern-cli/src/main/scala/io/shiftleft/joern/JoernParse.scala
@@ -1,96 +1,116 @@
 package io.shiftleft.joern
 
-import io.shiftleft.fuzzyc2cpg.FuzzyC2Cpg
+import io.shiftleft.codepropertygraph.generated.Languages
+import io.shiftleft.console.{FrontendConfig, InstallConfig}
+import io.shiftleft.console.cpgcreation.{cpgGeneratorForLanguage, guessLanguage}
 import io.shiftleft.joern.plume.PlumeCpgGenerator
 
-import scala.util.control.NonFatal
-
 object JoernParse extends App {
+
+  class InvalidLanguageError extends Error
+
+  // Special string used to separate joern-parse opts from frontend-specific opts
+  val ARGS_DELIMITER = "--frontend-args"
   val DEFAULT_CPG_OUT_FILE = "cpg.bin"
 
-  parseConfig.foreach { config =>
-    try {
-      generateCpg(config)
-    } catch {
-      case NonFatal(ex) =>
-        println("Error: Failed to generate/enhance CPG.", ex)
+  val (parserArgs, frontendArgs) = splitArgs()
+  val installConfig = new InstallConfig()
+
+  run() match {
+    case Right(msg) => println(msg)
+
+    case Left(errMsg) => println(s"ERROR: $errMsg")
+  }
+
+  def run(): Either[String, String] = {
+    for {
+      config <- parseConfig(parserArgs)
+      language <- getLanguage(config)
+      _ <- generateCpg(config, language)
+      _ <- enhanceCpg(config, language)
+    } yield "Operation success"
+  }
+
+  def splitArgs(): (List[String], List[String]) = {
+    println(s"Args to split: ${args.toList}")
+    args.indexOf(ARGS_DELIMITER) match {
+      case -1 => (args.toList, Nil)
+
+      case splitIdx =>
+        val (parseOpts, frontendOpts) = args.toList.splitAt(splitIdx)
+        (parseOpts, frontendOpts.tail) // Take the tail to ignore the delimiter
     }
   }
 
-  def generateCpg(config: ParserConfig): Unit = {
-    if (!config.enhanceOnly) {
-      config.language match {
-        case "c" =>
-          createCpgFromCSourceCode(config)
-        case "java" =>
-          PlumeCpgGenerator.createCpgForJava(config)
-        case _ =>
-          println(s"Error: Language ${config.language} not recognized")
-          return
+  def getLanguage(config: ParserConfig): Either[String, String] = {
+    if (config.language.isEmpty) {
+      val inputPath = config.inputPath
+      guessLanguage(inputPath) match {
+        case Some(guess) => Right(guess)
+
+        case None => Left(
+          s"Could not guess language from input path $inputPath. Please specify a language using the --language option."
+        )
+      }
+    } else {
+      Right(config.language)
+    }
+  }
+
+  def generateCpg(config: ParserConfig, language: String): Either[String, String] = {
+    if (config.enhanceOnly) {
+      Right("No generation required")
+    } else if (language == "java") {
+      PlumeCpgGenerator.createCpgForJava(config)
+    } else {
+      val generator = cpgGeneratorForLanguage(language, FrontendConfig(), installConfig.rootPath.path, frontendArgs).get
+      generator.generate(config.inputPath, outputPath = config.outputCpgFile, namespaces = config.namespaces) match {
+        case Some(cmd) => Right(cmd)
+
+        case None => Left(s"Could not generate CPG with language = $language and input = ${config.inputPath}")
       }
     }
-
-    if (config.enhance && config.language != "java") {
-      Cpg2Scpg.run(config.outputCpgFile, config.dataFlow).close()
-    }
-
   }
 
-  private def createCpgFromCSourceCode(config: ParserConfig): Unit = {
-    val fuzzyc = new FuzzyC2Cpg()
-    if (config.preprocessorConfig.usePreprocessor) {
-      fuzzyc.runWithPreprocessorAndOutput(
-        config.inputPaths,
-        config.cFrontendConfig.sourceFileExtensions,
-        config.preprocessorConfig.includeFiles,
-        config.preprocessorConfig.includePaths,
-        config.preprocessorConfig.defines,
-        config.preprocessorConfig.undefines,
-        config.preprocessorConfig.preprocessorExecutable
-      )
-    } else {
-      fuzzyc
-        .runAndOutput(config.inputPaths, config.cFrontendConfig.sourceFileExtensions, Some(config.outputCpgFile))
-        .close()
+  def enhanceCpg(config: ParserConfig, language: String): Either[String, String] = {
+    try {
+      if (config.enhance && language != "java") {
+        Cpg2Scpg.run(config.outputCpgFile, config.dataFlow).close()
+      }
+      Right("Enhance successful")
+    } catch {
+      case err: Throwable => Left(err.getMessage)
     }
   }
 
-  case class ParserConfig(inputPaths: Set[String] = Set.empty,
+  case class ParserConfig(inputPath: String = "",
                           outputCpgFile: String = DEFAULT_CPG_OUT_FILE,
+                          namespaces: List[String] = List.empty,
                           enhance: Boolean = true,
                           dataFlow: Boolean = true,
                           enhanceOnly: Boolean = false,
-                          language: String = "c",
-                          cFrontendConfig: CFrontendConfig = CFrontendConfig(),
-                          preprocessorConfig: PreprocessorConfig = PreprocessorConfig())
+                          language: String = "c")
 
-  case class CFrontendConfig(sourceFileExtensions: Set[String] = Set(".c", ".cc", ".cpp", ".h", ".hpp"))
-
-  case class PreprocessorConfig(preprocessorExecutable: String = "./bin/fuzzyppcli",
-                                verbose: Boolean = true,
-                                includeFiles: Set[String] = Set.empty,
-                                includePaths: Set[String] = Set.empty,
-                                defines: Set[String] = Set.empty,
-                                undefines: Set[String] = Set.empty) {
-    val usePreprocessor: Boolean =
-      includeFiles.nonEmpty || includePaths.nonEmpty || defines.nonEmpty || undefines.nonEmpty
-  }
-
-  def parseConfig: Option[ParserConfig] =
+  def parseConfig(parserArgs: List[String]): Either[String, ParserConfig] =
     new scopt.OptionParser[ParserConfig]("joern-parse") {
-
-      arg[String]("input-files")
-        .unbounded()
-        .text("directories containing: C/C++ source | Java classes | a Java archive (JAR/WAR)")
-        .action((x, c) => c.copy(inputPaths = c.inputPaths + x))
-
-      opt[String]("language")
-        .text("source language: [c|java]. Default: c")
-        .action((x, c) => c.copy(language = x))
+      arg[String]("input")
+        .required()
+        .text("source file or directory containing source files")
+        .action((x, c) => c.copy(inputPath = x))
 
       opt[String]("out")
         .text("output filename")
         .action((x, c) => c.copy(outputCpgFile = x))
+
+      opt[String]("language")
+        .optional()
+        .text("source language")
+        .action((x, c) => c.copy(language = x))
+
+      opt[String]("namespaces")
+        .optional()
+        .text("namespaces to include: comma separated string")
+        .action((x, c) => c.copy(namespaces = x.split(",").map(_.strip).toList))
 
       note("Enhancement stage")
 
@@ -104,42 +124,16 @@ object JoernParse extends App {
         .text("do not perform data flow analysis in enhancement stage")
         .action((x, c) => c.copy(dataFlow = false))
 
-      note("Options for C/C++")
-
-      opt[String]("source-file-ext")
-        .unbounded()
-        .text("source file extensions to include when gathering source files. Defaults are .c, .cc, .cpp, .h and .hpp")
-        .action((pat, cfg) =>
-          cfg.copy(cFrontendConfig =
-            cfg.cFrontendConfig.copy(sourceFileExtensions = cfg.cFrontendConfig.sourceFileExtensions + pat)))
-      opt[String]("include")
-        .unbounded()
-        .text("header include files")
-        .action((incl, cfg) =>
-          cfg.copy(preprocessorConfig =
-            cfg.preprocessorConfig.copy(includeFiles = cfg.preprocessorConfig.includeFiles + incl)))
-      opt[String]('I', "")
-        .unbounded()
-        .text("header include paths")
-        .action((incl, cfg) =>
-          cfg.copy(preprocessorConfig =
-            cfg.preprocessorConfig.copy(includePaths = cfg.preprocessorConfig.includePaths + incl)))
-      opt[String]('D', "define")
-        .unbounded()
-        .text("define a name")
-        .action((d, cfg) =>
-          cfg.copy(preprocessorConfig = cfg.preprocessorConfig.copy(defines = cfg.preprocessorConfig.defines + d)))
-      opt[String]('U', "undefine")
-        .unbounded()
-        .text("undefine a name")
-        .action((u, cfg) =>
-          cfg.copy(preprocessorConfig = cfg.preprocessorConfig.copy(defines = cfg.preprocessorConfig.undefines + u)))
-      opt[String]("preprocessor-executable")
-        .text("path to the preprocessor executable")
-        .action((s, cfg) => cfg.copy(preprocessorConfig = cfg.preprocessorConfig.copy(preprocessorExecutable = s)))
-
       note("Misc")
       help("help").text("display this help message")
-    }.parse(args, ParserConfig())
+
+      note(s"Args specified after the $ARGS_DELIMITER separator will be passed to the front-end verbatim")
+    }.parse(parserArgs, ParserConfig()) match {
+      case Some(config) => Right(config)
+
+      case None =>
+        println(s"args: $parserArgs")
+        Left("Could not parse command line options")
+    }
 
 }
