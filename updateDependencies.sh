bbabe0b3a7a9f55598bb241f7d688d765111e5d3
Michael Pollmeier <michael@michaelpollmeier.com>
2021-09-01 13:24:02 +0200

    add missing entries in updateDependencies.sh (#588)

--- updateDependencies.sh
+++ updateDependencies.sh
@@ -24,4 +24,6 @@ function update {
 }
 
 update cpg git@github.com:ShiftLeftSecurity/codepropertygraph.git
+update c2cpg git@github.com:ShiftLeftSecurity/codepropertygraph.git
+update fuzzyc2cpg git@github.com:ShiftLeftSecurity/codepropertygraph.git
 update ghidra2cpg git@github.com:joernio/ghidra2cpg.git
