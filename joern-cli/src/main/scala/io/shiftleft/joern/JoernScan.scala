4ca192ae36ffba5bc2bf90b69762a834eacdf8af
Claudiu-Vlad Ursache <claudiu+hw@shiftleft.io>
2021-09-21 13:15:57 +0200

    Bump cpg and qdb deps

--- joern-cli/src/main/scala/io/shiftleft/joern/JoernScan.scala
+++ joern-cli/src/main/scala/io/shiftleft/joern/JoernScan.scala
@@ -15,7 +15,7 @@ import io.shiftleft.semanticcpg.language.{DefaultNodeExtensionFinder, NodeExtens
 import scala.reflect.runtime.universe._
 
 object JoernScanConfig {
-  val defaultDbVersion: String = "0.0.97"
+  val defaultDbVersion: String = "0.0.106"
 }
 
 case class JoernScanConfig(src: String = "",
