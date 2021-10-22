9043be863897a4acd59633950fbedaf0a33b17b2
Michael Pollmeier <michael@michaelpollmeier.com>
2021-10-22 09:42:17 +0200

    adjust path

--- querydb/src/test/scala/io/joern/suites/GhidraQueryTestSuite.scala
+++ querydb/src/test/scala/io/joern/suites/GhidraQueryTestSuite.scala
@@ -12,7 +12,7 @@ import overflowdb.traversal.iterableToTraversal
 
 class GhidraQueryTestSuite extends DataFlowBinToCpgSuite {
   val argumentProvider = new QDBArgumentProvider(3)
-  override val binDirectory: String = ProjectRoot.relativise("src/test/resources/testbinaries")
+  override val binDirectory: String = ProjectRoot.relativise("querydb/src/test/resources/testbinaries")
 
   override def beforeAll(): Unit = {
     semanticsFilename = argumentProvider.testSemanticsFilename
