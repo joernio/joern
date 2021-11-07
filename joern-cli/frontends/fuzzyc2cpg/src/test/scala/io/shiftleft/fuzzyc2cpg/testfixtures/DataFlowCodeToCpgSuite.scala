7770890ee6b0ca8489848a53060ad6df4382870b
Michael Pollmeier <michael@michaelpollmeier.com>
2021-11-07 11:37:46 +0100

    adapt path

--- joern-cli/frontends/fuzzyc2cpg/src/test/scala/io/shiftleft/fuzzyc2cpg/testfixtures/DataFlowCodeToCpgSuite.scala
+++ joern-cli/frontends/fuzzyc2cpg/src/test/scala/io/shiftleft/fuzzyc2cpg/testfixtures/DataFlowCodeToCpgSuite.scala
@@ -9,6 +9,7 @@ import io.shiftleft.dataflowengineoss.semanticsloader.{Parser, Semantics}
 import io.shiftleft.semanticcpg.language._
 import io.shiftleft.semanticcpg.language.dotextension.ImageViewer
 import io.shiftleft.semanticcpg.layers.{Base, CallGraph, ControlFlow, LayerCreatorContext, TypeRelations}
+import io.shiftleft.utils.ProjectRoot
 import overflowdb.traversal.Traversal
 
 import scala.sys.process.Process
@@ -16,7 +17,7 @@ import scala.util.Try
 
 class DataFlowCodeToCpgSuite extends FuzzyCCodeToCpgSuite {
 
-  var semanticsFilename = "dataflowengineoss/src/test/resources/default.semantics"
+  val semanticsFilename = ProjectRoot.relativise("dataflowengineoss/src/test/resources/default.semantics")
   var semantics: Semantics = _
   implicit var context: EngineContext = _
 
