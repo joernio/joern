575af680c5bc4c7021802b736150499cd66f670a
Niko <niko@shiftleft.io>
2021-11-13 15:03:53 +0100

    Code cleaning

--- joern-cli/frontends/ghidra2cpg/src/main/scala/io/joern/ghidra2cpg/passes/mips/MipsFunctionPass.scala
+++ joern-cli/frontends/ghidra2cpg/src/main/scala/io/joern/ghidra2cpg/passes/mips/MipsFunctionPass.scala
@@ -4,6 +4,7 @@ import ghidra.app.decompiler.DecompInterface
 import ghidra.program.model.address.GenericAddress
 import ghidra.program.model.lang.Register
 import ghidra.program.model.listing.{Function, Instruction, Program}
+import ghidra.program.model.pcode.PcodeOp.{CALL, CALLIND}
 import ghidra.program.model.scalar.Scalar
 import ghidra.util.task.ConsoleTaskMonitor
 import io.joern.ghidra2cpg.Types
@@ -14,6 +15,7 @@ import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.codepropertygraph.generated.EdgeTypes
 import io.shiftleft.codepropertygraph.generated.nodes.CfgNodeNew
 import io.shiftleft.passes.{DiffGraph, IntervalKeyPool}
+import org.slf4j.LoggerFactory
 
 import scala.jdk.CollectionConverters._
 import scala.language.implicitConversions
@@ -26,8 +28,7 @@ class MipsFunctionPass(currentProgram: Program,
                        keyPool: IntervalKeyPool,
                        decompInterface: DecompInterface)
     extends FunctionPass(new MipsProcessor, currentProgram, function, cpg, keyPool, decompInterface) {
-
-  val mipsCallInstructions = List("jalr", "jal")
+  private val logger = LoggerFactory.getLogger(classOf[MipsFunctionPass])
   def resolveLiterals(instruction: Instruction, callNode: CfgNodeNew): Unit = {
 
     highFunction
@@ -77,7 +78,6 @@ class MipsFunctionPass(currentProgram: Program,
     }
   }
   def addCallArguments(instruction: Instruction, callee: Function, callNode: CfgNodeNew): Unit = {
-
     // Array of tuples containing (checked parameter name, parameter index, parameter data type)
     var checkedParameters: Array[(String, Int, String)] = Array.empty
     if (callee.isThunk) {
@@ -121,57 +121,63 @@ class MipsFunctionPass(currentProgram: Program,
 
   }
 
-  def handleInstruction(instruction: Instruction, callNode: CfgNodeNew): Unit = {
+  def addArguments(instruction: Instruction, instructionNode: CfgNodeNew): Unit = {
     for (index <- 0 until instruction.getNumOperands) {
       val opObjects = instruction.getOpObjects(index)
-      for (opObject <- opObjects) {
-        opObject match {
-          case register: Register =>
-            val node = createIdentifier(register.getName,
-                                        register.getName,
-                                        index + 1,
-                                        Types.registerType(register.getName),
-                                        instruction.getMinAddress.getOffsetAsBigInteger.intValue)
-            addArgumentEdge(callNode, node)
-          case scalar: Scalar =>
-            val node = createLiteral(scalar.toString(16, false, false, "", ""),
-                                     index + 1,
-                                     index + 1,
-                                     scalar.toString(16, false, false, "", ""),
-                                     instruction.getMinAddress.getOffsetAsBigInteger.intValue)
-            addArgumentEdge(callNode, node)
-          case genericAddress: GenericAddress =>
-            // TODO: try to resolve the address
-            val node = createLiteral(genericAddress.toString(),
-                                     index + 1,
-                                     index + 1,
-                                     genericAddress.toString(),
-                                     instruction.getMinAddress.getOffsetAsBigInteger.intValue)
-            addArgumentEdge(callNode, node)
-          case _ =>
-            println(
-              s"""Unsupported argument: $opObject ${opObject.getClass.getSimpleName}"""
-            )
+      if (opObjects.length > 1) {} else
+        for (opObject <- opObjects) {
+          opObject match {
+            case register: Register =>
+              val node = createIdentifier(register.getName,
+                                          register.getName,
+                                          index + 1,
+                                          Types.registerType(register.getName),
+                                          instruction.getMinAddress.getOffsetAsBigInteger.intValue)
+              addArgumentEdge(instructionNode, node)
+            case scalar: Scalar =>
+              val node = createLiteral(scalar.toString(16, false, false, "", ""),
+                                       index + 1,
+                                       index + 1,
+                                       scalar.toString(16, false, false, "", ""),
+                                       instruction.getMinAddress.getOffsetAsBigInteger.intValue)
+              addArgumentEdge(instructionNode, node)
+            case genericAddress: GenericAddress =>
+              val node = createLiteral(genericAddress.toString,
+                                       index + 1,
+                                       index + 1,
+                                       genericAddress.toString,
+                                       instruction.getMinAddress.getOffsetAsBigInteger.intValue)
+              addArgumentEdge(instructionNode, node)
+            case _ =>
+              logger.warn(
+                s"""Unsupported argument: $opObject ${opObject.getClass.getSimpleName}"""
+              )
+          }
         }
-      }
     }
   }
+
   // Iterating over operands and add edges to call
   override def handleArguments(
       instruction: Instruction,
       callNode: CfgNodeNew
   ): Unit = {
-    val mnemonicString = instruction.getMnemonicString
-    if (mipsCallInstructions.contains(mnemonicString)) {
-      val mipsPrefix = "^t9=>".r
-      val calledFunction =
-        mipsPrefix.replaceFirstIn(codeUnitFormat.getOperandRepresentationString(instruction, 0), "")
-      functions.find(function => function.getName().equals(calledFunction)).foreach { callee =>
-        resolveLiterals(instruction, callNode)
-        addCallArguments(instruction, callee, callNode)
-      }
-    } else {
-      handleInstruction(instruction, callNode)
+    instruction.getPcode.toList.lastOption.getOrElse() match {
+      case CALL | CALLIND =>
+        // Call arguments are treated different
+        val mipsPrefix = "^t9=>".r
+        val calledFunction =
+          mipsPrefix.replaceFirstIn(codeUnitFormat.getOperandRepresentationString(instruction, 0), "")
+        functions.find(function => function.getName().equals(calledFunction)).foreach { callee =>
+          // try to resolve literal arguments to call
+          // eg. printf("foo")
+          resolveLiterals(instruction, callNode)
+          // resolve other arguments, like registers
+          addCallArguments(instruction, callee, callNode)
+        }
+      case _ =>
+        // regular instructions, eg. add/sub
+        addArguments(instruction, callNode)
     }
   }
   override def runOnPart(part: String): Iterator[DiffGraph] = {
