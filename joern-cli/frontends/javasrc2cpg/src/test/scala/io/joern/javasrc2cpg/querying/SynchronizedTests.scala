5eaf0e0a4bd6440cc38ffcec71118c5891510aed
Johannes Coetzee <jcoetzee@shiftleft.io>
2021-11-17 15:53:29 +0200

    Add synchronized statements to javasrc2cpg

---
+++ joern-cli/frontends/javasrc2cpg/src/test/scala/io/joern/javasrc2cpg/querying/SynchronizedTests.scala
@@ -0,0 +1,45 @@
+package io.joern.javasrc2cpg.querying
+
+import io.joern.javasrc2cpg.testfixtures.JavaSrcCodeToCpgFixture
+import io.shiftleft.codepropertygraph.generated.nodes.{Block, Identifier, Method, MethodParameterIn, MethodReturn, Return}
+import io.shiftleft.semanticcpg.language._
+
+class SynchronizedTests extends JavaSrcCodeToCpgFixture {
+
+  override val code: String =
+    """
+      |public class Foo {
+      |  public synchronized String foo(String s) {
+      |    return s;
+      |  }
+      |
+      |  public String bar(String s) {
+      |    synchronized (this) {
+      |      s += "A";
+      |    }
+      |    return s;
+      |  }
+      |
+      |}
+      |""".stripMargin
+
+  "it should process a synchronized method the same as a non-synchronized method" in {
+    val List(method: Method) = cpg.method.name("foo").l
+
+    method.astChildren.size shouldBe 4
+    val List(param: MethodParameterIn, _, body: Block, _: MethodReturn) = method.astChildren.l
+    param.code shouldBe "String s"
+    body.astChildren.head shouldBe a[Return]
+  }
+
+  "it should create a synchronized block" in {
+    val List(method: Method) = cpg.method.name("bar").l
+    val List(syncBlock: Block) = method.ast.isBlock.codeExact("synchronized").l
+
+    syncBlock.astChildren.size shouldBe 2
+    val List(cond: Identifier, body: Block) = syncBlock.astChildren.l
+
+    cond.code shouldBe "this"
+    body.astChildren.head.code shouldBe "s += \"A\""
+  }
+}
