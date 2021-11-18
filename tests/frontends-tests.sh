e60b675a3e8efcd1b3e10628b35ab34ab354e65a
Michael Pollmeier <michael@michaelpollmeier.com>
2021-11-18 14:07:39 +0100

    don't include the language auto detection in this integration test

--- tests/frontends-tests.sh
+++ tests/frontends-tests.sh
@@ -6,28 +6,32 @@ SCRIPT_ABS_DIR=$(dirname "$SCRIPT_ABS_PATH")
 
 JOERN="$SCRIPT_ABS_DIR"/../joern
 
-languages=(c cat mouse)
+frontends=(c javasrc js jimple ghidra)
 declare -A minMethodCount=(
-  [c]=20
+  [c]=2
   [javasrc]=10000
-  [java]=10000
   [js]=10000
+  [jimple]=10000
+  [ghidra]=10000
 )
 declare -A expectedMethod=(
   [c]=print_number
   [javasrc]=foo
-  [java]=foo
   [js]=foo
+  [jimple]=foo
+  [ghidra]=foo
 )
 
-for language in "${languages[@]}"; do
-  $JOERN --script tests/frontends-testscript.sc --params pathToCode=tests/code/$language,minMethodCount=${minMethodCount[$language]},expectedMethod=${expectedMethod[$language]}
+for frontend in "${frontends[@]}"; do
+  $JOERN --script tests/frontends-testscript.sc --params inputPath=tests/code/$frontend,minMethodCount=${minMethodCount[$frontend]},expectedMethod=${expectedMethod[$frontend]},frontend=$frontend
 
   JOERN_EXIT_CODE=$?
   if [ $JOERN_EXIT_CODE != 0 ]; then
-    echo "Script [$script] failed to run successfully for language=$language"
+    echo "Script [$script] failed to run successfully for frontend=$frontend"
     exit 1
   fi
+
+  echo "Frontend [$frontend] tested successfully"
 done
 
 
--- tests/frontends-testscript.sc
+++ tests/frontends-testscript.sc
@@ -1,7 +1,10 @@
 import io.shiftleft.semanticcpg.language._
 
-@main def main(pathToCode: String, minMethodCount: Int, expectedMethod: String) = {
-  importCode(pathToCode)
+@main def main(inputPath: String,
+               minMethodCount: Int,
+               expectedMethod: String,
+               frontend: String) = {
+  importCode(inputPath, language = frontend.toUpperCase)
   val methodCount = cpg.method.size
   assert(methodCount >= minMethodCount,
          s"expected at least $minMethodCount methods, but only found $methodCount")
