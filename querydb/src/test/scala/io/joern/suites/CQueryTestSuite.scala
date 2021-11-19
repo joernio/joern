d97414c6f2d58d9e5a8608283f44211524c333de
grayfox <grayfox@outerhaven.de>
2021-11-19 13:41:48 +0100

    use newc frontend instead of oldc in CQueryTestSuite

--- querydb/src/test/scala/io/joern/suites/CQueryTestSuite.scala
+++ querydb/src/test/scala/io/joern/suites/CQueryTestSuite.scala
@@ -6,7 +6,7 @@ import io.shiftleft.codepropertygraph.generated.nodes
 import io.joern.console.scan._
 import io.joern.console.{QueryBundle, QueryDatabase}
 import io.shiftleft.console.Query
-import io.joern.fuzzyc2cpg.testfixtures.DataFlowCodeToCpgSuite
+import io.joern.c2cpg.testfixtures.DataFlowCodeToCpgSuite
 import io.shiftleft.semanticcpg.language._
 
 class CQueryTestSuite extends DataFlowCodeToCpgSuite {
