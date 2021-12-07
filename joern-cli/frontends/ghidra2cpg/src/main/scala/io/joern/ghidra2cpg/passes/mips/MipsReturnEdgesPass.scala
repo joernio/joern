faa82712ac43da2e2b1d7d741806ab18ffa3d947
Niko Schmidt <itsacoderepo@users.noreply.github.com>
2021-12-07 11:27:20 +0000

    Create MipsReturnEdgesPass.scala

---
+++ joern-cli/frontends/ghidra2cpg/src/main/scala/io/joern/ghidra2cpg/passes/mips/MipsReturnEdgesPass.scala
@@ -0,0 +1,24 @@
+package io.joern.ghidra2cpg.passes.mips
+
+import io.shiftleft.codepropertygraph.Cpg
+import io.shiftleft.codepropertygraph.generated.{EdgeTypes, PropertyNames}
+import io.shiftleft.passes.{CpgPass, DiffGraph}
+import io.shiftleft.semanticcpg.language._
+import org.slf4j.{Logger, LoggerFactory}
+
+class MipsReturnEdgesPass(cpg: Cpg) extends CpgPass(cpg) {
+  private val logger: Logger = LoggerFactory.getLogger(this.getClass)
+
+  override def run(): Iterator[DiffGraph] = {
+    logger.info("Running ReturnEdgesPass")
+    implicit val diffGraph: DiffGraph.Builder = DiffGraph.newBuilder
+    cpg.call.nameNot("<operator>.*").foreach { from =>
+      // We expect v1|v0 as return
+      val to = from.cfgNext.cfgNext.isCall.argument.code("v(0|1)").headOption
+      if (to.nonEmpty) {
+        diffGraph.addEdge(from, to.get, EdgeTypes.REACHING_DEF, Seq((PropertyNames.VARIABLE, from.code)))
+      }
+    }
+    Iterator(diffGraph.build())
+  }
+}
