202921015e0f8ddf5d5d1990ebf62ae71c8c7cdb
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2021-12-07 12:42:10 +0100

    Update DefaultDefines.scala

--- joern-cli/frontends/c2cpg/src/main/scala/io/joern/c2cpg/parser/DefaultDefines.scala
+++ joern-cli/frontends/c2cpg/src/main/scala/io/joern/c2cpg/parser/DefaultDefines.scala
@@ -2,8 +2,8 @@ package io.joern.c2cpg.parser
 
 object DefaultDefines {
   val DEFAULT_CALL_CONVENTIONS = Map(
-    "__fastcall" -> "__attribute((__fastcall__))",
-    "__cdecl" -> "__attribute((__cdecl__))",
-    "__pascal" -> "__attribute((__pascal__))"
+    "__fastcall" -> "__attribute((fastcall))",
+    "__cdecl" -> "__attribute((cdecl))",
+    "__pascal" -> "__attribute((pascal))"
   )
 }
