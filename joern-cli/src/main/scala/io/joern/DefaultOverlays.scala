41c16f7166ebf2762bf2aef3c7cf73e367bd3b3e
Fabian Yamaguchi <fabs@shiftleft.io>
2022-01-04 21:24:58 +0100

    Allow passing number of definitions to `joern-parse`

--- joern-cli/src/main/scala/io/joern/DefaultOverlays.scala
+++ joern-cli/src/main/scala/io/joern/DefaultOverlays.scala
@@ -7,19 +7,20 @@ import io.shiftleft.codepropertygraph.Cpg
 object DefaultOverlays {
 
   val DEFAULT_CPG_IN_FILE = "cpg.bin"
+  val defaultMaxNumberOfDefinitions = 4000
 
   /** Load the CPG at `storeFilename` and add enhancements,
     * turning the CPG into an SCPG.
     * @param storeFilename the filename of the cpg
     */
-  def create(storeFilename: String): Cpg = {
+  def create(storeFilename: String, maxNumberOfDefinitions: Int = defaultMaxNumberOfDefinitions): Cpg = {
     val cpg = CpgBasedTool.loadFromOdb(storeFilename)
     val context = new LayerCreatorContext(cpg)
     new Base().run(context)
     new TypeRelations().run(context)
     new ControlFlow().run(context)
     new CallGraph().run(context)
-    val options = new OssDataFlowOptions()
+    val options = new OssDataFlowOptions(maxNumberOfDefinitions)
     new OssDataFlow(options).run(context)
     cpg
   }
