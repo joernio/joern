73b5e8390d67b69ac0796c4497e9913da4dc9587
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2022-01-08 20:14:33 +0100

    Update AstForPrimitivesCreator.scala

--- joern-cli/frontends/c2cpg/src/main/scala/io/joern/c2cpg/astcreation/AstForPrimitivesCreator.scala
+++ joern-cli/frontends/c2cpg/src/main/scala/io/joern/c2cpg/astcreation/AstForPrimitivesCreator.scala
@@ -104,7 +104,7 @@ trait AstForPrimitivesCreator {
       case head :: Nil =>
         astForNode(head, order)
       case head :: tail =>
-        val code = s"${nodeSignature(head)}::${tail.mkString("::")}"
+        val code = s"${nodeSignature(head)}::${tail.map(nodeSignature).mkString("::")}"
         val callNode = newCallNode(head, op, op, DispatchTypes.STATIC_DISPATCH, order)
         callNode.code = code
         val arg1 = astForNode(head, 1)
