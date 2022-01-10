b8957af782a88ce367e5e623778e5b346b4e70b0
Markus Lottmann <markus.lottmann@mlbox.net>
2022-01-10 17:16:17 +0100

    No implicit chaining for implicits of  dataflowengineoss. (#878)

--- dataflowengineoss/src/main/scala/io/joern/dataflowengineoss/language/package.scala
+++ dataflowengineoss/src/main/scala/io/joern/dataflowengineoss/language/package.scala
@@ -3,7 +3,7 @@ package io.joern.dataflowengineoss
 import io.shiftleft.codepropertygraph.generated.nodes._
 import io.joern.dataflowengineoss.language.dotextension.DdgNodeDot
 import io.joern.dataflowengineoss.language.nodemethods.{ExpressionMethods, ExtendedCfgNodeMethods}
-import overflowdb.traversal.Traversal
+import overflowdb.traversal._
 
 package object language {
 
@@ -13,10 +13,13 @@ package object language {
   implicit def expressionMethods[NodeType <: Expression](node: NodeType): ExpressionMethods[NodeType] =
     new ExpressionMethods(node)
 
-  implicit def toExtendedCfgNode[A, NodeType <: CfgNode](a: A)(implicit f: A => Traversal[NodeType]): ExtendedCfgNode =
-    new ExtendedCfgNode(f(a).cast[CfgNode])
+  implicit def toExtendedCfgNode[NodeType <: CfgNode](traversal: IterableOnce[NodeType]): ExtendedCfgNode =
+    new ExtendedCfgNode(traversal)
 
-  implicit def toDdgNodeDot[A](a: A)(implicit f: A => Traversal[Method]): DdgNodeDot =
-    new DdgNodeDot(f(a))
+  implicit def toDdgNodeDot(traversal: IterableOnce[Method]): DdgNodeDot =
+    new DdgNodeDot(traversal)
+
+  implicit def toDdgNodeDotSingle(method: Method): DdgNodeDot =
+    new DdgNodeDot(Traversal.fromSingle(method))
 
 }
