becf3cfd63ea7e167bc1a53330c8f989cd53c37e
Claudiu-Vlad Ursache <claudiu+hw@shiftleft.io>
2022-01-12 14:55:17 +0100

    Add basic Kotlin test suite for BLOCK nodes

---
+++ joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/querying/BlockTests.scala
@@ -0,0 +1,27 @@
+package io.joern.kotlin2cpg.querying
+
+import io.joern.kotlin2cpg.Kt2CpgTestContext
+import io.shiftleft.codepropertygraph.generated.nodes.Block
+import io.shiftleft.semanticcpg.language._
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+
+class BlockTests extends AnyFreeSpec with Matchers {
+
+  "CPG for code with a simple function definition" - {
+    lazy val cpg = Kt2CpgTestContext.buildCpg("""
+        |package mypkg
+        |
+        |fun add1mul(x: Int, y: Int): Int {
+        |  return (x + 1) * y
+        |}
+        |""".stripMargin)
+
+    "should contain a BLOCK node with the correct props set" in {
+      val List(b) = cpg.method.block.take(1).l
+      b.lineNumber shouldBe Some(3)
+      b.columnNumber shouldBe Some(33)
+      b.typeFullName shouldBe "ANY"
+    }
+  }
+}
