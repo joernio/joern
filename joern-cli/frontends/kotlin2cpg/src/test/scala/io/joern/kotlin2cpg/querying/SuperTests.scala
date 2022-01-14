92386898d5c9728d74b62940df7239ac93bb5d62
Claudiu-Vlad Ursache <claudiu@cvursache.com>
2022-01-14 17:32:47 +0100

    Add basic test suite for Kotlin `super`-exprs (#891)

---
+++ joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/querying/SuperTests.scala
@@ -0,0 +1,44 @@
+package io.joern.kotlin2cpg.querying
+
+import io.joern.kotlin2cpg.Kt2CpgTestContext
+import io.shiftleft.codepropertygraph.generated.Operators
+import io.shiftleft.proto.cpg.Cpg.DispatchTypes
+import io.shiftleft.semanticcpg.language._
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+
+class SuperTests extends AnyFreeSpec with Matchers {
+  "CPG for code with simple call using _super_" - {
+    lazy val cpg = Kt2CpgTestContext.buildCpg("""
+        |package mypkg
+        |
+        |open class BClass {
+        |   open fun myfun() {
+        |       println("B.myfun")
+        |   }
+        |}
+        |
+        |class AClass : BClass() {
+        |    override fun myfun() {
+        |        super.myfun()
+        |        println("A.myfun")
+        |    }
+        |}
+        |
+        |fun main() {
+        |    val a = AClass()
+        |    a.myfun()
+        |}
+        |
+        |""".stripMargin)
+
+    "should have a CALL node for the call using _super_ with the correct props set" in {
+      val List(c) = cpg.call.code("super.*").l
+      c.dispatchType shouldBe DispatchTypes.DYNAMIC_DISPATCH.toString
+      c.methodFullName shouldBe "mypkg.BClass.myfun:kotlin.Unit()"
+      c.signature shouldBe "kotlin.Unit()"
+      c.lineNumber shouldBe Some(11)
+      c.columnNumber shouldBe Some(8)
+    }
+  }
+}
