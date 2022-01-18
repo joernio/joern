7ce5ac7f953c0272b4b7a43d6e35685f8285a737
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-01-18 17:03:25 +0100

    Update cats-effect to 3.3.4 (#911)

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@ object Versions {
   val js2cpg = parseVersion("js2cpgVersion")
   val antlr = "4.9.3"
   val scalatest = "3.2.10"
-  val cats = "3.3.0"
+  val cats = "3.3.4"
   val log4j = "2.17.1"
 
   private def parseVersion(key: String): String = { 
