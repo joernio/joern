6b8698776f3f1333018333f18a9b9ae37b9ce04e
Fabian Yamaguchi <fabs@joern.io>
2022-02-17 16:35:10 +0100

    Simpler positive/negative test case

--- joern-cli/frontends/c2cpg/src/test/scala/io/joern/c2cpg/querying/DataFlowTests.scala
+++ joern-cli/frontends/c2cpg/src/test/scala/io/joern/c2cpg/querying/DataFlowTests.scala
@@ -1708,25 +1708,36 @@ class DataFlowTest56 extends DataFlowCodeToCpgSuite {
 class DataFlowTest57 extends DataFlowCodeToCpgSuite {
   override val code: String =
     """
-      |void outer(char* ptr){
-      |  taint1(ptr);
-      |  inner(ptr);
-      |  return;
-      |}
-      |
-      |void inner(char * ptr)
+      |void abc()
       |{
-      |    taint2(ptr);
-      |    ptr = malloc(0x80);
-      |    sink(ptr);
-      |    return;
+      |    int a;
+      |    a = foo();
+      |    a = bar(0x80);
+      |    sink(a);
       |}
       |""".stripMargin
 
-  "should not find flow from taint1 to sink" in {
-    val src = cpg.call("taint1").argument
-    val snk = cpg.method("sink").parameter
+  "should not find a flow from 'a' at 'foo' to 'sink'" in {
+    def src = cpg.call("foo").inAssignment.target.head
+    def snk = cpg.method("sink").parameter
     snk.reachableByFlows(src).size shouldBe 0
   }
+}
 
+class DataFlowTest58 extends DataFlowCodeToCpgSuite {
+  override val code: String =
+    """
+      |void abc(int a)
+      |{
+      |    a = foo();
+      |    a = bar(0x80);
+      |    sink(a);
+      |}
+      |""".stripMargin
+
+  "should not find a flow from parameter 'a' to 'sink'" in {
+    def src = cpg.method("abc").parameter
+    def snk = cpg.method("sink").parameter
+    snk.reachableByFlows(src).size shouldBe 0
+  }
 }
