fd66607d8a2635b0908f0ab547a594793750921b
Claudiu-Vlad Ursache <claudiu+hw@shiftleft.io>
2022-02-22 08:43:56 +0100

    Add two simple test suites for `kotlin2cpg`

---
+++ joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/validation/DispatchTypeValidationTests.scala
@@ -0,0 +1,34 @@
+package io.joern.kotlin2cpg.validation
+
+import io.joern.kotlin2cpg.Kt2CpgTestContext
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+import io.shiftleft.codepropertygraph.generated.Operators
+import io.shiftleft.codepropertygraph.generated.edges.Ast
+import io.shiftleft.codepropertygraph.generated.nodes.{ClosureBinding, FieldIdentifier, Identifier}
+import io.shiftleft.codepropertygraph.generated.DispatchTypes
+import io.shiftleft.semanticcpg.language._
+import overflowdb.traversal.jIteratortoTraversal
+
+class DispatchTypeValidationTests extends AnyFreeSpec with Matchers {
+  // TODO: add inputs as arrays
+
+  "CPG for code with simple qualified-expression" - {
+    lazy val cpg = Kt2CpgTestContext.buildCpg("""
+        |package mypkg
+        |
+        |import java.lang.Runtime
+        |
+        |fun main() {
+        |   Runtime.getRuntime().exec("ls -al")
+        |}
+        """.stripMargin)
+
+    "should contain the correct DISPATCH_TYPE value" in {
+      val List(c) = cpg.call.methodFullNameExact("java.lang.Runtime.exec:java.lang.Process(java.lang.String)").l
+      c.dispatchType shouldBe DispatchTypes.DYNAMIC_DISPATCH
+    }
+  }
+
+  // TODO: add example with call to `super`
+}
