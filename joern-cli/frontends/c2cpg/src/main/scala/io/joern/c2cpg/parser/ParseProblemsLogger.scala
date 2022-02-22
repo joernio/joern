a3835d0d66f582899b3dbcc1bc0ad33a42c59622
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2022-02-22 10:54:20 +0100

    Improved parse failure logging (#1039)
    The exception message might be null for parse failures due to ambiguous nodes that can't be resolved successfully.
    We now extract better log messages for this case.

--- joern-cli/frontends/c2cpg/src/main/scala/io/joern/c2cpg/parser/ParseProblemsLogger.scala
+++ joern-cli/frontends/c2cpg/src/main/scala/io/joern/c2cpg/parser/ParseProblemsLogger.scala
@@ -22,4 +22,18 @@ trait ParseProblemsLogger {
     problems.foreach(logProblemNode)
   }
 
+  /** The exception message might be null for parse failures due to ambiguous nodes that can't be resolved successfully.
+    * We extract better log messages for this case here.
+    */
+  protected def extractParseException(exception: Throwable): String = Option(exception.getMessage) match {
+    case Some(message) => message
+    case None =>
+      exception.getStackTrace
+        .collectFirst {
+          case stackTraceElement: StackTraceElement if stackTraceElement.getClassName.endsWith("ASTAmbiguousNode") =>
+            "Could not resolve ambiguous node!"
+        }
+        .getOrElse(exception.getStackTrace.mkString(System.lineSeparator()))
+  }
+
 }
