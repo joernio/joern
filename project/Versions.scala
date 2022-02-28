aac6eb6ff2f840493cd15f2b11a74b08cb9666fd
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-02-28 11:13:35 +0100

    Update cats-effect to 3.3.6 (#1059)
    * Update cats-effect to 3.3.6
    
    * Revert commit(s) 00db2a14
    
    * Update cats-effect to 3.3.6

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@ object Versions {
   val js2cpg    = parseVersion("js2cpgVersion")
   val antlr     = "4.9.3"
   val scalatest = "3.2.11"
-  val cats      = "3.3.5"
+  val cats      = "3.3.6"
   val log4j     = "2.17.2"
 
   private def parseVersion(key: String): String = {
