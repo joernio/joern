115c0ee22acba92ee079cec26e418e8622e99109
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2022-03-09 13:01:07 +0100

    Fixed AST generation for for-loop with multiple assignments (#1095)
    Fixes: https://github.com/joernio/joern/issues/1093

--- joern-cli/frontends/c2cpg/src/test/scala/io/joern/c2cpg/standard/ControlStructureTests.scala
+++ joern-cli/frontends/c2cpg/src/test/scala/io/joern/c2cpg/standard/ControlStructureTests.scala
@@ -2,9 +2,10 @@ package io.joern.c2cpg.standard
 
 import io.joern.c2cpg.parser.FileDefaults
 import io.joern.c2cpg.testfixtures.CCodeToCpgSuite
+import io.shiftleft.codepropertygraph.generated.ControlStructureTypes
 import io.shiftleft.semanticcpg.language._
 
-class ControlStructureTests extends CCodeToCpgSuite(FileDefaults.CPP_EXT) {
+class ControlStructureTest1 extends CCodeToCpgSuite(FileDefaults.CPP_EXT) {
 
   override val code: String =
     """
@@ -74,6 +75,45 @@ class ControlStructureTests extends CCodeToCpgSuite(FileDefaults.CPP_EXT) {
 
   "should identify `continue`" in {
     cpg.method("foo").continue.code.l shouldBe List("continue;")
+  }
+
+}
+
+class ControlStructureTest2 extends CCodeToCpgSuite(FileDefaults.CPP_EXT) {
+
+  override val code: String =
+    """
+      |void foo() {
+      |  for (int x=1,y=1; x; --x) { bar(); };
+      |}
+      |""".stripMargin
+
+  "should be correct for for-loop with multiple assignments" in {
+    inside(cpg.controlStructure.l) { case List(forLoop) =>
+      forLoop.controlStructureType shouldBe ControlStructureTypes.FOR
+      inside(forLoop.astChildren.order(1).l) { case List(assignmentBlock) =>
+        inside(assignmentBlock.astChildren.l) { case List(localX, localY, assignmentX, assignmentY) =>
+          localX.code shouldBe "int x"
+          localX.order shouldBe 1
+          localY.code shouldBe "int y"
+          localY.order shouldBe 2
+          assignmentX.code shouldBe "x=1"
+          assignmentX.order shouldBe 3
+          assignmentY.code shouldBe "y=1"
+          assignmentY.order shouldBe 4
+        }
+      }
+      inside(forLoop.condition.l) { case List(x) =>
+        x.code shouldBe "x"
+        x.order shouldBe 2
+      }
+      inside(forLoop.astChildren.order(3).l) { case List(updateX) =>
+        updateX.code shouldBe "--x"
+      }
+      inside(forLoop.astChildren.order(4).l) { case List(loopBody) =>
+        loopBody.astChildren.isCall.head.code shouldBe "bar()"
+      }
+    }
   }
 
 }
