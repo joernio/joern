246690f34999acddafdcf3165473865ea38beede
Claudiu-Vlad Ursache <claudiu+hw@shiftleft.io>
2022-03-09 17:38:26 +0100

    Add validation test suite for basic Android app (#1099)

---
+++ joern-cli/frontends/kotlin2cpg/src/test/resources/code/barebone_android_app/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webview)
+        webView.loadUrl("http://phrack.org")
+    }
+}
