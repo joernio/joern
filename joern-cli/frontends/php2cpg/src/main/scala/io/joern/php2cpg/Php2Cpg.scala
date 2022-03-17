9d08f9dee3c27575c5451f9d15778219b8b1ab28
Fabian Yamaguchi <fabs@joern.io>
2022-03-17 15:35:50 +0100

    Add phpsrc2cpg that just creates a meta data node

---
+++ joern-cli/frontends/php2cpg/src/main/scala/io/joern/php2cpg/Php2Cpg.scala
@@ -0,0 +1,18 @@
+package io.joern.php2cpg
+
+import io.joern.x2cpg.X2Cpg.withNewEmptyCpg
+import io.joern.x2cpg.X2CpgFrontend
+import io.joern.x2cpg.passes.frontend.MetaDataPass
+import io.shiftleft.codepropertygraph.Cpg
+import io.shiftleft.codepropertygraph.generated.Languages
+
+import scala.util.Try
+
+class Php2Cpg extends X2CpgFrontend[Config] {
+  override def createCpg(config: Config): Try[Cpg] = {
+    withNewEmptyCpg(config.outputPath, config: Config) { (cpg, config) =>
+      new MetaDataPass(cpg, Languages.PHP).createAndApply()
+
+    }
+  }
+}
