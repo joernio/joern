59cfd76842c80fb778a49e8a6a6b351272e6013c
Fabian Yamaguchi <fabs@joern.io>
2022-03-17 17:48:51 +0100

    Use `X2CpgMain` in `ghidra2cpg`

--- joern-cli/frontends/ghidra2cpg/src/main/scala/io/joern/ghidra2cpg/Main.scala
+++ joern-cli/frontends/ghidra2cpg/src/main/scala/io/joern/ghidra2cpg/Main.scala
@@ -1,6 +1,7 @@
 package io.joern.ghidra2cpg
 
-import io.joern.x2cpg.{X2Cpg, X2CpgConfig}
+import io.joern.ghidra2cpg.Frontend._
+import io.joern.x2cpg.{X2CpgConfig, X2CpgMain}
 import scopt.OParser
 
 /** Command line configuration parameters
@@ -13,18 +14,20 @@ final case class Config(inputPaths: Set[String] = Set.empty, outputPath: String 
   override def withOutputPath(x: String): Config = copy(outputPath = x)
 }
 
-object Main extends App {
-  private val frontendSpecificOptions = {
+private object Frontend {
+
+  implicit val defaultConfig: Config = Config()
+
+  val cmdLineParser: OParser[Unit, Config] = {
     val builder = OParser.builder[Config]
     import builder.programName
     OParser.sequence(programName("ghidra2cpg"))
   }
 
-  X2Cpg.parseCommandLine(args, frontendSpecificOptions, Config()) match {
-    case Some(config) =>
-      new Ghidra2Cpg().run(config)
-    case None =>
-      System.exit(1)
+  def run(config: Config, ghidra2Cpg: Ghidra2Cpg): Unit = {
+    ghidra2Cpg.run(config)
   }
 
 }
+
+object Main extends X2CpgMain(cmdLineParser, run, new Ghidra2Cpg()) {}
