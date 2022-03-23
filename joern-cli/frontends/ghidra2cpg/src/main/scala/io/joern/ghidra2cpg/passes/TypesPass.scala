dc882cddc8f6a80c3f063cf5dcade735b23f9352
Michael Pollmeier <michael@michaelpollmeier.com>
2022-03-23 14:12:28 +0100

    dedeprecate: use new passes api

--- joern-cli/frontends/ghidra2cpg/src/main/scala/io/joern/ghidra2cpg/passes/TypesPass.scala
+++ joern-cli/frontends/ghidra2cpg/src/main/scala/io/joern/ghidra2cpg/passes/TypesPass.scala
@@ -3,18 +3,16 @@ package io.joern.ghidra2cpg.passes
 import io.joern.ghidra2cpg._
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.codepropertygraph.generated.nodes
-import io.shiftleft.passes.{CpgPass, DiffGraph}
+import io.shiftleft.passes.SimpleCpgPass
 
-class TypesPass(cpg: Cpg) extends CpgPass(cpg) {
+class TypesPass(cpg: Cpg) extends SimpleCpgPass(cpg) {
 
-  override def run(): Iterator[DiffGraph] = {
-    implicit val diffGraph: DiffGraph.Builder = DiffGraph.newBuilder
+  override def run(diffGraph: DiffGraphBuilder): Unit = {
     Types.types
       .foreach { typ =>
         val typeNode =
           nodes.NewType().name(typ).fullName(typ).typeDeclFullName(typ)
         diffGraph.addNode(typeNode)
       }
-    Iterator(diffGraph.build())
   }
 }
