0c94095081472904d7660878a500ebe1a4cd4053
Fabian Yamaguchi <fabs@shiftleft.io>
2022-03-25 18:48:17 +0100

    sync

--- joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/X2Cpg.scala
+++ joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/X2Cpg.scala
@@ -143,14 +143,19 @@ abstract class AstCreatorBase(filename: String) {
       .order(1)
   }
 
-  def methodReturnNode(line: Option[Integer], column: Option[Integer], order: Int, tpe: String): NewMethodReturn =
-    NewMethodReturn()
-      .order(order)
-      .typeFullName(tpe)
-      .code(tpe)
-      .evaluationStrategy(EvaluationStrategies.BY_VALUE)
-      .lineNumber(line)
-      .columnNumber(column)
+  /** Creates an AST that represents an entire method, including its content.
+    */
+  def methodAst(
+                 method: NewMethod,
+                 parameters: Seq[NewMethodParameterIn],
+                 body: Ast,
+                 methodReturn: NewMethodReturn
+               ): Ast =
+    Ast(method)
+      .withChildren(parameters.map(Ast(_)))
+      .withChild(body)
+      .withChild(Ast(methodReturn))
+
 
   /** Creates an AST that represents a method stub, containing information about the method, its parameters, and the
     * return type.
@@ -160,18 +165,17 @@ abstract class AstCreatorBase(filename: String) {
       .withChildren(parameters.map(Ast(_)))
       .withChild(Ast(methodReturn))
 
-  /** Creates an AST that represents an entire method, including its content.
-    */
-  def methodAst(
-    method: NewMethod,
-    parameters: Seq[NewMethodParameterIn],
-    body: Ast,
-    methodReturn: NewMethodReturn
-  ): Ast =
-    Ast(method)
-      .withChildren(parameters.map(Ast(_)))
-      .withChild(body)
-      .withChild(Ast(methodReturn))
+  /**
+    * Create a method return node
+    * */
+  def methodReturnNode(line: Option[Integer], column: Option[Integer], order: Int, tpe: String): NewMethodReturn =
+    NewMethodReturn()
+      .order(order)
+      .typeFullName(tpe)
+      .code(tpe)
+      .evaluationStrategy(EvaluationStrategies.BY_VALUE)
+      .lineNumber(line)
+      .columnNumber(column)
 
   /** Absolute path for the given file name
     */
