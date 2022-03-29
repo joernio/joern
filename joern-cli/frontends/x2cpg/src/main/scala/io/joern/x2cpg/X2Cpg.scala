d2dfc180a0c79ba2cc54e8393f83673ff39bf5aa
Fabian Yamaguchi <fabs@shiftleft.io>
2022-03-29 13:03:03 +0200

    Move `AstCreatorBase` to separate file

--- joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/X2Cpg.scala
+++ joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/X2Cpg.scala
@@ -3,19 +3,9 @@ package io.joern.x2cpg
 import better.files.File
 import io.joern.x2cpg.X2Cpg.withErrorsToConsole
 import io.joern.x2cpg.layers.{Base, CallGraph, ControlFlow, TypeRelations}
-import io.joern.x2cpg.passes.frontend.MetaDataPass
 import io.shiftleft.codepropertygraph.Cpg
-import io.shiftleft.codepropertygraph.generated.EvaluationStrategies
-import io.shiftleft.codepropertygraph.generated.nodes.{
-  NewMethod,
-  NewMethodParameterIn,
-  NewMethodReturn,
-  NewNamespaceBlock
-}
-import io.shiftleft.semanticcpg.language.types.structure.NamespaceTraversal
 import io.shiftleft.semanticcpg.layers.LayerCreatorContext
 import org.slf4j.LoggerFactory
-import overflowdb.BatchedUpdate.DiffGraphBuilder
 import overflowdb.Config
 import scopt.OParser
 
@@ -124,62 +114,6 @@ trait X2CpgFrontend[T <: X2CpgConfig[_]] {
     */
   def createCpg(inputNames: List[String])(implicit defaultConfig: T): Try[Cpg] =
     createCpg(inputNames, None)(defaultConfig)
-}
-
-abstract class AstCreatorBase(filename: String) {
-  val diffGraph: DiffGraphBuilder = new DiffGraphBuilder
-  def createAst(): DiffGraphBuilder
-
-  /** Create a global namespace block for the given `filename`
-    */
-  def globalNamespaceBlock(): NewNamespaceBlock = {
-    val absPath  = absolutePath(filename)
-    val name     = NamespaceTraversal.globalNamespaceName
-    val fullName = MetaDataPass.getGlobalNamespaceBlockFullName(Some(absPath))
-    NewNamespaceBlock()
-      .name(name)
-      .fullName(fullName)
-      .filename(absPath)
-      .order(1)
-  }
-
-  /** Creates an AST that represents an entire method, including its content.
-    */
-  def methodAst(
-    method: NewMethod,
-    parameters: Seq[NewMethodParameterIn],
-    body: Ast,
-    methodReturn: NewMethodReturn
-  ): Ast =
-    Ast(method)
-      .withChildren(parameters.map(Ast(_)))
-      .withChild(body)
-      .withChild(Ast(methodReturn))
-
-  /** Creates an AST that represents a method stub, containing information about the method, its parameters, and the
-    * return type.
-    */
-  def methodStubAst(method: NewMethod, parameters: Seq[NewMethodParameterIn], methodReturn: NewMethodReturn): Ast =
-    Ast(method)
-      .withChildren(parameters.map(Ast(_)))
-      .withChild(Ast(methodReturn))
-
-  /** Create a method return node
-    */
-  def methodReturnNode(line: Option[Integer], column: Option[Integer], order: Int, tpe: String): NewMethodReturn =
-    NewMethodReturn()
-      .order(order)
-      .typeFullName(tpe)
-      .code(tpe)
-      .evaluationStrategy(EvaluationStrategies.BY_VALUE)
-      .lineNumber(line)
-      .columnNumber(column)
-
-  /** Absolute path for the given file name
-    */
-  def absolutePath(filename: String): String =
-    better.files.File(filename).path.toAbsolutePath.normalize().toString
-
 }
 
 object X2Cpg {
