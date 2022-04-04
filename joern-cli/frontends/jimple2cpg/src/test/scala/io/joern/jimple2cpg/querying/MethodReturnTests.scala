c468db5b3faf24f37f5a1d342a117dcb20c14437
David Baker Effendi <dbe@sun.ac.za>
2022-04-04 17:32:41 +0200

    jimple2cpg: Return Node `argument` and `order` Fix + Defined `code` Property (#1223)
    * Added code for Return nodes, fixed lack of arg edges and issue with orders
    
    * Fixed local vs statements order disconnect
    
    * Ignoring unused units that are breaking ordering

--- joern-cli/frontends/jimple2cpg/src/test/scala/io/joern/jimple2cpg/querying/MethodReturnTests.scala
+++ joern-cli/frontends/jimple2cpg/src/test/scala/io/joern/jimple2cpg/querying/MethodReturnTests.scala
@@ -22,6 +22,15 @@ class MethodReturnTests extends JimpleCodeToCpgFixture {
     x.order shouldBe 2
   }
 
+  "should have a RETURN node with correct fields" in {
+    val List(x) = cpg.method.name("foo").ast.isReturn.l
+    x.code shouldBe "return 1;"
+    x.order shouldBe 2
+    x.argumentIndex shouldBe 2
+    x.astChildren.size shouldBe 1
+    x.argumentOut.size shouldBe 1
+  }
+
   "should allow traversing to method" in {
     cpg.methodReturn.code("int").method.name.l shouldBe List("foo")
   }
