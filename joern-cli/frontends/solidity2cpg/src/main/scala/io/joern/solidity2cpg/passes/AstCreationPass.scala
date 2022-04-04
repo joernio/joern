a9e35c633b26e26f0d43da1c91bcdb1669e6126e
Matthew Tam <matthewethan.tam@gmail.com>
2022-04-04 14:34:51 +0200

    saving solidity changes

--- joern-cli/frontends/solidity2cpg/src/main/scala/io/joern/solidity2cpg/passes/AstCreationPass.scala
+++ joern-cli/frontends/solidity2cpg/src/main/scala/io/joern/solidity2cpg/passes/AstCreationPass.scala
@@ -1,27 +1,25 @@
 package io.joern.solidity2cpg.passes
 
 import io.joern.solidity2cpg.domain.SuryaObject.SourceUnit
+import io.joern.x2cpg.datastructures.Global
 import io.shiftleft.codepropertygraph.Cpg
-import io.shiftleft.passes.{ConcurrentWriterCpgPass, IntervalKeyPool}
+import io.shiftleft.passes.ConcurrentWriterCpgPass
 import org.slf4j.LoggerFactory
 import spray.json._
 
-import java.util.concurrent.ConcurrentSkipListSet
 import scala.util.{Failure, Success, Try, Using}
 
-case class Global(usedTypes: ConcurrentSkipListSet[String] = new ConcurrentSkipListSet[String]())
+class AstCreationPass(codeDir: String, filenames: List[String], cpg: Cpg) extends ConcurrentWriterCpgPass[String](cpg) {
 
-class AstCreationPass(codeDir: String, filenames: List[String], cpg: Cpg, keyPool: IntervalKeyPool)
-    extends ConcurrentWriterCpgPass[String](cpg, keyPool = Some(keyPool)) {
-
-  val global: Global = Global()
-
+  val global: Global = new Global()
   private val logger = LoggerFactory.getLogger(classOf[AstCreationPass])
 
   override def generateParts(): Array[_ <: AnyRef] = filenames.toArray
 
   /** Creates an AST from the given JSON file.
-    * @param filename
+    * @param builder
+    *   the builder holding all the changes from this pass.
+    * @param part
     *   the path to the Surya generated AST JSON file.
     * @return
     *   a list of changes generated from this pass.
@@ -32,8 +30,10 @@ class AstCreationPass(codeDir: String, filenames: List[String], cpg: Cpg, keyPoo
     Using.resource(scala.io.Source.fromFile(part)) { source =>
       val lines = source.getLines() mkString "\n"
       Try(lines.parseJson.convertTo[SourceUnit]) match {
-        case Failure(e)          => logger.error(s"Unable to convert JSON to SourceUnit $part due to an exception", e)
-        case Success(sourceUnit) => new AstCreator(part, builder, global).createAst(sourceUnit)
+        case Failure(e) => logger.error(s"Unable to convert JSON to SourceUnit $part due to an exception", e)
+        case Success(sourceUnit) =>
+          val localDiff = new AstCreator(part, sourceUnit, global).createAst()
+          builder.absorb(localDiff)
       }
     }
   }
