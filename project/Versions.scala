5fe1fea7062afc68dbbcb88903fe3947be683ca8
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-04-12 11:45:32 +0200

    Update antlr4, antlr4-runtime to 4.10 (#1252)

--- project/Versions.scala
+++ project/Versions.scala
@@ -2,7 +2,7 @@
 object Versions {
   val cpg           = parseVersion("cpgVersion")
   val js2cpg        = parseVersion("js2cpgVersion")
-  val antlr         = "4.9.3"
+  val antlr         = "4.10"
   val scalatest     = "3.2.11"
   val cats          = "3.3.11"
   val log4j         = "2.17.2"
