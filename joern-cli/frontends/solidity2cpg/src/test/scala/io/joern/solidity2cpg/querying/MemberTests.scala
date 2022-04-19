8cb3f33fe9af5a4fde8c9a6b3594b15f307a8fe3
David Baker Effendi <dbe@sun.ac.za>
2022-04-19 11:26:12 +0200

    Added solidity member test

---
+++ joern-cli/frontends/solidity2cpg/src/test/scala/io/joern/solidity2cpg/querying/MemberTests.scala
@@ -0,0 +1,54 @@
+package io.joern.solidity2cpg.querying
+
+import io.joern.solidity2cpg.testfixtures.SolidityCodeToCpgFixture
+import io.shiftleft.codepropertygraph.generated.ModifierTypes
+import io.shiftleft.codepropertygraph.generated.nodes.Member
+import io.shiftleft.semanticcpg.language._
+
+class MemberTests extends SolidityCodeToCpgFixture {
+
+  override val code: String =
+    """// SPDX-License-Identifier: GPL-3.0
+      |pragma solidity ^0.8.4;
+      |
+      |contract Coin {
+      |
+      |   address public minter;
+      |
+      |   mapping (address => uint) public balances;
+      |
+      |   uint private storedData;
+      |
+      |}
+      |""".stripMargin
+
+  private def hasMod(member: Member, modifierType: String): Boolean = {
+    member.modifier.exists(_.modifierType == modifierType)
+  }
+
+  "should contain MEMBER node with correct properties" in {
+    val List(x, y, z) = cpg.member.l
+    x.name shouldBe "minter"
+    x.code shouldBe "address public minter"
+    x.typeFullName shouldBe "address"
+    x.order shouldBe 1
+    hasMod(x, ModifierTypes.PUBLIC)
+
+    y.name shouldBe "balances"
+    y.code shouldBe "mapping (address => uint) public balances"
+    y.typeFullName shouldBe "mapping" // don't worry about dynamic key-val types for now
+    y.order shouldBe 2
+    hasMod(y, ModifierTypes.PUBLIC)
+
+    z.name shouldBe "x"
+    z.code shouldBe "uint private storedData"
+    z.typeFullName shouldBe "uint"
+    z.order shouldBe 3
+    hasMod(y, ModifierTypes.PRIVATE)
+  }
+
+  "should allow traversing from MEMBER to TYPE_DECL" in {
+    val List(x) = cpg.member.typeDecl.l
+    x.name shouldBe "Coin"
+  }
+}
