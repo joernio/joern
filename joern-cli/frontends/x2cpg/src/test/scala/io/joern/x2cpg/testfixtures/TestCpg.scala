b6123cc61f39e2b5439e3133cf7167030fa4f408
Markus Lottmann <markus@shiftleft.io>
2022-04-25 19:33:36 +0200

    New test fixture. (#1303)
    * New test fixture.
    
    This fixture allows to specify multiple input files while not placing
    additional burden on the user.
    
    As example i migrated one java src test.
    
    But it is also possible to use the same cpg for multiple different
    tests like:
    
    ```
    class SomeTests extends JavaSrcCode2CpgFixture {
    
      "foo" should {
        val cpg = code("<someCode>")
    
        "test1" in {
    
        }
    
        "test2" in {
        }
      }
    }
    ```
    
    * Do not fail on warning.
    
    Deprecation warnings together with -Xfatal-warnings is too much of a
    pain.
    
    * Fix for scala 3.

---
+++ joern-cli/frontends/x2cpg/src/test/scala/io/joern/x2cpg/testfixtures/TestCpg.scala
@@ -0,0 +1,60 @@
+package io.joern.x2cpg.testfixtures
+
+import io.joern.x2cpg.X2Cpg
+import io.shiftleft.codepropertygraph.Cpg
+import overflowdb.Graph
+
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Path, Paths}
+import scala.collection.mutable
+
+// Lazily populated test CPG which is created upon first access to the underlying graph.
+class TestCpg(frontend: LanguageFrontend, registerCleanup: (Path, TestCpg) => Unit) extends Cpg() {
+  private var _graph            = Option.empty[Graph]
+  private val codeFileNamePairs = mutable.ArrayBuffer.empty[(String, Path)]
+  private var fileNameCounter   = 0
+
+  def moreCode(code: String): TestCpg = {
+    val result = moreCode(code, s"Test$fileNameCounter.java")
+    fileNameCounter += 1
+    result
+  }
+
+  def moreCode(code: String, fileName: String): TestCpg = {
+    checkGraphEmpty()
+    codeFileNamePairs.append((code, Paths.get(fileName)))
+    this
+  }
+
+  private def checkGraphEmpty(): Unit = {
+    if (_graph.isDefined) {
+      throw new RuntimeException("Modifying test data is not allowed after accessing graph.")
+    }
+  }
+
+  private def codeToFileSystem(): Path = {
+    val tmpDir = Files.createTempDirectory("x2cpgTestTmpDir")
+    codeFileNamePairs.foreach { case (code, fileName) =>
+      if (fileName.getParent != null) {
+        Files.createDirectories(tmpDir.resolve(fileName.getParent))
+      }
+      val codeAsBytes = code.getBytes(StandardCharsets.UTF_8)
+      Files.write(tmpDir.resolve(Paths.get(fileName.toString)), codeAsBytes)
+    }
+    tmpDir
+  }
+
+  override def graph: Graph = {
+    if (_graph.isEmpty) {
+      val codeDir = codeToFileSystem()
+      registerCleanup(codeDir, this)
+      _graph = Some(frontend.execute(codeDir.toFile).graph)
+      X2Cpg.applyDefaultOverlays(this)
+    }
+    _graph.get
+  }
+
+  override def close(): Unit = {
+    _graph.foreach(_.close())
+  }
+}
