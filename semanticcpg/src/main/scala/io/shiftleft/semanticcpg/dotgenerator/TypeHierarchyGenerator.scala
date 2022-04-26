439eaa0ea13dc397551f570dd1d47e5e0f99ebb0
David Baker Effendi <dbe@sun.ac.za>
2022-04-26 21:03:41 +0200

    Added dot file generator for type hierarchy

---
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/dotgenerator/TypeHierarchyGenerator.scala
@@ -0,0 +1,48 @@
+package io.shiftleft.semanticcpg.dotgenerator
+
+import io.shiftleft.codepropertygraph.Cpg
+import io.shiftleft.codepropertygraph.generated.nodes.{StoredNode, Type, TypeDecl}
+import io.shiftleft.semanticcpg.dotgenerator.DotSerializer.{Edge, Graph}
+import io.shiftleft.semanticcpg.language._
+
+import scala.collection.mutable
+
+class TypeHierarchyGenerator {
+
+  def generate(cpg: Cpg): Graph = {
+    val subgraph         = mutable.HashMap.empty[String, Seq[StoredNode]]
+    val vertices         = cpg.all.collect { case m: TypeDecl => m }.l
+    val typeToIsExternal = cpg.all.collect { case m: TypeDecl => m }.map { t => t.fullName -> t.isExternal }.toMap
+    val edges = for {
+      srcType <- vertices
+      _ = storeInSubgraph(srcType._typeViaRefIn.head, subgraph, typeToIsExternal)
+      tgtType <- srcType.inheritsFromOut
+    } yield {
+      storeInSubgraph(tgtType, subgraph, typeToIsExternal)
+      Edge(tgtType, srcType._typeViaRefIn.head)
+    }
+    Graph(vertices.flatMap(_._typeViaRefIn.l), edges.distinct, subgraph.toMap)
+  }
+
+  def storeInSubgraph(
+    typ: Type,
+    subgraph: mutable.Map[String, Seq[StoredNode]],
+    typeToIsExternal: Map[String, Boolean]
+  ): Unit = {
+    if (!typeToIsExternal(typ.fullName)) {
+      /*
+        We parse the namespace information instead of looking at the namespace node
+        as types such as inner classes may not be attached to a namespace block
+       */
+      val namespace =
+        if (typ.fullName.contains("."))
+          typ.fullName.stripSuffix(s".${typ.name}")
+        else
+          typ.fullName.stripSuffix(s"${typ.name}")
+      subgraph.put(namespace, subgraph.getOrElse(namespace, Seq()) ++ Seq(typ))
+    } else {
+      subgraph.put("<global>", subgraph.getOrElse("<global>", Seq()) ++ Seq(typ))
+    }
+  }
+
+}
