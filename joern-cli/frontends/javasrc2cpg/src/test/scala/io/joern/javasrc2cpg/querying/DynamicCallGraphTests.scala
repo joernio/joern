0a4e88b483e63c6f259149e33ab6c7f91aba2ced
David Baker Effendi <dbe@sun.ac.za>
2022-05-03 22:35:08 +0200

    Found instances where the tests fail

--- joern-cli/frontends/javasrc2cpg/src/test/scala/io/joern/javasrc2cpg/querying/DynamicCallGraphTests.scala
+++ joern-cli/frontends/javasrc2cpg/src/test/scala/io/joern/javasrc2cpg/querying/DynamicCallGraphTests.scala
@@ -17,10 +17,18 @@ class Foo {
 
 		A b2 = b1;
 		A c2 = c1;
+        A cOrD;
+
+        if (System.currentTimeMillis() > 100) {
+          cOrD = new C();
+        } else {
+          cOrD = new D();
+        }
 
 		// what will get printed?
 		b2.print(c2);
         c2.print(b2);
+        cOrD.print(b1);
 	}
 
 	public static class A extends Object {
@@ -61,6 +69,7 @@ class Foo {
   "should account for print calls from all subclasses due to using CHA with points-to information" in {
     cpg.call.codeExact("b2.print(c2)").callee.definingTypeDecl.fullName.toSetMutable shouldBe Set("Foo$B")
     cpg.call.codeExact("c2.print(b2)").callee.definingTypeDecl.fullName.toSetMutable shouldBe Set("Foo$C")
+    cpg.call.codeExact("cOrD.print(b1)").callee.definingTypeDecl.fullName.toSetMutable shouldBe Set("Foo$C", "Foo$D")
   }
 
 }
