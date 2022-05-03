94de2654af226f2383020ae924efd29791c7d6f9
Michael Pollmeier <michael@michaelpollmeier.com>
2022-05-03 08:21:49 +0200

    more details and next steps in php installer error msg
    https://github.com/joernio/joern/pull/1331 and https://github.com/joernio/joern/pull/1329#issuecomment-1114659847
    will happen again

--- joern-cli/frontends/php2cpg/installdeps.sh
+++ joern-cli/frontends/php2cpg/installdeps.sh
@@ -1,6 +1,6 @@
 # Install `composer`
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt - most like it was just updated on their side, in which case you can just update the expected hash.'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
