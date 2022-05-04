29be31c0b67b067a17aac8b21137c75370703dd5
David Baker Effendi <dbe@sun.ac.za>
2022-05-04 11:55:56 +0200

    Fixed alignment of code in DynamicCallGraphTests

--- joern-cli/frontends/javasrc2cpg/src/test/scala/io/joern/javasrc2cpg/querying/DynamicCallGraphTests.scala
+++ joern-cli/frontends/javasrc2cpg/src/test/scala/io/joern/javasrc2cpg/querying/DynamicCallGraphTests.scala
@@ -9,58 +9,58 @@ class DynamicCallGraphTests extends JavaSrcCodeToCpgFixture {
 
   override val code =
     """
-class Foo {
-
-	public static void main(String[] args){
-		A b1 = new B();
-		A c1 = new C();
-
-		A b2 = b1;
-		A c2 = c1;
-        A cOrD;
-
-        if (System.currentTimeMillis() > 100) {
-          cOrD = new C();
-        } else {
-          cOrD = new D();
-        }
-
-		// what will get printed?
-		b2.print(c2);
-        c2.print(b2);
-        cOrD.print(b1);
-	}
-
-	public static class A extends Object {
-		public void print(A object) {
-			System.out.println("An instance of " + object.getClass().getSimpleName()
-					+ " was passed to A's print(A object)");
-		}
-	}
-
-	public static class B extends A {
-		public void print(A object) {
-			System.out.println("An instance of " + object.getClass().getSimpleName()
-					+ " was passed to B's print(A object)");
-		}
-	}
-
-	public static class C extends B {
-		public void print(A object) {
-			System.out.println("An instance of " + object.getClass().getSimpleName()
-					+ " was passed to C's print(A object)");
-		}
-	}
-
-	public static class D extends A {
-		public void print(A object) {
-			System.out.println("An instance of " + object.getClass().getSimpleName()
-					+ " was passed to D's print(A object)");
-		}
-	}
-
-}
-    """
+      |class Foo {
+      |
+      |	public static void main(String[] args){
+      |		A b1 = new B();
+      |		A c1 = new C();
+      |
+      |		A b2 = b1;
+      |		A c2 = c1;
+      |     A cOrD;
+      |
+      |     if (System.currentTimeMillis() > 100) {
+      |       cOrD = new C();
+      |     } else {
+      |       cOrD = new D();
+      |     }
+      |
+      |		// what will get printed?
+      |		b2.print(c2);
+      |     c2.print(b2);
+      |     cOrD.print(b1);
+      |	}
+      |
+      |	public static class A extends Object {
+      |		public void print(A object) {
+      |			System.out.println("An instance of " + object.getClass().getSimpleName()
+      |					+ " was passed to A's print(A object)");
+      |		}
+      |	}
+      |
+      |	public static class B extends A {
+      |		public void print(A object) {
+      |			System.out.println("An instance of " + object.getClass().getSimpleName()
+      |					+ " was passed to B's print(A object)");
+      |		}
+      |	}
+      |
+      |	public static class C extends B {
+      |		public void print(A object) {
+      |			System.out.println("An instance of " + object.getClass().getSimpleName()
+      |					+ " was passed to C's print(A object)");
+      |		}
+      |	}
+      |
+      |	public static class D extends A {
+      |		public void print(A object) {
+      |			System.out.println("An instance of " + object.getClass().getSimpleName()
+      |					+ " was passed to D's print(A object)");
+      |		}
+      |	}
+      |
+      |}
+      |""".stripMargin
 
   "should find that add is called by main" in {
     cpg.method.name("print").caller.name.toSetMutable shouldBe Set("main")
