62152b2b0cece5d04cc61b748604fa52414335d9
Michael Pollmeier <michael@michaelpollmeier.com>
2022-05-09 17:01:06 +0200

    importer needs conversiontool for storage

--- formats/src/main/scala/io/joern/formats/ImporterMain.scala
+++ formats/src/main/scala/io/joern/formats/ImporterMain.scala
@@ -1,9 +1,14 @@
 package io.joern.formats
 
 import io.shiftleft.codepropertygraph.generated.{edges, nodes}
+import io.shiftleft.codepropertygraph.generated.Cpg.convertPropertyForPersistence
 
 object ImporterMain extends App {
 
-  val importerMain = overflowdb.formats.ImporterMain(nodes.Factories.all, edges.Factories.all)
+  val importerMain = overflowdb.formats.ImporterMain(
+    nodes.Factories.all,
+    edges.Factories.all,
+    convertPropertyForPersistence(_)
+  )
   importerMain(args)
 }
