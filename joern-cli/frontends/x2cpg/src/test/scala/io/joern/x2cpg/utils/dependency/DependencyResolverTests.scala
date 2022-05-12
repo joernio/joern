f9df9f6f5cf377666b074ceae667d90b377c6422
Claudiu-Vlad Ursache <claudiu+hw@shiftleft.io>
2022-05-12 14:38:03 +0200

    Test Gradle deps fetching for Android

--- joern-cli/frontends/x2cpg/src/test/scala/io/joern/x2cpg/utils/dependency/DependencyResolverTests.scala
+++ joern-cli/frontends/x2cpg/src/test/scala/io/joern/x2cpg/utils/dependency/DependencyResolverTests.scala
@@ -1,10 +1,11 @@
 package io.joern.x2cpg.utils.dependency
 
 import io.joern.x2cpg.utils.ExternalCommand
+import io.shiftleft.utils.ProjectRoot
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-import java.nio.file.{Files, Path}
+import java.nio.file.{Files, Path, Paths}
 import java.util.Comparator
 
 class DependencyResolverTests extends AnyWordSpec with Matchers {
@@ -84,6 +85,13 @@ class DependencyResolverTests extends AnyWordSpec with Matchers {
       fixture.test { dependenciesFiles =>
         dependenciesFiles.find(_.endsWith("log4j-1.2.17.jar")) should not be empty
       }
+    }
+
+    "test gradle dependency resolution for simple Android app" in {
+      val androidAppDir = ProjectRoot.relativise("joern-cli/src/test/resources/testcode/SlimAndroid")
+      val dependencies  = DependencyResolver.getDependencies(Paths.get(androidAppDir))
+      dependencies.filter(_.endsWith(".jar")) should not be Set()
+      // TODO: add test for `.aar` as soon as it's decided what to do about them
     }
   }
 
