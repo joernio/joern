a3cf9f8da5b1d68af1473f32c78969de3af563f9
Michael Pollmeier <michael@michaelpollmeier.com>
2022-05-12 17:04:55 +0200

    relativise project root for test

--- joern-cli/frontends/x2cpg/src/test/scala/io/joern/x2cpg/utils/HashUtilsTest.scala
+++ joern-cli/frontends/x2cpg/src/test/scala/io/joern/x2cpg/utils/HashUtilsTest.scala
@@ -1,14 +1,14 @@
 package io.joern.x2cpg.utils
 
+import io.shiftleft.utils.ProjectRoot
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
 class HashUtilsTest extends AnyWordSpec with Matchers {
 
   "generate sha256 hash" in {
-    HashUtil.sha256(
-      "joern-cli/frontends/x2cpg/src/test/resources/testfile"
-    ) shouldBe "5881707e54b0112f901bc83a1ffbacac8fab74ea46a6f706a3efc5f7d4c1c625"
+    val testFile = ProjectRoot.relativise("joern-cli/frontends/x2cpg/src/test/resources/testfile")
+    HashUtil.sha256(testFile) shouldBe "5881707e54b0112f901bc83a1ffbacac8fab74ea46a6f706a3efc5f7d4c1c625"
   }
 
 }
