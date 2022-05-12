dfbfe75040a40fbbc304c406d1b656b0fb3b4506
Claudiu-Vlad Ursache <claudiu+hw@shiftleft.io>
2022-05-12 13:36:24 +0200

    Add `SlimAndroid` test code

---
+++ joern-cli/src/test/resources/testcode/SlimAndroid/app/proguard-rules.pro
@@ -0,0 +1,2 @@
+# Keeping this file to have a close enough representation
+# Of the release config of an Android app
---
+++ joern-cli/src/test/resources/testcode/SlimAndroid/app/src/main/java/com/example/slimandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.slimandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
