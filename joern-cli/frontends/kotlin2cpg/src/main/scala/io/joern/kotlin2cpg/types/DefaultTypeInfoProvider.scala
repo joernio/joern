fab3788144fb0245e107e0795c78dac6a4b9a43a
Claudiu-Vlad Ursache <claudiu+hw@shiftleft.io>
2022-05-16 09:24:01 +0200

    Tweak single log message

--- joern-cli/frontends/kotlin2cpg/src/main/scala/io/joern/kotlin2cpg/types/DefaultTypeInfoProvider.scala
+++ joern-cli/frontends/kotlin2cpg/src/main/scala/io/joern/kotlin2cpg/types/DefaultTypeInfoProvider.scala
@@ -821,7 +821,7 @@ class DefaultTypeInfoProvider(environment: KotlinCoreEnvironment) extends TypeIn
           Some(TypeRenderer.render(typedDesc.getDefaultType))
         case unhandled: Any =>
           logger.debug(
-            s"Unhandled class type info fetch in `typeFullName[NameReference]` for `${expr.getText}` with class `${unhandled.getClass}`."
+            s"Unhandled class type info fetch in for `${expr.getText}` with class `${unhandled.getClass}`."
           )
           None
         case _ => None
