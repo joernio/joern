b0b3d54f7854d81537334726e77c84311f96226a
Claudiu-Vlad Ursache <claudiu+hw@shiftleft.io>
2022-05-16 14:02:22 +0200

    Revert "Address PR review"
    This reverts commit eb139f29192d752f0e84139634921526ce90193b.

--- joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/querying/LambdaTests.scala
+++ joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/querying/LambdaTests.scala
@@ -61,13 +61,10 @@ class LambdaTests extends AnyFreeSpec with Matchers {
         |}
         |""".stripMargin)
 
-    "should contain a METHOD_REF node with its CODE prop set" in {
+    "should contain a METHOD_REF node with a non-null CODE prop set" in {
       cpg.methodRef.size shouldBe 1
       val List(mr) = cpg.methodRef.l
-      mr.code shouldBe
-        """|{
-          |       println(baz)
-          |    }""".stripMargin
+      mr.code should not be null
     }
 
     "should contain two CAPTURE edges for the METHOD_REF" in {
