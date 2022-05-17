d34d64e5bc24554401745410d0fde5d173d4bdd0
Michael Pollmeier <michael@michaelpollmeier.com>
2022-05-17 10:59:45 +0200

    finish wiring

--- joern-cli/src/main/scala/io/joern/joerncli/JoernExport.scala
+++ joern-cli/src/main/scala/io/joern/joerncli/JoernExport.scala
@@ -4,7 +4,6 @@ import better.files.Dsl._
 import better.files.File
 import io.joern.dataflowengineoss.layers.dataflows._
 import io.joern.dataflowengineoss.semanticsloader.Semantics
-import io.joern.joerncli.JoernExport.{Format, Representation}
 import io.joern.joerncli.console.JoernWorkspaceLoader
 import io.joern.x2cpg.layers._
 import io.shiftleft.semanticcpg.layers._
@@ -15,12 +14,10 @@ import scala.util.Using
 
 object JoernExport extends App {
 
-  case class Config(
-                     cpgFileName: String = "cpg.bin",
-                     outDir: String = "out",
-                     repr: Representation.Value = Representation.cpg14,
-                     format: Format.Value = Format.dot
-   )
+  case class Config(cpgFileName: String = "cpg.bin",
+                    outDir: String = "out",
+                    repr: Representation.Value = Representation.cpg14,
+                    format: Format.Value = Format.dot)
 
   /**
     * Choose from either a subset of the graph, or the entire graph (all).
@@ -53,44 +50,41 @@ object JoernExport extends App {
     }.parse(args, Config())
 
   parseConfig.foreach { config =>
-    if (File(config.outDir).exists) {
-      System.err.println(s"Output directory ${config.outDir} already exists. Bailing out.")
-    } else {
-      if (!File(config.cpgFileName).exists) {
-        System.err.println(s"CPG at ${config.cpgFileName} does not exist. Bailing out.")
-      } else {
-        Using.resource(CpgBasedTool.loadFromOdb(config.cpgFileName)) { cpg =>
-          CpgBasedTool.addDataFlowOverlayIfNonExistent(cpg)
-          val context = new LayerCreatorContext(cpg)
+    if (File(config.outDir).exists)
+      throw new AssertionError(s"Output directory ${config.outDir} already exists.")
+    if (File(config.cpgFileName).notExists)
+      throw new AssertionError(s"CPG at ${config.cpgFileName} does not exist.")
 
-          mkdir(File(config.outDir))
-          implicit val semantics: Semantics = JoernWorkspaceLoader.defaultSemantics
-          if (semantics.elements.isEmpty) {
-            System.err.println("Warning: semantics are empty.")
-          }
+    Using.resource(CpgBasedTool.loadFromOdb(config.cpgFileName)) { cpg =>
+      CpgBasedTool.addDataFlowOverlayIfNonExistent(cpg)
+      val context = new LayerCreatorContext(cpg)
 
-          config.repr match {
-            case Representation.ast =>
-              new DumpAst(AstDumpOptions(config.outDir)).create(context)
-            case Representation.cfg =>
-              new DumpCfg(CfgDumpOptions(config.outDir)).create(context)
-            case Representation.ddg =>
-              new DumpDdg(DdgDumpOptions(config.outDir)).create(context)
-            case Representation.cdg =>
-              new DumpCdg(CdgDumpOptions(config.outDir)).create(context)
-            case Representation.pdg =>
-              new DumpPdg(PdgDumpOptions(config.outDir)).create(context)
-            case Representation.cpg14 =>
-              new DumpCpg14(Cpg14DumpOptions(config.outDir)).create(context)
-            case Representation.neo4jcsv =>
-              val ExportResult(nodeCount, edgeCount, files, additionalInfo) =
-                Neo4jCsvExporter.runExport(cpg.graph, config.outDir)
-              println(s"export completed successfully: $nodeCount nodes, $edgeCount edges in ${files.size} files")
-              println(additionalInfo)
-            case repr =>
-              System.err.println(s"unknown representation: $repr. Baling out.")
-          }
-        }
+      mkdir(File(config.outDir))
+      implicit val semantics: Semantics = JoernWorkspaceLoader.defaultSemantics
+      if (semantics.elements.isEmpty) {
+        System.err.println("Warning: semantics are empty.")
+      }
+
+      (config.repr, config.format) match {
+        case (Representation.ast, Format.dot) =>
+          new DumpAst(AstDumpOptions(config.outDir)).create(context)
+        case (Representation.cfg, Format.dot) =>
+          new DumpCfg(CfgDumpOptions(config.outDir)).create(context)
+        case (Representation.ddg, Format.dot) =>
+          new DumpDdg(DdgDumpOptions(config.outDir)).create(context)
+        case (Representation.cdg, Format.dot) =>
+          new DumpCdg(CdgDumpOptions(config.outDir)).create(context)
+        case (Representation.pdg, Format.dot) =>
+          new DumpPdg(PdgDumpOptions(config.outDir)).create(context)
+        case (Representation.cpg14, Format.dot) =>
+          new DumpCpg14(Cpg14DumpOptions(config.outDir)).create(context)
+        case (Representation.all, Format.neo4jcsv) =>
+          val ExportResult(nodeCount, edgeCount, files, additionalInfo) =
+            Neo4jCsvExporter.runExport(cpg.graph, config.outDir)
+          println(s"export completed successfully: $nodeCount nodes, $edgeCount edges in ${files.size} files")
+          println(additionalInfo)
+        case (repr, format) =>
+          throw new NotImplementedError(s"combination of repr=$repr and $format not (yet) supported")
       }
     }
   }
