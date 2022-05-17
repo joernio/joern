7c3008acfde1820563885e7027dd94e1e2c6d4a1
Claudiu-Vlad Ursache <claudiu+hw@shiftleft.io>
2022-05-17 11:42:41 +0200

    Switch SlimAndroid test code to `androidx`
    _android.support_ is too old to be relevant at the moment

--- joern-cli/src/test/resources/testcode/SlimAndroid/app/src/main/java/com/example/slimandroid/MainActivity.kt
+++ joern-cli/src/test/resources/testcode/SlimAndroid/app/src/main/java/com/example/slimandroid/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.slimandroid
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
