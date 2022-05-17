6c443b117e14223b403ce9b1c7fb2469cf05e240
Michael Pollmeier <michael@michaelpollmeier.com>
2022-05-17 10:14:56 +0200

    refactor, help texts

--- joern-cli/src/main/scala/io/joern/joerncli/JoernExport.scala
+++ joern-cli/src/main/scala/io/joern/joerncli/JoernExport.scala
@@ -4,7 +4,7 @@ import better.files.Dsl._
 import better.files.File
 import io.joern.dataflowengineoss.layers.dataflows._
 import io.joern.dataflowengineoss.semanticsloader.Semantics
-import io.joern.joerncli.JoernExport.{Format, Representations}
+import io.joern.joerncli.JoernExport.{Format, Representation}
 import io.joern.joerncli.console.JoernWorkspaceLoader
 import io.joern.x2cpg.layers._
 import io.shiftleft.semanticcpg.layers._
@@ -16,32 +16,40 @@ import scala.util.Using
 object JoernExport extends App {
 
   case class Config(
-     cpgFileName: String = "cpg.bin",
-     outDir: String = "out",
-     repr: Representations.Value = Representations.cpg14,
+                     cpgFileName: String = "cpg.bin",
+                     outDir: String = "out",
+                     repr: Representation.Value = Representation.cpg14,
+                     format: Format.Value = Format.dot
    )
 
   /**
     * Choose from either a subset of the graph, or the entire graph (all).
     */
-  object Representations extends Enumeration {
+  object Representation extends Enumeration {
     val ast, cfg, ddg, cdg, pdg, cpg14, all = Value
+  }
+  object Format extends Enumeration {
+    val dot, neo4jcsv = Value
   }
 
   private def parseConfig: Option[Config] =
     new scopt.OptionParser[Config]("joern-export") {
-      head("Dump intermediate graph representations of code onto disk")
+      head("Dump intermediate graph representations (or entire graph) of code in a given export format")
       help("help")
       arg[String]("cpg")
-        .text("CPG file name ('cpg.bin' by default)")
+        .text("input CPG file name - defaults to `cpg.bin`")
         .optional()
         .action((x, c) => c.copy(cpgFileName = x))
       opt[String]("out")
-        .text("output directory")
+        .text("output directory - will be created and must not yet exist")
         .action((x, c) => c.copy(outDir = x))
       opt[String]("repr")
-        .text(s"representation to extract: [${Representations.values.toSeq.sorted.mkString("|")}]")
-        .action((x, c) => c.copy(repr = Representations.withName(x)))
+        .text(s"representation to extract: [${Representation.values.toSeq.sorted.mkString("|")}] - defaults to ${Representation.cpg14}")
+        .action((x, c) => c.copy(repr = Representation.withName(x)))
+      opt[String]("format")
+        .required()
+        .action((x, c) => c.copy(format = Format.withName(x)))
+        .text(s"export format, one of [${Format.values.toSeq.sorted.mkString("|")}] - defaults to ${Format.dot}")
     }.parse(args, Config())
 
   parseConfig.foreach { config =>
@@ -62,19 +70,19 @@ object JoernExport extends App {
           }
 
           config.repr match {
-            case Representations.ast =>
+            case Representation.ast =>
               new DumpAst(AstDumpOptions(config.outDir)).create(context)
-            case Representations.cfg =>
+            case Representation.cfg =>
               new DumpCfg(CfgDumpOptions(config.outDir)).create(context)
-            case Representations.ddg =>
+            case Representation.ddg =>
               new DumpDdg(DdgDumpOptions(config.outDir)).create(context)
-            case Representations.cdg =>
+            case Representation.cdg =>
               new DumpCdg(CdgDumpOptions(config.outDir)).create(context)
-            case Representations.pdg =>
+            case Representation.pdg =>
               new DumpPdg(PdgDumpOptions(config.outDir)).create(context)
-            case Representations.cpg14 =>
+            case Representation.cpg14 =>
               new DumpCpg14(Cpg14DumpOptions(config.outDir)).create(context)
-            case Representations.neo4jcsv =>
+            case Representation.neo4jcsv =>
               val ExportResult(nodeCount, edgeCount, files, additionalInfo) =
                 Neo4jCsvExporter.runExport(cpg.graph, config.outDir)
               println(s"export completed successfully: $nodeCount nodes, $edgeCount edges in ${files.size} files")
