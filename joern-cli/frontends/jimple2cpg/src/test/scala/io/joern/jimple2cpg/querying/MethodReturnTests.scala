fa7cb0f391c41074166056de7a83f69a1e240659
David Baker Effendi <dbe@sun.ac.za>
2022-05-25 14:17:46 +0200

    Removed "this" local being generated

--- joern-cli/frontends/jimple2cpg/src/test/scala/io/joern/jimple2cpg/querying/MethodReturnTests.scala
+++ joern-cli/frontends/jimple2cpg/src/test/scala/io/joern/jimple2cpg/querying/MethodReturnTests.scala
@@ -25,8 +25,8 @@ class MethodReturnTests extends JimpleCodeToCpgFixture {
   "should have a RETURN node with correct fields" in {
     val List(x) = cpg.method.name("foo").ast.isReturn.l
     x.code shouldBe "return 1;"
-    x.order shouldBe 2
-    x.argumentIndex shouldBe 2
+    x.order shouldBe 1
+    x.argumentIndex shouldBe 1
     x.astChildren.size shouldBe 1
     x.argumentOut.size shouldBe 1
   }
