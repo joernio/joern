a5254c6e215ba69e39f8939cf6dd91c41d30e4b5
David Baker Effendi <dbe@sun.ac.za>
2022-05-26 22:30:15 +0200

    Added exception and conditional tests

---
+++ benchmarks/src/test/resources/ifspec/highconditional/HighConditional9.java
@@ -0,0 +1,20 @@
+class HighConditional9 {
+    private static boolean secret = true;
+
+    public static void main(String[] args) {
+        test();
+    }
+
+    public static int test() {
+        int value;
+
+        if (secret) {
+            value = 1;
+        } else {
+            value = 1;
+        }
+
+        return value;
+    }
+
+}
