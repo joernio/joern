5a93781de2af999f8d8e7562965b4634ebc9e09e
Michael Pollmeier <michael@michaelpollmeier.com>
2022-05-31 10:36:20 +0200

    run kotlin tests in parallel: execution time 62s -> 25s (#1451)
    * run kotlin tests in parallel: execution time 62s -> 25s
    
    note: the `kotlin2cpgdependencies*` temp files are now being created
    in the system temp dir rather than the project dir. it wouldn't really
    matter, but because we're not forking the jvm for test execution, they
    are now only being deleted when the user exits sbt...
    
    * sbt needs 2G memory now

--- joern-cli/frontends/kotlin2cpg/build.sbt
+++ joern-cli/frontends/kotlin2cpg/build.sbt
@@ -20,3 +20,4 @@ libraryDependencies ++= Seq(
 
 enablePlugins(JavaAppPackaging, LauncherJarPlugin)
 trapExit := false
+Test/fork := false
--- joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/types/CompilerAPIStabilityTests.scala
+++ joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/types/CompilerAPIStabilityTests.scala
@@ -1,13 +1,12 @@
 package io.joern.kotlin2cpg.types
 
-import io.joern.kotlin2cpg.types.ErrorLoggingMessageCollector
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
 class CompilerAPIStabilityTests extends AnyFreeSpec with Matchers {
   "TypeInfoProvioder execution on codebase containing KotlinScript gradle file" - {
     "should return a non-null binding context" in {
-      val sourceDir               = "src/test/resources/code/with_kotlin_script/"
+      val sourceDir               = "joern-cli/frontends/kotlin2cpg/src/test/resources/code/with_kotlin_script/"
       val dirsForSourcesToCompile = ContentSourcesPicker.dirsForRoot(sourceDir)
       val environment =
         CompilerAPI.makeEnvironment(dirsForSourcesToCompile, Seq(), Seq(), new ErrorLoggingMessageCollector)
