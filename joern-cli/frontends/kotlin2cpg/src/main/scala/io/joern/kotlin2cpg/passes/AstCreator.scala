88b1ee102b0fe988293b7a0654cff010a53b9219
Claudiu-Vlad Ursache <claudiu+hw@shiftleft.io>
2022-06-01 16:29:20 +0200

    Use destructuring in single spot

--- joern-cli/frontends/kotlin2cpg/src/main/scala/io/joern/kotlin2cpg/passes/AstCreator.scala
+++ joern-cli/frontends/kotlin2cpg/src/main/scala/io/joern/kotlin2cpg/passes/AstCreator.scala
@@ -70,16 +70,17 @@ class AstCreator(fileWithMeta: KtFileWithMeta, xTypeInfoProvider: TypeInfoProvid
     (bindingInfoQueue ++ lambdaBindingInfoQueue)
       .foreach { bindingInfo =>
         diffGraph.addNode(bindingInfo.node)
-        bindingInfo.edgeMeta.foreach { edgeMeta =>
-          diffGraph.addEdge(edgeMeta._1, edgeMeta._2, edgeMeta._3)
+        bindingInfo.edgeMeta.foreach { case (src, dst, label) =>
+          diffGraph.addEdge(src, dst, label)
         }
       }
 
-    closureBindingDefQueue.foreach { closureBindingDef =>
-      diffGraph.addNode(closureBindingDef.node)
-      diffGraph.addEdge(closureBindingDef.captureEdgeTo, closureBindingDef.node, EdgeTypes.CAPTURE)
-      diffGraph.addEdge(closureBindingDef.node, closureBindingDef.refEdgeTo, EdgeTypes.REF)
-    }
+    closureBindingDefQueue
+      .foreach { case ClosureBindingDef(node, captureEdgeTo, refEdgeTo) =>
+        diffGraph.addNode(node)
+        diffGraph.addEdge(captureEdgeTo, node, EdgeTypes.CAPTURE)
+        diffGraph.addEdge(node, refEdgeTo, EdgeTypes.REF)
+      }
   }
 
   def astForFile(fileWithMeta: KtFileWithMeta)(implicit typeInfoProvider: TypeInfoProvider): Ast = {
