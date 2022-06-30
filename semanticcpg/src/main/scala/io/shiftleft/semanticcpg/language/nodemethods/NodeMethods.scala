acbfbcc43f4e4e62252779ea428e0a45f87dd42f
Michael Pollmeier <michael@michaelpollmeier.com>
2022-06-30 16:19:25 +0200

    Revert "NodeMethods: remove everything _but_ location"
    This reverts commit 9a56e202521dab58b058e288cc7041802e9ffb30.

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/NodeMethods.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/NodeMethods.scala
@@ -7,12 +7,53 @@ import overflowdb.traversal._
 
 import scala.jdk.CollectionConverters._
 
+// Many method of this class should return individual nodes instead of Traversal[...].
+// But over time through some opague implicits the versions returning Traversal[...]
+// got exposed and for now we do not want to break the API.
 class NodeMethods(val node: AbstractNode) extends AnyVal with NodeExtension {
 
   def location(implicit finder: NodeExtensionFinder): NewLocation =
     node match {
       case storedNode: StoredNode => LocationCreator(storedNode)
       case _                      => LocationCreator.emptyLocation("", None)
+    }
+
+  def file: Traversal[File] =
+    node match {
+      case storedNode: StoredNode =>
+        Traversal.fromSingle(storedNode).file
+      case _ =>
+        Traversal.empty
+    }
+
+  def newTagNode(tagName: String): NewTagNodePairTraversal = newTagNodePair(tagName, "")
+
+  def newTagNodePair(tagName: String, tagValue: String): NewTagNodePairTraversal = {
+    new NewTagNodePairTraversal(
+      Traversal.fromSingle(
+        NewTagNodePair()
+          .tag(NewTag().name(tagName).value(tagValue))
+          .node(node)
+      )
+    )
+  }
+
+  def tagList: Traversal[TagBase] =
+    node match {
+      case storedNode: StoredNode =>
+        storedNode._taggedByOut.asScala
+          .map { case tagNode: HasName with HasValue =>
+            (tagNode.name, Option(tagNode.value))
+          }
+          .distinct
+          .collect { case (name, Some(value)) =>
+            NewTag()
+              .name(name)
+              .value(value)
+              .asInstanceOf[TagBase]
+          }
+      case _ =>
+        Traversal.empty
     }
 
 }
