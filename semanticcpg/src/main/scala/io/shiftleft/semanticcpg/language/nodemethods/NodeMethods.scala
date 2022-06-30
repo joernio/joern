74552cbbf381fb1d0e2050f2d7f437af1acfc6d2
Michael Pollmeier <michael@michaelpollmeier.com>
2022-06-30 13:53:13 +0200

    take a step back: AbstractNode.location still required at times

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/NodeMethods.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/NodeMethods.scala
@@ -10,16 +10,23 @@ import scala.jdk.CollectionConverters._
 // Many method of this class should return individual nodes instead of Traversal[...].
 // But over time through some opague implicits the versions returning Traversal[...]
 // got exposed and for now we do not want to break the API.
-class NodeMethods(val node: StoredNode) extends AnyVal with NodeExtension {
+class NodeMethods(val node: AbstractNode) extends AnyVal with NodeExtension {
 
   def location(implicit finder: NodeExtensionFinder): NewLocation =
-    LocationCreator(node)
+    node match {
+      case storedNode: StoredNode => LocationCreator(storedNode)
+      case _                      => LocationCreator.emptyLocation("", None)
+    }
 
   def file: Traversal[File] =
-    Traversal.fromSingle(node).file
+    node match {
+      case storedNode: StoredNode =>
+        Traversal.fromSingle(storedNode).file
+      case _ =>
+        Traversal.empty
+    }
 
-  def newTagNode(tagName: String): NewTagNodePairTraversal =
-    newTagNodePair(tagName, "")
+  def newTagNode(tagName: String): NewTagNodePairTraversal = newTagNodePair(tagName, "")
 
   def newTagNodePair(tagName: String, tagValue: String): NewTagNodePairTraversal = {
     new NewTagNodePairTraversal(
@@ -31,15 +38,22 @@ class NodeMethods(val node: StoredNode) extends AnyVal with NodeExtension {
     )
   }
 
-  def tagList: Traversal[NewTag] = {
-    node._taggedByOut.asScala
-      .map { case tagNode: HasName with HasValue =>
-        (tagNode.name, Option(tagNode.value))
-      }
-      .distinct
-      .collect { case (name, Some(value)) =>
-        NewTag().name(name).value(value)
-      }
-  }
+  def tagList: Traversal[TagBase] =
+    node match {
+      case storedNode: StoredNode =>
+        storedNode._taggedByOut.asScala
+          .map { case tagNode: HasName with HasValue =>
+            (tagNode.name, Option(tagNode.value))
+          }
+          .distinct
+          .collect { case (name, Some(value)) =>
+            NewTag()
+              .name(name)
+              .value(value)
+              .asInstanceOf[TagBase]
+          }
+      case _ =>
+        Traversal.empty
+    }
 
 }
--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/package.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/package.scala
@@ -1,33 +1,7 @@
 package io.shiftleft.semanticcpg
 
 import io.shiftleft.codepropertygraph.Cpg
-import io.shiftleft.codepropertygraph.generated.nodes.{
-  Annotation,
-  AnnotationParameterAssign,
-  AstNode,
-  Call,
-  CfgNode,
-  ControlStructure,
-  Expression,
-  File,
-  Identifier,
-  Import,
-  Literal,
-  Local,
-  Member,
-  Method,
-  MethodParameterIn,
-  MethodParameterOut,
-  MethodRef,
-  MethodReturn,
-  Namespace,
-  NamespaceBlock,
-  NewNode,
-  StoredNode,
-  Tag,
-  Type,
-  TypeDecl
-}
+import io.shiftleft.codepropertygraph.generated.nodes._
 import io.shiftleft.codepropertygraph.generated.traversal.NodeTraversalImplicits
 import io.shiftleft.semanticcpg.language.bindingextension.{
   MethodTraversal => BindingMethodTraversal,
@@ -56,7 +30,7 @@ package object language extends operatorextension.Implicits with LowPrioImplicit
   // to generated node types.
 
   implicit def cfgNodeToAsNode(node: CfgNode): AstNodeMethods                 = new AstNodeMethods(node)
-  implicit def toExtendedNode(node: StoredNode): NodeMethods                  = new NodeMethods(node)
+  implicit def toExtendedNode(node: AbstractNode): NodeMethods                = new NodeMethods(node)
   implicit def toExtendedStoredNode(node: StoredNode): StoredNodeMethods      = new StoredNodeMethods(node)
   implicit def toAstNodeMethods(node: AstNode): AstNodeMethods                = new AstNodeMethods(node)
   implicit def toCfgNodeMethods(node: CfgNode): CfgNodeMethods                = new CfgNodeMethods(node)
