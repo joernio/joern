1329623fbeef18f903093cf60b3355e40c839628
Claudiu-Vlad Ursache <claudiu+hw@shiftleft.io>
2022-07-11 17:29:20 +0200

    kotlin2cpg: test dataflows through try and if-else

--- joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/dataflow/SimpleDataFlowTests.scala
+++ joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/dataflow/SimpleDataFlowTests.scala
@@ -103,4 +103,125 @@ class SimpleDataFlowTests extends KotlinCode2CpgFixture(withOssDataflow = true) 
       sink.reachableByFlows(source).toSeq should not be Seq()
     }
   }
+
+  "CPG for code with simple control expressions" should {
+    val cpg = code("""
+        |package mypkg
+        |
+        |import kotlin.random.Random
+        |
+        |fun doSomething(p: Int): Int {
+        |  val someVal = 41
+        |
+        |  val afterIfNoBlocks = if (p % 2 == 0) someVal else 42
+        |  println(afterIfNoBlocks)
+        |
+        |  val afterIf = if (p % 2 == 0) { someVal } else { 42 }
+        |  println(afterIf)
+        |
+        |  val afterTry = try { p / 0 } catch(e: Exception) { someVal }
+        |  println(afterTry)
+        |
+        |  return 41
+        |}
+        |
+        |fun main() {
+        |  val dicey = Random.nextInt()
+        |  doSomething(dicey)
+        |}
+        |""".stripMargin)
+
+    def source = cpg.literal.code("41")
+
+    "should find a flow through if-expression without blocks" in {
+      def sink = cpg.identifier.name("afterIfNoBlocks")
+      sink.reachableByFlows(source).toSeq should not be Seq()
+    }
+
+    "should find a flow through if-expression" in {
+      val sink = cpg.identifier.name("afterIf")
+      sink.reachableByFlows(source).toSeq should not be Seq()
+    }
+
+    "should find a flow through try-expression" in {
+      val sink = cpg.identifier.name("afterTry")
+      sink.reachableByFlows(source).toSeq should not be Seq()
+    }
+  }
+
+  "CPG for code with simple control structures" should {
+    val cpg = code("""
+        |package mypkg
+        |
+        |import kotlin.random.Random
+        |
+        |fun doSomething(p: Int): Int {
+        |  var tickOne = 0
+        |  if (p % 2 == 0) {
+        |    tickOne = p
+        |  } else {
+        |    println("NOPNOPNOPNOPNOPNOPNOPNOPNOPNOP")
+        |  }
+        |  val afterIfFromThen = tickOne
+        |  println(afterIfFromThen)
+        |
+        |  var tickTwo = 0
+        |  if (p % 2 == 0) {
+        |    println("NOPNOPNOPNOPNOPNOPNOPNOPNOPNOP")
+        |  } else {
+        |    tickTwo = p
+        |  }
+        |  val afterIfFromElse = tickTwo
+        |  println(afterIfFromElse)
+        |
+        |  var tickThree = 0
+        |  try {
+        |    0x41414141 / 0
+        |  } catch (e: Exception) {
+        |    tickThree = p
+        |  }
+        |  val afterTryFromCatch = tickThree
+        |  println(afterTryFromCatch)
+        |
+        |  var tickFour = 0
+        |  try {
+        |    tickFour = p
+        |    0x41414141 / 0
+        |  } catch (e: Exception) {
+        |    println("NOPNOPNOPNOPNOPNOPNOPNOPNOPNOP")
+        |  }
+        |  val afterTryFromBody = tickFour
+        |  println(afterTryFromBody)
+        |
+        |  return 41
+        |}
+        |
+        |fun main() {
+        |  val dicey = Random.nextInt()
+        |  doSomething(dicey)
+        |}
+        |""".stripMargin)
+
+    def source = cpg.method.name("doSomething").parameter
+
+    "should find a flow through `then`-block of `if` control structure" in {
+      def sink = cpg.identifier.name("afterIfFromThen")
+      sink.reachableByFlows(source).toSeq should not be Seq()
+    }
+
+    "should find a flow through `else`-block of `if` control structure" in {
+      def sink = cpg.identifier.name("afterIfFromElse")
+      sink.reachableByFlows(source).toSeq should not be Seq()
+    }
+
+    "should find a flow through `catch`-block of `try` control structure" in {
+      def sink = cpg.identifier.name("afterTryFromCatch")
+      sink.reachableByFlows(source).toSeq should not be Seq()
+    }
+
+    "should find a flow through body of `try` control structure" in {
+      def sink = cpg.identifier.name("afterTryFromBody")
+      sink.reachableByFlows(source).toSeq should not be Seq()
+    }
+  }
 }
