2a5dab5b06e08ec2a3eff8e0a7a09cd50ecc7401
Claudiu-Vlad Ursache <claudiu+hw@shiftleft.io>
2022-07-11 16:20:57 +0200

    Add more simple DF test cases for `kotlin2cpg`

--- joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/dataflow/SimpleDataFlowTests.scala
+++ joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/dataflow/SimpleDataFlowTests.scala
@@ -17,4 +17,90 @@ class SimpleDataFlowTests extends KotlinCode2CpgFixture(withOssDataflow = true) 
       sink.reachableByFlows(source).size shouldBe 1
     }
   }
+
+  "CPG for code with simple arithmetic operators" should {
+    val cpg = code("""
+        |package mypkg
+        |
+        |fun doSomething(x: Int): Int {
+        |  val add41 = x + 41
+        |  val subtract41 = x - 41
+        |  val multiplyBy41 = x * 41
+        |  val divideBy41 = x / 41
+        |  return 41
+        |}
+        |""".stripMargin)
+
+    def source = cpg.method.name("doSomething").parameter
+
+    "should find a flow through addition" in {
+      val sink = cpg.identifier.name("add41")
+      sink.reachableByFlows(source).toSeq should not be Seq()
+    }
+
+    "should find a flow through subtraction" in {
+      val sink = cpg.identifier.name("subtract41")
+      sink.reachableByFlows(source).toSeq should not be Seq()
+    }
+
+    "should find a flow through multiplication" in {
+      val sink = cpg.identifier.name("multiplyBy41")
+      sink.reachableByFlows(source).toSeq should not be Seq()
+    }
+
+    "should find a flow through division" in {
+      val sink = cpg.identifier.name("divideBy41")
+      sink.reachableByFlows(source).toSeq should not be Seq()
+    }
+  }
+
+  "CPG for code with simple assignment operators" should {
+    val cpg = code("""
+        |package mypkg
+        |
+        |fun doSomething(x: Int): Int {
+        |  val modifyBy = 41
+        |
+        |  val x = 1
+        |  x += modifyBy
+        |  val addEq = x
+        |
+        |  val y = 1
+        |  y -= modifyBy
+        |  val subtractEq = y
+        |
+        |  val z = 1
+        |  z *= modifyBy
+        |  val multiplyEq = z
+        |
+        |  val p = 1
+        |  p /= modifyBy
+        |  val divideEq = p
+        |
+        |  return 41
+        |}
+        |""".stripMargin)
+
+    def source = cpg.literal.code("41")
+
+    "should find a flow through assign-addition" in {
+      val sink = cpg.identifier.name("addEq")
+      sink.reachableByFlows(source).toSeq should not be Seq()
+    }
+
+    "should find a flow through assign-subtraction" in {
+      val sink = cpg.identifier.name("subtractEq")
+      sink.reachableByFlows(source).toSeq should not be Seq()
+    }
+
+    "should find a flow through assign-multiplication" in {
+      val sink = cpg.identifier.name("multiplyEq")
+      sink.reachableByFlows(source).toSeq should not be Seq()
+    }
+
+    "should find a flow through assign-division" in {
+      val sink = cpg.identifier.name("divideEq")
+      sink.reachableByFlows(source).toSeq should not be Seq()
+    }
+  }
 }
