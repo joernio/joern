f990e3070290ae71601890993aa00e5e14e297ba
Max Leuthäuser <max.leuthaeuser@gmail.com>
2022-07-20 11:44:39 +0200

    Fixed joern-cli distribution
    We actually need to copy the binaries of astgen required for jssrc2cpg to the final distribution.
    Also added remaining missing .bat files for executing joern and joern-* under Windows.

---
+++ joern-cli/src/universal/joern-parse.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set "SCRIPT_ABS_DIR=%~dp0"
+set "SCRIPT=%SCRIPT_ABS_DIR%bin\joern-parse.bat"
+
+"%SCRIPT%" "-J-XX:+UseG1GC" "-J-XX:CompressedClassSpaceSize=128m" "-Dlog4j.configurationFile=%SCRIPT_ABS_DIR%conf\log4j2.xml" %*
