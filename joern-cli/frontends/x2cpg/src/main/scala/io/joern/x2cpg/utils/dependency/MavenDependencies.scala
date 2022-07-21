1da069b48eb11d1d7d1f421691a53bd8cd3218c2
maltek <1694194+maltek@users.noreply.github.com>
2022-07-21 14:58:59 +0200

    Fix Maven dependency fetching for multiple sub-projects (#1625)
    * Fix Maven dependency fetching for multiple sub-projects
    
    also add some more logging
    
    * set all maven logging to info to make tests pass

--- joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/utils/dependency/MavenDependencies.scala
+++ joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/utils/dependency/MavenDependencies.scala
@@ -3,40 +3,43 @@ package io.joern.x2cpg.utils.dependency
 import io.joern.x2cpg.utils.ExternalCommand
 import org.slf4j.LoggerFactory
 
-import java.nio.charset.StandardCharsets
-import java.nio.file.{Files, Path}
-import scala.util.{Failure, Success, Try}
+import java.nio.file.Path
+import scala.util.{Failure, Success}
 
 object MavenDependencies {
   private val logger = LoggerFactory.getLogger(getClass)
 
   private[dependency] def get(projectDir: Path): List[String] = {
-    val tmpFile = Files.createTempFile("mvnClassPass", ".txt")
-    tmpFile.toFile.deleteOnExit()
-    ExternalCommand.run(
-      s"mvn -B dependency:build-classpath -DincludeScope=compile -Dmdep.outputFile=$tmpFile",
+    // we can't use -Dmdep.outputFile because that keeps overwriting its own output for each sub-project it's running for
+    val lines = ExternalCommand.run(
+      s"mvn -B dependency:build-classpath -DincludeScope=compile -Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dorg.slf4j.simpleLogger.logFile=System.out",
       projectDir.toString
     ) match {
-      case Success(_) =>
+      case Success(lines) => lines
       case Failure(exception) =>
         logger.warn(
           s"Retrieval of compile class path via maven return with error.\n" +
             "The compile class path may be missing or partial.\n" +
-            "Results will suffer from poor type information.\n" +
-            s"Error: ${exception.getMessage}"
+            "Results will suffer from poor type information.\n\n" +
+            exception.getMessage
         )
+        // exception message is the program output - and we still want to look for potential partial results
+        exception.getMessage.linesIterator.toSeq
     }
 
-    Try {
-      // Regardless of the external maven comment error status we try to read the
-      // output file because sometimes it contains partial results.
-      val classPath = new String(Files.readAllBytes(tmpFile), StandardCharsets.UTF_8)
-      if (classPath.isEmpty) {
-        Nil
-      } else {
-        classPath.split(":").toList
+    var classPathNext = false
+    val deps = lines
+      .flatMap { line =>
+        val isClassPathNow = classPathNext
+        classPathNext = line.endsWith("Dependencies classpath:")
+
+        if (isClassPathNow) line.split(':') else Array.empty[String]
       }
-    }.getOrElse(Nil)
+      .distinct
+      .toList
+    logger.info("got {} Maven dependencies", deps.size)
+
+    deps
   }
 
 }
