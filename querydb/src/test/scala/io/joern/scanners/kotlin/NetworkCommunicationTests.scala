0c4e62975fb968a0012537cbe30cf392f3c5f68c
Claudiu-Vlad Ursache <claudiu+hw@shiftleft.io>
2022-07-22 18:18:03 +0200

    querydb: add new kotlin query `nopTrustManagerUsed` (#1632)
    * querydb: add new kotlin query `nopTrustManagerUsed`
    
    * Add helper fns for query test suites

---
+++ querydb/src/test/scala/io/joern/scanners/kotlin/NetworkCommunicationTests.scala
@@ -0,0 +1,24 @@
+package io.joern.scanners.kotlin
+
+import io.joern.suites.KotlinQueryTestSuite
+
+class NetworkCommunicationTests extends KotlinQueryTestSuite {
+
+  override def queryBundle = NetworkCommunication
+
+  "should match on all multi-file positive examples" in {
+    val q = queryBundle.nopTrustManagerUsed()
+    q.multiFileCodeExamples.positive.filter(_.nonEmpty).foreach { snippets =>
+      val cpg = cpgForSnippets(snippets)
+      findMatchingCalls(cpg, q).size shouldBe 1
+    }
+  }
+
+  "should not match on all multi-file negative examples" in {
+    val q = queryBundle.nopTrustManagerUsed()
+    q.multiFileCodeExamples.negative.filter(_.nonEmpty).foreach { snippets =>
+      val cpg = cpgForSnippets(snippets)
+      findMatchingCalls(cpg, q).size shouldBe 0
+    }
+  }
+}
