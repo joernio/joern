9f2643e8b11a9a54cc4bfd7bbfd139d88dbb53b6
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2022-07-22 08:54:16 +0200

    console: warn about non-existent dir (#1629)
    Fixes: https://github.com/joernio/joern/issues/1607

--- console/src/test/scala/io/joern/console/ConsoleTests.scala
+++ console/src/test/scala/io/joern/console/ConsoleTests.scala
@@ -24,6 +24,22 @@ class ConsoleTests extends AnyWordSpec with Matchers {
       )
 
   "importCode" should {
+    "warn about non-existent dir" in ConsoleFixture() { (console, _) =>
+      val nonExistentDir = "/does/not/exist/"
+      the[ConsoleException] thrownBy console.importCode(
+        nonExistentDir
+      ) should have message s"Input path does not exist: '$nonExistentDir'"
+      the[ConsoleException] thrownBy console.importCode.c(
+        nonExistentDir
+      ) should have message s"Input path does not exist: '$nonExistentDir'"
+      the[ConsoleException] thrownBy console.importCode.jssrc(
+        nonExistentDir
+      ) should have message s"Input path does not exist: '$nonExistentDir'"
+      the[ConsoleException] thrownBy console.importCode.java(
+        nonExistentDir
+      ) should have message s"Input path does not exist: '$nonExistentDir'"
+    }
+
     "provide overview of available language modules" in ConsoleFixture() { (console, _) =>
       console.importCode.toString.contains("| C") shouldBe true
     }
