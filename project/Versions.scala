b9e14252a54bf37a8587153e53fc26308c3bdd5c
Scala Steward <scala_steward@virtuslab.com>
2022-07-31 15:02:28 +0000

    Update scalatest to 3.2.13

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@ object Versions {
   // Dont upgrade antlr to 4.10 or above since those versions require java 11 or higher which
   // causes problems upstreams.
   val antlr         = "4.7"
-  val scalatest     = "3.2.12"
+  val scalatest     = "3.2.13"
   val cats          = "3.3.14"
   val log4j         = "2.18.0"
   val json4s        = "4.0.5"
