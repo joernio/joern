111fe08976db924e8465fd884af1539e5b28ef34
Michael Pollmeier <michael@michaelpollmeier.com>
2022-08-19 11:09:04 +0200

    console/compile and console/Test/compile are green now
    TypeTags were dropped in scala3, replaced with ??? for now, scripts
    won't work

---
+++ joern-cli/frontends/ghidra2cpg/src/test/scala/io/joern/ghidra2cpg/fixtures/GhidraBinToCpgSuite.scala
@@ -0,0 +1,39 @@
+package io.joern.ghidra2cpg.fixtures
+
+import io.joern.ghidra2cpg.{Config, Ghidra2Cpg}
+import io.shiftleft.codepropertygraph.Cpg
+import io.joern.x2cpg.testfixtures.LanguageFrontend
+import io.shiftleft.utils.ProjectRoot
+import org.apache.commons.io.FileUtils
+import io.shiftleft.codepropertygraph.generated.nodes
+import io.joern.dataflowengineoss.language._
+import io.shiftleft.semanticcpg.language._
+
+import java.nio.file.Files
+
+class GhidraFrontend extends LanguageFrontend {
+  override val fileSuffix: String = ""
+
+  override def execute(inputFile: java.io.File): Cpg = {
+    val dir = Files.createTempDirectory("ghidra2cpg-tests").toFile
+    Runtime.getRuntime.addShutdownHook(new Thread(new Runnable {
+      override def run(): Unit = FileUtils.deleteQuietly(dir)
+    }))
+
+    val tempDir = Files.createTempDirectory("ghidra2cpg").toFile
+    Runtime.getRuntime.addShutdownHook(new Thread(new Runnable {
+      override def run(): Unit = FileUtils.deleteQuietly(tempDir)
+    }))
+
+    val cpgBin                         = dir.getAbsolutePath
+    implicit val defaultConfig: Config = Config()
+    new Ghidra2Cpg().createCpg(inputFile.getAbsolutePath, Some(cpgBin)).get
+  }
+
+}
+
+class GhidraBinToCpgSuite extends BinToCpgFixture(new GhidraFrontend) {
+  override val binDirectory = ProjectRoot.relativise("joern-cli/frontends/ghidra2cpg/src/test/testbinaries/")
+
+  protected def flowToResultPairs(path: Path): List[String] = path.resultPairs().map(_._1)
+}
