bdcc5bf94be94e0b9245d3fbe4bd759ed73a814d
Fabian Yamaguchi <fabs@shiftleft.io>
2022-08-22 15:48:46 +0200

    Generalize embedding

--- joern-cli/src/main/scala/io/joern/joerncli/JoernEmbed.scala
+++ joern-cli/src/main/scala/io/joern/joerncli/JoernEmbed.scala
@@ -1,13 +1,70 @@
 package io.joern.joerncli
 
 import io.joern.joerncli.CpgBasedTool.exitIfInvalid
-import io.shiftleft.codepropertygraph.generated.nodes.Method
+import io.shiftleft.codepropertygraph.Cpg
+import io.shiftleft.codepropertygraph.generated.nodes.{Method, StoredNode}
 
 import scala.util.Using
 import io.shiftleft.semanticcpg.language._
-import overflowdb.traversal._
 
 import scala.util.hashing.MurmurHash3
+
+/** Creates an embedding from a code property graph by following three steps: (1) Objects are extracted from the graph,
+  * each of which is ultimately to be mapped to one vector (2) For each object, enumerate its sub structures. (3) Employ
+  * feature hashing to associate each sub structure with a dimension. See "Pattern-based Vulnerability Discovery -
+  * Chapter 3"
+  */
+trait EmbeddingGenerator[T, S] {
+
+  type SparseVector = Map[Int, (Double, S)]
+
+  case class Embedding(vectors: Map[T, SparseVector]) {
+    lazy val dimToStructure: Map[Int, S] = vectors.flatMap { case (_, vector) =>
+      vector.map { case (hash, (_, structure)) => hash -> structure }
+    }
+    lazy val structureToDim: Map[S, Int] = vectors.flatMap { case (_, vector) =>
+      vector.map { case (hash, (_, structure)) => structure -> hash }
+    }
+  }
+
+  /** Extract a sequence of (object, vector) pairs from a cpg.
+    */
+  def embed(cpg: Cpg): Embedding = {
+    Embedding(
+      extractObjects(cpg)
+        .map { obj => (obj, enumerateSubStructures(obj)) }
+        .map { case (obj, substructures) =>
+          obj -> vectorize(substructures)
+        }
+        .toMap
+    )
+
+  }
+
+  private def vectorize(substructures: List[S]): SparseVector = {
+    substructures
+      .groupBy(identity)
+      .view
+      .mapValues(_.size)
+      .map { case (s, v) =>
+        hash(s) -> (v.toDouble, s)
+      }
+      .toMap
+  }
+
+  /** A function that creates a sequence of objects from a CPG
+    */
+  def extractObjects(cpg: Cpg): Seq[T]
+
+  /** A function that, for a given object, extracts its sub structures
+    */
+  def enumerateSubStructures(obj: T): List[S]
+
+  /** A function that allows hashing of a sub structure
+    */
+  def hash(s: S): Int
+
+}
 
 object JoernEmbed extends App {
 
