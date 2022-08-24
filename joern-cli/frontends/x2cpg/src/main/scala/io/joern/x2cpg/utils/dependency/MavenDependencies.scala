6353b967d64f3ddb16d3b988d98267829cc31957
Johannes Coetzee <jcoetzee@shiftleft.io>
2022-08-24 11:40:45 +0200

    Recursively search for a mix of Maven and Gradle projects when fetching dependencies (#1704)
    * Recursively look for all gradle projects when fetching dependencies
    
    * Recursively search for a mix of gradle and maven projects

--- joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/utils/dependency/MavenDependencies.scala
+++ joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/utils/dependency/MavenDependencies.scala
@@ -10,52 +10,36 @@ import scala.util.{Failure, Success}
 object MavenDependencies {
   private val logger = LoggerFactory.getLogger(getClass)
 
-  private def findMavenProjectRoots(currentDir: File): List[Path] = {
-    val (childDirectories, childFiles) = currentDir.children.partition(_.isDirectory)
-    childFiles.find(_.name == "pom.xml") match {
-      case Some(pomFile) => pomFile.parent.path :: Nil
-
-      case None if childDirectories.isEmpty => Nil
-
-      case None => childDirectories.flatMap(findMavenProjectRoots).toList
-
+  private[dependency] def get(projectDir: Path): Option[collection.Seq[String]] = {
+    // we can't use -Dmdep.outputFile because that keeps overwriting its own output for each sub-project it's running for
+    val lines = ExternalCommand.run(
+      s"mvn -B dependency:build-classpath -DincludeScope=compile -Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dorg.slf4j.simpleLogger.logFile=System.out",
+      projectDir.toString
+    ) match {
+      case Success(lines) => lines
+      case Failure(exception) =>
+        logger.warn(
+          s"Retrieval of compile class path via maven return with error.\n" +
+            "The compile class path may be missing or partial.\n" +
+            "Results will suffer from poor type information.\n\n" +
+            exception.getMessage
+        )
+        // exception message is the program output - and we still want to look for potential partial results
+        exception.getMessage.linesIterator.toSeq
     }
-  }
 
-  private[dependency] def get(projectDir: Path): List[String] = {
-    val combinedDeps = findMavenProjectRoots(projectDir).flatMap { projectRoot =>
-      // we can't use -Dmdep.outputFile because that keeps overwriting its own output for each sub-project it's running for
-      val lines = ExternalCommand.run(
-        s"mvn -B dependency:build-classpath -DincludeScope=compile -Dorg.slf4j.simpleLogger.defaultLogLevel=info -Dorg.slf4j.simpleLogger.logFile=System.out",
-        projectRoot.toString
-      ) match {
-        case Success(lines) => lines
-        case Failure(exception) =>
-          logger.warn(
-            s"Retrieval of compile class path via maven return with error.\n" +
-              "The compile class path may be missing or partial.\n" +
-              "Results will suffer from poor type information.\n\n" +
-              exception.getMessage
-          )
-          // exception message is the program output - and we still want to look for potential partial results
-          exception.getMessage.linesIterator.toSeq
+    var classPathNext = false
+    val deps = lines
+      .flatMap { line =>
+        val isClassPathNow = classPathNext
+        classPathNext = line.endsWith("Dependencies classpath:")
+
+        if (isClassPathNow) line.split(':') else Array.empty[String]
       }
+      .distinct
+      .toList
 
-      var classPathNext = false
-      val deps = lines
-        .flatMap { line =>
-          val isClassPathNow = classPathNext
-          classPathNext = line.endsWith("Dependencies classpath:")
-
-          if (isClassPathNow) line.split(':') else Array.empty[String]
-        }
-        .distinct
-        .toList
-
-      deps
-    }
-    logger.info("got {} Maven dependencies", combinedDeps.size)
-    combinedDeps
+    logger.info("got {} Maven dependencies", deps.size)
+    Some(deps)
   }
-
 }
