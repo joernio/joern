6660ad24f17041e79094402d8da86a3fff5cb7d1
Michael Pollmeier <michael@michaelpollmeier.com>
2022-08-26 17:41:47 +0200

    poor man's predef

--- console2/src/main/scala/io2/joern/console/Console.scala
+++ console2/src/main/scala/io2/joern/console/Console.scala
@@ -7,4 +7,15 @@ object Console:
   def main(args: Array[String]): Unit =
     // pass classpath on into the repl
     val replArgs = args ++ Array("-classpath", System.getProperty("java.class.path"))
-    new ReplDriver(replArgs).tryRunning
+    // new ReplDriver(replArgs).tryRunning
+    val repl = new ReplDriver(replArgs)
+    val state = repl.initialState
+    println("imports: " + state.imports)
+    println(state.valIndex)
+    println(state.objectIndex)
+    val state2 = repl.run("def foo = 42")(state)
+    println(state2.valIndex)
+    println(state2.objectIndex)
+    repl.runUntilQuit(state2)
+
+    
