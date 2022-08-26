bf813600dc8cf43c55f4d23db9520211648c1ae3
Max Leuthäuser <max.leuthaeuser@gmail.com>
2022-08-26 14:14:48 +0200

    jssrc2cpg: added support for ejs template files

---
+++ joern-cli/frontends/jssrc2cpg/src/test/scala/io/joern/jssrc2cpg/preprocessing/EjsPassTest.scala
@@ -0,0 +1,62 @@
+package io.joern.jssrc2cpg.preprocessing
+
+import io.joern.jssrc2cpg.passes.AbstractPassTest
+import io.shiftleft.semanticcpg.language._
+
+class EjsPassTest extends AbstractPassTest {
+
+  "ejs files" should {
+    "be handled correctly" in AstFixture(
+      """
+        |<body>
+        |
+        |<h1>Welcome <%= user.name %></h1>
+        |
+        |<b><%_ foo.callWithWhitespaces() _%></b>
+        |
+        |<b><%- foo.callUnescaped() -%></b>
+        |
+        |<%# Just a comment here #%>
+        |<% if (admin) { %>
+        |    <a href="/admin">Admin</a>
+        |<% } %>
+        |
+        |<ul>
+        |    <% friends.forEach(function(friend, index) { %>
+        |        <li class="<%= index === 0 ? "first" : "" %> <%= friend.name === selected ? "selected" : "" %>"><%= friend.name %></li>
+        |    <% }); %>
+        |</ul>
+        |
+        |<%
+        |console.log(user);
+        |exampleWrite = 'some value';
+        |%>
+        |
+        |</body>
+        |""".stripMargin,
+      "index.ejs"
+    ) { cpg =>
+      cpg.file.name.l shouldBe List("index.ejs")
+      cpg.call.code.l.sorted shouldBe
+        List(
+          "console.log",
+          "console.log(user)",
+          "exampleWrite = 'some value'",
+          "foo.callUnescaped",
+          "foo.callUnescaped()",
+          "foo.callWithWhitespaces",
+          "foo.callWithWhitespaces()",
+          "friend.name",
+          "friend.name",
+          "friend.name === selected",
+          "friend.name === selected ? \"selected\" : \"\"",
+          "friends.forEach",
+          "friends.forEach(function(friend, index) { %>\n        <li class=\"<%= index === 0 ? \"first\" : \"\" %> <%= friend.name === selected ? \"selected\" : \"\" %>\"><%= friend.name %></li>\n    <% })",
+          "index === 0",
+          "index === 0 ? \"first\" : \"\"",
+          "user.name"
+        )
+    }
+  }
+
+}
