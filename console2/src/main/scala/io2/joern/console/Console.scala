507107ed5b517082ab0c0b6d2da256d29e25495e
Michael Pollmeier <michael@michaelpollmeier.com>
2022-08-26 17:21:59 +0200

    wip - `io` namespace is shadowed by `scala.io`

--- console2/src/main/scala/io2/joern/console/Console.scala
+++ console2/src/main/scala/io2/joern/console/Console.scala
@@ -1,5 +1,10 @@
 package io2.joern.console
 
-object Console {
-  def foo: Int = 43
-}
+import dotty.tools.repl._
+
+object Console:
+
+  def main(args: Array[String]): Unit =
+    // pass classpath on into the repl
+    val replArgs = args ++ Array("-classpath", System.getProperty("java.class.path"))
+    new ReplDriver(replArgs).tryRunning
