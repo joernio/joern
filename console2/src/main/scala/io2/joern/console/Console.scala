9041145f1c43afc6515d22d634313cbb1657283e
Michael Pollmeier <michael@michaelpollmeier.com>
2022-08-29 10:42:40 +0200

    old school syntax

--- console2/src/main/scala/io2/joern/console/Console.scala
+++ console2/src/main/scala/io2/joern/console/Console.scala
@@ -2,20 +2,21 @@ package io2.joern.console
 
 import dotty.tools.repl._
 
-object Console:
+object Console {
 
-  def main(args: Array[String]): Unit =
+  val predefCode =
+    """
+      |def foo = 42
+      |import Int.MaxValue
+      |""".stripMargin
+
+  def main(args: Array[String]): Unit = {
     // pass classpath on into the repl
     val replArgs = args ++ Array("-classpath", System.getProperty("java.class.path"))
-    // new ReplDriver(replArgs).tryRunning
     val repl = new ReplDriver(replArgs)
-    val predefCode =
-      """
-        |def foo = 42
-        |import Int.MaxValue
-        |""".stripMargin
 
-    val state = repl.run(predefCode)(repl.initialState)
-    repl.runUntilQuit(state)
+    val stateAfterPredef = repl.run(predefCode)(repl.initialState)
+    repl.runUntilQuit(stateAfterPredef)
+  }
 
-    
+}
