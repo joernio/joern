ec1a54759db717dba291aef30448605b8b20dd92
Michael Pollmeier <michael@michaelpollmeier.com>
2022-08-31 15:37:13 +0200

    wire up basics

--- console2/src/main/scala/io/joern/console/Console.scala
+++ console2/src/main/scala/io/joern/console/Console.scala
@@ -20,32 +20,6 @@ import scala.util.control.NoStackTrace
 import scala.util.{Failure, Success, Try}
 
 object Console {
-
-  def bar = "barz"
-
-  val predefCode =
-    """
-      |def foo = 42
-      |import Int.MaxValue
-      |import _root_.io.joern.console.Console.bar
-      |""".stripMargin
-
-  def main(args: Array[String]): Unit = {
-    val additionalArgs = Array(
-      "-classpath", // pass classpath on into the repl
-      System.getProperty("java.class.path"),
-      "-explain", // verbose scalac error messages
-    )
-    val greeting = "hey there!"
-    val repl = new ReplDriver(
-      args ++ additionalArgs,
-      scala.Console.out,
-      greeting)
-
-    val stateAfterPredef = repl.run(predefCode)(using repl.initialState)
-    repl.runUntilQuit(using stateAfterPredef)()
-  }
-
   def report(string: String): Unit = System.err.println(string)
   val nameOfLegacyCpgInProject     = "cpg.bin.zip"
 
