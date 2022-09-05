0b1d8608fc324879087177782d63a466e1d95d68
Scala Steward <scala_steward@virtuslab.com>
2022-09-05 15:17:48 +0000

    Update antlr4 to 4.11.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -3,7 +3,7 @@ object Versions {
   val cpg = parseVersion("cpgVersion")
   // Dont upgrade antlr to 4.10 or above since those versions require java 11 or higher which
   // causes problems upstreams.
-  val antlr         = "4.7"
+  val antlr         = "4.11.1"
   val scalatest     = "3.2.12"
   val cats          = "3.3.14"
   val log4j         = "2.18.0"
