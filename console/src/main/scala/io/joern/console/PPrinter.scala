00ac8ee5d845a78c6cc57529f472a3603efd0460
Michael Pollmeier <michael@michaelpollmeier.com>
2022-09-08 11:11:39 +0200

    start cleanup

--- console/src/main/scala/io/joern/console/PPrinter.scala
+++ console/src/main/scala/io/joern/console/PPrinter.scala
@@ -3,7 +3,12 @@ package io.joern.console
 import pprint.{PPrinter, Renderer, Result, Tree, Truncated}
 
 object PPrinter {
-  def apply(obj: Object, maxElements: Int): String = s"BBB PPrinter0 ${obj.isInstanceOf[Product]}"
+  private val printer = pprinter.create(pprint.PPrinter.BlackWhite)
+
+  def apply(obj: Object, maxElements: Int): String = {
+//    s"BBB PPrinter0 ${obj.isInstanceOf[Product]}"
+    printer.apply(obj).toString
+  }
 }
 
 object pprinter {
@@ -89,21 +94,4 @@ object pprinter {
       println(other.getClass.getInterfaces.contains(classOf[Product]))
       Tree.Literal(other.toString)
   }
-
-  class ASampleProduct(str: String) extends Product {
-    override def canEqual(that: Any): Boolean = true
-
-    override def productArity: Int = 1
-
-    override def productElement(n: Int): Any = n match {
-      case 0 => str
-    }
-
-    def productElementLabel(n: Int): String = n match {
-      case 0 => "str"
-    }
-  }
-
-  val aSampleProduct: ASampleProduct = new ASampleProduct("example product")
-
 }
