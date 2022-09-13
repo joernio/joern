aee887c42c75a155d0db189ab1de42f816ae00ce
Michael Pollmeier <michael@michaelpollmeier.com>
2022-09-13 14:04:52 +0200

    imports are still problematic... part of a different context...

--- console/src/main/scala/io/joern/console/ScriptingDriver.scala
+++ console/src/main/scala/io/joern/console/ScriptingDriver.scala
@@ -8,16 +8,24 @@ import dotty.tools.io.AbstractFile
 import java.io.File
 
 /**
- * Extends dotty.tools.scripting.ScriptingDriver to add support for predef, i.e. run some code before the main script. 
+ * Extends dotty.tools.scripting.ScriptingDriver to add support for predef, i.e. run some code before the main script.
  */
 class ScriptingDriver(predefCode: String, compilerArgs: Array[String], scriptFile: File, scriptArgs: Array[String])
   extends dotty.tools.scripting.ScriptingDriver(compilerArgs, scriptFile, scriptArgs) {
 
   override protected def doCompile(compiler: Compiler, files: List[AbstractFile])(using Context): Reporter = {
     val run = compiler.newRun
-    run.compileFromStrings(List(predefCode))
+    val predefCode0 =
+      """
+        |object WS1 {
+        |  def bar(i: Int): String = "ws1 bar " + i
+        |}
+        |import WS1.bar
+        |""".stripMargin
+    run.compileFromStrings(List(predefCode0))
+//    run.compileFromStrings(List(predefCode))
     finish(compiler, run)
-    
+
     super.doCompile(compiler, files)
   }
 
