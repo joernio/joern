18c1417d510730ecf1ad0bd5074ed2d529984a5c
Michael Pollmeier <michael@michaelpollmeier.com>
2022-09-17 15:34:27 +0200

    debug java classloader - no results - need dotty classloader

--- console/src/main/scala/io/joern/console/BridgeBase.scala
+++ console/src/main/scala/io/joern/console/BridgeBase.scala
@@ -14,9 +14,11 @@ import io.joern.console.embammonite.EmbeddedAmmonite
 import io.joern.x2cpg.utils.dependency.DependencyResolver
 import os.{pwd, Path}
 
-import java.io.{PrintStream, File as JFile}
+import java.io.{InputStream, PrintStream, File as JFile}
 import java.net.URLClassLoader
 import java.nio.file.{Files, Path, Paths}
+import java.util
+import java.util.stream
 
 case class Config(
   scriptFile: Option[os.Path] = None,
@@ -230,39 +232,99 @@ trait ScriptExecution { this: BridgeBase =>
          |""".stripMargin
     }
 
-//    val classLoader: ClassLoader = {
-//      import java.net.{URL, URLClassLoader}
-//      val parent = this.getClass.getClassLoader
-//      new ClassLoader(parent) {
-//
-//        override def loadClass(name: String, resolve: Boolean): Class[_] = {
-//          val ret = super.loadClass(name, resolve)
-////          os.write.append(os.Path("/home/mp/Projects/shiftleft/joern/foo.txt"), s"$name\n")
-////          println(s"XXXX loadClass resolve=$resolve $name=$ret")
-//          ret
-//        }
-//      }
-//    }
-//
+    val classLoader: ClassLoader = {
+      import java.net.{URL, URLClassLoader}
+      val parent = this.getClass.getClassLoader
+      new ClassLoader(parent) {
 
-    val scalacArgs0 = {
-      val hppcJar = "/home/mp/.cache/coursier/v1/https/repo1.maven.org/maven2/com/carrotsearch/hppc/0.7.1/hppc-0.7.1.jar"
-      val compilerArgs = Array.newBuilder[String]
-      val dependencyFiles = Seq(Paths.get(hppcJar).toFile)
-      compilerArgs ++= Array("-classpath", replClasspath(dependencyFiles))
-      compilerArgs += "-explain" // verbose scalac error messages
-      if (config.nocolors) compilerArgs ++= Array("-color", "never")
-      compilerArgs.result()
+        override def loadClass(name: String): Class[_]  = {
+         val ret = super.loadClass(name)
+         println("XXXX1 " + ret)
+         ret
+        }
+
+        override def findClass(name: String): Class[_]  = {
+         val ret = super.findClass(name)
+         println("XXXX3 " + ret)
+         ret
+        }
+
+        override def findClass(moduleName: String, name: String): Class[_]  = {
+         val ret = super.findClass(moduleName, name)
+         println("XXXX4 " + ret)
+         ret
+        }
+
+        override def findResource(moduleName: String, name: String): URL  = {
+         val ret = super.findResource(moduleName, name)
+         println("XXXX5 " + ret)
+         ret
+        }
+
+        override def getResource(name: String): URL  = {
+         val ret = super.getResource(name)
+         println("XXXX6 " + ret)
+         ret
+        }
+
+        override def getResources(name: String): util.Enumeration[URL]  = {
+         val ret = super.getResources(name)
+         println("XXXX7 " + ret)
+         ret
+        }
+
+        override def resources(name: String): stream.Stream[URL]  = {
+         val ret = super.resources(name)
+         println("XXXX8 " + ret)
+         ret
+        }
+
+        override def findResource(name: String): URL  = {
+         val ret = super.findResource(name)
+         println("XXXX9 " + ret)
+         ret
+        }
+
+        override def findResources(name: String): util.Enumeration[URL]  = {
+         val ret = super.findResources(name)
+         println("XXXX10 " + ret)
+         ret
+        }
+
+        override def getResourceAsStream(name: String): InputStream  = {
+         val ret = super.getResourceAsStream(name)
+         println("XXXX11 " + ret)
+         ret
+        }
+
+        override def loadClass(name: String, resolve: Boolean): Class[_] = {
+          val ret = super.loadClass(name, resolve)
+//          os.write.append(os.Path("/home/mp/Projects/shiftleft/joern/foo.txt"), s"$name\n")
+          println(s"XXXX loadClass resolve=$resolve $name=$ret")
+          ret
+        }
+      }
     }
 
+//    val scalacArgs0 = {
+//      val hppcJar = "/home/mp/.cache/coursier/v1/https/repo1.maven.org/maven2/com/carrotsearch/hppc/0.7.1/hppc-0.7.1.jar"
+//      val compilerArgs = Array.newBuilder[String]
+//      val dependencyFiles = Seq(Paths.get(hppcJar).toFile)
+//      compilerArgs ++= Array("-classpath", replClasspath(dependencyFiles))
+//      compilerArgs += "-explain" // verbose scalac error messages
+//      if (config.nocolors) compilerArgs ++= Array("-color", "never")
+//      compilerArgs.result()
+//    }
+
+    HackyGlobalState.classloader = classLoader
     val replDriver = new ReplDriver(
-//      compilerArgs(config),
-      scalacArgs0,
+      compilerArgs(config),
+//      scalacArgs0,
       onExitCode = Option(onExitCode),
       greeting = greeting,
       prompt = promptStr,
       maxPrintElements = Int.MaxValue,
-//      Option(classLoader)
+      Option(classLoader)
     )
 //    replDriver.addDependency(hppcJar)
 
@@ -289,26 +351,26 @@ trait ScriptExecution { this: BridgeBase =>
 
   // TODO test idea: if quit with C-c: restart with old state but new classloader - go via addDependency
     val state2 = replDriver.runUntilQuit(state)
-    println("running again...")
-    val scalacArgs1 = {
-      val versionSortJar = "/home/mp/.cache/coursier/v1/https/repo1.maven.org/maven2/com/michaelpollmeier/versionsort/1.0.7/versionsort-1.0.7.jar"
-      val compilerArgs = Array.newBuilder[String]
-      val dependencyFiles = Seq(Paths.get(versionSortJar).toFile)
-      compilerArgs ++= Array("-classpath", replClasspath(dependencyFiles))
-      compilerArgs += "-explain" // verbose scalac error messages
-      if (config.nocolors) compilerArgs ++= Array("-color", "never")
-      compilerArgs.result()
-    }
-    val replDriver2 = new ReplDriver(
-      scalacArgs1,
-      onExitCode = Option(onExitCode),
-      greeting = greeting,
-      prompt = promptStr,
-      maxPrintElements = Int.MaxValue,
-//      Option(classLoader)
-    )
-//    replDriver2.addDependency(versionSortJar)
-    replDriver2.runUntilQuit(state2)
+//    println("running again...")
+//    val scalacArgs1 = {
+//      val versionSortJar = "/home/mp/.cache/coursier/v1/https/repo1.maven.org/maven2/com/michaelpollmeier/versionsort/1.0.7/versionsort-1.0.7.jar"
+//      val compilerArgs = Array.newBuilder[String]
+//      val dependencyFiles = Seq(Paths.get(versionSortJar).toFile)
+//      compilerArgs ++= Array("-classpath", replClasspath(dependencyFiles))
+//      compilerArgs += "-explain" // verbose scalac error messages
+//      if (config.nocolors) compilerArgs ++= Array("-color", "never")
+//      compilerArgs.result()
+//    }
+//    val replDriver2 = new ReplDriver(
+//      scalacArgs1,
+//      onExitCode = Option(onExitCode),
+//      greeting = greeting,
+//      prompt = promptStr,
+//      maxPrintElements = Int.MaxValue,
+////      Option(classLoader)
+//    )
+////    replDriver2.addDependency(versionSortJar)
+//    replDriver2.runUntilQuit(state2)
   }
 
   protected def runScript(scriptFile: os.Path, config: Config): Unit = {
