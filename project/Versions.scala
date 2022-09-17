b4829631e0065c3e64f4a40a11ec2e9db78f5df7
Scala Steward <scala_steward@virtuslab.com>
2022-09-17 18:27:29 +0000

    Update log4j-slf4j-impl to 2.19.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,7 +6,7 @@ object Versions {
   val antlr         = "4.7"
   val scalatest     = "3.2.12"
   val cats          = "3.3.14"
-  val log4j         = "2.18.0"
+  val log4j         = "2.19.0"
   val json4s        = "4.0.5"
   val gradleTooling = "7.5.1"
 
