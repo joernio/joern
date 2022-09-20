229319b755936b7324b86a8efac6b6b4c370f6f7
Michael Pollmeier <michael@michaelpollmeier.com>
2022-09-20 15:19:48 +0200

    back to workaround: dynamic `using` works, but resets the session

--- console/src/main/scala/io/joern/console/ReplDriver.scala
+++ console/src/main/scala/io/joern/console/ReplDriver.scala
@@ -1,12 +1,10 @@
 package io.joern.console
 
-import dotty.tools.MainGenericCompiler.classpathSeparator
-import dotty.tools.dotc.core.Comments.{ContextDoc, ContextDocstrings}
-import dotty.tools.dotc.core.Contexts.{Context, ContextBase, ContextState, FreshContext, ctx}
+import dotty.tools.dotc.core.Contexts.{Context, ContextBase}
 import dotty.tools.dotc.ast.{Positioned, tpd, untpd}
 import dotty.tools.dotc.classpath.{AggregateClassPath, ClassPathFactory}
 import dotty.tools.dotc.config.{Feature, JavaPlatform, Platform}
-import dotty.tools.dotc.core.{Contexts, MacroClassLoader, Mode, TyperState}
+import dotty.tools.dotc.core.{Contexts, MacroClassLoader, Mode}
 import dotty.tools.io.{AbstractFile, ClassPath, ClassRepresentation}
 import dotty.tools.repl.{CollectTopLevelImports, Newline, ParseResult, Parsed, Quit, State}
 
@@ -14,7 +12,6 @@ import java.io.PrintStream
 import org.jline.reader.*
 
 import java.net.URL
-import javax.naming.InitialContext
 import scala.annotation.tailrec
 import scala.collection.mutable
 import scala.jdk.CollectionConverters.*
@@ -46,10 +43,8 @@ class ReplDriver(args: Array[String],
 //    val base: ContextBase = ctx.base
     val base: ContextBase = new ContextBase {
       override def newPlatform(using Context): Platform = {
-        println(s"initCtx: creating new platform; calledUsing=${HackyGlobalState.calledUsing}")
         val jp = new JavaPlatform {
           override def classPath(using Context): ClassPath = {
-            println(s"initial javaplatform -> classpath; calledUsing=${HackyGlobalState.calledUsing}")
 //            val oldScope = ctx.scope // always empty
 //            println(s"XXXX5 newPlatform.oldScope: $oldScope ${oldScope.size}")
             val original = super.classPath
@@ -62,18 +57,10 @@ class ReplDriver(args: Array[String],
 //            println(s"YYY1 new aggregate classpath; calledUsing=${HackyGlobalState.calledUsing}")
             val cpResult = if (HackyGlobalState.calledUsing) Seq(original, versionSortClassPath) else Seq(original)
 
-//            val cp = new AggregateClassPath(cpResult)
+            val cp = new AggregateClassPath(cpResult)
 //            println(s"YYY3 JavaPlatform.classpath called; calledUsing=${HackyGlobalState.calledUsing}")
-//            HackyGlobalState.initialCp = cp
-//            HackyGlobalState.initialCp
-            new AggregateClassPath(cpResult) {
-              override def list(inPackage: String) = {
-                println("XXXX8 calling `classpath.list`")
-//                if (HackyGlobalState.calledUsing) throw new AssertionError("boom")
-//                else super.list(inPackage)
-                super.list(inPackage)
-              }
-            }
+            HackyGlobalState.initialCp = cp
+            HackyGlobalState.initialCp
           }
         }
         HackyGlobalState.jp = jp
@@ -98,21 +85,7 @@ class ReplDriver(args: Array[String],
 //    println(s"XXXXX classloader: ${rendering.myClassLoader.findClass("foobar")}")
 
     /** Blockingly read a line, getting back a parse result */
-    def readLine(state0: State): ParseResult = {
-      val state =
-        if (HackyGlobalState.calledUsing) {
-          println("XXX7 fiddling with state")
-          val versionSortJar = "/home/mp/.cache/coursier/v1/https/repo1.maven.org/maven2/com/michaelpollmeier/versionsort/1.0.7/versionsort-1.0.7.jar"
-          val oldCtx = state0.context
-//          val newCp = s"${oldCtx.settings.classpath.value}$versionSortJar$classpathSeparator"
-//          val ctx1 = oldCtx.fresh.setSetting(oldCtx.settings.classpath, newCp)
-          val ctx1 = oldCtx.fresh.setSetting(oldCtx.settings.classpath, versionSortJar)
-          val ctx2 = ctx1.setNewScope.setNewTyperState()
-//          ctx2.typerState.fresh()
-          val newState = state0.copy(context = ctx2)
-//          ctx1.initialize()(using ctx1)
-          newState
-        } else state0
+    def readLine(state: State): ParseResult = {
       val completer: Completer = { (_, line, candidates) =>
         val comps = completions(line.cursor, line.line, state)
         candidates.addAll(comps.asJava)
@@ -140,7 +113,7 @@ class ReplDriver(args: Array[String],
 //          rootCtx = initCtx
 
 //          rootCtx = initialCtx(Nil)
-//          val settings = args
+          val settings = args
 //          val newRootCtx = initCtx//.fresh//.addMode(Mode.ReadPositions | Mode.Interactive)
 //          newRootCtx.setSetting(newRootCtx.settings.YcookComments, true)
 //          newRootCtx.setSetting(newRootCtx.settings.YreadComments, true)
@@ -158,29 +131,25 @@ class ReplDriver(args: Array[String],
           // happens in `setup - drill in there...
 //          rootCtx = setup(settings, newRootCtx).get._2
 
-//          def setup(args: Array[String], rootCtx: Context): Context = {
-//            val oldScope = rootCtx.scope // empty scope?
-//            println(s"XXXX3 oldScope: $oldScope ${oldScope.size}") // always empty... look elsewhere
-//
-//            val ictx = rootCtx
-//            val cmdDistill = command.distill(args, ictx.settings)(ictx.settingsState)(using ictx)
-//            ictx
-//          }
+          import dotty.tools.dotc.core.Comments.{ContextDoc, ContextDocstrings}
+          import Contexts.ctx
+          // TODO dive deeper here - currently still reproduces - simplify further...
+          def setup(args: Array[String], rootCtx: Context): Context = {
+            val oldScope = rootCtx.scope // empty scope?
+            println(s"XXXX3 oldScope: $oldScope ${oldScope.size}") // always empty... look elsewhere
+
+            val ictx = rootCtx
+//            val ictx = rootCtx.fresh
+            val cmdDistill = command.distill(args, ictx.settings)(ictx.settingsState)(using ictx)
+            ictx
+          }
 //          val newRootCtx = this.initCtx
-
-            // no difference...
-//            val freshContext: FreshContext = rootCtx.asInstanceOf[FreshContext]
-//          freshContext.typerState.fresh()
-//          freshContext.setDebug
-
-          rootCtx = {
-            val oldCtx: FreshContext = rootCtx.asInstanceOf[FreshContext]
-            val baseCtx: ContextBase = new ContextBase {
-              override val initialCtx: Context = oldCtx
+          val newRootCtx = {
+            val ctx = super.initCtx
+            val base: ContextBase = new ContextBase {
               override def newPlatform(using Context): Platform = {
                 new JavaPlatform {
                   override def classPath(using Context): ClassPath = {
-                    println("new javaplatform -> classpath")
                     val original = super.classPath
                     val versionSortJar = "/home/mp/.cache/coursier/v1/https/repo1.maven.org/maven2/com/michaelpollmeier/versionsort/1.0.7/versionsort-1.0.7.jar"
                     val versionSortClassPath = ClassPathFactory.newClassPath(AbstractFile.getFile(versionSortJar))
@@ -191,35 +160,9 @@ class ReplDriver(args: Array[String],
                 }
               }
             }
-//
-//            // TODO how can i connect the two? idea: create a new FreshContext, copy (almost) everything over
-//            oldCtx.withTyperState(TyperState.initialState())
-//            // rootCtx.fresh // maintains history, but doesn't use new platform
-//            val newRootCtx = new Contexts.InitialContext(baseCtx, rootCtx.settings) // works, but loses history
-//            command.distill(args, newRootCtx.settings)(newRootCtx.settingsState)(using newRootCtx)
-//            newRootCtx
-
-//            oldCtx.base.reset()
-//            oldCtx
-//              new FreshContext(baseCtx)
-//              println(s"XXX5 cp=${oldCtx.settings.classpath.value}")
-            val versionSortJar = "/home/mp/.cache/coursier/v1/https/repo1.maven.org/maven2/com/michaelpollmeier/versionsort/1.0.7/versionsort-1.0.7.jar"
-//              val newCtx = oldCtx.setSetting(oldCtx.settings.classpath, s"${oldCtx.settings.classpath.value}$classpathSeparator$versionSortJar")
-//oldCtx.settings.classpath.updateIn()
-            // TODO call `fromTastySetup(additionalFiles)
-            val ctx1 = oldCtx.fresh
-            val newCp = s"${oldCtx.settings.classpath.value}$versionSortJar$classpathSeparator"
-//            ctx1.settings.classpath.updateIn(ctx1.settings, versionSortJar)
-            val ctx2 = ctx1.setSetting(ctx1.settings.classpath, newCp)
-            //            val newCtx = oldCtx.fresh.setSetting(oldCtx.settings.classpath, s"${oldCtx.settings.classpath.value}$classpathSeparator$versionSortJar")
-            println(s"XXX6 new cp=${ctx2.settings.classpath.value}")
-            println(s"XXX7 settingsState: ${ctx2.settingsState}")
-            ctx2
-//            println(s"oldRootCtx class=${rootCtx.getClass}") // FreshContext
-//            println(s"newRootCtx class=${newRootCtx.getClass}") //InitialContext
-//            newRootCtx.freshOver(rootCtx) // no workie
-//            rootCtx.freshOver(newRootCtx) // doesn't work either...
+            new Contexts.InitialContext(base, ctx.settings)
           }
+          rootCtx = setup(settings, newRootCtx)
 
           rendering.myClassLoader = null
 
