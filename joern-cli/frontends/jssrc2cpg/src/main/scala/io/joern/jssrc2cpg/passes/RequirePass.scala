88eec3051a254ae2d18b32a91dc6ae6cf581a249
Fabian Yamaguchi <fabs@shiftleft.io>
2022-10-05 19:56:28 +0200

    jssrc2cpg: handle calls to methods imported via `import` (#1804)
    * Work on `import` statement
    
    * More work on `import` statements
    
    * Works
    
    * Remove unused import
    
    * Make test work with scala3

--- joern-cli/frontends/jssrc2cpg/src/main/scala/io/joern/jssrc2cpg/passes/RequirePass.scala
+++ joern-cli/frontends/jssrc2cpg/src/main/scala/io/joern/jssrc2cpg/passes/RequirePass.scala
@@ -12,38 +12,85 @@ import java.nio.file.Paths
 
 class RequirePass(cpg: Cpg) extends SimpleCpgPass(cpg) {
 
-  private val codeRoot                = cpg.metaData.root.headOption.getOrElse("")
-  private val moduleExportAssignments = cpg.assignment.where(_.target.codeExact("module.exports")).l
-  private val fileNameToMethodFullName = Traversal(moduleExportAssignments).source.isMethodRef.flatMap { ref =>
-    ref.file.name.headOption.map(x => (Paths.get(codeRoot, x).toAbsolutePath.normalize.toString -> ref.methodFullName))
-  }.toMap
+  private val codeRoot = cpg.metaData.root.headOption.getOrElse("")
+
+  /** A map from file names to method full names based on assignments to `module.exports`.
+    */
+  val fileNameToMethodFullName: Map[String, String] = {
+    val moduleExportAssignments = cpg.assignment.where(_.target.codeExact("module.exports")).l
+    Traversal(moduleExportAssignments).source.isMethodRef.flatMap { ref =>
+      ref.file.name.headOption.map { x =>
+        val filename = fileToAbsolutePath(x)
+        filename -> ref.methodFullName
+      }
+    }.toMap
+  }
+
+  /** A map from (filename, symbol) pairs to method full names based on `export` statements.
+    */
+  val exportTable: Map[(String, String), String] = {
+    val assignments = cpg.methodRef
+      .where(_.method.fullName(".*::program"))
+      .inAssignment
+    val pairs =
+      for {
+        assignment     <- assignments
+        identifier     <- Traversal(assignment).target.isIdentifier.name.l
+        filename       <- Traversal(assignment).file.name.map(fileToAbsolutePath).l
+        methodFullName <- Traversal(assignment).source.isMethodRef.methodFullName.l
+      } yield ((filename, identifier) -> methodFullName)
+    pairs.toMap
+  }
+
+  private def fileToAbsolutePath(file: String) =
+    Paths.get(codeRoot, file).toAbsolutePath.normalize.toString
 
   case class Require(call: Call) {
+
+    val symbol: String = call.astParent.fieldAccess.fieldIdentifier.canonicalName.headOption.getOrElse("")
+
     val dirHoldingModule: String = call.file.name.headOption
       .map(x => Paths.get(codeRoot, x).getParent.toAbsolutePath.normalize.toString)
       .getOrElse("")
+
     val fileToInclude: String = call
       .argument(1)
       .start
       .isLiteral
       .code
       .map(stripQuotes)
-      .map { x => Paths.get(dirHoldingModule, x).toAbsolutePath.normalize.toString + ".js" }
+      .map { x =>
+        val path = Paths.get(dirHoldingModule, x).toAbsolutePath.normalize.toString
+        if (path.endsWith(".mjs")) {
+          path
+        } else {
+          path + ".js"
+        }
+      }
       .headOption
       .getOrElse("")
-    val methodFullName: Option[String] = fileNameToMethodFullName.get(fileToInclude)
+
+    val methodFullName: Option[String] = {
+      if (symbol == "") {
+        fileNameToMethodFullName.get(fileToInclude)
+      } else {
+        exportTable.get((fileToInclude, symbol))
+      }
+    }
+
+    val target: String = call.inAssignment.target.code.head
+
+    val nodesToPatch: List[Call] = call.file.method.ast.isCall.nameExact(target).dedup.l
+
   }
 
   override def run(diffGraph: BatchedUpdate.DiffGraphBuilder): Unit = {
-    cpg.call("require").map(Require).foreach { require =>
+    cpg.call("require").where(_.inAssignment.target).map(Require).foreach { require =>
       require.methodFullName.foreach { fullName =>
         cpg.method.fullNameExact(fullName).foreach { method =>
-          require.call.inAssignment.target.code.headOption.foreach { target =>
-            val nodesToPatch = require.call.file.method.ast.isCall.nameExact(target).dedup.l
-            nodesToPatch.foreach { node =>
-              diffGraph.setNodeProperty(node, "METHOD_FULL_NAME", fullName)
-              diffGraph.addEdge(node, method, EdgeTypes.CALL)
-            }
+          require.nodesToPatch.foreach { node =>
+            diffGraph.setNodeProperty(node, "METHOD_FULL_NAME", fullName)
+            diffGraph.addEdge(node, method, EdgeTypes.CALL)
           }
         }
       }
