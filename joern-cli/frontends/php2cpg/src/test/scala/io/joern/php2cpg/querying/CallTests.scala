8dc2f9ad00e2c36a6884b1547540f17ce67060fb
Johannes Coetzee <jcoetzee@shiftleft.io>
2022-10-13 13:49:40 +0200

    Add inline HTML testcase

--- joern-cli/frontends/php2cpg/src/test/scala/io/joern/php2cpg/querying/CallTests.scala
+++ joern-cli/frontends/php2cpg/src/test/scala/io/joern/php2cpg/querying/CallTests.scala
@@ -1,6 +1,7 @@
 package io.joern.php2cpg.querying
 
 import io.joern.php2cpg.astcreation.AstCreator.NameConstants
+import io.joern.php2cpg.parser.Domain.PhpBuiltins
 import io.joern.php2cpg.testfixtures.PhpCode2CpgFixture
 import io.shiftleft.semanticcpg.language._
 
@@ -14,6 +15,15 @@ class CallTests extends PhpCode2CpgFixture {
       haltCompiler.code shouldBe s"${NameConstants.HaltCompiler}()"
       haltCompiler.lineNumber shouldBe Some(2)
       haltCompiler.astChildren.size shouldBe 0
+    }
+  }
+
+  "inline HTML should be represented as an echo statement" in {
+    val cpg = code("TEST CODE PLEASE IGNORE")
+
+    inside(cpg.call.name(".*echo").l) { case List(echoCall) =>
+      echoCall.name shouldBe "echo"
+      echoCall.argument.code.l shouldBe List("\"TEST CODE PLEASE IGNORE\"")
     }
   }
 }
