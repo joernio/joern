4dc4db05d14a416bd5da9f2e6366e319ff4d4494
Fabian Yamaguchi <fabs@shiftleft.io>
2022-10-17 20:35:20 +0200

    Fix build on macos-11

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/types/structure/DependencyTraversal.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/types/structure/DependencyTraversal.scala
@@ -2,7 +2,7 @@ package io.shiftleft.semanticcpg.language.types.structure
 
 import io.shiftleft.codepropertygraph.generated.{EdgeTypes, nodes}
 import io.shiftleft.codepropertygraph.generated.nodes.Import
-import overflowdb.traversal.Traversal
+import overflowdb.traversal._
 
 class DependencyTraversal(val traversal: Traversal[nodes.Dependency]) extends AnyVal {
   def imports: Traversal[Import] = traversal.in(EdgeTypes.IMPORTS).cast[Import]
