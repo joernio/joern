4617fbf8bbb3338addad8ec8da67e2eed0b2ec2c
Michael Pollmeier <michael@michaelpollmeier.com>
2022-10-26 08:16:17 +0200

    add a test

---
+++ joern-cli/src/test/scala/io/joern/joerncli/JoernExportTests.scala
@@ -0,0 +1,22 @@
+package io.joern.joerncli
+
+import better.files.File
+import io.joern.joerncli.JoernExport.Representation
+import io.shiftleft.codepropertygraph.Cpg
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class JoernExportTests extends AnyWordSpec with Matchers with AbstractJoernCliTest {
+
+  "export to graphson, for example code 'testcode/free'" should withTestCpg(
+    File(getClass.getClassLoader.getResource("testcode/free"))
+  ) { case (cpg: Cpg, _) =>
+    "split output by method" in {
+      val tempDir = os.temp.dir(prefix = "joern-export-test")
+      JoernExport.export(cpg, Representation.SplitByMethod, JoernExport.Format.Graphson, tempDir.toNIO)
+      val exportedFiles = os.walk(tempDir).filter(_.toIO.isFile)
+      exportedFiles.size should be >= 100
+    }
+  }
+
+}
