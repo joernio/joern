dda24860a9a5a19751be70f80c83bf2804a6b7d7
Claudiu-Vlad Ursache <claudiu+hw@shiftleft.io>
2022-10-27 08:05:28 +0200

    kotlin2cpg: add dataflow test suite for generics (#1905)

---
+++ joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/dataflow/GenericsTests.scala
@@ -0,0 +1,39 @@
+package io.joern.kotlin2cpg.dataflow
+
+import io.joern.dataflowengineoss.language.toExtendedCfgNode
+import io.joern.kotlin2cpg.testfixtures.KotlinCode2CpgFixture
+import io.shiftleft.semanticcpg.language._
+
+class GenericsTests extends KotlinCode2CpgFixture(withOssDataflow = true) {
+  implicit val resolver: ICallResolver = NoResolve
+
+  "CPG for code with simple fn using type parameter with no upper bound" should {
+    val cpg = code("""
+        |fun main() = f1("AVALUE")
+        |fun <T> f1(p: T) = println(p)
+        |""".stripMargin)
+
+    "should find a flow the argument of the callsite to a call in the fn body" in {
+      val source = cpg.literal.code(".*AVALUE.*")
+      val sink   = cpg.call.methodFullName(".*println.*").argument
+      val flows  = sink.reachableByFlows(source)
+      flows.map(flowToResultPairs).toSet shouldBe
+        Set(List(("f1(\"AVALUE\")", Some(2)), ("f1(p)", Some(3)), ("println(p)", Some(3))))
+    }
+  }
+
+  "CPG for code with simple fn using type parameter with upper bound" should {
+    val cpg = code("""
+       |fun main() = f1(0.30000000000000004f)
+       |fun <T : kotlin.Number> f1(p: T) = println(p)
+       |""".stripMargin)
+
+    "should find a flow the argument of the callsite to a call in the fn body" in {
+      val source = cpg.literal.codeExact("0.30000000000000004f")
+      val sink   = cpg.call.methodFullName(".*println.*").argument
+      val flows  = sink.reachableByFlows(source)
+      flows.map(flowToResultPairs).toSet shouldBe
+        Set(List(("f1(0.30000000000000004f)", Some(2)), ("f1(p)", Some(3)), ("println(p)", Some(3))))
+    }
+  }
+}
