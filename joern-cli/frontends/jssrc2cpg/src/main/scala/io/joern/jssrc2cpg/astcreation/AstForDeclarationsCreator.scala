8508961dd62986e942903ad2d3be393701b650ae
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2022-10-27 13:32:20 +0200

    jssrc2cpg: fixed module export assignments

--- joern-cli/frontends/jssrc2cpg/src/main/scala/io/joern/jssrc2cpg/astcreation/AstForDeclarationsCreator.scala
+++ joern-cli/frontends/jssrc2cpg/src/main/scala/io/joern/jssrc2cpg/astcreation/AstForDeclarationsCreator.scala
@@ -94,7 +94,7 @@ trait AstForDeclarationsCreator { this: AstCreator =>
 
   private def extractExportFromNameFromExportDecl(declaration: BabelNodeInfo): String =
     safeObj(declaration.json, "source")
-      .map { d => s"_${code(d).stripPrefix("\"").stripSuffix("\"")}" }
+      .map { d => s"_${stripQuotes(code(d))}" }
       .getOrElse(EXPORT_KEYWORD)
 
   private def cleanImportName(name: String): String = if (name.contains("/")) {
@@ -218,7 +218,7 @@ trait AstForDeclarationsCreator { this: AstCreator =>
 
   protected def astForExportAllDeclaration(declaration: BabelNodeInfo): Ast = {
     val exportName = extractExportFromNameFromExportDecl(declaration)
-    val depGroupId = code(declaration.json("source")).stripPrefix("\"").stripSuffix("\"")
+    val depGroupId = stripQuotes(code(declaration.json("source")))
     val name       = cleanImportName(depGroupId)
     if (exportName != EXPORT_KEYWORD) {
       diffGraph.addNode(createDependencyNode(name, depGroupId, REQUIRE_KEYWORD))
@@ -229,7 +229,7 @@ trait AstForDeclarationsCreator { this: AstCreator =>
     Ast.storeInDiffGraph(exportCallAst, diffGraph)
     val assignmentCallAst = createExportAssignmentCallAst(s"_$name", exportCallAst, declaration)
 
-    val asts = List(fromCallAst, exportCallAst, assignmentCallAst)
+    val asts = List(fromCallAst, assignmentCallAst)
     setIndices(asts)
     blockAst(createBlockNode(declaration), asts)
   }
