3048fea64e56e3375fb940114c37b24cc2473ddd
Niko <niko@shiftleft.io>
2022-11-08 16:21:03 +0100

    SQLi init

---
+++ querydb/src/main/scala/io/joern/scanners/php/SQLInjection.scala
@@ -0,0 +1,39 @@
+package io.joern.scanners.php
+
+import io.joern.console._
+import io.joern.dataflowengineoss.language._
+import io.joern.dataflowengineoss.queryengine.EngineContext
+import io.joern.macros.QueryMacros._
+import io.joern.scanners._
+import io.shiftleft.codepropertygraph.generated.Operators
+import io.shiftleft.semanticcpg.language._
+
+object SQLInjection extends QueryBundle {
+
+  implicit val resolver: ICallResolver = NoResolve
+
+  @q
+  def shellExec()(implicit context: EngineContext): Query =
+    Query.make(
+      name = "sql-injection",
+      author = Crew.niko,
+      title = "SQL injection: A parameter is used in an insecure database API call.",
+      description = """
+          |An attacker controlled parameter is used in an insecure database API call.
+          |
+          |If the parameter is not validated and sanitized, this is a SQL injection.
+          |""".stripMargin,
+      score = 5,
+      withStrRep({ cpg =>
+        // $_REQUEST["foo"], $_GET["foo"], $_POST["foo"]
+        // are identifier (at the moment)
+        def source =
+          cpg.call.name(Operators.assignment).argument.code(".*_(REQUEST|GET|POST).*")
+
+        def sink = cpg.call.name("query").filter(_.receiver.nonEmpty).argument
+
+        sink.reachableBy(source).l
+      }),
+      tags = List(QueryTags.remoteCodeExecution, QueryTags.default)
+    )
+}
