ff7996e6cc771573b6db4a20ee27857c55b5afb0
Michael Pollmeier <michael@michaelpollmeier.com>
2022-11-24 16:43:36 +0100

    WIP add (failing) test to verify that two calls are part of the DDG
    I'm not the best person to judge wether those calls really _should_
    be part of the DDG, but from our discussion on
    [discord](https://discord.com/channels/832209896089976854/832209896089976857/1044687371800678420)
    it sounds like the answer is 'yes'.
    Anywhere, since I was asked about this I just added a (failing) test
    to hopefully make it easier to grok the context and take it from
    there.

--- joern-cli/frontends/c2cpg/src/test/scala/io/joern/c2cpg/io/dotgenerator/DotDdgGeneratorTests.scala
+++ joern-cli/frontends/c2cpg/src/test/scala/io/joern/c2cpg/io/dotgenerator/DotDdgGeneratorTests.scala
@@ -1,7 +1,9 @@
 package io.joern.c2cpg.io.dotgenerator
 
 import io.joern.c2cpg.testfixtures.DataFlowCodeToCpgSuite
+import io.joern.dataflowengineoss.dotgenerator.DdgGenerator
 import io.joern.dataflowengineoss.language._
+import io.shiftleft.codepropertygraph.generated.nodes.Call
 import io.shiftleft.semanticcpg.language._
 
 class DotDdgGeneratorTests extends DataFlowCodeToCpgSuite {
@@ -46,7 +48,48 @@ class DotDdgGeneratorTests extends DataFlowCodeToCpgSuite {
         }
       }
     }
+  }
 
+  "include DDG relationship between two calls" in {
+    val cpg = code("""
+      |static int mwifiex_pcie_delete_cmdrsp_buf(struct mwifiex_adapter *adapter)
+      |{
+      |    struct pcie_service_card *card;
+      |    card = adapter->card;
+      |
+      |    if (card && card->cmdrsp_buf) {
+      |        // the following two lines share the access to 'card->cmdrsp_buf' and should therefor appear in the DDG
+      |        dev_kfree_skb_any(card->cmdrsp_buf);
+      |        card->cmdrsp_buf = NULL;
+      |    }
+      |
+      |    return 0;
+      |}
+      |""".stripMargin)
+
+
+    val ddgGenerator = new DdgGenerator()
+    val method = cpg.method.name("mwifiex_pcie_delete_cmdrsp_buf").head
+    val ddg = ddgGenerator.generate(method)
+    val call1 = cpg.call.code("dev_kfree_skb_any\\(card->cmdrsp_buf\\)").head
+    val call2 = cpg.call.code("card->cmdrsp_buf = NULL").head
+
+    ddg.vertices should contain(method)
+    ddg.vertices should contain(call1)
+    ddg.vertices should contain(call2)
+
+    // TODO remove debug code
+    // according to our discussion on discord, the following suggests that we _should_ see that link
+    call1.ddgIn().cast[Call].foreach(call => println(call.code)) // 'card->cmdrsp_buf'
+    // however, I'm not so sure, since there's no ddg edge to the `card->cmdrsp_buf = NULL` call...
+    // TODO debug code end
+    
+    val call1ToCall2Edges = ddg.edges.filter { edge => edge.src == call1 && edge.dst == call2 }
+    val call2ToCall1Edges = ddg.edges.filter { edge => edge.src == call2 && edge.dst == call1 }
+
+    // WIP: one of the following should be true, at least ccording to our discussion on discord...
+    call1ToCall2Edges.nonEmpty shouldBe true
+    call2ToCall1Edges.nonEmpty shouldBe true
   }
 
 }
