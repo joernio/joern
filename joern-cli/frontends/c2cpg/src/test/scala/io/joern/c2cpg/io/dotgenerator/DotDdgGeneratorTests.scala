ca47bcf89d0067aa82cd948b738c85a1da6ea9b4
Bernhard <bernhard@shiftleft.io>
2022-11-25 10:59:30 +0100

    clarify the example

--- joern-cli/frontends/c2cpg/src/test/scala/io/joern/c2cpg/io/dotgenerator/DotDdgGeneratorTests.scala
+++ joern-cli/frontends/c2cpg/src/test/scala/io/joern/c2cpg/io/dotgenerator/DotDdgGeneratorTests.scala
@@ -3,7 +3,8 @@ package io.joern.c2cpg.io.dotgenerator
 import io.joern.c2cpg.testfixtures.DataFlowCodeToCpgSuite
 import io.joern.dataflowengineoss.dotgenerator.DdgGenerator
 import io.joern.dataflowengineoss.language._
-import io.shiftleft.codepropertygraph.generated.nodes.Call
+import io.shiftleft.codepropertygraph.Cpg
+import io.shiftleft.codepropertygraph.generated.nodes.{AstNode, Call, CfgNode, Method}
 import io.shiftleft.semanticcpg.language._
 
 class DotDdgGeneratorTests extends DataFlowCodeToCpgSuite {
@@ -50,46 +51,118 @@ class DotDdgGeneratorTests extends DataFlowCodeToCpgSuite {
     }
   }
 
-  "include DDG relationship between two calls" in {
-    val cpg = code("""
-      |static int mwifiex_pcie_delete_cmdrsp_buf(struct mwifiex_adapter *adapter)
-      |{
-      |    struct pcie_service_card *card;
-      |    card = adapter->card;
-      |
-      |    if (card && card->cmdrsp_buf) {
-      |        // the following two lines share the access to 'card->cmdrsp_buf' and should therefor appear in the DDG
-      |        dev_kfree_skb_any(card->cmdrsp_buf);
-      |        card->cmdrsp_buf = NULL;
-      |    }
-      |
-      |    return 0;
-      |}
-      |""".stripMargin)
+  def dumpDDg(method: Method): String = {
+    def printNode(n: AstNode): String = s"${n.label}:[${n.code}]@${n.lineNumber.getOrElse(-1)}"
 
+    method.ast.collectAll[CfgNode].map { n => (n, n.ddgIn.l) }.collect { case (node, ddgIn) if ddgIn.nonEmpty => printNode(node) + " <- " + ddgIn.map(printNode).mkString(", ") }.mkString("\n")
+  }
 
-    val ddgGenerator = new DdgGenerator()
-    val method = cpg.method.name("mwifiex_pcie_delete_cmdrsp_buf").head
-    val ddg = ddgGenerator.generate(method)
-    val call1 = cpg.call.code("dev_kfree_skb_any\\(card->cmdrsp_buf\\)").head
-    val call2 = cpg.call.code("card->cmdrsp_buf = NULL").head
+  "Compute dependencies according to Kuck et al 81 (Dependence Graphs and Compiler Optimizations)" should {
+    //cf https://dl.acm.org/doi/pdf/10.1145/567532.567555
+    // Note that this test is descriptive, not normative: It only documents current behavior and makes no judgement whether
+    // output, or anti, or even input dependencies should be included in the DDG.
+    "include flow-dependencies" in {
+      val cpg = code(
+        """
+          |int fun(int* p)
+          |{
+          | int tmp = *p;
+          | *p = tmp + 1;
+          | return 0;
+          |}
+          |""".stripMargin)
+      dumpDDg(cpg.method.name("fun").head) shouldBe
+        """CALL:[tmp = *p]@4 <- IDENTIFIER:[tmp]@4
+          |IDENTIFIER:[tmp]@4 <- CALL:[*p]@4
+          |IDENTIFIER:[p]@4 <- METHOD_PARAMETER_IN:[int* p]@2
+          |CALL:[*p = tmp + 1]@5 <- CALL:[*p]@5, IDENTIFIER:[tmp]@4
+          |CALL:[*p]@5 <- CALL:[tmp + 1]@5
+          |IDENTIFIER:[p]@5 <- METHOD_PARAMETER_IN:[int* p]@2
+          |CALL:[tmp + 1]@5 <- IDENTIFIER:[tmp]@4
+          |IDENTIFIER:[tmp]@5 <- IDENTIFIER:[tmp]@4
+          |RETURN:[return 0;]@6 <- LITERAL:[0]@6
+          |METHOD_RETURN:[int]@2 <- RETURN:[return 0;]@6, METHOD_PARAMETER_IN:[int* p]@2, CALL:[*p]@5, IDENTIFIER:[tmp]@5, CALL:[tmp + 1]@5
+          |METHOD_PARAMETER_OUT:[int* p]@2 <- METHOD_PARAMETER_IN:[int* p]@2, CALL:[*p]@5""".stripMargin
+      //we see that there is a ddgIn edge to the store (line 5) via identifier tmp from the load (line 4)
+    }
 
-    ddg.vertices should contain(method)
-    ddg.vertices should contain(call1)
-    ddg.vertices should contain(call2)
+    "Not include output-dependencies" in {
+      val cpg = code(
+        """
+          |int fun(int* p)
+          |{
+          | *p = 0;
+          | *p = 1;
+          | return 0;
+          |}
+          |""".stripMargin)
+      dumpDDg(cpg.method.name("fun").head) shouldBe
+      """CALL:[*p = 0]@4 <- CALL:[*p]@4
+        |CALL:[*p]@4 <- LITERAL:[0]@4
+        |IDENTIFIER:[p]@4 <- METHOD_PARAMETER_IN:[int* p]@2
+        |CALL:[*p = 1]@5 <- CALL:[*p]@5
+        |CALL:[*p]@5 <- LITERAL:[1]@5
+        |IDENTIFIER:[p]@5 <- METHOD_PARAMETER_IN:[int* p]@2, CALL:[*p]@4
+        |RETURN:[return 0;]@6 <- LITERAL:[0]@6
+        |METHOD_RETURN:[int]@2 <- RETURN:[return 0;]@6, METHOD_PARAMETER_IN:[int* p]@2, CALL:[*p]@5
+        |METHOD_PARAMETER_OUT:[int* p]@2 <- METHOD_PARAMETER_IN:[int* p]@2, CALL:[*p]@5""".stripMargin
+      //we see that there is no ddgIn edge to the second store (line 5) from the first (line 4)
+    }
 
-    // TODO remove debug code
-    // according to our discussion on discord, the following suggests that we _should_ see that link
-    call1.ddgIn().cast[Call].foreach(call => println(call.code)) // 'card->cmdrsp_buf'
-    // however, I'm not so sure, since there's no ddg edge to the `card->cmdrsp_buf = NULL` call...
-    // TODO debug code end
-    
-    val call1ToCall2Edges = ddg.edges.filter { edge => edge.src == call1 && edge.dst == call2 }
-    val call2ToCall1Edges = ddg.edges.filter { edge => edge.src == call2 && edge.dst == call1 }
-
-    // WIP: one of the following should be true, at least ccording to our discussion on discord...
-    call1ToCall2Edges.nonEmpty shouldBe true
-    call2ToCall1Edges.nonEmpty shouldBe true
+    "Not include anti-dependencies" in {
+      val cpg = code(
+        """
+          |int fun(int* p)
+          |{
+          | int tmp = *p;
+          | *p = 1;
+          | return tmp;
+          |}
+          |""".stripMargin)
+      dumpDDg(cpg.method.name("fun").head) shouldBe
+        """CALL:[tmp = *p]@4 <- IDENTIFIER:[tmp]@4
+          |IDENTIFIER:[tmp]@4 <- CALL:[*p]@4
+          |IDENTIFIER:[p]@4 <- METHOD_PARAMETER_IN:[int* p]@2
+          |CALL:[*p = 1]@5 <- CALL:[*p]@5
+          |CALL:[*p]@5 <- LITERAL:[1]@5
+          |IDENTIFIER:[p]@5 <- METHOD_PARAMETER_IN:[int* p]@2
+          |RETURN:[return tmp;]@6 <- IDENTIFIER:[tmp]@6, IDENTIFIER:[tmp]@4
+          |METHOD_RETURN:[int]@2 <- RETURN:[return tmp;]@6, METHOD_PARAMETER_IN:[int* p]@2, IDENTIFIER:[tmp]@4, CALL:[*p]@5
+          |METHOD_PARAMETER_OUT:[int* p]@2 <- METHOD_PARAMETER_IN:[int* p]@2, CALL:[*p]@5""".stripMargin
+      //we see that there is no ddgIn path to the store (line 5) from the load (line 4)
+    }
+    "Not include input dependencies" in {
+      val cpg = code(
+        """
+          |int fun(volatile int* p)
+          |{
+          | int tmp = *p;
+          | int tmp2 = *p;
+          | return tmp + tmp2;
+          |}
+          |""".stripMargin)
+      dumpDDg(cpg.method.name("fun").head) shouldBe
+        """CALL:[tmp = *p]@4 <- IDENTIFIER:[tmp]@4
+          |IDENTIFIER:[tmp]@4 <- CALL:[*p]@4
+          |IDENTIFIER:[p]@4 <- METHOD_PARAMETER_IN:[volatile int* p]@2
+          |CALL:[tmp2 = *p]@5 <- IDENTIFIER:[tmp2]@5
+          |IDENTIFIER:[tmp2]@5 <- CALL:[*p]@5
+          |IDENTIFIER:[p]@5 <- METHOD_PARAMETER_IN:[volatile int* p]@2
+          |RETURN:[return tmp + tmp2;]@6 <- CALL:[tmp + tmp2]@6
+          |CALL:[tmp + tmp2]@6 <- IDENTIFIER:[tmp]@4, IDENTIFIER:[tmp2]@5
+          |IDENTIFIER:[tmp]@6 <- IDENTIFIER:[tmp]@4
+          |IDENTIFIER:[tmp2]@6 <- IDENTIFIER:[tmp2]@5
+          |METHOD_RETURN:[int]@2 <- RETURN:[return tmp + tmp2;]@6, METHOD_PARAMETER_IN:[volatile int* p]@2, CALL:[*p]@5, IDENTIFIER:[tmp]@6, IDENTIFIER:[tmp2]@6, CALL:[tmp + tmp2]@6
+          |METHOD_PARAMETER_OUT:[volatile int* p]@2 <- METHOD_PARAMETER_IN:[volatile int* p]@2, CALL:[*p]@5""".stripMargin
+      //we see that there is no ddgIn path to the load (line 5) from the other load (line 4).
+      /*FIXME
+         Choose a more instructive example, volatile has too crazy semantics. But we want an example that also works if we
+         use an optimizing compiler and then use llvm2cpg or ghidra2cpg. This is conceptual and not about the C frontend.
+         Without volatile, an optimizing compiler would load from memory once and then re-use the register value.
+         With volatile, the two loads are IO operations and are ordered (i.e. one could argue that they are flow-dependent
+         via global state. Volatile reads can have side-effects! We are not modeling that in c2cpg, though).
+       */
+    }
   }
 
 }
