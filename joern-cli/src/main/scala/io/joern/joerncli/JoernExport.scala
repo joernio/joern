1c2e284c6ee55393aae9e2d70a5c605a563dbe12
Brandon Fergerson <bfergerson@apache.org>
2022-11-28 19:40:14 +0400

    Update JoernExport.scala

--- joern-cli/src/main/scala/io/joern/joerncli/JoernExport.scala
+++ joern-cli/src/main/scala/io/joern/joerncli/JoernExport.scala
@@ -101,10 +101,9 @@ object JoernExport extends App {
     val context = new LayerCreatorContext(cpg)
 
     format match {
+      case Format.Dot if representation == Representation.All || representation == Representation.Cpg =>
+        exportWithOdbFormat(cpg, representation, outDir, DotExporter)
       case Format.Dot =>
-        if (representation == Representation.All || representation == Representation.Cpg)
-          exportWithOdbFormat(cpg, representation, outDir, DotExporter)
-        else
           exportDot(representation, outDir, context)
       case Format.Neo4jCsv =>
         exportWithOdbFormat(cpg, representation, outDir, Neo4jCsvExporter)
