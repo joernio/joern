bf991657c79597a215c0510e9b8d72c168cee8d3
Claudiu-Vlad Ursache <claudiu@shiftleft.io>
2022-11-29 16:20:31 +0100

    Remove unnecessary `val`

--- joern-cli/frontends/kotlin2cpg/src/main/scala/io/joern/kotlin2cpg/Kotlin2Cpg.scala
+++ joern-cli/frontends/kotlin2cpg/src/main/scala/io/joern/kotlin2cpg/Kotlin2Cpg.scala
@@ -138,9 +138,8 @@ class Kotlin2Cpg extends X2CpgFrontend[Config] {
       if (config.includeJavaSourceFiles && filesWithJavaExtension.nonEmpty) {
         val javaAstCreator = JavasrcInterop.astCreationPass(filesWithJavaExtension, cpg)
         javaAstCreator.createAndApply()
-        val javaAstCreatorTypes                           = javaAstCreator.global.usedTypes.keys().asScala.toList
-        val javaAstCreatorTypesMinusKotlinAstCreatorTypes = javaAstCreatorTypes.toSet -- kotlinAstCreatorTypes.toSet
-        new TypeNodePass(javaAstCreatorTypesMinusKotlinAstCreatorTypes.toList, cpg).createAndApply()
+        val javaAstCreatorTypes = javaAstCreator.global.usedTypes.keys().asScala.toList
+        new TypeNodePass((javaAstCreatorTypes.toSet -- kotlinAstCreatorTypes.toSet).toList, cpg).createAndApply()
       }
 
       val configCreator = new ConfigPass(configFiles, cpg)
