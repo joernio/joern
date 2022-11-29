b36fda2d4c385ac605690f1686c978d37c56c94d
Claudiu-Vlad Ursache <claudiu+hw@shiftleft.io>
2022-11-29 16:58:30 +0100

    kotlin2cpg: deduplicate types for Java interop (#2037)
    * kotlin2cpg: deduplicate types for Java interop
    
    * Remove unnecessary `val`

--- joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/compiler/JavaInteroperabilityTests.scala
+++ joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/compiler/JavaInteroperabilityTests.scala
@@ -7,6 +7,7 @@ class JavaInteroperabilityTests extends KotlinCode2CpgFixture(withOssDataflow = 
   "CPG for code with Java interop" should {
     val cpg = code(
       """package no.such.pkg
+        |fun someFun(x: String)
         |fun main() {
         |  val c = SomeJavaClass()
         |  c.someFunction("greetings")
@@ -33,6 +34,10 @@ class JavaInteroperabilityTests extends KotlinCode2CpgFixture(withOssDataflow = 
     "should contain a CALL node for the call found inside the file written in Java" in {
       val List(c) = cpg.call.codeExact("System.out.println(text)").l
       c.methodFullName shouldBe "java.io.PrintStream.println:void(java.lang.String)"
+    }
+
+    "should deduplicate types if found in both Java and Kotlin code" in {
+      cpg.typ.fullNameExact("java.lang.String").size shouldBe 1
     }
   }
 }
