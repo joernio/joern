b693dcfd6740c0ddeae4b2fb0b826f77af10d2d8
Fabian Yamaguchi <fabs@joern.io>
2022-12-08 16:49:39 +0100

    MethodStubCreator: add 0-parameters for dynamic calls

--- joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/passes/base/MethodStubCreator.scala
+++ joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/passes/base/MethodStubCreator.scala
@@ -3,7 +3,7 @@ package io.joern.x2cpg.passes.base
 import io.joern.x2cpg.Defines
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.codepropertygraph.generated.nodes._
-import io.shiftleft.codepropertygraph.generated.{EdgeTypes, EvaluationStrategies, NodeTypes}
+import io.shiftleft.codepropertygraph.generated.{DispatchTypes, EdgeTypes, EvaluationStrategies, NodeTypes}
 import io.shiftleft.passes.CpgPass
 import io.shiftleft.semanticcpg.language._
 import overflowdb.BatchedUpdate
@@ -11,7 +11,7 @@ import overflowdb.BatchedUpdate
 import scala.collection.mutable
 import scala.util.Try
 
-case class NameAndSignature(name: String, signature: String, fullName: String)
+case class CallSummary(name: String, signature: String, fullName: String, dispatchType: String)
 
 /** This pass has no other pass as prerequisite.
   */
@@ -20,7 +20,7 @@ class MethodStubCreator(cpg: Cpg) extends CpgPass(cpg) {
   // Since the method fullNames for fuzzyc are not unique, we do not have
   // a 1to1 relation and may overwrite some values. This is ok for now.
   private val methodFullNameToNode   = mutable.LinkedHashMap[String, Method]()
-  private val methodToParameterCount = mutable.LinkedHashMap[NameAndSignature, Int]()
+  private val methodToParameterCount = mutable.LinkedHashMap[CallSummary, Int]()
 
   override def run(dstGraph: BatchedUpdate.DiffGraphBuilder): Unit = {
     for (method <- cpg.method) {
@@ -28,14 +28,17 @@ class MethodStubCreator(cpg: Cpg) extends CpgPass(cpg) {
     }
 
     for (call <- cpg.call if call.methodFullName != Defines.DynamicCallUnknownFallName) {
-      methodToParameterCount.put(NameAndSignature(call.name, call.signature, call.methodFullName), call.argument.size)
+      methodToParameterCount.put(
+        CallSummary(call.name, call.signature, call.methodFullName, call.dispatchType),
+        call.argument.size
+      )
     }
 
     for (
-      (NameAndSignature(name, signature, fullName), parameterCount) <- methodToParameterCount
+      (CallSummary(name, signature, fullName, dispatchType), parameterCount) <- methodToParameterCount
       if !methodFullNameToNode.contains(fullName)
     ) {
-      createMethodStub(name, fullName, signature, parameterCount, dstGraph)
+      createMethodStub(name, fullName, signature, dispatchType, parameterCount, dstGraph)
     }
   }
 
@@ -64,6 +67,7 @@ class MethodStubCreator(cpg: Cpg) extends CpgPass(cpg) {
     name: String,
     fullName: String,
     signature: String,
+    dispatchType: String,
     parameterCount: Int,
     dstGraph: DiffGraphBuilder
   ): NewMethod = {
@@ -81,7 +85,14 @@ class MethodStubCreator(cpg: Cpg) extends CpgPass(cpg) {
 
     dstGraph.addNode(methodNode)
 
-    (1 to parameterCount).foreach { parameterOrder =>
+    val firstParameterIndex = dispatchType match {
+      case DispatchTypes.DYNAMIC_DISPATCH =>
+        0
+      case _ =>
+        1
+    }
+
+    (firstParameterIndex to parameterCount).foreach { parameterOrder =>
       val nameAndCode = s"p$parameterOrder"
       val param = NewMethodParameterIn()
         .code(nameAndCode)
