7a8600428e4d80af3cfad3308f30e101b27c4cc1
Fabian Yamaguchi <fabs@joern.io>
2023-01-18 11:59:05 +0200

    Make it compile

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/types/structure/ImportTraversal.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/types/structure/ImportTraversal.scala
@@ -3,10 +3,15 @@ package io.shiftleft.semanticcpg.language.types.structure
 import io.shiftleft.codepropertygraph.generated.EdgeTypes
 import io.shiftleft.codepropertygraph.generated.nodes.{Call, Import, NamespaceBlock}
 import overflowdb.traversal._
+import io.shiftleft.semanticcpg.language._
 
 class ImportTraversal(val traversal: Traversal[Import]) extends AnyVal {
 
   def call : Traversal[Call] = traversal.in(EdgeTypes.IS_CALL_FOR_IMPORT).cast[Call]
+
+  def code: Traversal[String] = call.code
+
+  def code(regex : String): Traversal[Import] = traversal.where(_.call.code(regex))
 
   def namespaceBlock: Traversal[NamespaceBlock] =
     call.flatMap(_.astIn).collectAll[NamespaceBlock]
