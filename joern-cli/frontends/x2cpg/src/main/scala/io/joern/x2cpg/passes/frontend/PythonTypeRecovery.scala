d317062813488467ae307d9ca386ea3e18022215
David Baker Effendi <dbe@sun.ac.za>
2023-01-19 11:06:11 +0200

    Changed granularity of type recovery to be more memory efficient and work per compilation unit

--- joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/passes/frontend/PythonTypeRecovery.scala
+++ joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/passes/frontend/PythonTypeRecovery.scala
@@ -6,8 +6,10 @@ import io.shiftleft.codepropertygraph.generated.nodes._
 import io.shiftleft.codepropertygraph.generated.{Operators, PropertyNames}
 import io.shiftleft.passes.CpgPass
 import io.shiftleft.semanticcpg.language._
+import io.shiftleft.semanticcpg.language.operatorextension.OpNodes
 import io.shiftleft.semanticcpg.language.operatorextension.OpNodes.Assignment
 import org.apache.logging.log4j.LogManager
+import overflowdb.traversal.Traversal
 
 import java.io.{File => JFile}
 import java.util.concurrent.RecursiveTask
@@ -19,49 +21,52 @@ import scala.util.Try
 /** Based on a flow-insensitive symbol-table-style approach. This does not accurately determine the difference between
   * shadowed variables in the same file but this is due to REF edges not connecting children methods to parent scope
   * (yet).
-  *
-  * TODO: Currently this just improves the type recovery of imported function and types.
   */
 class PythonTypeRecovery(cpg: Cpg) extends CpgPass(cpg) {
 
-  private val symbolTable = new SymbolTable()
+  private val globalTable = new SymbolTable()
   private val logger      = LogManager.getLogger(classOf[PythonTypeRecovery])
 
   override def run(builder: DiffGraphBuilder): Unit =
     try {
-      // Set aliases that point to imports for local and external methods/modules
-      def importCalls = cpg.call.nameExact("import")
-
-      def internalMethods = cpg.method.isExternal(false)
-
-      (importCalls ++ internalMethods).map(f => new SetImportTask(f).fork()).foreach(_.get())
+      // Set known aliases that point to imports for local and external methods/modules
+      setImportsFromDeclaredProcedures(importNodes ++ internalMethodNodes)
       // Prune import names if the methods exist in the CPG
-      pruneImports()
+      postVisitImports()
       // Find identifiers that have been declared with some literal or function known in the symbol table
-      cpg.assignment.map(a => new SetDeclaredIdentifiersTask(a).fork()).foreach(_.get())
-      // Persist findings
-      symbolTable.view
-        .map { case (varDecl, typeHints) => new PersistRecoveredTypeTask(builder, varDecl, typeHints).fork() }
-        .foreach(_.get())
+      cpg.file.map(unit => new RecoverForCompilationUnit(unit, builder).fork()).foreach(_.get())
     } finally {
-      symbolTable.clear
+      globalTable.clear()
     }
+
+  /** Using import information and internally defined procedures, will generate a mapping between how functions and
+    * types are aliased and called and themselves.
+    *
+    * @param procedureDeclarations
+    *   imports to types or functions and internally defined methods themselves.
+    */
+  private def setImportsFromDeclaredProcedures(procedureDeclarations: Traversal[CfgNode]): Unit =
+    procedureDeclarations.map(f => new SetImportTask(f).fork()).foreach(_.get())
+
+  private def importNodes: Traversal[CfgNode] = cpg.call.nameExact("import")
+
+  private def internalMethodNodes: Traversal[Method] = cpg.method.isExternal(false)
 
   /** The initial import setting is over-approximated, so this step checks the CPG for any matches and prunes against
     * these findings. If there are no findings, it will leave the table as is. The latter is significant for external
     * types or methods.
     */
-  private def pruneImports(): Unit = {
-    symbolTable.view.foreach { case (k, v) =>
+  private def postVisitImports(): Unit = {
+    globalTable.view.foreach { case (k, v) =>
       val ms = cpg.method.fullNameExact(v.toSeq: _*).l
       val ts = cpg.typeDecl.fullNameExact(v.toSeq: _*).l
       if (ts.nonEmpty)
-        symbolTable.put(k, ts.fullName.toSet)
+        globalTable.put(k, ts.fullName.toSet)
       else if (ms.nonEmpty)
-        symbolTable.put(k, ms.fullName.toSet)
+        globalTable.put(k, ms.fullName.toSet)
       else {
         // This is likely external and we will ignore the init variant to be consistent
-        symbolTable.put(k, symbolTable(k).filterNot(_.contains("__init__.py")))
+        globalTable.put(k, globalTable(k).filterNot(_.contains("__init__.py")))
       }
     }
   }
@@ -71,10 +76,11 @@ class PythonTypeRecovery(cpg: Cpg) extends CpgPass(cpg) {
     *   a node that references import information.
     */
   class SetImportTask(node: CfgNode) extends RecursiveTask[Unit] {
+
     override def compute(): Unit = {
       node match {
-        case x: Method => setImports(x)
-        case x: Call   => setImports(x)
+        case x: Method => visitImport(x)
+        case x: Call   => visitImport(x)
       }
     }
 
@@ -83,15 +89,15 @@ class PythonTypeRecovery(cpg: Cpg) extends CpgPass(cpg) {
       * @param importCall
       *   the call that imports entities into this scope.
       */
-    private def setImports(importCall: Call): Unit = {
+    private def visitImport(importCall: Call): Unit = {
       importCall.argumentOut.l match {
         case List(path: Literal, funcOrModule: Literal) =>
           val calleeNames = extractMethodDetailsFromImport(path.code, funcOrModule.code).possibleCalleeNames
-          symbolTable.put(funcOrModule, calleeNames)
+          globalTable.put(funcOrModule, calleeNames)
         case List(path: Literal, funcOrModule: Literal, alias: Literal) =>
           val calleeNames =
             extractMethodDetailsFromImport(path.code, funcOrModule.code, Option(alias.code)).possibleCalleeNames
-          symbolTable.put(alias, calleeNames)
+          globalTable.put(alias, calleeNames)
         case x => logger.warn(s"Unknown import pattern: ${x.map(_.label).mkString(", ")}")
       }
     }
@@ -142,22 +148,49 @@ class PythonTypeRecovery(cpg: Cpg) extends CpgPass(cpg) {
       * @param m
       *   an internal method
       */
-    private def setImports(m: Method): Unit = {
+    private def visitImport(m: Method): Unit = {
       val calleeNames = ProcedureInScope(m.name, m.fullName).possibleCalleeNames
-      symbolTable.put(m, calleeNames)
+      globalTable.put(m, calleeNames)
     }
   }
 
-  /** Using assignment and import information (in the symbol table), will propagate these types in the symbol table.
-    * @param assignment
-    *   assignment call pointer.
+  /** Performs type recovery from the root of a compilation unit level
+    * @param cu
+    *   a compilation unit, e.g. file, procedure, type, etc.
+    * @param builder
+    *   the graph builder
     */
-  class SetDeclaredIdentifiersTask(assignment: Assignment) extends RecursiveTask[Unit] {
-    override def compute(): Unit = {
+  class RecoverForCompilationUnit(cu: AstNode, builder: DiffGraphBuilder) extends RecursiveTask[Unit] {
+
+    private val symbolTable = new SymbolTable()
+
+    private def assignments: Traversal[Assignment] =
+      cu.ast.isCall.name(Operators.assignment).map(new OpNodes.Assignment(_))
+
+    override def compute(): Unit = try {
+      // Conservatively populate local table with interprocedural knowledge
+      // TODO: Only populate global knowledge that is in scope, i.e. imported
+      symbolTable.from(globalTable)
+      // Populate local symbol table with assignments
+      assignments.foreach(visitAssignments)
+      symbolTable.view.foreach { case (k, v) => println(k.idName, v) }
+      // Persist findings
+      setTypeInformation()
+    } finally {
+      symbolTable.clear()
+    }
+
+    /** Using assignment and import information (in the global symbol table), will propagate these types in the symbol
+      * table.
+      *
+      * @param assignment
+      *   assignment call pointer.
+      */
+    private def visitAssignments(assignment: Assignment): Unit = {
       // TODO: Handle fields being imported and loaded with a new value
       assignment.argumentOut.take(2).l match {
-        case List(i: Identifier, c: Call) if symbolTable.contains(c) =>
-          val importedTypes = symbolTable.get(c)
+        case List(i: Identifier, c: Call) if globalTable.contains(c) =>
+          val importedTypes = globalTable.get(c)
           if (!c.code.endsWith(")")) {
             // Case 1: The identifier is at the assignment to a function pointer. Lack of parenthesis should indicate this.
             symbolTable.append(i, importedTypes)
@@ -187,6 +220,54 @@ class PythonTypeRecovery(cpg: Cpg) extends CpgPass(cpg) {
         case _ =>
       }
     }
+
+    /** Using an entry from the symbol table, will queue the CPG modification to persist the recovered type information.
+      */
+    private def setTypeInformation(): Unit = {
+      cu.ast
+        .foreach {
+          case x: Local if symbolTable.contains(x) =>
+            builder.setNodeProperty(x, PropertyNames.DYNAMIC_TYPE_HINT_FULL_NAME, symbolTable.get(x).toSeq)
+          case x: Identifier if symbolTable.contains(x) =>
+            (x.inCall.headOption, x.inCall.argument.take(2).l) match {
+              // Case 1: 'call' is an assignment
+              case (Some(call: Call), List(i: Identifier, c: Call)) if call.name.equals(Operators.assignment) =>
+                val idTypes   = symbolTable.get(i)
+                val callTypes = symbolTable.get(c)
+                persistType(call, callTypes)(builder)
+                if (idTypes.nonEmpty || callTypes.nonEmpty) {
+                  if (idTypes.equals(callTypes))
+                    // Case 1.1: This is a function pointer or constructor
+                    persistType(i, callTypes)(builder)
+                  else
+                    // Case 1.2: This is the return value of the function
+                    persistType(i, idTypes)(builder)
+                }
+              // Case 2: 'i' is the receiver of 'call'
+              case (Some(call: Call), List(i: Identifier, _)) if !call.name.equals(Operators.fieldAccess) =>
+                val idHints = symbolTable.get(i)
+                persistType(i, idHints)(builder)
+                persistType(call, idHints)(builder)
+              // Case 3: 'i' is the receiver for a field access on member 'f'
+              case (Some(call: Call), List(i: Identifier, f: FieldIdentifier))
+                  if call.name.equals(Operators.fieldAccess) =>
+                persistType(i, symbolTable.get(x))(builder)
+              // TODO: Handle fields
+              // Case 4: We are elsewhere
+              case _ => persistType(x, symbolTable.get(x))(builder)
+            }
+          case x: Call if symbolTable.contains(x) =>
+            builder.setNodeProperty(x, PropertyNames.DYNAMIC_TYPE_HINT_FULL_NAME, symbolTable.get(x).toSeq)
+          case _ =>
+        }
+    }
+
+    private def persistType(x: StoredNode, types: Set[String])(implicit builder: DiffGraphBuilder): Unit =
+      if (types.nonEmpty)
+        if (types.size == 1)
+          builder.setNodeProperty(x, PropertyNames.TYPE_FULL_NAME, types.head)
+        else
+          builder.setNodeProperty(x, PropertyNames.DYNAMIC_TYPE_HINT_FULL_NAME, types.toSeq)
 
   }
 
@@ -219,69 +300,6 @@ class PythonTypeRecovery(cpg: Cpg) extends CpgPass(cpg) {
     override def toString: String = s"Either($fullNameAsPyFile or $fullNameAsInit)"
   }
 
-  /** Using an entry from the symbol table, will queue the CPG modification to persist the recovered type information.
-    *
-    * @param builder
-    *   the graph builder.
-    * @param sbKey
-    *   the variable declaration identifier.
-    * @param typeHints
-    *   the recovered type hints.
-    */
-  class PersistRecoveredTypeTask(builder: DiffGraphBuilder, sbKey: SBKey, typeHints: Set[String])
-      extends RecursiveTask[Unit] {
-    override def compute(): Unit = {
-      val SBKey(filename, _) = sbKey
-      cpg.file
-        .nameExact(filename)
-        .method
-        .ast
-        .foreach {
-          case x: Local if sbKey.equals(x) =>
-            builder.setNodeProperty(x, PropertyNames.DYNAMIC_TYPE_HINT_FULL_NAME, typeHints.toSeq)
-          case x: Identifier if sbKey.equals(x) =>
-            (x.inCall.headOption, x.inCall.argument.take(2).l) match {
-              // Case 1: 'call' is an assignment
-              case (Some(call: Call), List(i: Identifier, c: Call)) if call.name.equals(Operators.assignment) =>
-                val idTypes   = symbolTable.get(i)
-                val callTypes = symbolTable.get(c)
-                persistType(call, callTypes)(builder)
-                if (idTypes.nonEmpty || callTypes.nonEmpty) {
-                  if (idTypes.equals(callTypes))
-                    // Case 1.1: This is a function pointer or constructor
-                    persistType(i, callTypes)(builder)
-                  else
-                    // Case 1.2: This is the return value of the function
-                    persistType(i, idTypes)(builder)
-                }
-              // Case 2: 'i' is the receiver of 'call'
-              case (Some(call: Call), List(i: Identifier, _)) if !call.name.equals(Operators.fieldAccess) =>
-                val idHints = symbolTable.get(i)
-                persistType(i, idHints)(builder)
-                persistType(call, idHints)(builder)
-              // Case 3: 'i' is the receiver for a field access on member 'f'
-              case (Some(call: Call), List(i: Identifier, f: FieldIdentifier))
-                  if call.name.equals(Operators.fieldAccess) =>
-                persistType(i, typeHints)(builder)
-              // TODO: Handle fields
-              // Case 4: We are elsewhere
-              case _ => persistType(x, typeHints)(builder)
-            }
-          case x: Call if sbKey.equals(x) =>
-            builder.setNodeProperty(x, PropertyNames.DYNAMIC_TYPE_HINT_FULL_NAME, typeHints.toSeq)
-          case _ =>
-        }
-    }
-
-    private def persistType(x: StoredNode, types: Set[String])(implicit builder: DiffGraphBuilder): Unit =
-      if (types.nonEmpty)
-        if (types.size == 1)
-          builder.setNodeProperty(x, PropertyNames.TYPE_FULL_NAME, types.head)
-        else
-          builder.setNodeProperty(x, PropertyNames.DYNAMIC_TYPE_HINT_FULL_NAME, types.toSeq)
-
-  }
-
   /** Represents an identifier at a specific scope.
     *
     * @param filename
@@ -302,18 +320,19 @@ class PythonTypeRecovery(cpg: Cpg) extends CpgPass(cpg) {
   }
 
   object SBKey {
-    def fromNode(cfgNode: CfgNode): SBKey = {
-      val name = cfgNode match {
+    def fromNode(node: AstNode): SBKey = {
+      val name = node match {
         case x: Identifier => x.name
         case x: Method     => x.name
         case x: Call       => x.name
+        case x: Local      => x.name
         case x             => x.code
       }
-      val filename = cfgNode.file.name.headOption match {
+      val filename = node.file.name.headOption match {
         case Some(fileName) => fileName
         case None =>
           logger.warn(
-            s"Unable to successfully use file name for symbol table, type recovery may become more imprecise. Node: ${cfgNode.propertiesMap()}"
+            s"Unable to successfully use file name for symbol table, type recovery may become more imprecise. Node: ${node.propertiesMap()}"
           ); ""
       }
       SBKey(filename, name)
@@ -331,18 +350,20 @@ class PythonTypeRecovery(cpg: Cpg) extends CpgPass(cpg) {
 
     def apply(sbKey: SBKey): Set[String] = table(sbKey)
 
-    def apply(node: CfgNode): Set[String] = table(fromNode(node))
+    def apply(node: AstNode): Set[String] = table(fromNode(node))
+
+    def from(sb: SymbolTable): SymbolTable = { table.addAll(sb.table); this }
 
     def put(sbKey: SBKey, typeFullNames: Set[String]): Option[Set[String]] =
       table.put(sbKey, typeFullNames)
 
-    def put(node: CfgNode, typeFullNames: Set[String]): Option[Set[String]] =
+    def put(node: AstNode, typeFullNames: Set[String]): Option[Set[String]] =
       put(fromNode(node), typeFullNames)
 
-    def append(node: CfgNode, typeFullName: String): Option[Set[String]] =
+    def append(node: AstNode, typeFullName: String): Option[Set[String]] =
       append(node, Set(typeFullName))
 
-    def append(node: CfgNode, typeFullNames: Set[String]): Option[Set[String]] =
+    def append(node: AstNode, typeFullNames: Set[String]): Option[Set[String]] =
       append(fromNode(node), typeFullNames)
 
     private def append(sbKey: SBKey, typeFullNames: Set[String]): Option[Set[String]] = {
@@ -354,15 +375,15 @@ class PythonTypeRecovery(cpg: Cpg) extends CpgPass(cpg) {
 
     private def contains(sbKey: SBKey): Boolean = table.contains(sbKey)
 
-    def contains(node: CfgNode): Boolean = contains(fromNode(node))
+    def contains(node: AstNode): Boolean = contains(fromNode(node))
 
     private def get(sbKey: SBKey): Set[String] = table.getOrElse(sbKey, Set.empty)
 
-    def get(node: CfgNode): Set[String] = get(fromNode(node))
+    def get(node: AstNode): Set[String] = get(fromNode(node))
 
     def view: MapView[SBKey, Set[String]] = table.view
 
-    def clear: Unit = table.clear()
+    def clear(): Unit = table.clear()
 
   }
 
