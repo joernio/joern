eed3a2c3d16d2485014ccf4f427c1318613ba721
Scala Steward <scala_steward@virtuslab.com>
2023-02-02 17:03:35 +0000

    Revert commit(s) ff936277

--- project/Versions.scala
+++ project/Versions.scala
@@ -5,7 +5,7 @@ object Versions {
   // causes problems upstreams.
   val antlr         = "4.7"
   val scalatest     = "3.2.15"
-  val cats          = "3.4.5"
+  val cats          = "3.3.14"
   val log4j         = "2.19.0"
   val json4s        = "4.0.6"
   val gradleTooling = "7.5.1"
