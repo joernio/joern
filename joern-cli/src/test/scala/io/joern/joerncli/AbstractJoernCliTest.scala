370f0bd1c9f36e4357e615bd706e38719d61226f
Michael Pollmeier <michael@michaelpollmeier.com>
2023-03-08 11:13:59 +0100

    invoke JsSrc via system.exec as well, improve error handling

--- joern-cli/src/test/scala/io/joern/joerncli/AbstractJoernCliTest.scala
+++ joern-cli/src/test/scala/io/joern/joerncli/AbstractJoernCliTest.scala
@@ -1,13 +1,13 @@
 package io.joern.joerncli
 
 import better.files.File
-import io.joern.console.cpgcreation.CCpgGenerator
+import io.joern.console.cpgcreation.{CCpgGenerator, JsSrcCpgGenerator}
 import io.joern.console.FrontendConfig
-import io.joern.jssrc2cpg.JsSrc2Cpg
-import io.joern.jssrc2cpg
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.codepropertygraph.generated.Languages
 import io.shiftleft.utils.ProjectRoot
+
+import scala.util.{Failure, Success}
 
 trait AbstractJoernCliTest {
 
@@ -20,25 +20,18 @@ trait AbstractJoernCliTest {
     val cpgOutFileName = tmpFile.pathAsString
     tmpFile.delete()
 
-    language match {
-      case Languages.C | Languages.CSHARP         => withC2Cpg(file, cpgOutFileName)
-      case Languages.JSSRC | Languages.JAVASCRIPT => withJsSrc2Cpg(file, cpgOutFileName)
-      case _                                      => ???
+    val cpgGenerator = language match {
+      case Languages.C | Languages.CSHARP         => CCpgGenerator(new FrontendConfig(), ProjectRoot.find.path)
+      case Languages.JSSRC | Languages.JAVASCRIPT => JsSrcCpgGenerator(new FrontendConfig(), ProjectRoot.find.path)
     }
+    cpgGenerator.generate(inputPath = file.pathAsString, outputPath = cpgOutFileName) match {
+      case Failure(exception) => throw new AssertionError("error while invoking cpgGenerator", exception)
+      case _                  =>
+    }
+
     // Link CPG fragments and enhance to create semantic CPG
     val cpg = DefaultOverlays.create(cpgOutFileName)
     (cpg, cpgOutFileName)
-  }
-
-  private def withC2Cpg(inputFile: File, outputPath: String): Unit = {
-    CCpgGenerator(new FrontendConfig(), ProjectRoot.find.path)
-      .generate(inputPath = inputFile.pathAsString, outputPath = outputPath)
-  }
-
-  private def withJsSrc2Cpg(inputFile: File, outputPath: String): Unit = {
-    val frontend = new JsSrc2Cpg()
-    val config   = jssrc2cpg.Config(inputPath = inputFile.pathAsString, outputPath = outputPath)
-    frontend.run(config)
   }
 
 }
