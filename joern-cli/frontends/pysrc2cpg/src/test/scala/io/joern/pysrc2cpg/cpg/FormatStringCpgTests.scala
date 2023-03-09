f4c98e126918b832aaeba8c2fbf89679008f5fb3
Markus Lottmann <markus@shiftleft.io>
2023-03-09 16:22:58 +0100

    Implement nested format string handling. (#2356)
    * Wrap token advance in error recovery in try catch.
    
    This is necessary because getNextToken() might itself throw exceptions
    in which case we do not want to forget to register the current error.
    
    * Implement nested replacement field handling.
    
    * Add multiple replacement field test.

--- joern-cli/frontends/pysrc2cpg/src/test/scala/io/joern/pysrc2cpg/cpg/FormatStringCpgTests.scala
+++ joern-cli/frontends/pysrc2cpg/src/test/scala/io/joern/pysrc2cpg/cpg/FormatStringCpgTests.scala
@@ -44,4 +44,23 @@ class FormatStringCpgTests extends AnyFreeSpec with Matchers {
     }
   }
 
+  "test nested format string" in {
+    val cpg = Py2CpgTestContext.buildCpg("""
+        |f"{f'{a}'} "
+        |""".stripMargin)
+    val outerCall = cpg.call.methodFullName("<operator>.formatString").columnNumber(1).head
+    outerCall.code shouldBe """f"{f'{a}'} """"
+
+    val innerCall = cpg.call.methodFullName("<operator>.formatString").columnNumber(4).head
+    innerCall.code shouldBe """f'{a}'"""
+  }
+
+  "test format string with multiple replacement fields" in {
+    val cpg = Py2CpgTestContext.buildCpg("""
+                                           |f"{a} {b}"
+                                           |""".stripMargin)
+    val callNode = cpg.call.methodFullName("<operator>.formatString").head
+    callNode.code shouldBe """f"{a} {b}""""
+  }
+
 }
