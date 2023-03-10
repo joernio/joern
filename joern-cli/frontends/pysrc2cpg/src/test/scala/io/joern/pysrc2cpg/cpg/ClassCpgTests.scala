c42a95d125318b1875bda9203988a5b56902cee6
Markus Lottmann <markus.lottmann@mlbox.net>
2023-03-10 12:14:17 +0100

    Change python instance class type full names.
    So far the full name for the instance class type of:
    ```
    class Foo:
      pass
    ```
    
    was `fileName.py:<module>.Foo.Foo`. This is now changed to:
    `fileName.py:<module>.Foo`.
    
    C.f.: https://github.com/joernio/joern/issues/2225

--- joern-cli/frontends/pysrc2cpg/src/test/scala/io/joern/pysrc2cpg/cpg/ClassCpgTests.scala
+++ joern-cli/frontends/pysrc2cpg/src/test/scala/io/joern/pysrc2cpg/cpg/ClassCpgTests.scala
@@ -4,6 +4,48 @@ import io.joern.pysrc2cpg.PySrc2CpgFixture
 import io.shiftleft.semanticcpg.language._
 
 class ClassCpgTests extends PySrc2CpgFixture(withOssDataflow = false) {
+  "class" should {
+    val cpg = code("""class Foo:
+        |  pass
+        |""".stripMargin)
+    "have correct instance class type and typeDecl" in {
+      cpg.typ.name("Foo").fullName.head shouldBe "Test0.py:<module>.Foo"
+      val typeDecl = cpg.typeDecl.name("Foo").head
+      typeDecl.fullName shouldBe "Test0.py:<module>.Foo"
+      typeDecl.astParent.head shouldBe cpg.method.name("<module>").head
+    }
+
+    "have correct meta class type and typeDecl" in {
+      cpg.typ.name("Foo<meta>").fullName.head shouldBe "Test0.py:<module>.Foo<meta>"
+      val typeDecl = cpg.typeDecl.name("Foo<meta>").head
+      typeDecl.fullName shouldBe "Test0.py:<module>.Foo<meta>"
+      typeDecl.astParent.head shouldBe cpg.method.name("<module>").head
+    }
+
+    "have correct class body type and typeDecl" in {
+      cpg.typ.name("Foo<body>").fullName.head shouldBe "Test0.py:<module>.Foo.Foo<body>"
+      val typeDecl = cpg.typeDecl.name("Foo<body>").head
+      typeDecl.fullName shouldBe "Test0.py:<module>.Foo.Foo<body>"
+      typeDecl.astParent.head shouldBe cpg.typeDecl.name("Foo<meta>").head
+    }
+
+    "have correct meta class call handler type and typeDecl" in {
+      cpg.typ.name("<metaClassCallHandler>").fullName.head shouldBe
+        "Test0.py:<module>.Foo.<metaClassCallHandler>"
+      val typeDecl = cpg.typeDecl.name("<metaClassCallHandler>").head
+      typeDecl.fullName shouldBe "Test0.py:<module>.Foo.<metaClassCallHandler>"
+      typeDecl.astParent.head shouldBe cpg.typeDecl.name("Foo<meta>").head
+    }
+
+    "have correct fake new type and typeDecl" in {
+      cpg.typ.name("<fakeNew>").fullName.head shouldBe
+        "Test0.py:<module>.Foo.<fakeNew>"
+      val typeDecl = cpg.typeDecl.name("<fakeNew>").head
+      typeDecl.fullName shouldBe "Test0.py:<module>.Foo.<fakeNew>"
+      typeDecl.astParent.head shouldBe cpg.typeDecl.name("Foo<meta>").head
+    }
+  }
+
   "class meta call handler" should {
     "have no self parameter if self is explicit" in {
       val cpg = code("""class Foo:
