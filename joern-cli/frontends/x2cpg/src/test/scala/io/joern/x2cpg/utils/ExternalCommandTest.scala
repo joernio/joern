0905eb80ed03bb7b0f09eef833049cb77e7a82d6
Claudiu-Vlad Ursache <claudiu@ursache.io>
2023-03-23 13:17:26 +0100

    Add new function to `ExternalCommand`

--- joern-cli/frontends/x2cpg/src/test/scala/io/joern/x2cpg/utils/ExternalCommandTest.scala
+++ joern-cli/frontends/x2cpg/src/test/scala/io/joern/x2cpg/utils/ExternalCommandTest.scala
@@ -4,12 +4,29 @@ import better.files.File
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
+import scala.util.{Failure, Success}
+
 class ExternalCommandTest extends AnyWordSpec with Matchers {
-  "ExternalCommand" should {
+  "ExternalCommand.run" should {
     "be able to run `ls` successfully" in {
       File.usingTemporaryDirectory("sample") { sourceDir =>
         val cmd = "ls " + sourceDir.pathAsString
         ExternalCommand.run(cmd, sourceDir.pathAsString) should be a Symbol("success")
+      }
+    }
+  }
+
+  "ExternalCommand.runMultiple" should {
+    "be able to run multiple `ls` invocations successfully" in {
+      File.usingTemporaryDirectory("sample") { sourceDir =>
+        File.usingTemporaryFile(parent = Some(sourceDir)) { tempFile =>
+          val cmd    = "ls " + sourceDir.pathAsString + " && " + "ls " + sourceDir.pathAsString
+          val result = ExternalCommand.runMultiple(cmd, sourceDir.pathAsString)
+          result match {
+            case Success(value) => value.split("\n").size shouldBe 2
+            case Failure(_)     => false shouldBe true
+          }
+        }
       }
     }
   }
