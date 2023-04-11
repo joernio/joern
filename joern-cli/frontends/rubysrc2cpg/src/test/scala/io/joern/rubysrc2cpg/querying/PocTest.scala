98b5bb03a069f9e4f9cfa74a2418391c66c5c2b2
rahul-privado <rahul.godbole@privado.ai>
2023-04-11 14:11:08 +0530

    Initial failing test

---
+++ joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/querying/PocTest.scala
@@ -0,0 +1,55 @@
+package io.joern.php2cpg.querying
+
+import io.joern.rubysrc2cpg.testfixtures.RubyCode2CpgFixture
+import io.shiftleft.semanticcpg.language._
+
+class PocTest extends RubyCode2CpgFixture {
+
+  "The CPG generated for a very simple example" should {
+    val cpg = code(
+      """
+        |# define a class
+        |class Box
+        |   BOX_COMPANY = "TATA Inc"
+        |   BOXWEIGHT = 10
+        |   # constructor method
+        |   def initialize(w,h)
+        |      @width, @height = w, h
+        |   end
+        |   # instance method
+        |   def getArea
+        |      @width * @height
+        |   end
+        |end
+        |
+        |# create an object
+        |box = Box.new(10, 20)
+        |
+        |# call instance methods
+        |a = box.getArea()
+        |puts "Area of the box is : #{a}"
+        |puts Box::BOX_COMPANY
+        |puts "Box weight is: #{Box::BOXWEIGHT}"
+        |""".stripMargin,
+      fileName = "printhello.php"
+    )
+
+
+    "have the correct namespace set" in {
+      cpg.namespaceBlock.fullName(".*printhello.php.*").l match {
+        case namespaceBlock :: Nil =>
+          namespaceBlock.name shouldBe "<global>"
+        case result => fail(s"expected namespaceBlock found $result")
+      }
+    }
+
+    "have a call node for the printHello call" in {
+      cpg.call.nameExact("printHello").l match {
+        case call :: Nil =>
+          call.lineNumber shouldBe Some(7)
+
+        case result => fail(s"Expected printHello call got $result")
+      }
+    }
+  }
+}
