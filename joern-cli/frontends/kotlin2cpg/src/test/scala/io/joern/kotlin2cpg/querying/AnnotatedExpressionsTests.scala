0f8f39a3671dccdde5fda16adba1abfaabd14a29
Claudiu-Vlad Ursache <claudiu@ursache.io>
2023-04-13 11:41:39 +0200

    kotlin2cpg: add nodes for annotations

--- joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/querying/AnnotatedExpressionsTests.scala
+++ joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/querying/AnnotatedExpressionsTests.scala
@@ -1,6 +1,7 @@
 package io.joern.kotlin2cpg.querying
 
 import io.joern.kotlin2cpg.testfixtures.KotlinCode2CpgFixture
+import io.shiftleft.codepropertygraph.generated.nodes.Annotation
 import io.shiftleft.semanticcpg.language._
 
 class AnnotatedExpressionsTests extends KotlinCode2CpgFixture(withOssDataflow = false) {
@@ -21,6 +22,44 @@ class AnnotatedExpressionsTests extends KotlinCode2CpgFixture(withOssDataflow = 
       c1.code shouldBe c2.code
       c1.methodFullName shouldBe c2.methodFullName
       c1.dispatchType shouldBe c2.dispatchType
+    }
+  }
+
+  "CPG for code with an annotated class" should {
+    val cpg = code("""|package mypkg
+        |
+        |annotation class Controller
+        |annotation class Route(val path: String)
+        |
+        |@Controller
+        |class HealthController {
+        |    @Route("/health")
+        |    fun health(): String {
+        |        return "ok"
+        |    }
+        |}
+        |""".stripMargin)
+
+    "should contain an ANNOTATION node attached to the annotated class" in {
+      val List(td)    = cpg.typeDecl.nameExact("HealthController").l
+      def annotations = td.astChildren.collect { case c: Annotation => c }
+      annotations.size shouldBe 1
+      val List(annotation) = annotations.l
+      annotation.code shouldBe "@Controller"
+      annotation.name shouldBe "Controller"
+      annotation.fullName shouldBe "mypkg.Controller"
+    }
+
+    "should contain an ANNOTATION node attached to the annotated method" in {
+      val List(m) = cpg.method.nameExact("health").l
+      m.name shouldBe "health"
+
+      def annotations = m.astChildren.collect { case c: Annotation => c }
+      annotations.size shouldBe 1
+      val List(annotation) = annotations.l
+      annotation.code shouldBe "@Route(\"/health\")"
+      annotation.name shouldBe "Route"
+      annotation.fullName shouldBe "mypkg.Route"
     }
   }
 }
