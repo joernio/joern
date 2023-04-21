92106a774b17c5feac58cb859787e589ff84af91
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2023-04-21 16:42:25 +0200

    jssrc2cpg: added simple detection of transpiled files (#2549)
    Fixes: https://github.com/joernio/joern/issues/2547

---
+++ joern-cli/frontends/jssrc2cpg/src/test/scala/io/joern/jssrc2cpg/io/TranspiledFileDetectionTest.scala
@@ -0,0 +1,100 @@
+package io.joern.jssrc2cpg.io
+
+import better.files.File
+import io.joern.jssrc2cpg.Config
+import io.joern.jssrc2cpg.passes.AstCreationPass
+import io.joern.jssrc2cpg.utils.AstGenRunner
+import io.joern.x2cpg.X2Cpg.newEmptyCpg
+import io.shiftleft.semanticcpg.language._
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class TranspiledFileDetectionTest extends AnyWordSpec with Matchers {
+
+  "Detecting transpiled files" should {
+    "skip transpiled files correctly (with source map comment)" in {
+      File.usingTemporaryDirectory("jssrc2cpgTest") { sourceDir =>
+        (sourceDir / "index.ts").createFile().writeText("console.log('Hello World!');")
+        (sourceDir / "index.js")
+          .createFile()
+          .writeText("""
+            |console.log('Hello World!');
+            |//sourceMappingURL=foo
+            |""".stripMargin)
+        val cpg          = newEmptyCpg()
+        val config       = Config(inputPath = sourceDir.toString, outputPath = sourceDir.toString, tsTypes = false)
+        val astGenResult = new AstGenRunner(config).execute(sourceDir)
+        new AstCreationPass(cpg, astGenResult, config).createAndApply()
+        cpg.file.name.l shouldBe List("index.ts")
+      }
+    }
+
+    "skip transpiled files correctly (with source map file)" in {
+      File.usingTemporaryDirectory("jssrc2cpgTest") { sourceDir =>
+        (sourceDir / "index.ts").createFile().writeText("console.log('Hello World!');")
+        (sourceDir / "index.js").createFile().writeText("console.log('Hello World!');")
+        (sourceDir / "index.js.map").createFile()
+        val cpg          = newEmptyCpg()
+        val config       = Config(inputPath = sourceDir.toString, outputPath = sourceDir.toString, tsTypes = false)
+        val astGenResult = new AstGenRunner(config).execute(sourceDir)
+        new AstCreationPass(cpg, astGenResult, config).createAndApply()
+        cpg.file.name.l shouldBe List("index.ts")
+      }
+    }
+
+    "skip transpiled files correctly (with other file types)" in {
+      File.usingTemporaryDirectory("jssrc2cpgTest") { sourceDir =>
+        (sourceDir / "index.vue").createFile().writeText("console.log('Hello World!');")
+        (sourceDir / "index.ejs").createFile().writeText("console.log('Hello World!');")
+        (sourceDir / "index.js").createFile().writeText("console.log('Hello World!');")
+        (sourceDir / "index.js.map").createFile()
+        val cpg          = newEmptyCpg()
+        val config       = Config(inputPath = sourceDir.toString, outputPath = sourceDir.toString, tsTypes = false)
+        val astGenResult = new AstGenRunner(config).execute(sourceDir)
+        new AstCreationPass(cpg, astGenResult, config).createAndApply()
+        cpg.file.name.l shouldBe List("index.ejs", "index.vue")
+      }
+    }
+
+    "not skip transpiled files when there is no source map file or comment" in {
+      File.usingTemporaryDirectory("jssrc2cpgTest") { sourceDir =>
+        (sourceDir / "index.ts").createFile().writeText("console.log('Hello World!');")
+        (sourceDir / "index.js").createFile().writeText("console.log('Hello World!');")
+        val cpg          = newEmptyCpg()
+        val config       = Config(inputPath = sourceDir.toString, outputPath = sourceDir.toString, tsTypes = false)
+        val astGenResult = new AstGenRunner(config).execute(sourceDir)
+        new AstCreationPass(cpg, astGenResult, config).createAndApply()
+        cpg.file.name.l shouldBe List("index.js", "index.ts")
+      }
+    }
+
+    "not skip transpiled files when there is no sibling file with same name but a source map file only" in {
+      File.usingTemporaryDirectory("jssrc2cpgTest") { sourceDir =>
+        (sourceDir / "index.js").createFile().writeText("console.log('Hello World!');")
+        (sourceDir / "index.js.map").createFile()
+        val cpg          = newEmptyCpg()
+        val config       = Config(inputPath = sourceDir.toString, outputPath = sourceDir.toString, tsTypes = false)
+        val astGenResult = new AstGenRunner(config).execute(sourceDir)
+        new AstCreationPass(cpg, astGenResult, config).createAndApply()
+        cpg.file.name.l shouldBe List("index.js")
+      }
+    }
+
+    "not skip transpiled files when there is no sibling file with same name but a source map comment only" in {
+      File.usingTemporaryDirectory("jssrc2cpgTest") { sourceDir =>
+        (sourceDir / "index.js")
+          .createFile()
+          .writeText("""
+              |console.log('Hello World!');
+              |//sourceMappingURL=foo
+              |""".stripMargin)
+        val cpg          = newEmptyCpg()
+        val config       = Config(inputPath = sourceDir.toString, outputPath = sourceDir.toString, tsTypes = false)
+        val astGenResult = new AstGenRunner(config).execute(sourceDir)
+        new AstCreationPass(cpg, astGenResult, config).createAndApply()
+        cpg.file.name.l shouldBe List("index.js")
+      }
+    }
+  }
+
+}
