c88181e2cad63ba1959c27c5f889f448442eccf6
David Baker Effendi <dbe@sun.ac.za>
2023-06-12 18:09:30 +0200

    XInheritancePass: Switch to Name Exact Matching (#2852)
    * XInheritancePass: Switch to Name Exact Matching
    The regex matcher is much too expensive with arguably not much benefit. This PR simply makes the matching use `cpg.typDecl.nameExact` on the entity imported. This comes with great performance improvements.
    
    * Removed maps as they can take a large toll on memory
    
    * using full name exact

--- joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/passes/frontend/XInheritanceFullNamePass.scala
+++ joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/passes/frontend/XInheritanceFullNamePass.scala
@@ -10,8 +10,6 @@ import io.shiftleft.semanticcpg.language._
 import java.io.File
 import java.nio.file.Paths
 import java.util.regex.{Matcher, Pattern}
-import scala.collection.concurrent.TrieMap
-import scala.collection.mutable
 
 /** Using some basic heuristics, will try to resolve type full names from types found within the CPG. Requires
   * ImportPass as a pre-requisite.
@@ -23,14 +21,7 @@ abstract class XInheritanceFullNamePass(cpg: Cpg) extends ForkJoinParallelCpgPas
   protected val moduleName: String
   protected val fileExt: String
 
-  protected val typeDeclMap: TrieMap[String, TypeDeclBase] =
-    TrieMap.from[String, TypeDeclBase](cpg.typeDecl.map(t => t.fullName -> t))
-  protected val typeMap: mutable.Map[String, TypeBase] =
-    mutable.HashMap.from[String, TypeBase](cpg.typ.map(t => t.fullName -> t))
-
-  // Regex matching is expensive, so we cache pattern strings to their compiled pattern objects
-  private val typeRegexCache: TrieMap[String, Pattern] = TrieMap.empty[String, Pattern]
-  private val relativePathPattern                      = Pattern.compile("^[.]+/?.*")
+  private val relativePathPattern = Pattern.compile("^[.]+/?.*")
 
   override def generateParts(): Array[TypeDecl] =
     cpg.typeDecl
@@ -42,14 +33,8 @@ abstract class XInheritanceFullNamePass(cpg: Cpg) extends ForkJoinParallelCpgPas
     if (resolvedTypeDecls.nonEmpty) {
       val fullNames = resolvedTypeDecls.map(_.fullName)
       builder.setNodeProperty(source, PropertyNames.INHERITS_FROM_TYPE_FULL_NAME, fullNames)
-      fullNames.flatMap(typeMap.get).foreach(tgt => builder.addEdge(source, tgt, EdgeTypes.INHERITS_FROM))
+      cpg.typ.fullNameExact(fullNames: _*).foreach(tgt => builder.addEdge(source, tgt, EdgeTypes.INHERITS_FROM))
     }
-  }
-
-  override def finish(): Unit = {
-    typeDeclMap.clear()
-    typeMap.clear()
-    typeRegexCache.clear()
   }
 
   protected def inheritsNothingOfInterest(inheritedTypes: Seq[String]): Boolean =
@@ -67,30 +52,23 @@ abstract class XInheritanceFullNamePass(cpg: Cpg) extends ForkJoinParallelCpgPas
           }
         case imp => imp
       }.headOption
-    case x: TypeDecl if typeDeclMap.contains(x.fullName) => Option(x.fullName)
-    case _                                               => None
+    case x: TypeDecl => Option(x.fullName)
+    case _           => None
   }
-
-  private def nameToPattern(n: String): Pattern = typeRegexCache.getOrElseUpdate(
-    n, {
-      n match {
-        case x if x.contains(pathSep) =>
-          val splitName = x.split(pathSep)
-          Pattern.compile(s".*${Pattern.quote(splitName.head)}.*${Pattern.quote(splitName.last)}")
-        case x => Pattern.compile(s".*${Pattern.quote(x)}")
-      }
-    }
-  )
 
   protected def resolveInheritedTypeFullName(td: TypeDecl, builder: DiffGraphBuilder): Seq[TypeDeclBase] = {
     val qualifiedNamesInScope = td.file.ast
       .flatMap(extractTypeDeclFromNode)
       .filterNot(_.endsWith(moduleName))
       .l
-    val matchersInScope = qualifiedNamesInScope.map(nameToPattern).distinct
-    val validTypeDecls =
-      typeDeclMap.filter { case (name, _) => matchersInScope.exists(m => m.matcher(name).matches()) }.values.toList
-    val filteredTypes = validTypeDecls.filter(vt => td.inheritsFromTypeFullName.contains(vt.name))
+    val matchersInScope = qualifiedNamesInScope.map {
+      case x if x.contains(pathSep) =>
+        val splitName = x.split(pathSep)
+        splitName.last
+      case x => x
+    }.distinct
+    val validTypeDecls = cpg.typeDecl.nameExact(matchersInScope: _*).toList
+    val filteredTypes  = validTypeDecls.filter(vt => td.inheritsFromTypeFullName.contains(vt.name))
     if (filteredTypes.isEmpty) {
       // Usually in the case of inheriting external types
       qualifiedNamesInScope
@@ -108,12 +86,11 @@ abstract class XInheritanceFullNamePass(cpg: Cpg) extends ForkJoinParallelCpgPas
   }
 
   private def createTypeStub(name: String, fullName: String, builder: DiffGraphBuilder): TypeDeclBase =
-    typeDeclMap.get(fullName) match {
+    cpg.typeDecl.fullNameExact(fullName).headOption match {
       case Some(typeDecl) => typeDecl
       case None =>
         val typeDecl = TypeDeclStubCreator.createTypeDeclStub(name, fullName)
         builder.addNode(typeDecl)
-        typeDeclMap.put(fullName, typeDecl)
         typeDecl
     }
 
