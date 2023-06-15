3ba69a1edf28bd83f7e034c2b2a3453923ed8424
Sooraj Sathyanarayanan <32236127+iAnonymous3000@users.noreply.github.com>
2023-06-15 14:53:30 -0400

    Update testDistro.sh

--- testDistro.sh
+++ testDistro.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
-set -e #stop on error
-# set -x #verbose on
 
+set -e # Stop on error
+set -u # Treat unset variables as an error
+# set -x # Uncomment this for debugging
+
+# Define log file path
+LOG_FILE="/var/log/my_script.log"
+
+# Function to log messages with timestamp
+log() {
+    echo "$(date "+%Y-%m-%d %H:%M:%S") - $1" | tee -a "$LOG_FILE"
+}
+
+# Absolute paths
 readonly SCRIPT_ABS_PATH=$(readlink -f "$0")
 readonly SCRIPT_ABS_DIR=$(dirname "$SCRIPT_ABS_PATH")
 readonly REPO_ROOT=$SCRIPT_ABS_DIR
 
-echo "staging joern"
-pushd $REPO_ROOT
-  sbt -Dsbt.log.noformat=true clean joerncli/stage querydb/createDistribution
-  tests/querydb-test.sh
-  tests/frontends-tests.sh
-  tests/scripts-test.sh
-popd
+log "Staging joern"
+pushd $REPO_ROOT || { log "Failed to change directory to $REPO_ROOT"; exit 1; }
 
-echo "success. go analyse some code"
+sbt -Dsbt.log.noformat=true clean joerncli/stage querydb/createDistribution || { log "sbt tasks failed"; exit 1; }
+
+# Run the tests
+tests=( "querydb-test.sh" "frontends-tests.sh" "scripts-test.sh" )
+for test in "${tests[@]}"; do
+    log "Running $test"
+    tests/"$test" || { log "Test $test failed"; exit 1; }
+done
+
+popd || { log "Failed to change back to the original directory"; exit 1; }
+
+log "Success. Go analyse some code."
