d350a2b4d33a60de1ecf6a1b0979117cd80abc07
Pandurang Patil <pandurang.patil@privado.ai>
2023-06-20 17:15:30 +0530

    Made change to handle both go2cpg as well as goSrc2Cpg
    Before invoking either `go2cpg` or `goSrc2Cpg`, checking if `go2cpg`
    script is present, if it is then it will invoke `go2cpg` otherwise it
    will invoke `gosrc2cpg`

--- console/src/main/scala/io/joern/console/cpgcreation/GoCpgGenerator.scala
+++ console/src/main/scala/io/joern/console/cpgcreation/GoCpgGenerator.scala
@@ -8,15 +8,41 @@ import scala.util.Try
 /** Language frontend for Go code. Translates Go source code into Code Property Graphs.
   */
 case class GoCpgGenerator(config: FrontendConfig, rootPath: Path) extends CpgGenerator {
-  private lazy val command: Path = if (isWin) rootPath.resolve("gosrc2cpg.bat") else rootPath.resolve("gosrc2cpg")
+  private lazy val goSrc2CpgCommand: Path =
+    if (isWin) rootPath.resolve("gosrc2cpg.bat") else rootPath.resolve("gosrc2cpg")
 
+  /** Generate a CPG for the given input path. Returns the output path, or None, if no CPG was generated.
+    */
   override def generate(inputPath: String, outputPath: String): Try[String] = {
-    val arguments = List(inputPath) ++ Seq("-o", outputPath) ++ config.cmdLineParams
-    runShellCommand(command.toString, arguments).map(_ => outputPath)
+    if (go2CpgAvailable()) go2CpgGenerate(inputPath, outputPath) else goSrc2CpgGenerate(inputPath, outputPath)
   }
 
-  override def isAvailable: Boolean =
-    command.toFile.exists
+  def go2CpgGenerate(inputPath: String, outputPath: String = "cpg.bin.zip"): Try[String] = {
+    var command   = rootPath.resolve("go2cpg.sh").toString
+    var arguments = Seq("--output", outputPath) ++ config.cmdLineParams ++ Seq("generate") ++ List(inputPath)
+
+    if (System.getProperty("os.name").startsWith("Windows")) {
+      command = "powershell"
+      arguments = Seq(rootPath.resolve("go2cpg.ps1").toString) ++ arguments
+    }
+
+    runShellCommand(command, arguments).map(_ => outputPath)
+  }
+
+  def goSrc2CpgGenerate(inputPath: String, outputPath: String): Try[String] = {
+    val arguments = List(inputPath) ++ Seq("-o", outputPath) ++ config.cmdLineParams
+    runShellCommand(goSrc2CpgCommand.toString, arguments).map(_ => outputPath)
+  }
+
+  def go2CpgAvailable(): Boolean = {
+    if (isWin) rootPath.resolve("go2cpg.ps1").toFile.exists() else rootPath.resolve("go2cpg.sh").toFile.exists()
+  }
+
+  def goSrc2CpgAvailable(): Boolean = {
+    goSrc2CpgCommand.toFile.exists
+  }
+
+  override def isAvailable: Boolean = go2CpgAvailable() || goSrc2CpgAvailable()
 
   override def isJvmBased = false
 }
