f9eafbca87b39ab1ef0c9a5fad4f6547c26e2ffb
Pandurang Patil <pandurang.patil@privado.ai>
2023-06-23 14:47:17 +0530

    goastgen integration to generate ast
    1. Integrated goastgen to be invoked to parse the go source code and
    generate the AST for each file in JSON format at a temp location.
    2. Created AstCreationPass passing the parsed file list, which is being
    logged to console at this moment.

--- joern-cli/frontends/gosrc2cpg/src/main/scala/io/joern/gosrc2cpg/utils/AstGenRunner.scala
+++ joern-cli/frontends/gosrc2cpg/src/main/scala/io/joern/gosrc2cpg/utils/AstGenRunner.scala
@@ -1,9 +1,14 @@
 package io.joern.gosrc2cpg.utils
 
 import better.files.File
+import com.typesafe.config.ConfigFactory
 import io.joern.gosrc2cpg.Config
+import io.joern.x2cpg.SourceFiles
+import io.joern.x2cpg.utils.{Environment, ExternalCommand}
 import org.slf4j.LoggerFactory
+import versionsort.VersionHelper
 
+import java.nio.file.Paths
 import scala.util.{Failure, Success, Try}
 
 object AstGenRunner {
@@ -12,23 +17,123 @@ object AstGenRunner {
     parsedFiles: List[(String, String)] = List.empty,
     skippedFiles: List[(String, String)] = List.empty
   )
+  lazy val GoAstgenWin      = "goastgen-windows.exe"
+  lazy val GoAstgenLinux    = "goastgen-linux"
+  lazy val GoAstgenLinuxArm = "goastgen-linux-arm64"
+  lazy val GoAstgenMac      = "goastgen-macos"
+  lazy val GoAstgenMacArm   = "goastgen-macos-arm64"
+
+  lazy private val executableName = Environment.operatingSystem match {
+    case Environment.OperatingSystemType.Windows => GoAstgenWin
+    case Environment.OperatingSystemType.Linux =>
+      Environment.architecture match {
+        case Environment.ArchitectureType.X86 => GoAstgenLinux
+        case Environment.ArchitectureType.ARM => GoAstgenLinuxArm
+      }
+    case Environment.OperatingSystemType.Mac =>
+      Environment.architecture match {
+        case Environment.ArchitectureType.X86 => GoAstgenMac
+        case Environment.ArchitectureType.ARM => GoAstgenMacArm
+      }
+    case Environment.OperatingSystemType.Unknown =>
+      logger.warn("Could not detect OS version! Defaulting to 'Linux'.")
+      GoAstgenLinux
+  }
+
+  lazy private val executableDir: String = {
+    val dir        = getClass.getProtectionDomain.getCodeSource.getLocation.toString
+    val indexOfLib = dir.lastIndexOf("lib")
+    val fixedDir = if (indexOfLib != -1) {
+      new java.io.File(dir.substring("file:".length, indexOfLib)).toString
+    } else {
+      val indexOfTarget = dir.lastIndexOf("target")
+      if (indexOfTarget != -1) {
+        new java.io.File(dir.substring("file:".length, indexOfTarget)).toString
+      } else {
+        "."
+      }
+    }
+    Paths.get(fixedDir, "/bin/goastgen").toAbsolutePath.toString
+  }
+
+  private def hasCompatibleAstGenVersion(goastGenVersion: String): Boolean = {
+    ExternalCommand.run("goastgen -version", ".").toOption.map(_.mkString.strip()) match {
+      case Some(installedVersion)
+          if installedVersion != "unknown" &&
+            Try(VersionHelper.compare(installedVersion, goastGenVersion)).toOption.getOrElse(-1) >= 0 =>
+        logger.debug(s"Using local goastgen v$installedVersion from systems PATH")
+        true
+      case Some(installedVersion) =>
+        logger.debug(
+          s"Found local goastgen v$installedVersion in systems PATH but gosrc2cpg requires at least v$goastGenVersion"
+        )
+        false
+      case _ => false
+    }
+  }
+
+  private lazy val astGenCommand = {
+    val conf            = ConfigFactory.load
+    val goastGenVersion = conf.getString("gosrc2cpg.goastgen_version")
+    if (hasCompatibleAstGenVersion(goastGenVersion)) {
+      "goastgen"
+    } else {
+      s"$executableDir/$executableName"
+    }
+  }
 }
 
 class AstGenRunner(config: Config) {
   import io.joern.gosrc2cpg.utils.AstGenRunner._
 
-  private def runAstGenNative(in: String, out: File): Try[String] = {
-    ???
+  private def isIgnoredByUserConfig(filePath: String): Boolean = {
+    lazy val isInIgnoredFiles = config.ignoredFiles.exists {
+      case ignorePath if File(ignorePath).isDirectory => filePath.startsWith(ignorePath)
+      case ignorePath                                 => filePath == ignorePath
+    }
+    lazy val isInIgnoredFileRegex = config.ignoredFilesRegex.matches(filePath)
+    if (isInIgnoredFiles || isInIgnoredFileRegex) {
+      logger.debug(s"'$filePath' ignored by user configuration")
+      true
+    } else {
+      false
+    }
   }
 
+  private def skippedFiles(in: File, astGenOut: List[String]): List[String] = {
+    val skipped = astGenOut.collect {
+      case out if !out.startsWith("Converted") =>
+        val filename = out.substring(0, out.indexOf(" "))
+        val reason   = out.substring(out.indexOf(" ") + 1)
+        logger.warn(s"\t- failed to parse '${in / filename}': '$reason'")
+        Option(filename)
+      case out =>
+        logger.debug(s"\t+ $out")
+        None
+    }
+    skipped.flatten
+  }
+
+  private def filterFiles(files: List[String], out: File): List[String] = {
+    files.filter { file =>
+      file.stripSuffix(".json").replace(out.pathAsString, config.inputPath) match {
+        case filePath if isIgnoredByUserConfig(filePath) => false
+        case _                                           => true
+      }
+    }
+  }
+
+  private def runAstGenNative(in: String, out: File): Try[Seq[String]] =
+    ExternalCommand.run(s"$astGenCommand -out ${out.toString()} $in", ".")
+
   def execute(out: File): AstGenRunnerResult = {
+    val in = File(config.inputPath)
     logger.info(s"Running goastgen in '$config.inputPath' ...")
     runAstGenNative(config.inputPath, out) match {
       case Success(result) =>
-        AstGenRunnerResult()
-//        val parsed = filterFiles(SourceFiles.determine(out.toString(), Set(".json")), out)
-//        val skipped = skippedFiles(in, result.toList)
-//        AstGenRunnerResult(parsed.map((in.toString(), _)), skipped.map((in.toString(), _)))
+        val parsed  = filterFiles(SourceFiles.determine(out.toString(), Set(".json")), out)
+        val skipped = skippedFiles(in, result.toList)
+        AstGenRunnerResult(parsed.map((in.toString(), _)), skipped.map((in.toString(), _)))
       case Failure(f) =>
         logger.error("\t- running astgen failed!", f)
         AstGenRunnerResult()
