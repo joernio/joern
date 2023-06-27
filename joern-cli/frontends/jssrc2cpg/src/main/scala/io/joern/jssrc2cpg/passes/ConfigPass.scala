045cbc983fa1c30ed72ed5de5b97918237ce3454
Pandurang Patil <pandurang.patil@gmail.com>
2023-06-27 19:35:34 +0530

    [go2cpg] Initial basic layer of code to create the CPG (#2946)
    1. Initial test setup with metaData pass
    2. Refactored and moved some common utilities from jssrc2cpg to x2cpg module.
    3. Created setup for consuming AST Json
    
    ---------
    
    Co-authored-by: Khemraj Rathore <khemraj.rathore@privado.ai>

--- joern-cli/frontends/jssrc2cpg/src/main/scala/io/joern/jssrc2cpg/passes/ConfigPass.scala
+++ joern-cli/frontends/jssrc2cpg/src/main/scala/io/joern/jssrc2cpg/passes/ConfigPass.scala
@@ -1,10 +1,9 @@
 package io.joern.jssrc2cpg.passes
 
 import better.files.File
-import io.joern.jssrc2cpg.utils.Report
-import io.joern.jssrc2cpg.utils.TimeUtils
 import io.joern.jssrc2cpg.Config
 import io.joern.x2cpg.SourceFiles
+import io.joern.x2cpg.utils.{Report, TimeUtils}
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.codepropertygraph.generated.nodes.NewConfigFile
 import io.shiftleft.passes.ConcurrentWriterCpgPass
