384413ab84319a51f676ee9fcd5a0e0c3276d5d9
Pandurang Patil <pandurang.patil@privado.ai>
2023-06-27 14:19:23 +0530

    JSON ast into consumable form
    1. Refactored and moved some common utilities from jssrc2cpg to x2cpg
    module.
    2. Created setup for consuming AST Json

--- joern-cli/frontends/gosrc2cpg/src/main/scala/io/joern/gosrc2cpg/GoSrc2Cpg.scala
+++ joern-cli/frontends/gosrc2cpg/src/main/scala/io/joern/gosrc2cpg/GoSrc2Cpg.scala
@@ -5,17 +5,20 @@ import io.joern.gosrc2cpg.passes.AstCreationPass
 import io.joern.gosrc2cpg.utils.AstGenRunner
 import io.joern.x2cpg.X2Cpg.withNewEmptyCpg
 import io.joern.x2cpg.X2CpgFrontend
+import io.joern.x2cpg.utils.Report
 import io.shiftleft.codepropertygraph.Cpg
 
 import scala.util.Try
 
 class GoSrc2Cpg extends X2CpgFrontend[Config] {
+  private val report: Report = new Report()
 
   def createCpg(config: Config): Try[Cpg] = {
     withNewEmptyCpg(config.outputPath, config) { (cpg, config) =>
       File.usingTemporaryDirectory("gosrc2cpgOut") { tmpDir =>
         val astGenResult = new AstGenRunner(config).execute(tmpDir)
-        new AstCreationPass(cpg, astGenResult, config).createAndApply()
+        new AstCreationPass(cpg, astGenResult, config, report).createAndApply()
+        report.print()
       }
     }
   }
--- joern-cli/frontends/gosrc2cpg/src/main/scala/io/joern/gosrc2cpg/passes/AstCreationPass.scala
+++ joern-cli/frontends/gosrc2cpg/src/main/scala/io/joern/gosrc2cpg/passes/AstCreationPass.scala
@@ -1,18 +1,40 @@
 package io.joern.gosrc2cpg.passes
 
 import io.joern.gosrc2cpg.Config
+import io.joern.gosrc2cpg.astcreation.AstCreator
+import io.joern.gosrc2cpg.parser.GoAstJsonParser
 import io.joern.gosrc2cpg.utils.AstGenRunner.AstGenRunnerResult
+import io.joern.x2cpg.utils.{Report, TimeUtils}
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.passes.ConcurrentWriterCpgPass
+import io.shiftleft.utils.IOUtils
 import org.slf4j.{Logger, LoggerFactory}
 
-class AstCreationPass(cpg: Cpg, astGenRunnerResult: AstGenRunnerResult, config: Config)
+import java.nio.file.Paths
+import scala.util.{Failure, Success, Try}
+
+class AstCreationPass(cpg: Cpg, astGenRunnerResult: AstGenRunnerResult, config: Config, report: Report)
     extends ConcurrentWriterCpgPass[(String, String)](cpg) {
   private val logger: Logger                            = LoggerFactory.getLogger(classOf[AstCreationPass])
   override def generateParts(): Array[(String, String)] = astGenRunnerResult.parsedFiles.toArray
-  override def runOnPart(builder: DiffGraphBuilder, input: (String, String)): Unit = {
+  override def runOnPart(diffGraph: DiffGraphBuilder, input: (String, String)): Unit = {
     val (rootPath, jsonFilename) = input
-    println(rootPath)
-    println(jsonFilename)
+    val ((gotCpg, filename), duration) = TimeUtils.time {
+      val parseResult = GoAstJsonParser.readFile(Paths.get(jsonFilename))
+      val fileLOC     = IOUtils.readLinesInFile(Paths.get(parseResult.fullPath)).size
+      report.addReportInfo(parseResult.filename, fileLOC, parsed = true)
+      Try {
+        val localDiff = new AstCreator(config, parseResult).createAst()
+        diffGraph.absorb(localDiff)
+      } match {
+        case Failure(exception) =>
+          logger.warn(s"Failed to generate a CPG for: '${parseResult.fullPath}'", exception)
+          (false, parseResult.filename)
+        case Success(_) =>
+          logger.info(s"Generated a CPG for: '${parseResult.fullPath}'")
+          (true, parseResult.filename)
+      }
+    }
+    report.updateReport(filename, cpg = gotCpg, duration)
   }
 }
