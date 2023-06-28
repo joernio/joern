59fa99860a49dac110166e48ee58bdc695d0e415
Khemraj Rathore <khemraj.rathore@privado.ai>
2023-06-28 20:13:23 +0530

    add - ast for variable declaration and initialization

--- joern-cli/frontends/gosrc2cpg/src/main/scala/io/joern/gosrc2cpg/astcreation/AstCreatorHelper.scala
+++ joern-cli/frontends/gosrc2cpg/src/main/scala/io/joern/gosrc2cpg/astcreation/AstCreatorHelper.scala
@@ -1,8 +1,9 @@
 package io.joern.gosrc2cpg.astcreation
 
-import io.joern.gosrc2cpg.parser.ParserAst.{ParserNode, fromString}
+import io.joern.gosrc2cpg.parser.ParserAst.{ImportSpec, ParserNode, fromString}
 import ujson.Value
 import io.joern.gosrc2cpg.parser.{ParserKeys, ParserNodeInfo}
+import io.joern.x2cpg.Ast
 import org.apache.commons.lang.StringUtils
 
 import scala.util.Try
@@ -35,5 +36,30 @@ trait AstCreatorHelper { this: AstCreator =>
   protected def line(node: Value): Option[Integer] = Try(node(ParserKeys.NodeLineNo).num).toOption.map(_.toInt)
 
   protected def column(node: Value): Option[Integer] = Try(node(ParserKeys.NodeLineNo).num).toOption.map(_.toInt)
+
+  def isImportDeclaration(genDecl: ParserNodeInfo) = {
+    Try(genDecl.json(ParserKeys.Specs).arr.map(createParserNodeInfo).exists(_.node == ImportSpec)).toOption
+      .getOrElse(false)
+  }
+  def astForImport(genDecl: ParserNodeInfo): Ast = {
+    genDecl.json(ParserKeys.Specs).arr.map(createParserNodeInfo).foreach { nodeInfo =>
+      nodeInfo.node match {
+        case ImportSpec =>
+          val basicLit       = createParserNodeInfo(nodeInfo.json(ParserKeys.Path))
+          val importedEntity = nodeInfo.json(ParserKeys.Path).obj(ParserKeys.Value).str
+          val importedAs =
+            Try(nodeInfo.json(ParserKeys.Name).obj(ParserKeys.Name).str).toOption.getOrElse(importedEntity)
+          val importedAsReplacement = if (importedEntity.equals(importedAs)) "" else s"$importedAs "
+          // This may be better way to add code for import node
+          val importNode =
+            newImportNode(s"import $importedAsReplacement$importedEntity", importedEntity, importedAs, basicLit)
+          // Adding import node directly because it is not a Ast Node
+          diffGraph.addNode(importNode)
+        case _ =>
+      }
+    }
+    // We add imports directly to the graph, so return empty Ast
+    Ast()
+  }
 
 }
