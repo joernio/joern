2854777465382adbd8c9b0b06fba5c8015d45957
Reuben Steenekamp <reuben.steenekamp@gmail.com>
2023-07-04 11:56:34 +0200

    Add nix flake devShell.

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
+  };
+
+  outputs = { self, nixpkgs, utils, ... }@inputs:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            sbt
+            jdk19
+          ];
+        };
+      });
+}
