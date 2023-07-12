e7698b2bed28f64ebf2c5b965d682659a1639016
Michael Pollmeier <michael@michaelpollmeier.com>
2023-07-12 17:27:19 +0200

    upgrade other com-haoyili deps

--- project/Versions.scala
+++ project/Versions.scala
@@ -10,6 +10,7 @@ object Versions {
   val gradleTooling = "8.2"
   val circe         = "0.14.5"
   val requests      = "0.8.0"
+  val upickle       = "3.1.2"
 
   private def parseVersion(key: String): String = {
     val versionRegexp = s""".*val $key[ ]+=[ ]?"(.*?)"""".r
