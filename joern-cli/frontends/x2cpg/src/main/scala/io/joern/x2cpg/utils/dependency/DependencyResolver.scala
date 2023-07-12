f88269ed5eac3836d0c96417b987beda70771e14
Claudiu-Vlad Ursache <claudiu+hw@shiftleft.io>
2023-07-12 15:27:27 -0400

    x2cpg: move maven coordinates extraction to class (#3095)

--- joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/utils/dependency/DependencyResolver.scala
+++ joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/utils/dependency/DependencyResolver.scala
@@ -56,53 +56,7 @@ object DependencyResolver {
         Seq()
     }
 
-    /*
-    on the following regex, for the following input:
-    ```
-    |    |    +--- org.springframework.boot:spring-boot-starter-logging:3.0.5
-    |    |    |    +--- ch.qos.logback:logback-classic:1.4.6
-    |    |    |    |    +--- ch.qos.logback:logback-core:1.4.6
-    |    |    |    |    \--- org.slf4j:slf4j-api:2.0.4 -> 2.0.7
-    |    |    |    +--- org.apache.logging.log4j:log4j-to-slf4j:2.19.0
-    |    |    |    |    +--- org.slf4j:slf4j-api:1.7.36 -> 2.0.7
-    |    |    |    |    \--- org.apache.logging.log4j:log4j-api:2.19.0 -> 2.15.0
-    |    |    |    \--- org.slf4j:jul-to-slf4j:2.0.7
-    ```
-    the resulting matches and their groups are:
-    ```
-    org.springframework.boot:spring-boot-starter-logging:3.0.5
-    ^g1 ------------------------------------------------^^g2 ^
-    ch.qos.logback:logback-classic:1.4.6
-    ^g1 --------------------------^^g2 ^
-    ch.qos.logback:logback-core:1.4.6
-    ^g1 -----------------------^^g2 ^
-    org.slf4j:slf4j-api:2.0.4 -> 2.0.7
-    ^g1 ---------------^^g2 ^^g3^^g4 ^
-    org.apache.logging.log4j:log4j-to-slf4j:2.19.0
-    ^g1 -----------------------------------^^g2 -^
-    org.slf4j:slf4j-api:1.7.36 -> 2.0.7
-    ^g1 ---------------^^g2 -^^g3^^g4 ^
-    org.apache.logging.log4j:log4j-api:2.19.0 -> 2.15.0
-    ^g1 ------------------------------^^g2 -^^g3^^g4 -^
-    org.slf4j:jul-to-slf4j:2.0.7
-    ^g1 ------------------^^g2 ^
-    ```
-     */
-    val pattern = """^[| ]?[+\\]\s*[-]*\s*([^:]+:[^:]+:)([^\s]+)(\s+->\s+)?([^\s]+)?""".r
-    val coordinates = lines
-      .flatMap { l =>
-        pattern.findFirstMatchIn(l) match {
-          case Some(m) =>
-            if (Option(m.group(4)).isEmpty)
-              Some(m.group(1) + m.group(2))
-            else
-              Some(m.group(1) + m.group(4))
-          case _ => None
-        }
-      }
-      .distinct
-      .toList
-
+    val coordinates = MavenCoordinates.fromGradleOutput(lines)
     logger.info("Got {} Maven coordinates", coordinates.size)
     Some(coordinates)
   }
