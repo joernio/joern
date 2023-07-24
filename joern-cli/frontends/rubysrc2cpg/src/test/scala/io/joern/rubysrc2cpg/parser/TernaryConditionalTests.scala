3af3177a22ec387a3fb7aef1280b51925701f0db
Xavier Pinho <xpinho@shiftleft.io>
2023-07-24 14:39:44 +0100

    scalafmt

--- joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/parser/TernaryConditionalTests.scala
+++ joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/parser/TernaryConditionalTests.scala
@@ -3,9 +3,9 @@ package io.joern.rubysrc2cpg.parser
 class TernaryConditionalTests extends RubyParserAbstractTest {
 
   "Ternary conditional expressions" should {
-    
+
     "be parsed as expressions" when {
-      
+
       "they are a standalone one-line expression" in {
         val code = "x ? y : z"
         printAst(_.expression(), code) shouldEqual
@@ -30,9 +30,9 @@ class TernaryConditionalTests extends RubyParserAbstractTest {
             |   VariableIdentifierVariableReference
             |    VariableIdentifier
             |     z""".stripMargin
-        
+
       }
-      
+
       "they are a standalone multi-line expression" in {
         val code =
           """x ?
@@ -65,6 +65,5 @@ class TernaryConditionalTests extends RubyParserAbstractTest {
       }
     }
   }
-  
-  
+
 }
