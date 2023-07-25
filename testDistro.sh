062b66a1e7cd0499a1d74a8a6514fc8c8c9d6c58
Michael Pollmeier <michael@michaelpollmeier.com>
2023-07-25 15:41:01 +0200

    WIP do not merge - various things for debugging CI
    * minimise test
    * use ubuntu 20
    * use oracle jdk17
    * run in a loop, don't stop on error
    * log exit code
    * upload artifacts if smoketest failed

--- testDistro.sh
+++ testDistro.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e #stop on error
+# set -e #stop on error
 # set -x #verbose on
 
 readonly SCRIPT_ABS_PATH=$(readlink -f "$0")
@@ -7,11 +7,30 @@ readonly SCRIPT_ABS_DIR=$(dirname "$SCRIPT_ABS_PATH")
 readonly REPO_ROOT=$SCRIPT_ABS_DIR
 
 echo "staging joern"
+
 pushd $REPO_ROOT
-  sbt -Dsbt.log.noformat=true clean joerncli/stage querydb/createDistribution
-  tests/frontends-tests.sh
-  tests/scripts-test.sh
-  tests/querydb-test.sh
+  sbt -Dsbt.log.noformat=true clean joerncli/stage
+
+  # when only running frontends-test 10 times the result is either consistently green or consistently red...
+  # for i in {0..9}; do
+    # echo "XX0 run $i start"
+    # tests/frontends-tests.sh 
+    # exit_code=$?
+    # echo "XX0 run $i end; exit_code=$exit_code"
+  # done
+
+  tests/frontends-tests.sh 
+  exit_code=$?
+  mkdir target/debug-artifacts
+
+  if [ $exit_code -ne 0 ]
+  then
+    echo "XX1 error"
+    cp -rp joern-cli/target/universal/stage/lib/* target/debug-artifacts
+  else
+    echo "XX1 success"
+    echo success > target/debug-artifacts/success.txt
+  fi
 popd
 
 echo "success. go analyse some code"
