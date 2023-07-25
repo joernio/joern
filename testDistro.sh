6a52b2ea494991880cf10413c8b200221bed3b44
Michael Pollmeier <michael@michaelpollmeier.com>
2023-07-25 15:41:01 +0200

    WIP do not merge - various things for debugging CI
    * minimise test
    * use ubuntu 20
    * use oracle jdk17
    * run in a loop, don't stop on error
    * log exit code
    * upload artifacts if smoketest failed
    * run entire test in a loop, incl. staging

--- testDistro.sh
+++ testDistro.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e #stop on error
+# set -e #stop on error
 # set -x #verbose on
 
 readonly SCRIPT_ABS_PATH=$(readlink -f "$0")
@@ -7,11 +7,27 @@ readonly SCRIPT_ABS_DIR=$(dirname "$SCRIPT_ABS_PATH")
 readonly REPO_ROOT=$SCRIPT_ABS_DIR
 
 echo "staging joern"
-pushd $REPO_ROOT
-  sbt -Dsbt.log.noformat=true clean joerncli/stage querydb/createDistribution
-  tests/frontends-tests.sh
-  tests/scripts-test.sh
-  tests/querydb-test.sh
-popd
 
-echo "success. go analyse some code"
+pushd $REPO_ROOT
+  # sbt -Dsbt.log.noformat=true clean joerncli/stage
+
+
+  for i in {0..9}; do
+    echo "XX0 run $i start"
+    sbt -Dsbt.log.noformat=true clean joerncli/stage
+    tests/frontends-tests.sh 
+    exit_code=$?
+    echo "XX0 run $i end; exit_code=$exit_code"
+
+    mkdir -p target/debug-artifacts/$i
+    if [ $exit_code -ne 0 ]
+    then
+      echo "XX1 error"
+      cp -rp joern-cli/target/universal/stage/lib/* target/debug-artifacts/$i
+    else
+      echo "XX1 success"
+      echo success > target/debug-artifacts/$i/success.txt
+    fi
+  done
+
+popd
