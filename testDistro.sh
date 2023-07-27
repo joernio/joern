704c7b84f3befee37fb63b1bc2f1527faa14cd6e
Michael Pollmeier <michael@michaelpollmeier.com>
2023-07-27 12:14:17 +0200

    run in a loop, don't stop on error

--- testDistro.sh
+++ testDistro.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e #stop on error
+# set -e #stop on error
 # set -x #verbose on
 
 readonly SCRIPT_ABS_PATH=$(readlink -f "$0")
@@ -8,8 +8,14 @@ readonly REPO_ROOT=$SCRIPT_ABS_DIR
 
 echo "staging joern"
 pushd $REPO_ROOT
-  sbt -Dsbt.log.noformat=true clean joerncli/stage querydb/createDistribution
-  tests/frontends-tests.sh
+  sbt -Dsbt.log.noformat=true clean joerncli/stage
+
+  # run test 10 times - sometimes fails...
+  for i in {0..9}; do
+    echo "XX0 run $i start"
+    tests/frontends-tests.sh 
+    echo "XX0 run $i end"
+  done
 popd
 
 echo "success. go analyse some code"
