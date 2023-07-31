c4af52aea341e7aad2f4e6795f53ae54039b67c2
Xavier Pinho <xpinho@shiftleft.io>
2023-07-31 15:04:01 +0100

    scalafmt

--- joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/parser/ArrayTests.scala
+++ joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/parser/ArrayTests.scala
@@ -1,11 +1,11 @@
 package io.joern.rubysrc2cpg.parser
 
 class ArrayTests extends RubyParserAbstractTest {
-  
+
   "An empty array literal" should {
-    
+
     "be parsed as a primary expression" when {
-      
+
       "it uses the traditional [, ] delimiters" in {
         val code = "[]"
         printAst(_.primary(), code) shouldEqual
@@ -14,7 +14,7 @@ class ArrayTests extends RubyParserAbstractTest {
             |  [
             |  ]""".stripMargin
       }
-      
+
       "it uses the %w[ ] delimiters" in {
         val code = "%w[]"
         printAst(_.primary(), code) shouldEqual
@@ -22,15 +22,15 @@ class ArrayTests extends RubyParserAbstractTest {
             | ArrayConstructor
             |  %w[
             |  ]""".stripMargin
-        
+
       }
     }
   }
-  
+
   "A non-empty array literal" should {
-    
+
     "be parsed as a primary expression" when {
-      
+
       "it uses the %w[ ] delimiters" in {
         val code = "%w[x y z]"
         printAst(_.primary(), code) shouldEqual
@@ -42,7 +42,7 @@ class ArrayTests extends RubyParserAbstractTest {
             |  z
             |  ]""".stripMargin
       }
-      
+
       "it uses the %w( ) delimiters" in {
         val code = "%w(x y z)"
         printAst(_.primary(), code) shouldEqual
@@ -54,7 +54,7 @@ class ArrayTests extends RubyParserAbstractTest {
             |  z
             |  )""".stripMargin
       }
-      
+
       "it uses the %w{ } delimiters" in {
         val code = "%w{x y z}"
         printAst(_.primary(), code) shouldEqual
@@ -66,7 +66,7 @@ class ArrayTests extends RubyParserAbstractTest {
             |  z
             |  }""".stripMargin
       }
-      
+
       "it uses the %w- - delimiters" in {
         val code = "%w-x y z-"
         printAst(_.primary(), code) shouldEqual
