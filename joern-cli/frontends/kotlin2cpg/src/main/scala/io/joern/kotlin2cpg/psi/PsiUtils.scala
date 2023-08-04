3cc7630d7903b4844ce01b87b8a1ab2dc85c06da
Claudiu-Vlad Ursache <claudiu@ursache.io>
2023-08-04 11:02:10 +0200

    kotlin2cpg: improve object literal idx counting
    a tree visitor is much more suited to the task

--- joern-cli/frontends/kotlin2cpg/src/main/scala/io/joern/kotlin2cpg/psi/PsiUtils.scala
+++ joern-cli/frontends/kotlin2cpg/src/main/scala/io/joern/kotlin2cpg/psi/PsiUtils.scala
@@ -2,16 +2,11 @@ package io.joern.kotlin2cpg.psi
 
 import org.jetbrains.kotlin.com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.psi.{
-  KtBlockExpression,
-  KtCallExpression,
   KtDestructuringDeclaration,
   KtDestructuringDeclarationEntry,
   KtElement,
-  KtExpression,
-  KtFunctionLiteral,
-  KtNamedFunction,
-  KtObjectLiteralExpression,
-  KtProperty
+  KtObjectDeclaration,
+  KtTreeVisitorVoid
 }
 
 import scala.jdk.CollectionConverters.CollectionHasAsScala
@@ -24,42 +19,28 @@ object PsiUtils {
   }
 
   def objectIdxMaybe(psiElem: PsiElement, containing: PsiElement) = {
-    def idxFor(bodyBlockExpression: Option[KtBlockExpression], bodyExpression: Option[KtExpression]): Option[Int] = {
-      val bodyStatements =
-        if (bodyBlockExpression.nonEmpty) bodyBlockExpression.get.getStatements.asScala.toSeq
-        else if (bodyExpression.nonEmpty) Seq(bodyExpression.get)
-        else Seq()
-
-      val anonymousObjects =
-        bodyStatements.collect {
-          case pt: KtProperty =>
-            pt.getDelegateExpressionOrInitializer match {
-              case ol: KtObjectLiteralExpression => Some(ol.getObjectDeclaration)
-              case _                             => None
-            }
-          case c: KtCallExpression =>
-            c.getValueArguments.asScala
-              .map(_.getArgumentExpression)
-              .collect {
-                case ol: KtObjectLiteralExpression => Some(ol.getObjectDeclaration)
-                case _                             => None
-              }
-              .flatten
-          case _ => Seq()
-        }.flatten
-      var outIdx: Option[Int] = None
-      anonymousObjects.zipWithIndex.foreach { case (elem: PsiElement, idx) =>
-        if (elem == psiElem) outIdx = Some(idx + 1)
+    class ForEachTreeVisitor(block: (KtElement) => Unit) extends KtTreeVisitorVoid {
+      override def visitKtElement(element: KtElement): Unit = {
+        super.visitKtElement(element)
+        block(element)
       }
-      outIdx
     }
-    containing match {
-      case l: KtFunctionLiteral =>
-        idxFor(Option(l.getBodyBlockExpression), Option(l.getBodyExpression))
-      case t: KtNamedFunction =>
-        idxFor(Option(t.getBodyBlockExpression), Option(t.getBodyExpression))
-      case _ => None
+
+    val buf = scala.collection.mutable.ListBuffer.empty[KtObjectDeclaration]
+    val visitor =
+      new ForEachTreeVisitor({ elem =>
+        elem match {
+          case e: KtObjectDeclaration =>
+            buf.append(e)
+          case _ =>
+        }
+      })
+    visitor.visitKtElement(containing.asInstanceOf[KtElement])
+    var outIdx: Option[Int] = None
+    buf.zipWithIndex.foreach { case (elem: PsiElement, idx) =>
+      if (elem == psiElem) outIdx = Some(idx + 1)
     }
+    outIdx
   }
 }
 
