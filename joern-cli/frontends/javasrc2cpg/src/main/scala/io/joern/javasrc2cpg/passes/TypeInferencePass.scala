02a375ed132aac18e7430b55d8c70331b1639bad
Johannes Coetzee <jcoetzee@qwiet.ai>
2023-08-15 14:03:43 +0200

    Speed up javasrc type inference pass (#3500)
    * Speed up javasrc type inference pass
    
    * Clean up
    
    * Fix multi-method detection logic
    
    * Don't group calls by fullname
    
    * Fix case where argument types differ
    
    * Add clarifying comment
    
    * Fix non-unique cache keys

--- joern-cli/frontends/javasrc2cpg/src/main/scala/io/joern/javasrc2cpg/passes/TypeInferencePass.scala
+++ joern-cli/frontends/javasrc2cpg/src/main/scala/io/joern/javasrc2cpg/passes/TypeInferencePass.scala
@@ -5,80 +5,97 @@ import com.google.common.cache.CacheBuilder
 import io.joern.x2cpg.Defines
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.codepropertygraph.generated.ModifierTypes
-import io.shiftleft.codepropertygraph.generated.nodes.Call.PropertyNames
 import io.shiftleft.codepropertygraph.generated.nodes.{Call, Method}
 import io.shiftleft.passes.ConcurrentWriterCpgPass
 import io.shiftleft.semanticcpg.language._
 import org.slf4j.LoggerFactory
 
 import scala.jdk.OptionConverters.RichOptional
+import io.joern.x2cpg.Defines.UnresolvedNamespace
+import io.shiftleft.codepropertygraph.generated.nodes.Call.PropertyNames
 
 class TypeInferencePass(cpg: Cpg) extends ConcurrentWriterCpgPass[Call](cpg) {
 
-  private val logger         = LoggerFactory.getLogger(this.getClass)
-  private val cache          = new GuavaCache(CacheBuilder.newBuilder().build[String, Option[Method]]())
-  private val namePartsCache = new GuavaCache(CacheBuilder.newBuilder().build[String, NameParts]())
+  private val cache = new GuavaCache(CacheBuilder.newBuilder().build[String, Option[Method]]())
+  private val resolvedMethodIndex = cpg.method
+    .filterNot(_.fullName.startsWith(Defines.UnresolvedNamespace))
+    .filterNot(_.signature.startsWith(Defines.UnresolvedSignature))
+    .groupBy(_.name)
 
-  private case class NameParts(typeDecl: Option[String], signature: Option[String])
-
-  private val NamePartsPattern = raw"(.*\.)*.*:(.+\(.*\))".r
+  private case class NameParts(typeDecl: Option[String], signature: String)
 
   override def generateParts(): Array[Call] = {
-    cpg.call.methodFullName(s".*(${Defines.UnresolvedNamespace}|${Defines.UnresolvedSignature}).*").toArray
+    cpg.call
+      .filter(_.signature.startsWith(Defines.UnresolvedSignature))
+      .filterNot { _.name.startsWith(UnresolvedNamespace) }
+      .toArray
   }
 
   private def isMatchingMethod(method: Method, call: Call, callNameParts: NameParts): Boolean = {
     // An erroneous `this` argument is added for unresolved calls to static methods.
-    val argSizeMod = if (method.modifier.modifierType.iterator.contains(ModifierTypes.STATIC)) 1 else 0
-    if (
-      method.parameter.size != (call.argument.size - argSizeMod) || callNameParts.typeDecl.contains(
-        Defines.UnresolvedNamespace
-      )
-    ) {
-      false
-    } else {
-      val methodNameParts = getNameParts(method.fullName)
-      callNameParts.typeDecl == methodNameParts.typeDecl
-    }
+    val argSizeMod           = if (method.modifier.modifierType.iterator.contains(ModifierTypes.STATIC)) 1 else 0
+    lazy val methodNameParts = getNameParts(method.name, method.fullName)
 
+    val parameterSizesMatch =
+      (method.parameter.size == (call.argument.size - argSizeMod))
+
+    lazy val argTypesMatch = doArgumentTypesMatch(method: Method, call: Call, skipCallThis = argSizeMod == 1)
+
+    lazy val typeDeclMatches = (callNameParts.typeDecl == methodNameParts.typeDecl)
+
+    parameterSizesMatch && argTypesMatch && typeDeclMatches
   }
 
-  private def getNameParts(fullName: String): NameParts = {
-    namePartsCache.get(fullName).toScala.getOrElse {
-      val nameParts = fullName match {
-        case NamePartsPattern(maybeTd, maybeSig) =>
-          val typeDecl  = Option(maybeTd).map(_.init)
-          val signature = Option(maybeSig)
-          NameParts(typeDecl, signature)
+  /** Check if argument types match by comparing exact full names. TODO: Take inheritance hierarchies into account
+    */
+  private def doArgumentTypesMatch(method: Method, call: Call, skipCallThis: Boolean): Boolean = {
+    val callArgs = if (skipCallThis) call.argument.toList.tail else call.argument.toList
 
-        case _ =>
-          logger.warn(s"Could not extract name parts from $fullName")
-          NameParts(None, None)
-      }
-      namePartsCache.put(fullName, nameParts)
-      nameParts
+    val hasDifferingArg = method.parameter.zip(callArgs).exists { case (parameter, argument) =>
+      !Option(argument.property(PropertyNames.TypeFullName).toString()).contains(parameter.typeFullName)
     }
+
+    !hasDifferingArg
+  }
+
+  private def getNameParts(name: String, fullName: String): NameParts = {
+    val Array(qualifiedName, signature) = fullName.split(":", 2)
+
+    val typeDeclName = qualifiedName.stripSuffix(name) match {
+      case "" => None
+
+      case typeDeclName => Some(typeDeclName)
+    }
+
+    NameParts(typeDeclName, signature)
   }
 
   private def getReplacementMethod(call: Call): Option[Method] = {
-    cache.get(call.methodFullName).toScala.getOrElse {
-      val callNameParts    = getNameParts(call.methodFullName)
-      val candidateMethods = cpg.method.internal.nameExact(call.name)
-
-      val uniqueMatchingMethod = candidateMethods.find(isMatchingMethod(_, call, callNameParts)).flatMap { method =>
-        val otherMatchingMethod = candidateMethods.find(isMatchingMethod(_, call, callNameParts))
-        // Only return a resulting method if exactly one matching method is found.
-        Option.when(otherMatchingMethod.isEmpty)(method)
+    val argTypes =
+      call.argument.flatMap(arg => Option(arg.property(PropertyNames.TypeFullName)).map(_.toString)).mkString(":")
+    val callKey =
+      s"${call.methodFullName}:$argTypes"
+    cache.get(callKey).toScala.getOrElse {
+      val callNameParts = getNameParts(call.name, call.methodFullName)
+      resolvedMethodIndex.get(call.name).flatMap { candidateMethods =>
+        val candidateMethodsIter = candidateMethods.iterator
+        val uniqueMatchingMethod =
+          candidateMethodsIter.find(isMatchingMethod(_, call, callNameParts)).flatMap { method =>
+            val otherMatchingMethod = candidateMethodsIter.find(isMatchingMethod(_, call, callNameParts))
+            // Only return a resulting method if exactly one matching method is found.
+            Option.when(otherMatchingMethod.isEmpty)(method)
+          }
+        cache.put(callKey, uniqueMatchingMethod)
+        uniqueMatchingMethod
       }
-      cache.put(call.methodFullName, uniqueMatchingMethod)
-      uniqueMatchingMethod
     }
   }
+
   override def runOnPart(diffGraph: DiffGraphBuilder, call: Call): Unit = {
-    getReplacementMethod(call).foreach { method =>
-      diffGraph.setNodeProperty(call, PropertyNames.MethodFullName, method.fullName)
-      diffGraph.setNodeProperty(call, PropertyNames.Signature, method.signature)
-      diffGraph.setNodeProperty(call, PropertyNames.TypeFullName, method.methodReturn.typeFullName)
+    getReplacementMethod(call).foreach { replacementMethod =>
+      diffGraph.setNodeProperty(call, PropertyNames.MethodFullName, replacementMethod.fullName)
+      diffGraph.setNodeProperty(call, PropertyNames.Signature, replacementMethod.signature)
+      diffGraph.setNodeProperty(call, PropertyNames.TypeFullName, replacementMethod.methodReturn.typeFullName)
     }
   }
 }
