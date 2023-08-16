8b732790b0c4990dc85df6dd8c1c5760c4818571
Karan Batavia <karan.batavia@privado.ai>
2023-08-16 15:42:25 +0530

    more dataflow tests for operators

--- joern-cli/frontends/gosrc2cpg/src/test/scala/io/joern/go2cpg/dataflow/OperatorDataflowTests.scala
+++ joern-cli/frontends/gosrc2cpg/src/test/scala/io/joern/go2cpg/dataflow/OperatorDataflowTests.scala
@@ -41,5 +41,93 @@ class OperatorDataflowTests extends GoCodeToCpgSuite(withOssDataflow = true) {
       sink.reachableByFlows(source).size shouldBe 2
 
     }
+
+    "be reachable (case 3)" in {
+      val cpg = code("""
+          |package main
+          |func main() {
+          |	a := 10
+          |	b := 5
+          | c := 10
+          |	// Arithmetic Operators
+          |	c := a + b
+          |	c := a - b
+          |	c := a * b
+          |	c := a / b
+          |	c := a % b
+          |
+          |	// Comparison Operators
+          |	c := a == b
+          |	c := a != b
+          |	c := a > b
+          |	c := a < b
+          |	c := a >= b
+          |	c := a <= b
+          |
+          |	// Logical Operators
+          |	c := a && b
+          |	c := a || b
+          |
+          |	// Bitwise Operators
+          |	c := a & b
+          |	c := a | b
+          |	c := a ^ b
+          |	c := a << 2
+          |	c := a >> 2
+          |
+          | var d int = a
+          |}
+          |
+          |""".stripMargin)
+
+      val source = cpg.identifier("a")
+      val sink   = cpg.identifier("d")
+      sink.reachableByFlows(source).size shouldBe 20
+    }
+
+    "be reachable using shorthand assignment operators" in {
+      val cpg = code("""
+          |package main
+          |func main() {
+          | a := 5
+          | b := 10
+          | a += b
+          | a -= b
+          | a *= b
+          | a %= b
+          | a /= b
+          | a <<= b
+          | a >>= b
+          | a &= b
+          | a ^= b
+          | a |= b
+          | var c int = a
+          |}
+          |""".stripMargin)
+
+      val source = cpg.identifier("a")
+      val sink   = cpg.identifier("c")
+      sink.reachableByFlows(source).size shouldBe 12
+    }
+
+    "be reachable using unary operators" in {
+      val cpg = code("""
+          |package main
+          |func main() {
+          | var a int = 5
+          | var b int = 10
+          | b := !a
+          | c := &b
+          | d := +c
+          | e := -d
+          | c := *e
+          |}
+          |""".stripMargin)
+
+      val source = cpg.identifier("a")
+      val sink   = cpg.identifier("c")
+      sink.reachableByFlows(source).size shouldBe 4
+    }
   }
+
 }
