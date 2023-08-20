a9338ead6a2b3fa4a68f50e834dc4ff998794f5e
Michael Pollmeier <michael@michaelpollmeier.com>
2023-08-20 16:59:56 +0200

    upgrade to latest repl: fix `browse`, pipe operators (#3504)
    * bring `exit` back - does the same as `:exit`
    
    * old pipe operators: `|>` etc: deprecate and forward to new ones
    
    * upgrade to latest repl
    
    * upgrade all
    
    * browse: don't use `show`
    
    * latest repl without classpath issue
    
    * drop UnixUtilsTest - that functionality is now entirely part of replpp

--- console/src/main/scala/io/joern/console/package.scala
+++ console/src/main/scala/io/joern/console/package.scala
@@ -1,35 +1,43 @@
 package io.joern
 
-import better.files._
+import replpp.Operators.*
+import replpp.Colors
 
+// TODO remove any time after the end of 2023 - this is completely deprecated
 package object console {
 
   implicit class UnixUtils[A](content: Iterable[A]) {
+    given Colors = Colors.Default
 
     /** Iterate over left hand side operand and write to file. Think of it as the Ocular version of the Unix `>` shell
       * redirection.
       */
+    @deprecated("please use `#>` instead", "2.0.45 (August 2023)")
     def |>(outfile: String): Unit =
-      File(outfile).write(content.mkString("\n"))
+      content #> outfile
 
     /** Iterate over left hand side operand and append to file. Think of it as the Ocular version of the Unix `>>` shell
       * redirection.
       */
+    @deprecated("please use `#>>` instead", "2.0.45 (August 2023)")
     def |>>(outfile: String): Unit =
-      File(outfile).append("\n").append(content.mkString("\n"))
+      content #>> outfile
   }
 
   implicit class StringOps(value: String) {
+    given Colors = Colors.Default
 
     /** Pipe string to file. Think of it as the Ocular version of the Unix `>` shell redirection.
       */
+    @deprecated("please use `#>` instead", "2.0.45 (August 2023)")
     def |>(outfile: String): Unit =
-      File(outfile).write(value)
+      value #> outfile
 
     /** Append string to file. Think of it as the Ocular version of the Unix `>>` shell redirection.
       */
+    @deprecated("please use `#>>` instead", "2.0.45 (August 2023)")
     def |>>(outfile: String): Unit =
-      File(outfile).append("\n").append(value)
+      value #>> outfile
   }
 
 }
