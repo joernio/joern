451a972b09f205ddc76890d87c0210872e57390e
Johannes Coetzee <jcoetzee@qwiet.ai>
2023-08-21 17:17:51 +0200

    Have ANY argument types match every argument type in javasrc type inference pass

--- joern-cli/frontends/javasrc2cpg/src/test/scala/io/joern/javasrc2cpg/querying/TypeInferenceTests.scala
+++ joern-cli/frontends/javasrc2cpg/src/test/scala/io/joern/javasrc2cpg/querying/TypeInferenceTests.scala
@@ -10,6 +10,77 @@ import java.io.File
 
 class NewTypeInferenceTests extends JavaSrcCode2CpgFixture {
 
+  "a call with an unresolved argument with an ANY type" when {
+    "there is only one matching method should be resolved" in {
+      val cpg = code(
+        """
+          |package foo;
+          |
+          |import a.b.Bar;
+          |
+          |public class Foo {
+          |  public static Bar test(String s) {
+          |    return new Bar(s);
+          |  }
+          |
+          |  public void sink(Bar b) {}
+          |}
+          |""".stripMargin,
+        fileName = "Foo.java"
+      ).moreCode("""
+          |import foo.Foo;
+          |
+          |import baz.Baz;
+          |
+          |class Test {
+          |  public static Bar stringCall() {
+          |    sink(Foo.test(Baz.getString()));
+          |  }
+          |}
+          |""".stripMargin)
+
+      cpg.method.name("stringCall").call.name("test").methodFullName.l shouldBe List(
+        "foo.Foo.test:a.b.Bar(java.lang.String)"
+      )
+    }
+
+    "there are multiple matching methods should not be resolved" in {
+      val cpg = code(
+        """
+          |package foo;
+          |
+          |import a.b.Bar;
+          |
+          |public class Foo {
+          |  public static Bar test(String s) {
+          |    return new Bar(s);
+          |  }
+          |
+          |  public static Bar test(Integer i) {
+          |    return new Bar(s);
+          |  }
+          |  public void sink(Bar b) {}
+          |}
+          |""".stripMargin,
+        fileName = "Foo.java"
+      ).moreCode("""
+          |import foo.Foo;
+          |
+          |import baz.Baz;
+          |
+          |class Test {
+          |  public static Bar stringCall() {
+          |    sink(Foo.test(Baz.getString()));
+          |  }
+          |}
+          |""".stripMargin)
+
+      cpg.method.name("stringCall").call.name("test").methodFullName.l shouldBe List(
+        "foo.Foo.test:<unresolvedSignature>(1)"
+      )
+    }
+  }
+
   "methodFullNames for unresolved methods in source" should {
     val cpg = code(
       """
