e22cbc6dd67ff6eca822f7f25a30c8070fc15c44
“Hitesh <hitesh.bedre@privado.com>
2023-09-01 15:52:01 +0530

    gosrc2cpg: added structed test cases

--- joern-cli/frontends/gosrc2cpg/src/test/scala/io/joern/go2cpg/passes/ast/ASTCreationForStructureTests.scala
+++ joern-cli/frontends/gosrc2cpg/src/test/scala/io/joern/go2cpg/passes/ast/ASTCreationForStructureTests.scala
@@ -10,26 +10,31 @@ import io.shiftleft.semanticcpg.language.operatorextension.OpNodes
 import scala.collection.immutable.List
 
 class ASTCreationForStructureTests extends GoCodeToCpgSuite {
-  "AST Creation for structures" should {
-    "structure with single element" in {
 
-      val cpg = code("""
+  "structure with single element" should {
+
+    val cpg = code("""
           package main
           |
           |type Person struct {
           |    Name string
           |}
   """.stripMargin)
-      cpg.typeDecl.name("Person").size shouldBe 1
-      cpg.typeDecl.name("Person").head.fullName shouldBe "Person"
-      cpg.typeDecl.name("Person").member.size shouldBe 1
-      cpg.typeDecl("Person").member.name("Name").size shouldBe 1
-      cpg.typeDecl("Person").member.name("Name").typeFullName.head shouldBe "string"
+    cpg.typeDecl.name("Person").size shouldBe 1
+    cpg.typeDecl.name("Person").head.fullName shouldBe "Person"
+    cpg.typeDecl.name("Person").member.size shouldBe 1
+
+    "Check member node under type decl node" in {
+      val typeDeclNodes = cpg.typeDecl("Person").l
+      typeDeclNodes.member.name("Name").size shouldBe 1
+      typeDeclNodes.member.name("Name").typeFullName.head shouldBe "string"
+
     }
+  }
 
-    "structure with multiple element" in {
+  "structure with multiple element" should {
 
-      val cpg = code("""
+    val cpg = code("""
           package main
           |
           |type Person struct {
@@ -37,20 +42,27 @@ class ASTCreationForStructureTests extends GoCodeToCpgSuite {
           |    Age  int
           |}
   """.stripMargin)
-      cpg.typeDecl.name("Person").size shouldBe 1
-      cpg.typeDecl.name("Person").head.fullName shouldBe "Person"
+    cpg.typeDecl.name("Person").size shouldBe 1
+    cpg.typeDecl.name("Person").head.fullName shouldBe "Person"
+    cpg.typeDecl.name("Person").member.size shouldBe 2
 
-      cpg.typeDecl.name("Person").member.size shouldBe 2
-      cpg.typeDecl("Person").member.name("Name").size shouldBe 1
-
-      cpg.typeDecl("Person").member.name("Age").size shouldBe 1
-      cpg.typeDecl("Person").member.name("Name").typeFullName.head shouldBe "string"
-      cpg.typeDecl("Person").member.name("Age").typeFullName.head shouldBe "int"
+    "Check member node under type decl node" in {
+      val typeDeclNodes = cpg.typeDecl("Person").l
+      typeDeclNodes.member.name("Name").size shouldBe 1
+      typeDeclNodes.member.name("Age").size shouldBe 1
+      typeDeclNodes.member.name("Name").typeFullName.head shouldBe "string"
+      typeDeclNodes.member.name("Age").typeFullName.head shouldBe "int"
     }
 
-    "structure with element of type another structure" in {
+    "Traversing member node to typedecl node" in {
+      val typeDeclNode = cpg.member.name("Name").typeDecl.head
+      typeDeclNode.fullName shouldBe "Person"
+    }
+  }
 
-      val cpg = code("""
+  "structure with element of type another structure" should {
+
+    val cpg = code("""
           package main
           |
           |type Person struct {
@@ -63,24 +75,39 @@ class ASTCreationForStructureTests extends GoCodeToCpgSuite {
           |    Salary  int
           |}
   """.stripMargin)
-      cpg.typeDecl.name("Person").size shouldBe 1
-      cpg.typeDecl.name("Employee").size shouldBe 1
+    cpg.typeDecl.name("Person").size shouldBe 1
+    cpg.typeDecl.name("Employee").size shouldBe 1
 
-      cpg.typeDecl.name("Person").head.fullName shouldBe "Person"
-      cpg.typeDecl.name("Employee").head.fullName shouldBe "Employee"
+    cpg.typeDecl.name("Person").head.fullName shouldBe "Person"
+    cpg.typeDecl.name("Employee").head.fullName shouldBe "Employee"
 
-      cpg.typeDecl.name("Person").member.size shouldBe 2
-      cpg.typeDecl.name("Employee").member.size shouldBe 2
+    "Check member node under type decl node for Person typeDecl" in {
+      val personNode = cpg.typeDecl.name("Person").l
 
-      cpg.typeDecl("Person").member.name("Name").size shouldBe 1
-      cpg.typeDecl("Person").member.name("Age").size shouldBe 1
-      cpg.typeDecl("Employee").member.name("Salary").size shouldBe 1
-
-      cpg.typeDecl("Person").member.name("Name").typeFullName.head shouldBe "string"
-      cpg.typeDecl("Person").member.name("Age").typeFullName.head shouldBe "int"
-
-      cpg.typeDecl("Employee").member.name("Salary").typeFullName.head shouldBe "int"
-      cpg.typeDecl("Employee").member.name("person").typeFullName.head shouldBe "main.Person"
+      personNode.member.size shouldBe 2
+      personNode.member.name("Name").size shouldBe 1
+      personNode.member.name("Age").size shouldBe 1
+      personNode.member.name("Name").typeFullName.head shouldBe "string"
+      personNode.member.name("Age").typeFullName.head shouldBe "int"
     }
+
+    "Check member node under type decl node for Employee typeDecl" in {
+      val employeeNode = cpg.typeDecl("Employee").l
+      employeeNode.member.size shouldBe 2
+      employeeNode.member.name("Salary").size shouldBe 1
+      employeeNode.member.name("Salary").typeFullName.head shouldBe "int"
+      employeeNode.member.name("person").typeFullName.head shouldBe "main.Person"
+    }
+
+    "traverse from member node to typedecl node" in {
+      val memberNodes = cpg.member.l
+      memberNodes.name("person").typeDecl.head.fullName shouldBe "Employee"
+      memberNodes.name("Salary").typeDecl.head.fullName shouldBe "Employee"
+
+      memberNodes.name("Name").typeDecl.head.fullName shouldBe "Person"
+      memberNodes.name("Age").typeDecl.head.fullName shouldBe "Person"
+
+    }
+
   }
 }
