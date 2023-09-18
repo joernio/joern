113a4a7a6cc21090a1d216909539731e9d7979be
Michael Pollmeier <michael@michaelpollmeier.com>
2023-09-18 09:51:36 +0200

    NodeTypeStarters: just the plain starters

--- semanticcpg/src/main/scala.bak/io/shiftleft/semanticcpg/language/NodeSteps.scala
+++ semanticcpg/src/main/scala.bak/io/shiftleft/semanticcpg/language/NodeSteps.scala
@@ -1,28 +1,27 @@
 package io.shiftleft.semanticcpg.language
 
 import io.shiftleft.codepropertygraph.Cpg
-import io.shiftleft.codepropertygraph.generated.nodes._
-import io.shiftleft.codepropertygraph.generated.{EdgeTypes, NodeTypes}
+import io.shiftleft.codepropertygraph.generated.v2.nodes._
+import io.shiftleft.codepropertygraph.generated.v2.{EdgeTypes, NodeTypes}
 import io.shiftleft.semanticcpg.codedumper.CodeDumper
-import overflowdb.Node
-import overflowdb.traversal._
-import overflowdb.traversal.help.Doc
+// import overflowdb.traversal._
+// import overflowdb.traversal.help.Doc
 
 /** Steps for all node types
   *
   * This is the base class for all steps defined on
   */
-@help.Traversal(elementType = classOf[StoredNode])
+// @help.Traversal(elementType = classOf[StoredNode])
 class NodeSteps[NodeType <: StoredNode](val traversal: Iterator[NodeType]) extends AnyVal {
 
-  @Doc(
-    info = "The source file this code is in",
-    longInfo = """
-      |Not all but most node in the graph can be associated with
-      |a specific source file they appear in. `file` provides
-      |the file node that represents that source file.
-      |"""
-  )
+  // @Doc(
+  //   info = "The source file this code is in",
+  //   longInfo = """
+  //     |Not all but most node in the graph can be associated with
+  //     |a specific source file they appear in. `file` provides
+  //     |the file node that represents that source file.
+  //     |"""
+  // )
   def file: Iterator[File] =
     traversal
       .choose(_.label) {
@@ -33,41 +32,41 @@ class NodeSteps[NodeType <: StoredNode](val traversal: Iterator[NodeType]) exten
       }
       .cast[File]
 
-  @Doc(
-    info = "Location, including filename and line number",
-    longInfo = """
-      |Most nodes of the graph can be associated with a specific
-      |location in code, and `location` provides this location.
-      |The return value is an object providing, e.g., filename,
-      |line number, and method name, as opposed to being a flat
-      |string. For example `.location.lineNumber` provides access
-      |to the line number alone, without requiring any parsing
-      |on the user's side.
-      |"""
-  )
+  // @Doc(
+  //   info = "Location, including filename and line number",
+  //   longInfo = """
+  //     |Most nodes of the graph can be associated with a specific
+  //     |location in code, and `location` provides this location.
+  //     |The return value is an object providing, e.g., filename,
+  //     |line number, and method name, as opposed to being a flat
+  //     |string. For example `.location.lineNumber` provides access
+  //     |to the line number alone, without requiring any parsing
+  //     |on the user's side.
+  //     |"""
+  // )
   def location(implicit finder: NodeExtensionFinder): Iterator[NewLocation] =
     traversal.map(_.location)
 
-  @Doc(
-    info = "Display code (with syntax highlighting)",
-    longInfo = """
-      |For methods, dump the method code. For expressions,
-      |dump the method code along with an arrow pointing
-      |to the expression. Uses ansi-color highlighting.
-      |This only works for source frontends.
-      |"""
-  )
+  // @Doc(
+  //   info = "Display code (with syntax highlighting)",
+  //   longInfo = """
+  //     |For methods, dump the method code. For expressions,
+  //     |dump the method code along with an arrow pointing
+  //     |to the expression. Uses ansi-color highlighting.
+  //     |This only works for source frontends.
+  //     |"""
+  // )
   def dump(implicit finder: NodeExtensionFinder): List[String] =
     _dump(highlight = true)
 
-  @Doc(
-    info = "Display code (without syntax highlighting)",
-    longInfo = """
-      |For methods, dump the method code. For expressions,
-      |dump the method code along with an arrow pointing
-      |to the expression. No color highlighting.
-      |"""
-  )
+  // @Doc(
+  //   info = "Display code (without syntax highlighting)",
+  //   longInfo = """
+  //     |For methods, dump the method code. For expressions,
+  //     |dump the method code along with an arrow pointing
+  //     |to the expression. No color highlighting.
+  //     |"""
+  // )
   def dumpRaw(implicit finder: NodeExtensionFinder): List[String] =
     _dump(highlight = false)
 
@@ -89,21 +88,21 @@ class NodeSteps[NodeType <: StoredNode](val traversal: Iterator[NodeType]) exten
     traversal
       .repeat(_.in(edgeType))(_.until(_.in(edgeType).countTrav.filter(_ == 0)))
 
-  @Doc(
-    info = "Tag node with `tagName`",
-    longInfo = """
-      |This method can be used to tag nodes in the graph such that
-      |they can later be looked up easily via `cpg.tag`. Tags are
-      |key value pairs, and they can be created with `newTagNodePair`.
-      |Since in many cases, a key alone is sufficient, we provide the
-      |utility method `newTagNode(key)`, which is equivalent to
-      |`newTagNode(key, "")`.
-      |""",
-    example = """.newTagNode("foo")"""
-  )
+  // @Doc(
+  //   info = "Tag node with `tagName`",
+  //   longInfo = """
+  //     |This method can be used to tag nodes in the graph such that
+  //     |they can later be looked up easily via `cpg.tag`. Tags are
+  //     |key value pairs, and they can be created with `newTagNodePair`.
+  //     |Since in many cases, a key alone is sufficient, we provide the
+  //     |utility method `newTagNode(key)`, which is equivalent to
+  //     |`newTagNode(key, "")`.
+  //     |""",
+  //   example = """.newTagNode("foo")"""
+  // )
   def newTagNode(tagName: String): NewTagNodePairTraversal = newTagNodePair(tagName, "")
 
-  @Doc(info = "Tag node with (`tagName`, `tagValue`)", longInfo = "", example = """.newTagNodePair("key","val")""")
+  // @Doc(info = "Tag node with (`tagName`, `tagValue`)", longInfo = "", example = """.newTagNodePair("key","val")""")
   def newTagNodePair(tagName: String, tagValue: String): NewTagNodePairTraversal = {
     new NewTagNodePairTraversal(traversal.map { node =>
       NewTagNodePair()
@@ -112,13 +111,13 @@ class NodeSteps[NodeType <: StoredNode](val traversal: Iterator[NodeType]) exten
     })
   }
 
-  @Doc(info = "Tags attached to this node")
+  // @Doc(info = "Tags attached to this node")
   def tagList: List[List[Tag]] =
     traversal.map { taggedNode =>
       taggedNode.tag.l
     }.l
 
-  @Doc(info = "Tags attached to this node")
+  // @Doc(info = "Tags attached to this node")
   def tag: Iterator[Tag] = {
     traversal.flatMap { node =>
       node.tag
--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/Show.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/Show.scala
@@ -1,9 +1,8 @@
 package io.shiftleft.semanticcpg.language
 
-import io.shiftleft.codepropertygraph.generated.nodes.NewNode
-import overflowdb.Node
+import io.shiftleft.codepropertygraph.generated.v2.nodes.AbstractNode
 
-import scala.jdk.CollectionConverters._
+import scala.jdk.CollectionConverters.*
 
 /** Typeclass for (pretty) printing an object
   */
@@ -16,27 +15,12 @@ object Show {
 
   private val Default = new Show[Any] {
     override def apply(a: Any): String = a match {
-      case node: NewNode =>
-        val label      = node.label
-        val properties = propsToString(node.properties.toList)
-        s"($label): $properties"
-
-      case node: Node =>
-        val label      = node.label
-        val id         = node.id().toString
-        val properties = propsToString(node.propertiesMap.asScala.toList)
-        s"($label,$id): $properties"
+      case node: AbstractNode =>
+      s"${node.label}: ${node.propertiesMap}"
 
       case other => other.toString
     }
 
-    private def propsToString(keyValues: List[(String, Any)]): String = {
-      keyValues
-        .filter(_._2.toString.nonEmpty)
-        .sortBy(_._1)
-        .map { case (key, value) => s"$key: $value" }
-        .mkString(", ")
-    }
   }
 
 }
