b7b3fa53a0b05da4e2500ad7f93ff841f5be54a3
Michael Pollmeier <michael@michaelpollmeier.com>
2023-09-19 12:57:45 +0200

    bring back semanticcpg language package object, implement call.isDynamic/isStatic

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/types/expressions/CallTraversal.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/types/expressions/CallTraversal.scala
@@ -3,40 +3,38 @@ package io.shiftleft.semanticcpg.language.types.expressions
 import io.shiftleft.codepropertygraph.generated.v2.nodes.*
 import io.shiftleft.semanticcpg.language.*
 
-/** A call site
-  */
-class CallTraversal(val traversal: Iterator[Call]) extends AnyVal {
+/** A call site */
+class CallTraversal[NodeType <: CallBase](val traversal: Iterator[NodeType]) extends AnyVal {
 
-  /** Only statically dispatched calls
-    */
-  def isStatic: Iterator[Call] =
-    traversal.flatMap(_.isStatic)
+  /** Only statically dispatched calls */
+  def isStatic: Iterator[NodeType] =
+    traversal.filter(_.isStatic)
 
-  /** Only dynamically dispatched calls
-    */
-  def isDynamic: Iterator[Call] =
-    traversal.flatMap(_.isDynamic)
+  /** Only dynamically dispatched calls */
+  def isDynamic: Iterator[NodeType] =
+    traversal.filter(_.isDynamic)
 
-  /** The receiver of a call if the call has a receiver associated.
-    */
-  def receiver: Iterator[Expression] =
-    traversal.flatMap(_.receiver)
-
-  /** Arguments of the call
-    */
-  def argument: Iterator[Expression] =
-    traversal.flatMap(_.argument)
-
-  /** `i'th` arguments of the call
-    */
-  def argument(i: Integer): Iterator[Expression] =
-    traversal.flatMap(_.arguments(i))
-
-  /** To formal method return parameter
-    */
-  def toMethodReturn(implicit callResolver: ICallResolver): Iterator[MethodReturn] =
-    traversal
-      .flatMap(callResolver.getCalledMethodsAsTraversal)
-      .flatMap(_.methodReturn)
-
+  // TODO continue here
+//  /** The receiver of a call if the call has a receiver associated.
+//    */
+//  def receiver: Iterator[Expression] =
+//    traversal.flatMap(_.receiver)
+//
+//  /** Arguments of the call
+//    */
+//  def argument: Iterator[Expression] =
+//    traversal.flatMap(_.argument)
+//
+//  /** `i'th` arguments of the call
+//    */
+//  def argument(i: Integer): Iterator[Expression] =
+//    traversal.flatMap(_.arguments(i))
+//
+//  /** To formal method return parameter
+//    */
+//  def toMethodReturn(implicit callResolver: ICallResolver): Iterator[MethodReturn] =
+//    traversal
+//      .flatMap(callResolver.getCalledMethodsAsTraversal)
+//      .flatMap(_.methodReturn)
+//
 }
