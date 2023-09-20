700015c343d33521fb02e8f4a6508cdf8d724d14
Pandurang Patil <pandurang.patil@privado.ai>
2023-09-20 19:47:23 +0530

    Fix for issue #3678
    Fix for issue #3678.
    
    For now this PR fixes the issue related to breaking error for Local node
    being pushed to as condition AST node for switch AST. Fixed the issue
    and resolved the breaking error.
    
    This will still require a proper fix to push the Assign AST as first
    child of which block AST. We will handle this issue at later stage.

--- joern-cli/frontends/gosrc2cpg/src/main/scala/io/joern/gosrc2cpg/astcreation/AstForStatementsCreator.scala
+++ joern-cli/frontends/gosrc2cpg/src/main/scala/io/joern/gosrc2cpg/astcreation/AstForStatementsCreator.scala
@@ -3,13 +3,11 @@ package io.joern.gosrc2cpg.astcreation
 import io.joern.gosrc2cpg.parser.ParserAst.*
 import io.joern.gosrc2cpg.parser.{ParserKeys, ParserNodeInfo}
 import io.joern.gosrc2cpg.utils.Operator
-import io.joern.x2cpg.utils.NodeBuilders.{newCallNode, newOperatorCallNode}
 import io.joern.x2cpg.{Ast, ValidationMode}
-import io.shiftleft.codepropertygraph.generated.nodes.{ExpressionNew, NewCall, NewIdentifier, NewLiteral, NewLocal}
+import io.shiftleft.codepropertygraph.generated.nodes.{ExpressionNew, NewIdentifier, NewLocal}
 import io.shiftleft.codepropertygraph.generated.{ControlStructureTypes, DispatchTypes, Operators}
 import ujson.Value
 
-import scala.annotation.tailrec
 import scala.util.Try
 
 trait AstForStatementsCreator(implicit withSchemaValidation: ValidationMode) { this: AstCreator =>
@@ -190,7 +188,17 @@ trait AstForStatementsCreator(implicit withSchemaValidation: ValidationMode) { t
     }
     val switchNode = controlStructureNode(typeSwitchStmt, ControlStructureTypes.SWITCH, s"switch $code")
     val stmtAsts   = astsForStatement(createParserNodeInfo(typeSwitchStmt.json(ParserKeys.Body)))
-    controlStructureAst(switchNode, conditionAst.headOption, stmtAsts)
+    val id = conditionAst
+      .flatMap(_.root)
+      .collectFirst { case x: NewIdentifier => identifierNode(conditionParserNode.get, x.name, x.code, x.typeFullName) }
+      .get
+    val identifier = Ast(id)
+    val isOp =
+      callNode(conditionParserNode.get, s"${id.name}.(type)", Operators.is, Operators.is, DispatchTypes.STATIC_DISPATCH)
+    val condition = Option(callAst(isOp, Seq(identifier)))
+
+    val newStmtAst = stmtAsts // TODO: Push conditionAst to the front of the block
+    controlStructureAst(switchNode, condition, newStmtAst)
   }
 
   private def astForCaseClause(caseStmt: ParserNodeInfo): Seq[Ast] = {
