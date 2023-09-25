d37aa6248f136c30fc522a1a899d2c5bc2a8dea1
Pandurang Patil <pandurang.patil@privado.ai>
2023-09-25 13:43:33 +0530

    Log the line number for the warning

--- joern-cli/frontends/gosrc2cpg/src/main/scala/io/joern/gosrc2cpg/astcreation/AstForMethodCallExpressionCreator.scala
+++ joern-cli/frontends/gosrc2cpg/src/main/scala/io/joern/gosrc2cpg/astcreation/AstForMethodCallExpressionCreator.scala
@@ -50,7 +50,9 @@ trait AstForMethodCallExpressionCreator(implicit withSchemaValidation: Validatio
         val xNode = createParserNodeInfo(funcDetails.json(ParserKeys.X))
         (Some(xNode), funcDetails.json(ParserKeys.Sel)(ParserKeys.Name).str)
       case x =>
-        logger.warn(s"Unhandled class ${x.getClass} under astForCallExpression! file -> ${parserResult.fullPath}")
+        logger.warn(
+          s"Unhandled class ${x.getClass} under astForCallExpression! file -> ${parserResult.fullPath} -> Line no -> ${funcDetails.lineNumber.get}"
+        )
         (None, "")
     callMethodFullNameTypeFullNameAndSignature(methodName, aliasOpt)
   }
