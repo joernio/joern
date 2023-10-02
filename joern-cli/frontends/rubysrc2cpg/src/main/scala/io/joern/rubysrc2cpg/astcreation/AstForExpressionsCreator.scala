ae746183b9a26b475209a22fa2efa1f76bbfe817
David Baker Effendi <dbe@sun.ac.za>
2023-10-02 10:42:08 +0200

    [rubysrc2cpg] Fixed Bug with Higher-Order Functions When chained calls are made, there may be a higher order function spawned from some sort of do-block. As these do-blocks now also give the method and type in the AST, these needed to be separated as to not become arguments to a call.
    This PR fixes this case by extracting these from a call's arguments and placing them before a call node in the sequence of AST nodes.
    
    May fix #3693

--- joern-cli/frontends/rubysrc2cpg/src/main/scala/io/joern/rubysrc2cpg/astcreation/AstForExpressionsCreator.scala
+++ joern-cli/frontends/rubysrc2cpg/src/main/scala/io/joern/rubysrc2cpg/astcreation/AstForExpressionsCreator.scala
@@ -4,7 +4,14 @@ import io.joern.rubysrc2cpg.parser.RubyParser.*
 import io.joern.rubysrc2cpg.passes.Defines
 import io.joern.rubysrc2cpg.passes.Defines.getBuiltInType
 import io.joern.x2cpg.{Ast, ValidationMode}
-import io.shiftleft.codepropertygraph.generated.nodes.{AstNodeNew, NewCall, NewIdentifier, NewMethod}
+import io.shiftleft.codepropertygraph.generated.nodes.{
+  AstNodeNew,
+  NewCall,
+  NewIdentifier,
+  NewMethod,
+  NewType,
+  NewTypeDecl
+}
 import io.shiftleft.codepropertygraph.generated.{ControlStructureTypes, DispatchTypes, ModifierTypes, Operators}
 import org.antlr.v4.runtime.ParserRuleContext
 import org.slf4j.LoggerFactory
@@ -17,45 +24,45 @@ trait AstForExpressionsCreator(implicit withSchemaValidation: ValidationMode) { 
   private val logger                         = LoggerFactory.getLogger(this.getClass)
   protected var lastModifier: Option[String] = None
 
-  protected def astForPowerExpression(ctx: PowerExpressionContext): Ast =
+  protected def astForPowerExpression(ctx: PowerExpressionContext): Seq[Ast] =
     astForBinaryOperatorExpression(ctx, Operators.exponentiation, ctx.expression().asScala)
 
-  protected def astForOrExpression(ctx: OperatorOrExpressionContext): Ast =
+  protected def astForOrExpression(ctx: OperatorOrExpressionContext): Seq[Ast] =
     astForBinaryOperatorExpression(ctx, Operators.or, ctx.expression().asScala)
 
-  protected def astForAndExpression(ctx: OperatorAndExpressionContext): Ast =
+  protected def astForAndExpression(ctx: OperatorAndExpressionContext): Seq[Ast] =
     astForBinaryOperatorExpression(ctx, Operators.and, ctx.expression().asScala)
 
-  protected def astForUnaryExpression(ctx: UnaryExpressionContext): Ast = ctx.op.getType match {
+  protected def astForUnaryExpression(ctx: UnaryExpressionContext): Seq[Ast] = ctx.op.getType match {
     case TILDE => astForBinaryOperatorExpression(ctx, Operators.not, Seq(ctx.expression()))
     case PLUS  => astForBinaryOperatorExpression(ctx, Operators.plus, Seq(ctx.expression()))
     case EMARK => astForBinaryOperatorExpression(ctx, Operators.not, Seq(ctx.expression()))
   }
 
-  protected def astForUnaryMinusExpression(ctx: UnaryMinusExpressionContext): Ast =
+  protected def astForUnaryMinusExpression(ctx: UnaryMinusExpressionContext): Seq[Ast] =
     astForBinaryOperatorExpression(ctx, Operators.minus, Seq(ctx.expression()))
 
-  protected def astForAdditiveExpression(ctx: AdditiveExpressionContext): Ast = ctx.op.getType match {
+  protected def astForAdditiveExpression(ctx: AdditiveExpressionContext): Seq[Ast] = ctx.op.getType match {
     case PLUS  => astForBinaryOperatorExpression(ctx, Operators.addition, ctx.expression().asScala)
     case MINUS => astForBinaryOperatorExpression(ctx, Operators.subtraction, ctx.expression().asScala)
   }
 
-  protected def astForMultiplicativeExpression(ctx: MultiplicativeExpressionContext): Ast = ctx.op.getType match {
+  protected def astForMultiplicativeExpression(ctx: MultiplicativeExpressionContext): Seq[Ast] = ctx.op.getType match {
     case STAR    => astForMultiplicativeStarExpression(ctx)
     case SLASH   => astForMultiplicativeSlashExpression(ctx)
     case PERCENT => astForMultiplicativePercentExpression(ctx)
   }
 
-  protected def astForMultiplicativeStarExpression(ctx: MultiplicativeExpressionContext): Ast =
+  protected def astForMultiplicativeStarExpression(ctx: MultiplicativeExpressionContext): Seq[Ast] =
     astForBinaryOperatorExpression(ctx, Operators.multiplication, ctx.expression().asScala)
 
-  protected def astForMultiplicativeSlashExpression(ctx: MultiplicativeExpressionContext): Ast =
+  protected def astForMultiplicativeSlashExpression(ctx: MultiplicativeExpressionContext): Seq[Ast] =
     astForBinaryOperatorExpression(ctx, Operators.division, ctx.expression().asScala)
 
-  protected def astForMultiplicativePercentExpression(ctx: MultiplicativeExpressionContext): Ast =
+  protected def astForMultiplicativePercentExpression(ctx: MultiplicativeExpressionContext): Seq[Ast] =
     astForBinaryOperatorExpression(ctx, Operators.modulo, ctx.expression().asScala)
 
-  protected def astForEqualityExpression(ctx: EqualityExpressionContext): Ast = ctx.op.getType match {
+  protected def astForEqualityExpression(ctx: EqualityExpressionContext): Seq[Ast] = ctx.op.getType match {
     case LTEQGT     => astForBinaryOperatorExpression(ctx, Operators.compare, ctx.expression().asScala)
     case EQ2        => astForBinaryOperatorExpression(ctx, Operators.equals, ctx.expression().asScala)
     case EQ3        => astForBinaryOperatorExpression(ctx, Operators.is, ctx.expression().asScala)
@@ -64,22 +71,22 @@ trait AstForExpressionsCreator(implicit withSchemaValidation: ValidationMode) { 
     case EMARKTILDE => astForBinaryOperatorExpression(ctx, RubyOperators.notPatternMatch, ctx.expression().asScala)
   }
 
-  protected def astForRelationalExpression(ctx: RelationalExpressionContext): Ast = ctx.op.getType match {
+  protected def astForRelationalExpression(ctx: RelationalExpressionContext): Seq[Ast] = ctx.op.getType match {
     case GT   => astForBinaryOperatorExpression(ctx, Operators.greaterThan, ctx.expression().asScala)
     case GTEQ => astForBinaryOperatorExpression(ctx, Operators.greaterEqualsThan, ctx.expression().asScala)
     case LT   => astForBinaryOperatorExpression(ctx, Operators.lessThan, ctx.expression().asScala)
     case LTEQ => astForBinaryOperatorExpression(ctx, Operators.lessEqualsThan, ctx.expression().asScala)
   }
 
-  protected def astForBitwiseOrExpression(ctx: BitwiseOrExpressionContext): Ast = ctx.op.getType match {
+  protected def astForBitwiseOrExpression(ctx: BitwiseOrExpressionContext): Seq[Ast] = ctx.op.getType match {
     case BAR   => astForBinaryOperatorExpression(ctx, Operators.logicalOr, ctx.expression().asScala)
     case CARET => astForBinaryOperatorExpression(ctx, Operators.logicalOr, ctx.expression().asScala)
   }
 
-  protected def astForBitwiseAndExpression(ctx: BitwiseAndExpressionContext): Ast =
+  protected def astForBitwiseAndExpression(ctx: BitwiseAndExpressionContext): Seq[Ast] =
     astForBinaryOperatorExpression(ctx, Operators.logicalAnd, ctx.expression().asScala)
 
-  protected def astForBitwiseShiftExpression(ctx: BitwiseShiftExpressionContext): Ast = ctx.op.getType match {
+  protected def astForBitwiseShiftExpression(ctx: BitwiseShiftExpressionContext): Seq[Ast] = ctx.op.getType match {
     case LT2 => astForBinaryOperatorExpression(ctx, Operators.shiftLeft, ctx.expression().asScala)
     case GT2 => astForBinaryOperatorExpression(ctx, Operators.logicalShiftRight, ctx.expression().asScala)
   }
@@ -88,13 +95,19 @@ trait AstForExpressionsCreator(implicit withSchemaValidation: ValidationMode) { 
     ctx: ParserRuleContext,
     name: String,
     arguments: Iterable[ExpressionContext]
-  ): Ast = {
-    val argsAst = arguments.flatMap(astForExpressionContext)
-    val call    = callNode(ctx, text(ctx), name, name, DispatchTypes.STATIC_DISPATCH)
-    callAst(call, argsAst.toList)
+  ): Seq[Ast] = {
+    val (argsAst, otherAst) = arguments
+      .flatMap(astForExpressionContext)
+      .partition(_.root match
+        case Some(_: NewMethod)   => false
+        case Some(_: NewTypeDecl) => false
+        case _                    => true
+      )
+    val call = callNode(ctx, text(ctx), name, name, DispatchTypes.STATIC_DISPATCH)
+    otherAst.toSeq :+ callAst(call, argsAst.toList)
   }
 
-  protected def astForIsDefinedExpression(ctx: IsDefinedExpressionContext): Ast =
+  protected def astForIsDefinedExpression(ctx: IsDefinedExpressionContext): Seq[Ast] =
     astForBinaryOperatorExpression(ctx, RubyOperators.defined, Seq(ctx.expression()))
 
   // TODO: Maybe merge (in RubyParser.g4) isDefinedExpression with isDefinedPrimaryExpression?
@@ -362,7 +375,7 @@ trait AstForExpressionsCreator(implicit withSchemaValidation: ValidationMode) { 
   }
 
   def astForRangeExpressionContext(ctx: RangeExpressionContext): Seq[Ast] =
-    Seq(astForBinaryOperatorExpression(ctx, Operators.range, ctx.expression().asScala))
+    astForBinaryOperatorExpression(ctx, Operators.range, ctx.expression().asScala)
 
   protected def astForSuperExpression(ctx: SuperExpressionPrimaryContext): Ast = {
     val argsAst = Option(ctx.argumentsWithParentheses()) match
