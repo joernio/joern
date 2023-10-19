96ad9c72e5d1fba231e97904ecc5dcf3ac375987
Michael Pollmeier <michael@michaelpollmeier.com>
2023-10-19 12:44:57 +0200

    MethodMethods: mostly

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/MethodMethods.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/MethodMethods.scala
@@ -1,6 +1,8 @@
 package io.shiftleft.semanticcpg.language.nodemethods
 
-import io.shiftleft.codepropertygraph.generated.nodes.*
+import io.shiftleft.codepropertygraph.generated.v2.nodes.*
+import io.shiftleft.codepropertygraph.generated.v2.accessors.Lang.*
+import io.shiftleft.codepropertygraph.generated.v2.neighboraccessors.Lang.*
 import io.shiftleft.semanticcpg.NodeExtension
 import io.shiftleft.semanticcpg.language.*
 
@@ -16,8 +18,11 @@ class MethodMethods(val method: Method) extends AnyVal with NodeExtension with H
 
   /** All control structures of this method
     */
-  def controlStructure: Iterator[ControlStructure] =
-    method.ast.isControlStructure
+  def controlStructure: Iterator[ControlStructure] = {
+    ???
+    // TODO bring back: need AstNodeMethods first
+//    method.ast.isControlStructure
+  }
 
   def numberOfLines: Int = {
     if (method.lineNumber.isDefined && method.lineNumberEnd.isDefined) {
@@ -37,21 +42,32 @@ class MethodMethods(val method: Method) extends AnyVal with NodeExtension with H
   /** List of CFG nodes in reverse post order
     */
   def reversePostOrder: Iterator[CfgNode] = {
-    def expand(x: CfgNode) = { x.cfgNext.iterator }
+    def expand(x: CfgNode) = {
+      // TODO: bring back: need CfgNodeMethods first
+      //      x.cfgNext.iterator
+      ???
+    }
+
     NodeOrdering.reverseNodeList(NodeOrdering.postOrderNumbering(method, expand).toList).iterator
   }
 
   /** List of CFG nodes in post order
     */
   def postOrder: Iterator[CfgNode] = {
-    def expand(x: CfgNode) = { x.cfgNext.iterator }
+    def expand(x: CfgNode) = {
+      // TODO: bring back: need CfgNodeMethods first
+      //      x.cfgNext.iterator
+      ???
+    }
     NodeOrdering.nodeList(NodeOrdering.postOrderNumbering(method, expand).toList).iterator
   }
 
   /** The type declaration associated with this method, e.g., the class it is defined in.
     */
   def definingTypeDecl: Option[TypeDecl] =
-    Iterator.single(method).definingTypeDecl.headOption
+    // TODO: bring back: need MethodTraversal.scala first
+//    Iterator.single(method).definingTypeDecl.headOption
+    ???
 
   /** The type declaration associated with this method, e.g., the class it is defined in. Alias for 'definingTypeDecl'
     */
