e3740f6e8cd2e84eb34ca747f5d1520468545e24
Michael Pollmeier <michael@michaelpollmeier.com>
2023-10-19 11:23:36 +0200

    mostly complete Call steps

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/types/expressions/CallTraversal.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/types/expressions/CallTraversal.scala
@@ -3,7 +3,7 @@ package io.shiftleft.semanticcpg.language.types.expressions
 import io.shiftleft.codepropertygraph.generated.v2.nodes.*
 import io.shiftleft.semanticcpg.language.*
 
-/** A call site */
+/** A call site. */
 class CallTraversal(val traversal: Iterator[Call]) extends AnyVal {
 
   /** Only statically dispatched calls */
@@ -14,25 +14,26 @@ class CallTraversal(val traversal: Iterator[Call]) extends AnyVal {
   def isDynamic: Iterator[Call] =
     traversal.filter(_.isDynamic)
 
-  /** The receiver of a call if the call has a receiver associated. */
-//  def receiver: Iterator[Expression] =
-//    traversal.flatMap(_.receiver)
+//  /** The receiver of a call if the call has a receiver associated. */
+  def receiver: Iterator[Expression] =
+    traversal.flatMap(_.receiver)
 
-//  /** Arguments of the call
-//    */
-//  def argument: Iterator[Expression] =
-//    traversal.flatMap(_.argument)
-//
-//  /** `i'th` arguments of the call
-//    */
-//  def argument(i: Integer): Iterator[Expression] =
-//    traversal.flatMap(_.arguments(i))
-//
-//  /** To formal method return parameter
-//    */
+  /** Arguments of the call
+    */
+  def argument: Iterator[Expression] =
+    traversal.flatMap(_.argument)
+
+  /** `i'th` arguments of the call
+    */
+  def argument(i: Integer): Iterator[Expression] =
+    traversal.flatMap(_.arguments(i))
+
+  /** To formal method return parameter
+    */
+  // TODO reimplement
 //  def toMethodReturn(implicit callResolver: ICallResolver): Iterator[MethodReturn] =
 //    traversal
 //      .flatMap(callResolver.getCalledMethodsAsTraversal)
 //      .flatMap(_.methodReturn)
-//
+
 }
