f04255596ca2a28c6c3ff9cd53eeda3beef0c2bf
Andreas Kellas <andreas.kellas@columbia.edu>
2023-10-20 11:40:58 -0400

    WIP: Refactor PhpSetKnownTypes pass to be cleaner
    Currently the method return types are set correctly, but need to still
    update the parameter types.

--- joern-cli/frontends/php2cpg/src/main/scala/io/joern/php2cpg/passes/PhpSetKnownTypes.scala
+++ joern-cli/frontends/php2cpg/src/main/scala/io/joern/php2cpg/passes/PhpSetKnownTypes.scala
@@ -1,5 +1,6 @@
 package io.joern.php2cpg.passes
 
+import better.files.File
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.passes.ForkJoinParallelCpgPass
 import io.shiftleft.codepropertygraph.generated.nodes._
@@ -11,6 +12,7 @@ import org.slf4j.{Logger, LoggerFactory}
 import overflowdb.BatchedUpdate
 
 import scala.io.Source
+import java.io.{File => JFile}
 
 // Corresponds to a parsed row in the known functions file
 case class KnownFunction(
@@ -25,36 +27,57 @@ case class KnownFunction(
  *
  * TODO: Need to handle variadic arguments.
  */
-class PhpSetKnownTypesPass(cpg: Cpg, knownTypesFile: Option[File] = None)
+class PhpSetKnownTypesPass(cpg: Cpg, knownTypesFile: Option[JFile] = None)
   extends ForkJoinParallelCpgPass[KnownFunction](cpg) {
+
+  private val logger = LoggerFactory.getLogger(getClass)
 
   override def generateParts(): Array[KnownFunction] = {
     /* parse file and return each row as a KnownFunction object */
-    knownTypesFile match {
-      case Some(file) => {
-        /* parse file and return each row as a KnownFunction object */
-        Array()
-      }
-      case _ => Array()
+    val source = knownTypesFile match {
+      case Some(file) => Source.fromFile(file)
+      case _ => Source.fromResource("known_function_signatures.txt")
     }
+    /* parse file and return each row as a KnownFunction object */
+    val contents = source.getLines().filterNot(_.startsWith("//"))
+    val arr = contents.map(line => lineToKnownFunction(line)).toArray
+    source.close
+    arr
   }
 
   override def runOnPart(builder: overflowdb.BatchedUpdate.DiffGraphBuilder, part: KnownFunction): Unit = {
     /* calculate the result of this part - this is done as a concurrent task */
+    val builtinMethod = cpg.method.fullNameExact(part.name).l
+    builtinMethod.foreach(mNode => {
+      setTypes(builder, mNode.methodReturn, part.rTypes)
+    })
   }
 
-  /*
-  override def run(builder: BatchedUpdate.DiffGraphBuilder): Unit = {
-    val typeSetObject = generateSetKnownTypesObject(cpg, builder)
-    typeSetObject.setParamTypes()
-    typeSetObject.setReturnTypes()
+  def lineToKnownFunction(line: String): KnownFunction = {
+    val fields = line.split(";")
+    val name = fields(0)
+    fields.length match {
+      case length if (length < 2) => {
+        logger.warn(s"invalid known function signature for ${name}: no parameter types")
+        KnownFunction(fields(0), Seq(), Seq())
+      }
+      case length if (length < 3) => {
+        KnownFunction(fields(0), scanReturnTypes(fields(1)), Seq())
+      }
+      case length => {
+        KnownFunction(fields(0).strip, scanReturnTypes(fields(1)), scanParamTypes(fields.slice(2, length)))
+      }
+    }
   }
 
-  def generateSetKnownTypesObject(
-    cpg: Cpg,
-    builder: BatchedUpdate.DiffGraphBuilder
-  ): SetKnownTypes = new SetKnownTypes(cpg, builder)
-  */
+  def scanReturnTypes(rTypesRaw: String): Seq[String] = rTypesRaw.split(",").map(_.strip).toSeq
+
+  def scanParamTypes(pTypesRawArr: Array[String]): Seq[Seq[String]] =
+    pTypesRawArr.map(paramTypeRaw => paramTypeRaw.split(",").map(_.strip).toSeq).toSeq
+
+  protected def setTypes(builder: overflowdb.BatchedUpdate.DiffGraphBuilder, n: StoredNode, types: Seq[String]): Unit =
+    if (types.size == 1) builder.setNodeProperty(n, PropertyNames.TYPE_FULL_NAME, types.head)
+    else builder.setNodeProperty(n, PropertyNames.DYNAMIC_TYPE_HINT_FULL_NAME, types)
 }
 
 class SetKnownTypes(cpg: Cpg, builder: BatchedUpdate.DiffGraphBuilder) {
