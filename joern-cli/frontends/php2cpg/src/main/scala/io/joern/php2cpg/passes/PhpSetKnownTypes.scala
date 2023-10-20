2d35ca58b491d8100e654ab2fccc750e338258e9
Andreas Kellas <andreas.kellas@columbia.edu>
2023-10-20 14:37:23 -0400

    Refactor PhpSetKnownTypesPass as a ForkJoinParallelCpgPass

--- joern-cli/frontends/php2cpg/src/main/scala/io/joern/php2cpg/passes/PhpSetKnownTypes.scala
+++ joern-cli/frontends/php2cpg/src/main/scala/io/joern/php2cpg/passes/PhpSetKnownTypes.scala
@@ -17,8 +17,10 @@ import java.io.{File => JFile}
 // Corresponds to a parsed row in the known functions file
 case class KnownFunction(
   name: String,
-  rTypes: Seq[String],      // return types
-  pTypes: Seq[Seq[String]]  // Index 0 = parameter at P0
+  // return types. A function has at most one return value, but with one or more types.
+  rTypes: Seq[String],
+  // Index 0 = parameter at P0. A function has potentially multiple parameters, each with one or more types.
+  pTypes: Seq[Seq[String]]
 )
 
 /**
@@ -38,9 +40,8 @@ class PhpSetKnownTypesPass(cpg: Cpg, knownTypesFile: Option[JFile] = None)
       case Some(file) => Source.fromFile(file)
       case _ => Source.fromResource("known_function_signatures.txt")
     }
-    /* parse file and return each row as a KnownFunction object */
     val contents = source.getLines().filterNot(_.startsWith("//"))
-    val arr = contents.map(line => lineToKnownFunction(line)).toArray
+    val arr = contents.map(line => createKnownFunctionFromLine(line)).toArray
     source.close
     arr
   }
@@ -50,12 +51,12 @@ class PhpSetKnownTypesPass(cpg: Cpg, knownTypesFile: Option[JFile] = None)
     val builtinMethod = cpg.method.fullNameExact(part.name).l
     builtinMethod.foreach(mNode => {
       setTypes(builder, mNode.methodReturn, part.rTypes)
+      (mNode.parameter.l zip part.pTypes).map((p, pTypes) => setTypes(builder, p, pTypes))
     })
   }
 
-  def lineToKnownFunction(line: String): KnownFunction = {
-    val fields = line.split(";")
-    val name = fields(0)
+  def createKnownFunctionFromLine(line: String): KnownFunction = {
+    val fields = line.split(";").map(_.strip)
     fields.length match {
       case length if (length < 2) => {
         logger.warn(s"invalid known function signature for ${name}: no parameter types")
@@ -70,87 +71,19 @@ class PhpSetKnownTypesPass(cpg: Cpg, knownTypesFile: Option[JFile] = None)
     }
   }
 
+  /* From comma separated list of types, create list of types. */
   def scanReturnTypes(rTypesRaw: String): Seq[String] = rTypesRaw.split(",").map(_.strip).toSeq
 
+  /* From a semicolon separated list of parameters, each with a comma separated list of types,
+   * create a list of lists of types. */
   def scanParamTypes(pTypesRawArr: Array[String]): Seq[Seq[String]] =
     pTypesRawArr.map(paramTypeRaw => paramTypeRaw.split(",").map(_.strip).toSeq).toSeq
 
-  protected def setTypes(builder: overflowdb.BatchedUpdate.DiffGraphBuilder, n: StoredNode, types: Seq[String]): Unit =
-    if (types.size == 1) builder.setNodeProperty(n, PropertyNames.TYPE_FULL_NAME, types.head)
-    else builder.setNodeProperty(n, PropertyNames.DYNAMIC_TYPE_HINT_FULL_NAME, types)
-}
-
-class SetKnownTypes(cpg: Cpg, builder: BatchedUpdate.DiffGraphBuilder) {
-
-  private val logger = LoggerFactory.getLogger(getClass)
-
-  protected val builtinsParams: Map[String, Seq[Set[String]]] =
-    KnownFunctionSignatures.Signatures.map(
-      l => if (l.length < 3) {
-        logger.warn(s"invalid known function signature - no parameter types")
-        (l.head.strip, Seq())
-      } else {
-        l.head.strip -> l.tail.tail.map(
-          _.split(",").map(_.strip).toSet
-        ).toSeq
-      }
-    ).toMap
-
-  protected val builtinsReturns: Map[String, Set[String]] =
-    KnownFunctionSignatures.Signatures.map(
-      l => if (l.length < 2) {
-        logger.warn(s"invalid known function signature - no return types")
-        (l.head.strip, Set())
-      } else {
-        l(0).strip -> l(1).split(",").map(_.strip).toSet
-      }
-    ).toMap
-
-  object KnownFunctionSignatures {
-    val Signatures: List[Array[String]] = {
-      Source.fromResource("known_function_signatures.txt").getLines()
-        .filterNot(_.startsWith("//"))
-        .map(_.split(";")).toList
-    }
-  }
-
-  def setParamTypes(): Unit = {
-    builtinsParams.foreach {case (name, params) => setMethodParams(name, params) }
-  }
-
-  def setReturnTypes(): Unit = {
-
-    builtinsReturns.foreach {case (name, returnTypes) => setMethodReturns(name, returnTypes) }
-  }
-
-  def setMethodParams(name: String, paramTypes: Seq[Set[String]]): Unit = {
-    val paramNodes = cpg.method.fullNameExact(name).parameter.l
-    logger.debug(s"method ${name} parameters: ${paramNodes.collect(_.name).mkString(", ")}")
-    logger.debug(s"- param types: ${paramTypes.map(_.mkString(", ")).mkString("; ")}")
-    // Do I know that the paramNodes will be ordered by index?
-    val zipped_params = paramNodes zip paramTypes
-    logger.debug(s"zipped: ${zipped_params.mkString(", ")}")
-    zipped_params.map {
-      case (node, paramType) => {
-        setParamTypes(node, paramType)
-      }
-    }
-  }
-
-  def setParamTypes(paramNode: MethodParameterIn, types: Set[String]): Unit = {
-    logger.debug(s"- setting param ${paramNode.name} to type: [${types.mkString(", ")}]")
-    setTypes(paramNode, types.toSeq)
-  }
-
-  def setMethodReturns(name: String, returnTypes: Set[String]): Unit = {
-    val returnNodes = cpg.method.fullNameExact(name).methodReturn.l
-    returnNodes.foreach {
-      logger.debug(s"setting method ${name} return types ${returnTypes.mkString(", ")}")
-      setTypes(_, returnTypes.toSeq)
-    }
-  }
-
-  protected def setTypes(n: StoredNode, types: Seq[String]): Unit =
+  protected def setTypes(
+    builder: overflowdb.BatchedUpdate.DiffGraphBuilder,
+    n: StoredNode,
+    types: Seq[String]
+  ): Unit =
     if (types.size == 1) builder.setNodeProperty(n, PropertyNames.TYPE_FULL_NAME, types.head)
     else builder.setNodeProperty(n, PropertyNames.DYNAMIC_TYPE_HINT_FULL_NAME, types)
 }
