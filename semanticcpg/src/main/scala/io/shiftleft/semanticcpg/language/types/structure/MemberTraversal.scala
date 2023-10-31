b07efd2db6eac7b30480ef72ebf20fda92b3a42b
Michael Pollmeier <michael@michaelpollmeier.com>
2023-10-31 12:58:59 +0100

    more traversals - next up: fix repeat

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/types/structure/MemberTraversal.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/types/structure/MemberTraversal.scala
@@ -1,7 +1,7 @@
 package io.shiftleft.semanticcpg.language.types.structure
 
-import io.shiftleft.codepropertygraph.generated.v2._
-import io.shiftleft.codepropertygraph.generated.v2.nodes.{Call, Member}
+import io.shiftleft.codepropertygraph.generated.v2.Language.*
+import io.shiftleft.codepropertygraph.generated.v2.nodes.{Annotation, Call, Member}
 import io.shiftleft.semanticcpg.language.*
 
 /** A member variable of a class/type.
@@ -10,7 +10,7 @@ class MemberTraversal(val traversal: Iterator[Member]) extends AnyVal {
 
   /** Traverse to annotations of member
     */
-  def annotation: Iterator[nodes.Annotation] =
+  def annotation: Iterator[Annotation] =
     traversal.flatMap(_._annotationViaAstOut)
 
   /** Places where
