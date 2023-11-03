6ad8f8797025bf6b3adbddf714f1470c28ae8987
Michael Pollmeier <michael@michaelpollmeier.com>
2023-11-03 11:46:20 +0100

    Statements

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/utils/Statements.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/utils/Statements.scala
@@ -1,7 +1,8 @@
 package io.shiftleft.semanticcpg.utils
 
 import io.shiftleft.codepropertygraph.Cpg
-import io.shiftleft.semanticcpg.language._
+import io.shiftleft.codepropertygraph.generated.v2.Language.*
+import io.shiftleft.semanticcpg.language.*
 
 object Statements {
   def countAll(cpg: Cpg): Long =
