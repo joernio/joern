f67868b85cb22d4347c13ed4b8c1d33cb2bf83ab
Michael Pollmeier <michael@michaelpollmeier.com>
2023-11-03 11:43:49 +0100

    AccessPathHandling

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/AccessPathHandling.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/AccessPathHandling.scala
@@ -1,7 +1,8 @@
 package io.shiftleft.semanticcpg.language
 
-import io.shiftleft.codepropertygraph.generated.v2.{Operators, Properties, PropertyNames}
+import io.shiftleft.codepropertygraph.generated.v2.{Operators, PropertyNames}
 import io.shiftleft.codepropertygraph.generated.v2.nodes._
+import io.shiftleft.codepropertygraph.generated.v2.Language.*
 import io.shiftleft.semanticcpg.accesspath._
 import org.slf4j.LoggerFactory
 
@@ -42,9 +43,14 @@ object AccessPathHandling {
           .collect {
             case node: Literal    => ConstantAccess(node.code)
             case node: Identifier => ConstantAccess(node.name)
-            case other if other.propertyOption(PropertyNames.NAME).isPresent =>
-              logger.warn(s"unexpected/deprecated node encountered: $other with properties: ${other.propertiesMap()}")
-              ConstantAccess(other.property(Properties.NAME))
+            // TODO implement differently - asked bernhard about how we want to allow generic property access
+//            case other if other.propertyOption(PropertyNames.NAME).isPresent =>
+//              logger.warn(s"unexpected/deprecated node encountered: $other with properties: ${other.propertiesMap()}")
+//              ConstantAccess(other.property(Properties.NAME))
+            case other if other.propertiesMap.containsKey(PropertyNames.NAME) =>
+              val properties = other.propertiesMap
+              logger.warn(s"unexpected/deprecated node encountered: $other with properties: $properties")
+              ConstantAccess(properties.get(PropertyNames.NAME).asInstanceOf[String])
           }
           .getOrElse(VariableAccess) :: tail
 
