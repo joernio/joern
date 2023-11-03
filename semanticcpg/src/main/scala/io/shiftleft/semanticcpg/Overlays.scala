45a46942afd7eda8d26cb0762223e94ba540ca59
Michael Pollmeier <michael@michaelpollmeier.com>
2023-11-03 11:47:44 +0100

    overlays - partly

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/Overlays.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/Overlays.scala
@@ -1,20 +1,21 @@
 package io.shiftleft.semanticcpg
 
 import io.shiftleft.codepropertygraph.Cpg
-import io.shiftleft.codepropertygraph.generated.v2.Properties
+import io.shiftleft.codepropertygraph.generated.v2.Language.*
+import io.shiftleft.codepropertygraph.generated.v2.PropertyNames
 import io.shiftleft.passes.CpgPass
-import io.shiftleft.semanticcpg.language._
-import overflowdb.BatchedUpdate
+import io.shiftleft.semanticcpg.language.*
+import io.joern.odb2.DiffGraphBuilder
 
 object Overlays {
 
   def appendOverlayName(cpg: Cpg, overlayName: String): Unit = {
     new CpgPass(cpg) {
-      override def run(diffGraph: BatchedUpdate.DiffGraphBuilder): Unit = {
+      override def run(diffGraph: DiffGraphBuilder): Unit = {
         cpg.metaData.headOption match {
           case Some(metaData) =>
             val newValue = metaData.overlays :+ overlayName
-            diffGraph.setNodeProperty(metaData, Properties.OVERLAYS.name, newValue)
+            diffGraph.setNodeProperty(metaData, PropertyNames.OVERLAYS, newValue)
           case None =>
             System.err.println("Missing metaData block")
         }
@@ -24,11 +25,11 @@ object Overlays {
 
   def removeLastOverlayName(cpg: Cpg): Unit = {
     new CpgPass(cpg) {
-      override def run(diffGraph: BatchedUpdate.DiffGraphBuilder): Unit = {
+      override def run(diffGraph: DiffGraphBuilder): Unit = {
         cpg.metaData.headOption match {
           case Some(metaData) =>
             val newValue = metaData.overlays.dropRight(1)
-            diffGraph.setNodeProperty(metaData, Properties.OVERLAYS.name, newValue)
+            diffGraph.setNodeProperty(metaData, PropertyNames.OVERLAYS, newValue)
           case None =>
             System.err.println("Missing metaData block")
         }
