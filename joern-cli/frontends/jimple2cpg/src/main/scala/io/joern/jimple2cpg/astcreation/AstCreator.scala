8e1481f1978f7bbaf3e11a8a79783313af01ce1f
Reuben Steenekamp <reuben.steenekamp@gmail.com>
2023-11-10 16:59:15 +0200

    [jimple2cpg] Fix unimplemented method in AstCreator (#3815)
    Hotfix for: https://github.com/joernio/joern/actions/runs/6825626462/job/18563864239
    
    ---------
    
    Co-authored-by: David Baker Effendi <dbe@sun.ac.za>

--- joern-cli/frontends/jimple2cpg/src/main/scala/io/joern/jimple2cpg/astcreation/AstCreator.scala
+++ joern-cli/frontends/jimple2cpg/src/main/scala/io/joern/jimple2cpg/astcreation/AstCreator.scala
@@ -271,6 +271,8 @@ class AstCreator(protected val filename: String, protected val cls: SootClass, g
 
   override def lineEnd(node: Host): Option[Integer] = None
 
+  override def code(node: Host): String = node.toString
+
   /** Tracks AST scope.
     */
   protected val stack: mutable.Stack[Ast] = mutable.Stack.empty
