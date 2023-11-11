6d5c1a2d971b2ae869e3d9bb41692b65e51cf046
Michael Pollmeier <michael@michaelpollmeier.com>
2023-11-11 10:27:34 +0100

    NodeExtFinder

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/NodeExtensionFinder.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/NodeExtensionFinder.scala
@@ -11,15 +11,15 @@ trait NodeExtensionFinder {
 object DefaultNodeExtensionFinder extends NodeExtensionFinder {
   override def apply(node: StoredNode): Option[NodeExtension] = {
     node match {
-      case n: Method             => ??? // Some(new MethodMethods(n))
-      case n: MethodParameterIn  => ??? // Some(new MethodParameterInMethods(n))
-      case n: MethodParameterOut => ??? // Some(new MethodParameterOutMethods(n))
-      case n: MethodReturn       => ??? // Some(new MethodReturnMethods(n))
+      case n: Method             => Some(new MethodMethods(n))
+      case n: MethodParameterIn  => Some(new MethodParameterInMethods(n))
+      case n: MethodParameterOut => Some(new MethodParameterOutMethods(n))
+      case n: MethodReturn       => Some(new MethodReturnMethods(n))
       case n: Call               => Some(new CallMethods(n))
-      case n: Identifier         => ??? // Some(new IdentifierMethods(n))
-      case n: Literal            => ??? // Some(new LiteralMethods(n))
-      case n: Local              => ??? // Some(new LocalMethods(n))
-      case n: MethodRef          => ??? // Some(new MethodRefMethods(n))
+      case n: Identifier         => Some(new IdentifierMethods(n))
+      case n: Literal            => Some(new LiteralMethods(n))
+      case n: Local              => Some(new LocalMethods(n))
+      case n: MethodRef          => Some(new MethodRefMethods(n))
       case _                     => None
     }
   }
