3702929d11f83cdfb395b3f75caf3cc7e526e6c7
Michael Pollmeier <michael@michaelpollmeier.com>
2023-11-13 12:31:41 +0100

    more steps tests

--- semanticcpg/src/test/scala/io/shiftleft/semanticcpg/language/StepsTest.scala
+++ semanticcpg/src/test/scala/io/shiftleft/semanticcpg/language/StepsTest.scala
@@ -2,15 +2,13 @@ package io.shiftleft.semanticcpg.language
 
 import io.shiftleft.codepropertygraph.Cpg
 import io.shiftleft.codepropertygraph.generated.v2.nodes.*
-import io.shiftleft.codepropertygraph.generated.v2.{NodeTypes, PropertyNames}
+import io.shiftleft.codepropertygraph.generated.v2.NodeKinds
 import io.shiftleft.semanticcpg.testing.MockCpg
 import io.shiftleft.semanticcpg.language.*
 import org.json4s.*
 import org.json4s.native.JsonMethods.parse
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
-
-import scala.jdk.CollectionConverters.IteratorHasAsScala
 
 class StepsTest extends AnyWordSpec with Matchers {
 
@@ -153,17 +151,18 @@ class StepsTest extends AnyWordSpec with Matchers {
        mainMethods.p.head shouldBe "package defined pretty printer"
      }
    }
-//
-//   "should work on both Iterator (e.g. Traversal) and IterableOnce (e.g. Seq)" in {
-//     val values  = Seq("a", "b")
-//     val stream1 = values.iterator.s
-//     val stream2 = values.s
-//     // most importantly we want to verify that it compiles ^
-//
-//     stream1 shouldBe values
-//     stream2 shouldBe values
-//   }
-//
+
+   "should work on both Iterator (e.g. Traversal) and IterableOnce (e.g. Seq)" in {
+     val values  = Seq("a", "b")
+     val stream1 = values.iterator.s
+     val stream2 = values.s
+     // most importantly we want to verify that it compiles ^
+
+     stream1 shouldBe values
+     stream2 shouldBe values
+   }
+
+// TODO MP get back .help step
 //   ".help step" should {
 //     "show domain overview" in {
 //       val domainStartersHelp = Cpg.emptyCpg.help
@@ -203,128 +202,120 @@ class StepsTest extends AnyWordSpec with Matchers {
 //       }
 //     }
 //   }
-//
-//   "provides extension steps for Traversals and Nodes" in {
-//     /* n.b. interestingly, intellij puts some red squiggles on `Traversal.file` etc. if one imports
-//      * `overflowdb.traversal.iterableToTraversal`,  e.g. via `import overflowdb.traversal._`
-//      * Looks like thats a bug in intellij's presentation compiler, esp. given that both sbt and intellij compile this
-//      * code without errors, and intellij's autocomplete works.
-//      */
-//     def literal = cpg.literal.code("moo")
-//     literal.method.name.head shouldBe "foo"
-//     literal.head.method.name shouldBe "foo"
-//
-//     def typ = cpg.typ.nameExact("AClass")
-//     typ.namespace.name.head shouldBe "anamespace"
-//     typ.head.namespace.name.head shouldBe "anamespace"
-//
-//     def typeDecl = cpg.typeDecl.nameExact("AClass")
-//     typeDecl.namespace.name.head shouldBe "anamespace"
-//     typeDecl.head.namespace.name.head shouldBe "anamespace"
-//
-//     def call = cpg.call.nameExact("acall")
-//     call.method.name.size shouldBe 1
-//     call.map(_.method.name).size shouldBe 1
-//
-//     // not testable in this cpg, but if it compiles it's probably fine
-//     def controlStructure = cpg.controlStructure
-//     controlStructure.condition
-//     controlStructure.headOption.map(_.condition)
-//
-//     def identifier = cpg.identifier.name("anidentifier")
-//     identifier.refsTo.size shouldBe 1
-//     identifier.head.refsTo.size shouldBe 1
-//
-//     def member = cpg.member.name("amember")
-//     member.typeDecl.name.head shouldBe "AClass"
-//     member.head.typeDecl.name shouldBe "AClass"
-//
-//     def local = cpg.local.name("local")
-//     local.typ.name.head shouldBe "alocaltype"
-//     local.head.typ.name.head shouldBe "alocaltype"
-//
-//     def method = cpg.method.name("foo")
-//     method.parameter.size shouldBe 1
-//     method.head.parameter.size shouldBe 1
-//
-//     def methodParameterIn = cpg.parameter.name("param1")
-//     methodParameterIn.typ.name.head shouldBe "paramtype"
-//     methodParameterIn.head.typ.name shouldBe "paramtype"
-//
-//     def methodParameterOut =
-//       cpg.graph
-//         .nodes(NodeTypes.METHOD_PARAMETER_OUT)
-//         .asScala
-//         .cast[MethodParameterOut]
-//         .name("param1")
-//     methodParameterOut.typ.name.head shouldBe "paramtype"
-//     methodParameterOut.head.typ.name.head shouldBe "paramtype"
-//
-//     def methodReturn = cpg.methodReturn.typeFullNameExact("int")
-//     methodReturn.method.name.toSetMutable shouldBe Set("foo", "woo")
-//     methodReturn.map(_.method.name).toSetMutable shouldBe Set("foo", "woo")
-//
-//     def namespace = cpg.namespace.name("anamespace")
-//     namespace.typeDecl.name.toSetMutable shouldBe Set("AClass")
-//     namespace.head.typeDecl.name.toSetMutable shouldBe Set("AClass")
-//
-//     def namespaceBlock = cpg.namespaceBlock.name("anamespace")
-//     namespaceBlock.typeDecl.name.toSetMutable shouldBe Set("AClass")
-//     namespaceBlock.flatMap(_.typeDecl.name).toSetMutable shouldBe Set("AClass")
-//
-//     def file = cpg.file.name("afile.c")
-//     file.typeDecl.name.head shouldBe "AClass"
-//     file.head.typeDecl.name.head shouldBe "AClass"
-//
-//     def block = cpg.graph.nodes(NodeTypes.BLOCK).asScala.cast[Block].typeFullName("int")
-//     block.local.name.size shouldBe 1
-//     block.flatMap(_.local.name).size shouldBe 1
-//
-//     // not testable in this cpg, but if it compiles it's probably fine
-//     def methodRef = cpg.methodRef
-//     methodRef.referencedMethod
-//     methodRef.headOption.map(_.referencedMethod)
-//
-//     def expression: Iterator[Expression] = cpg.identifier.name("anidentifier").cast[Expression]
-//     expression.expressionUp.isCall.size shouldBe 1
-//     expression.head.expressionUp.isCall.size shouldBe 1
-//
-// //    def cfg: Iterator[CfgNode] = cpg.method.name("add")
-//
-//     def ast: Iterator[AstNode] = cpg.method.name("foo").cast[AstNode]
-//     ast.astParent.property(Properties.NAME).head shouldBe "AClass"
-//     ast.head.astParent.property(Properties.NAME) shouldBe "AClass"
-//
-//     // methodForCallGraph
-//     method.call.size shouldBe 1
-//     method.head.call.size shouldBe 1
-//
-//     // callForCallGraph - only verifying that it compiles
-//     call.callee(NoResolve)
-//     call.head.callee(NoResolve)
-//
-//     // AstNodeDot - only verifying that it compiles
-//     ast.dotAst
-//     ast.head.dotAst
-//
-//     // dotCfg
-//     method.dotCfg.head.startsWith("digraph add {")
-//     method.head.dotCfg.head.startsWith("digraph add {")
-//
-//     // typeFullName
-//     local.typeFullName.head shouldBe "alocaltype"
-//     local.head.typeFullName shouldBe "alocaltype"
-//
-//     // modifierAccessors
-//     method.modifier.modifierType.toSetMutable shouldBe Set("modifiertype")
-//     method.head.modifier.modifierType.toSetMutable shouldBe Set("modifiertype")
-//   }
-//
-//   "id starter step" in {
-//     // only verifying what compiles and what doesn't...
-//     // if it compiles, :shipit:
-//     assertCompiles("cpg.id(1).out")
-//     assertDoesNotCompile("cpg.id(1).outV") // `.outV` is only available on Traversal[Edge]
-//   }
+
+   "provides extension steps for Traversals and Nodes" in {
+     /* n.b. interestingly, intellij puts some red squiggles on `Traversal.file` etc. if one imports
+      * `overflowdb.traversal.iterableToTraversal`,  e.g. via `import overflowdb.traversal._`
+      * Looks like thats a bug in intellij's presentation compiler, esp. given that both sbt and intellij compile this
+      * code without errors, and intellij's autocomplete works.
+      */
+     def literal = cpg.literal.code("moo")
+     literal.method.name.head shouldBe "foo"
+     literal.head.method.name shouldBe "foo"
+
+     def typ = cpg.typ.nameExact("AClass")
+     typ.namespace.name.head shouldBe "anamespace"
+     typ.head.namespace.name.head shouldBe "anamespace"
+
+     def typeDecl = cpg.typeDecl.nameExact("AClass")
+     typeDecl.namespace.name.head shouldBe "anamespace"
+     typeDecl.head.namespace.name.head shouldBe "anamespace"
+
+     def call = cpg.call.nameExact("acall")
+     call.method.name.size shouldBe 1
+     call.map(_.method.name).size shouldBe 1
+
+     // not testable in this cpg, but if it compiles it's probably fine
+     def controlStructure = cpg.controlStructure
+     controlStructure.condition
+     controlStructure.headOption.map(_.condition)
+
+     def identifier = cpg.identifier.name("anidentifier")
+     identifier.refsTo.size shouldBe 1
+     identifier.head.refsTo.size shouldBe 1
+
+     def member = cpg.member.name("amember")
+     member.typeDecl.name.head shouldBe "AClass"
+     member.head.typeDecl.name shouldBe "AClass"
+
+     def local = cpg.local.name("local")
+     local.typ.name.head shouldBe "alocaltype"
+     local.head.typ.name.head shouldBe "alocaltype"
+
+     def method = cpg.method.name("foo")
+     method.parameter.size shouldBe 1
+     method.head.parameter.size shouldBe 1
+
+     def methodParameterIn = cpg.parameter.name("param1")
+     methodParameterIn.typ.name.head shouldBe "paramtype"
+     methodParameterIn.head.typ.name shouldBe "paramtype"
+
+     def methodParameterOut =
+       cpg.graph
+         .nodes(NodeKinds.METHOD_PARAMETER_OUT)
+         .cast[MethodParameterOut]
+         .name("param1")
+     methodParameterOut.typ.name.head shouldBe "paramtype"
+     methodParameterOut.head.typ.name.head shouldBe "paramtype"
+
+     def methodReturn = cpg.methodReturn.typeFullNameExact("int")
+     methodReturn.method.name.toSetMutable shouldBe Set("foo", "woo")
+     methodReturn.map(_.method.name).toSetMutable shouldBe Set("foo", "woo")
+
+     def namespace = cpg.namespace.name("anamespace")
+     namespace.typeDecl.name.toSetMutable shouldBe Set("AClass")
+     namespace.head.typeDecl.name.toSetMutable shouldBe Set("AClass")
+
+     def namespaceBlock = cpg.namespaceBlock.name("anamespace")
+     namespaceBlock.typeDecl.name.toSetMutable shouldBe Set("AClass")
+     namespaceBlock.flatMap(_.typeDecl.name).toSetMutable shouldBe Set("AClass")
+
+     def file = cpg.file.name("afile.c")
+     file.typeDecl.name.head shouldBe "AClass"
+     file.head.typeDecl.name.head shouldBe "AClass"
+
+     def block = cpg.graph.nodes(NodeKinds.BLOCK).cast[Block].typeFullName("int")
+     block.local.name.size shouldBe 1
+     block.flatMap(_.local.name).size shouldBe 1
+
+     // not testable in this cpg, but if it compiles it's probably fine
+     def methodRef = cpg.methodRef
+     methodRef.referencedMethod
+     methodRef.headOption.map(_.referencedMethod)
+
+     def expression: Iterator[Expression] = cpg.identifier.name("anidentifier").cast[Expression]
+     expression.expressionUp.isCall.size shouldBe 1
+     expression.head.expressionUp.isCall.size shouldBe 1
+
+ //    def cfg: Iterator[CfgNode] = cpg.method.name("add")
+
+     def ast: Iterator[AstNode] = cpg.method.name("foo").cast[AstNode]
+     ast.astParent.cast[TypeDecl].name.head shouldBe "AClass"
+     ast.head.astParent.asInstanceOf[TypeDecl].name shouldBe "AClass"
+
+     // methodForCallGraph
+     method.call.size shouldBe 1
+     method.head.call.size shouldBe 1
+
+     // callForCallGraph - only verifying that it compiles
+     call.callee(NoResolve)
+     call.head.callee(NoResolve)
+
+     // AstNodeDot - only verifying that it compiles
+     ast.dotAst
+     ast.head.dotAst
+
+     // dotCfg
+     method.dotCfg.head.startsWith("digraph add {")
+     method.head.dotCfg.head.startsWith("digraph add {")
+
+     // typeFullName
+     local.typeFullName.head shouldBe "alocaltype"
+     local.head.typeFullName shouldBe "alocaltype"
+
+     // modifierAccessors
+     method.modifier.modifierType.toSetMutable shouldBe Set("modifiertype")
+     method.head.modifier.modifierType.toSetMutable shouldBe Set("modifiertype")
+   }
 
 }
