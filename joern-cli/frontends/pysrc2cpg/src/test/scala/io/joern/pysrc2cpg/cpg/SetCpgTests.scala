fb2ee309a2fe247ba42327068dba5b43673fe215
Markus Lottmann <markus@shiftleft.io>
2023-11-16 18:13:31 +0100

    Fix empty dict literal parsing. (#3833)
    It was errornously parsed as a set literal.

---
+++ joern-cli/frontends/pysrc2cpg/src/test/scala/io/joern/pysrc2cpg/cpg/SetCpgTests.scala
@@ -0,0 +1,20 @@
+package io.joern.pysrc2cpg.cpg
+
+import io.joern.pysrc2cpg.Py2CpgTestContext
+import io.shiftleft.codepropertygraph.generated.DispatchTypes
+import io.shiftleft.semanticcpg.language.*
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class SetCpgTests extends AnyWordSpec with Matchers {
+
+  "set" should {
+    lazy val cpg = Py2CpgTestContext.buildCpg("""{1}""".stripMargin)
+
+    "be represented as `setLiteral`" in {
+      val callOption = cpg.call.methodFullName("<operator>.setLiteral").nextOption()
+      callOption.isDefined shouldBe true
+      callOption.get.code shouldBe "{1}"
+    }
+  }
+}
