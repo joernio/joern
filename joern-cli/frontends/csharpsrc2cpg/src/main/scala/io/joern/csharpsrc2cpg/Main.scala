d0bb3707f28234c7e5b267ee0361c038d1f2adc6
David Baker Effendi <dbe@sun.ac.za>
2023-11-27 10:32:55 +0200

    Replaced System.exit with logger warning

--- joern-cli/frontends/csharpsrc2cpg/src/main/scala/io/joern/csharpsrc2cpg/Main.scala
+++ joern-cli/frontends/csharpsrc2cpg/src/main/scala/io/joern/csharpsrc2cpg/Main.scala
@@ -4,6 +4,7 @@ import io.joern.csharpsrc2cpg.Frontend.{cmdLineParser, defaultConfig}
 import io.joern.x2cpg.{X2CpgConfig, X2CpgMain}
 import io.joern.x2cpg.passes.frontend.{TypeRecoveryParserConfig, XTypeRecovery}
 import io.joern.x2cpg.utils.Environment
+import org.slf4j.LoggerFactory
 import scopt.OParser
 
 import java.nio.file.Paths
@@ -23,12 +24,14 @@ object Frontend {
 
 object Main extends X2CpgMain(cmdLineParser, new CSharpSrc2Cpg()) {
 
+  private val logger = LoggerFactory.getLogger(getClass)
+
   def run(config: Config, csharpsrc2cpg: CSharpSrc2Cpg): Unit = {
     val absPath = Paths.get(config.inputPath).toAbsolutePath.toString
     if (Environment.pathExists(absPath)) {
       csharpsrc2cpg.run(config.withInputPath(absPath))
     } else {
-      System.exit(1)
+      logger.warn(s"Given path '$absPath' does not exist, skipping")
     }
   }
 
