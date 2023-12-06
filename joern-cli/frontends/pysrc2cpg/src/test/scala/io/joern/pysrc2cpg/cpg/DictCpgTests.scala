43de06471a1d8f99a349e4774f53fdfbfbbbc273
yzn12345 <56081290+yzn12345@users.noreply.github.com>
2023-12-06 17:52:58 +0800

    [pysrc2cpg] Truncate large dict to speed up cpg creation (#3896)
    Similar to #3847

--- joern-cli/frontends/pysrc2cpg/src/test/scala/io/joern/pysrc2cpg/cpg/DictCpgTests.scala
+++ joern-cli/frontends/pysrc2cpg/src/test/scala/io/joern/pysrc2cpg/cpg/DictCpgTests.scala
@@ -1,12 +1,12 @@
 package io.joern.pysrc2cpg.cpg
 
-import io.joern.pysrc2cpg.Py2CpgTestContext
+import io.joern.pysrc2cpg.{Py2CpgTestContext, PySrc2CpgFixture}
 import io.shiftleft.codepropertygraph.generated.DispatchTypes
 import io.shiftleft.semanticcpg.language.*
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
-class DictCpgTests extends AnyWordSpec with Matchers {
+class DictCpgTests extends PySrc2CpgFixture(withOssDataflow = false) {
 
   "empty dict" should {
     lazy val cpg = Py2CpgTestContext.buildCpg("""{}""".stripMargin)
@@ -17,4 +17,12 @@ class DictCpgTests extends AnyWordSpec with Matchers {
       callOption.get.code shouldBe "{}"
     }
   }
+  "Truncate large dict" should {
+    lazy val cpg = code(s"dict = {${("k" * 1001).map(k => s"$k: v").mkString(", ")}}")
+
+    "more than 1000 key value pairs`" in {
+      cpg.call.codeExact("<too-many-key-value-pairs>").size shouldBe 1
+    }
+  }
+
 }
