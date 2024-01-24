79d2f4c07ba236b13351c9ff1c416ac326af0b59
David Baker Effendi <dave@whirlylabs.com>
2024-01-24 12:50:29 +0200

    [c#] Fix RHS of EqualsValueClause Simple data-flows were not being propagated after some re-assignment. This was found to be caused by only a limited class of RHS nodes being handled on the EqualsValueClause. This has now been fixed and tested.
    Other style-related changes added.
    
    Resolves #4078

---
+++ joern-cli/frontends/csharpsrc2cpg/src/test/scala/io/joern/csharpsrc2cpg/querying/dataflow/SimpleDataflowTests.scala
@@ -0,0 +1,26 @@
+package io.joern.csharpsrc2cpg.querying.dataflow
+
+import io.joern.csharpsrc2cpg.testfixtures.CSharpCode2CpgFixture
+import io.shiftleft.semanticcpg.language.*
+import io.joern.dataflowengineoss.language.*
+
+class SimpleDataflowTests extends CSharpCode2CpgFixture(withDataFlow = true) {
+
+  "a source with a simple re-assignment" should {
+
+    val cpg = code(basicBoilerplate("""
+        |int i = 0;
+        |int a = i + 1;
+        |Console.WriteLine(a);
+        |""".stripMargin))
+
+    "still propagate to the sink" in {
+      cpg.method("Main").dotAst.foreach(println)
+      val src  = cpg.assignment.target.isIdentifier.nameExact("i").l
+      val sink = cpg.call.nameExact("WriteLine").l
+      sink.reachableBy(src).p.foreach(println)
+    }
+
+  }
+
+}
