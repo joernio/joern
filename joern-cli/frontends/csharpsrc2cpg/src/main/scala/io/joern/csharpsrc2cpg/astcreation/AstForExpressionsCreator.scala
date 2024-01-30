d0ab37a0f498a1c5a0676dfe93349275f37417df
Karan Batavia <karan.batavia@privado.ai>
2024-01-30 11:38:09 +0530

    scalafmt

--- joern-cli/frontends/csharpsrc2cpg/src/main/scala/io/joern/csharpsrc2cpg/astcreation/AstForExpressionsCreator.scala
+++ joern-cli/frontends/csharpsrc2cpg/src/main/scala/io/joern/csharpsrc2cpg/astcreation/AstForExpressionsCreator.scala
@@ -105,7 +105,7 @@ trait AstForExpressionsCreator(implicit withSchemaValidation: ValidationMode) { 
     val signature = Option(s"${typeFullName.getOrElse(Defines.Any)}:($argString)")
 
     val expression = createDotNetNodeInfo(invocationExpr.json(ParserKeys.Expression))
-    val name = nameFromNode(expression)
+    val name       = nameFromNode(expression)
 
     val (receiver, baseTypeFullName) = expression.node match
       case SimpleMemberAccessExpression =>
