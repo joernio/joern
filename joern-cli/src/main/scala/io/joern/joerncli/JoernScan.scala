b4ee5f2d445e48ea8eb525ef66b3f83751404243
Michael Pollmeier <michael@michaelpollmeier.com>
2024-02-01 12:42:45 +0100

    RIP SLProduct - not needed any longer - and I never liked it anyway :) (#4110)
    * RIP SLProduct - not needed any longer - and I never liked it anyway :)
    
    * turns out we still need some sort of applicationName for some display purposes
    
    which is still much better than having a sealed trait that included
    downstream internal applications...

--- joern-cli/src/main/scala/io/joern/joerncli/JoernScan.scala
+++ joern-cli/src/main/scala/io/joern/joerncli/JoernScan.scala
@@ -2,7 +2,7 @@ package io.joern.joerncli
 
 import better.files.*
 import io.joern.console.scan.{ScanPass, outputFindings}
-import io.joern.console.{BridgeBase, DefaultArgumentProvider, JoernProduct, Query, QueryDatabase}
+import io.joern.console.{BridgeBase, DefaultArgumentProvider, Query, QueryDatabase}
 import io.joern.dataflowengineoss.queryengine.{EngineConfig, EngineContext}
 import io.joern.dataflowengineoss.semanticsloader.Semantics
 import io.joern.joerncli.JoernScan.getQueriesFromQueryDb
@@ -39,7 +39,7 @@ case class JoernScanConfig(
 )
 
 object JoernScan extends BridgeBase {
-  override val slProduct = JoernProduct
+  override val applicationName = "joern"
 
   val implementationVersion = getClass.getPackage.getImplementationVersion
 
