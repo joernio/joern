b2e4db838f1e8ee1d6fac7e7afc239fa52dce6c4
Karan Batavia <118820668+karan-batavia@users.noreply.github.com>
2024-02-01 15:07:02 +0530

    [c#] Ast creation for jump statements (#4105)
    This PR includes,
    1. AST Creation for `break`, `continue` and `goto` statements.
    2. Corresponding tests.

--- joern-cli/frontends/csharpsrc2cpg/src/main/scala/io/joern/csharpsrc2cpg/parser/DotNetJsonAst.scala
+++ joern-cli/frontends/csharpsrc2cpg/src/main/scala/io/joern/csharpsrc2cpg/parser/DotNetJsonAst.scala
@@ -31,7 +31,10 @@ object DotNetJsonAst {
 
   sealed trait BaseLabel extends DotNetParserNode
 
-  object GlobalStatement     extends BaseStmt
+  sealed trait JumpStatement extends BaseStmt
+
+  object GlobalStatement extends BaseStmt
+
   object ExpressionStatement extends BaseStmt
 
   object NotHandledType extends DotNetParserNode
@@ -173,6 +176,12 @@ object DotNetJsonAst {
   object CasePatternSwitchLabel extends BaseLabel
 
   object DefaultSwitchLabel extends BaseLabel
+
+  object BreakStatement extends JumpStatement
+
+  object ContinueStatement extends JumpStatement
+
+  object GotoStatement extends JumpStatement
 
   object Unknown extends DotNetParserNode
 
