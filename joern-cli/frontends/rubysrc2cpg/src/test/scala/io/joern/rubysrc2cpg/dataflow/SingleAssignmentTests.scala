252de3716d2df378fb0ac4656343b16d8b6d429c
Michael Pollmeier <michael@michaelpollmeier.com>
2024-02-15 13:36:51 +0100

    test: fix sorting (some flows have the same length...)

--- joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/dataflow/SingleAssignmentTests.scala
+++ joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/dataflow/SingleAssignmentTests.scala
@@ -17,12 +17,17 @@ class SingleAssignmentTests extends RubyCode2CpgFixture(withPostProcessing = tru
     val sink   = cpg.method.name("puts").callIn.argument.l
     val flows  = sink.reachableByFlows(source).l
     flows.size shouldBe 6
-    val List(flow1, flow2, flow3, flow4, flow5, flow6) = flows.map(flowToResultPairs).distinct.sortBy(_.length).toList
-    flow1 shouldBe List(("y = 1", 2), ("y", 2), ("puts y", 3))
-    flow2 shouldBe List(("y = 1", 2), ("x", 2), ("puts x", 4))
+    val List(flow1, flow2, flow3, flow4, flow5, flow6) =
+      flows
+        .map(flowToResultPairs)
+        .distinct
+        .sortBy(flow => s"${flow.length} - $flow")
+        .toList
+    flow1 shouldBe List(("y = 1", 2), ("x", 2), ("puts x", 4))
+    flow2 shouldBe List(("y = 1", 2), ("y", 2), ("puts y", 3))
     flow3 shouldBe List(("y = 1", 2), ("z", 2), ("puts z", 5))
-    flow4 shouldBe List(("y = 1", 2), ("y", 2), ("y = 1", 2), ("x", 2), ("puts x", 4))
-    flow5 shouldBe List(("y = 1", 2), ("x", 2), ("x = y = 1", 2), ("z", 2), ("puts z", 5))
+    flow4 shouldBe List(("y = 1", 2), ("x", 2), ("x = y = 1", 2), ("z", 2), ("puts z", 5))
+    flow5 shouldBe List(("y = 1", 2), ("y", 2), ("y = 1", 2), ("x", 2), ("puts x", 4))
     flow6 shouldBe List(("y = 1", 2), ("y", 2), ("y = 1", 2), ("x", 2), ("x = y = 1", 2), ("z", 2), ("puts z", 5))
   }
 }
