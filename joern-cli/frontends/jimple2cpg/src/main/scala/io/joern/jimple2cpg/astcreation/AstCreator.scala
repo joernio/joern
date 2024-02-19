b73fe9897c75fb0f87a3fcc9db880154e11e3df5
liyiwei <979621500@qq.com>
2024-02-20 00:34:46 +0800

    catch error

--- joern-cli/frontends/jimple2cpg/src/main/scala/io/joern/jimple2cpg/astcreation/AstCreator.scala
+++ joern-cli/frontends/jimple2cpg/src/main/scala/io/joern/jimple2cpg/astcreation/AstCreator.scala
@@ -287,6 +287,7 @@ implicit class JvmStringOpts(s: String) {
     * <code>I</code> to <code>int</code>.
     * @return
     */
-  def parseAsJavaType: String = Type.getType(s).getClassName.replaceAll("/", ".")
+  def parseAsJavaType: String = 
+    Try{ Type.getType(s).getClassName.replaceAll("/", ".") }.getOrElse("unknownType")
 
 }
