196dc8ff6b5d42dabf2afdd38bf406555e906e0c
Suchakra Sharma <suchakra@privado.ai>
2024-02-26 19:48:04 -0500

    Add more builtin types

--- joern-cli/frontends/csharpsrc2cpg/src/main/scala/io/joern/csharpsrc2cpg/datastructures/CSharpProgramSummary.scala
+++ joern-cli/frontends/csharpsrc2cpg/src/main/scala/io/joern/csharpsrc2cpg/datastructures/CSharpProgramSummary.scala
@@ -5,8 +5,11 @@ import io.joern.x2cpg.datastructures.{FieldLike, MethodLike, ProgramSummary, Typ
 import org.slf4j.LoggerFactory
 import upickle.default.*
 
-import java.io.InputStream
+import java.io.{ByteArrayInputStream, File, InputStream}
+import scala.io.Source
+import scala.collection.mutable.ListBuffer
 import scala.annotation.targetName
+import scala.collection.JavaConverters.enumerationAsScalaIteratorConverter
 import scala.util.{Failure, Success, Try}
 
 type NamespaceToTypeMap = Map[String, Set[CSharpType]]
@@ -37,10 +40,11 @@ object CSharpProgramSummary {
   /** @return
     *   a mapping of the `System` package types.
     */
-  def BuiltinTypes: NamespaceToTypeMap =
-    jsonToInitialMapping(getClass.getResourceAsStream("/builtin_types.json")) match
+  def BuiltinTypes: NamespaceToTypeMap = {
+    jsonToInitialMapping(mergeBuiltInTypesJson) match
       case Failure(exception) => logger.warn("Unable to parse JSON type entry from builtin types", exception); Map.empty
       case Success(mapping)   => mapping
+  }
 
   /** Converts a JSON type mapping to a NamespaceToTypeMap entry.
     * @param jsonInputStream
@@ -50,6 +54,30 @@ object CSharpProgramSummary {
     */
   def jsonToInitialMapping(jsonInputStream: InputStream): Try[NamespaceToTypeMap] =
     Try(read[NamespaceToTypeMap](ujson.Readable.fromByteArray(jsonInputStream.readAllBytes())))
+
+  def mergeBuiltInTypesJson: InputStream = {
+    val classLoader      = getClass.getClassLoader
+    val builtinDirectory = "builtin"
+    val resourcePaths = Option(classLoader.getResources(s"$builtinDirectory/"))
+      .map(_.asScala.map(_.getPath.stripPrefix("/")).toList)
+      .getOrElse(List.empty[String])
+      .filter(_.endsWith("builtin_types.json"))
+
+    if (resourcePaths.isEmpty) {
+      logger.warn("No builtin_types.json found.")
+      InputStream.nullInputStream()
+    } else {
+      val mergedJsonObjects = ListBuffer[ujson.Obj]()
+      for (resourcePath <- resourcePaths) {
+        val inputStream = classLoader.getResourceAsStream(resourcePath)
+        val jsonString  = Source.fromInputStream(inputStream).mkString
+        val jsonObject  = ujson.read(jsonString).obj
+        mergedJsonObjects += jsonObject
+      }
+      val mergedJson = write(mergedJsonObjects.map(_.value).reduce(_ ++ _))
+      new ByteArrayInputStream(mergedJson.getBytes())
+    }
+  }
 
 }
 
