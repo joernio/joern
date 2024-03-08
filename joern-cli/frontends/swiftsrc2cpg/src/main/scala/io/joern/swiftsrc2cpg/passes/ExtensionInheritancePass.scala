d03ea9e916c92a1609f52be67d34028645e1cac2
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2024-03-08 15:46:33 +0100

    [swiftsrc2cpg] Added ExtensionInheritancePass (#4303)
    This pass adds the inheritance relation between base types and their extensions.
    Although, that is semantically not quite correct in the way Swift extensions work
    its still the closest lowering in our CPG world.

---
+++ joern-cli/frontends/swiftsrc2cpg/src/main/scala/io/joern/swiftsrc2cpg/passes/ExtensionInheritancePass.scala
@@ -0,0 +1,47 @@
+package io.joern.swiftsrc2cpg.passes
+
+import io.shiftleft.codepropertygraph.Cpg
+import io.shiftleft.codepropertygraph.generated.nodes.TypeDecl
+import io.shiftleft.codepropertygraph.generated.EdgeTypes
+import io.shiftleft.codepropertygraph.generated.PropertyNames
+import io.shiftleft.passes.ConcurrentWriterCpgPass
+import io.shiftleft.semanticcpg.language.*
+
+import java.io.File
+
+class ExtensionInheritancePass(cpg: Cpg) extends ConcurrentWriterCpgPass[TypeDecl](cpg) {
+
+  private val SourceFolder: String = "/Source/"
+
+  override def generateParts(): Array[TypeDecl] = cpg.typeDecl.filterNot(_.fullName.endsWith("<extension>")).toArray
+
+  private def setInheritsFromFullNames(
+    builder: DiffGraphBuilder,
+    part: TypeDecl,
+    fromExtensions: Iterator[TypeDecl]
+  ): Unit = {
+    val fullNames = (part.inheritsFromTypeFullName ++ fromExtensions.map(_.fullName)).toSet.toSeq
+    builder.setNodeProperty(part, PropertyNames.INHERITS_FROM_TYPE_FULL_NAME, fullNames)
+    cpg.typ.fullNameExact(fullNames: _*).foreach(tgt => builder.addEdge(part, tgt, EdgeTypes.INHERITS_FROM))
+  }
+
+  override def runOnPart(builder: DiffGraphBuilder, part: TypeDecl): Unit = {
+    val folderOption = Option(new File(part.filename).getParent)
+    if (folderOption.isEmpty) {
+      val extensions = cpg.typeDecl.filter(_.fullName.endsWith(s":${part.name}<extension>"))
+      setInheritsFromFullNames(builder, part, extensions)
+    } else {
+      val folder = s"${folderOption.get}/".replaceAll("\\\\", "/")
+      val folderPrefix = if (folder.contains(SourceFolder)) {
+        folder.substring(0, folder.indexOf(SourceFolder))
+      } else {
+        ""
+      }
+      val extensions = cpg.typeDecl.filter(t =>
+        t.fullName.startsWith(folderPrefix) && t.fullName.endsWith(s":${part.name}<extension>")
+      )
+      setInheritsFromFullNames(builder, part, extensions)
+    }
+  }
+
+}
