ee6dc0f6489539ba389d001ab4327b1458293956
Karan Batavia <karan.batavia@privado.ai>
2024-04-15 15:18:24 +0530

    refactor tests

--- joern-cli/frontends/php2cpg/src/test/scala/io/joern/php2cpg/querying/AnnotationTests.scala
+++ joern-cli/frontends/php2cpg/src/test/scala/io/joern/php2cpg/querying/AnnotationTests.scala
@@ -16,23 +16,34 @@ class AnnotationTests extends PhpCode2CpgFixture {
           |>
           |""".stripMargin)
 
-      inside(cpg.typeDecl("Foo").annotation.l) { case route :: Nil =>
-        route.name shouldBe "Route"
-        inside(route.astChildren.l) { case arg1 :: Nil =>
-          arg1.code shouldBe "\"/api\""
-        }
+      inside(cpg.typeDecl("Foo").annotation.l) {
+        case route :: Nil =>
+          route.name shouldBe "Route"
+          inside(route.astChildren.l) {
+            case arg1 :: Nil =>
+              arg1.code shouldBe "\"/api\""
+            case _ => fail("Expected exactly 1 argument for the annotation")
+          }
+        case _ => fail("No annotation link found for the class `Foo`")
       }
 
-      inside(cpg.method("bar").annotation.l) { case route :: Nil =>
-        route.name shouldBe "Route"
-        inside(route.astChildren.l) { case arg1 :: arg2 :: Nil =>
-          arg1.code shouldBe "\"/edit\""
-          arg2.code shouldBe "\"hello\""
-        }
+      inside(cpg.method("bar").annotation.l) {
+        case route :: Nil =>
+          route.name shouldBe "Route"
+          inside(route.astChildren.l) {
+            case arg1 :: arg2 :: Nil =>
+              arg1.code shouldBe "\"/edit\""
+              arg2.code shouldBe "\"hello\""
+            case _ => fail("Expected exactly 2 arguments for the annotation")
+          }
+        case _ => fail("No annotation link found for the method `bar`")
+
       }
 
-      inside(cpg.parameter("pBar").annotation.l) { case someAttr :: Nil =>
-        someAttr.name shouldBe "SomeAttr"
+      inside(cpg.parameter("pBar").annotation.l) {
+        case someAttr :: Nil =>
+          someAttr.name shouldBe "SomeAttr"
+        case _ => fail("No annotation link found for the parameter `pBar`")
       }
     }
   }
