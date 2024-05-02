a40037b52bf8b1fa1f8948c1049065ae04b0a738
itsacoderepo <itsacoderepo@users.noreply.github.com>
2024-05-02 17:38:16 +0200

    [jimple] Remove jsp add html and json
    I would not consider jsp to be config files but code. Also added json and HTML files. The latter usually also does not contain configurations but i would expect it there (currently).

--- joern-cli/frontends/jimple2cpg/src/main/scala/io/joern/jimple2cpg/util/ProgramHandlingUtil.scala
+++ joern-cli/frontends/jimple2cpg/src/main/scala/io/joern/jimple2cpg/util/ProgramHandlingUtil.scala
@@ -76,7 +76,7 @@ object ProgramHandlingUtil {
       f.zipInputStream.apply { zis => isValidZipFile(zis) }
 
     def isConfigFile: Boolean = {
-      val configExt = Set(".xml", ".properties", ".yaml", ".yml", ".tf", ".tfvars", ".vm", ".jsp", ".conf", ".mf")
+      val configExt = Set(".xml", ".properties", ".yaml", ".yml", ".tf", ".tfvars", ".vm", ".json", ".conf", ".mf", ".html")
 
       def hasConfigExt(f: File): Boolean = configExt.exists(f.`extension`.map(_.toLowerCase).contains(_))
 
