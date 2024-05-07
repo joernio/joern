ebd93bf07ab3261c25fb27756d772768fec02979
David Baker Effendi <dave@whirlylabs.com>
2024-05-07 14:30:08 +0200

    [ruby] Validate & Enable String Interpolation Flows (#4538)
    Added handling for symbol interpolation & made some failures less silent. The issue resolved was resolved as part of some other change, but I validated and enabled the LiteralTests
    
    Resolves #4521

--- joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/dataflow/LiteralTests.scala
+++ joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/dataflow/LiteralTests.scala
@@ -5,45 +5,42 @@ import io.joern.rubysrc2cpg.testfixtures.RubyCode2CpgFixture
 import io.shiftleft.semanticcpg.language.*
 
 class LiteralTests extends RubyCode2CpgFixture(withPostProcessing = true, withDataFlow = true) {
+
   "Data flow through string interpolation" in {
     val cpg = code("""
-                     |x = 1
-                     |str = "The source is #{x}"
-                     |puts str
+                     |def foo(x)
+                     |  str = "The source is #{x}"
+                     |  puts str
+                     |end
                      |""".stripMargin)
 
-    val source = cpg.identifier.name("x").l
-    val sink   = cpg.call.name("puts").l
-    sink.reachableByFlows(source).l.size shouldBe 2
+    val source = cpg.method("foo").parameter.index(1).l
+    val sink   = cpg.call.name("puts").argument(1).l
+    sink.reachableByFlows(source).size shouldBe 1
   }
 
-  // Works in deprecated - Could not represent expression x on new frontend
-  "flow through interpolated double-quoted string literal " ignore {
-    val cpg = code("""
-                     |x = "foo"
-                     |y = :"bar #{x}"
-                     |puts y
+  "flow through interpolated double-quoted symbol literal" in {
+    val cpg = code("""def foo(x)
+                     |  y = :"bar #{x}"
+                     |  puts y
+                     |end
                      |""".stripMargin)
 
-    val source = cpg.identifier.name("x").l
-    val sink   = cpg.call.name("puts").l
-    sink.reachableByFlows(source).size shouldBe 2
+    val source = cpg.method("foo").parameter.index(1).l
+    val sink   = cpg.call.name("puts").argument(1).l
+    sink.reachableByFlows(source).size shouldBe 1
   }
 
-  // Works in deprecated
-  "flow through symbol literal defined using \\:" ignore {
+  "flow through symbol literal defined using \\:" in {
     val cpg = code("""
                      |def foo(arg)
-                     |hash = {:y => arg}
-                     |puts hash
+                     |  hash = {:y => arg}
+                     |  puts hash
                      |end
-                     |
-                     |x = 3
-                     |foo(x)
                      |""".stripMargin)
 
-    val source = cpg.identifier.name("x").l
-    val sink   = cpg.call.name("puts").l
-    sink.reachableByFlows(source).size shouldBe 2
+    val source = cpg.method("foo").parameter.index(1).l
+    val sink   = cpg.call.name("puts").argument(1).l
+    sink.reachableByFlows(source).size shouldBe 1
   }
 }
