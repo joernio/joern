fcf0be83cc04db8441562dd66545c815fcb6d83d
Andrei Dreyer <andrei@whirlylabs.com>
2024-05-16 10:51:34 +0200

    [ruby] type recovery on method returns and constructor initializations working

--- joern-cli/frontends/rubysrc2cpg/src/main/scala/io/joern/rubysrc2cpg/passes/RubyTypeRecoveryPassGenerator.scala
+++ joern-cli/frontends/rubysrc2cpg/src/main/scala/io/joern/rubysrc2cpg/passes/RubyTypeRecoveryPassGenerator.scala
@@ -52,84 +52,20 @@ private class RecoverForRubyFile(cpg: Cpg, cu: File, builder: DiffGraphBuilder, 
     }
   }
 
-  override def getLiteralType(l: Literal): Set[String] = {
-    println("y")
-    super.getLiteralType(l)
+  override def visitIdentifierAssignedToConstructor(i: Identifier, c: Call): Set[String] = {
+    associateTypes(i, Set(i.typeFullName))
   }
 
-  //  override def visitIdentifierAssignedToConstructor(i: Identifier, c: Call): Set[String] = {
-//
-//    def isMatching(cName: String, code: String) = {
-//      val cNameList = cName.split(":program").last.split("\\.").filterNot(_.isEmpty).dropRight(1)
-//      val codeList  = code.split("\\(").head.split("[:.]").filterNot(_.isEmpty).dropRight(1)
-//      cNameList sameElements codeList
-//    }
-//
-//    val constructorPaths =
-//      symbolTable.get(c).filter(isMatching(_, c.code)).map(_.stripSuffix(s"$pathSep${XDefines.ConstructorMethodName}"))
-//    associateTypes(i, constructorPaths)
-//  }
-//
-//  override def methodReturnValues(methodFullNames: Seq[String]): Set[String] = {
-//    // Check if we have a corresponding member to resolve type
-//    val memberTypes = methodFullNames.flatMap { fullName =>
-//      val memberName = fullName.split("\\.").lastOption
-//      if (memberName.isDefined) {
-//        val typeDeclFullName = fullName.stripSuffix(s".${memberName.get}")
-//        cpg.typeDecl.fullName(typeDeclFullName).member.nameExact(memberName.get).typeFullName.l
-//      } else
-//        List.empty
-//    }.toSet
-//    if (memberTypes.nonEmpty) memberTypes else super.methodReturnValues(methodFullNames)
-//  }
-//
-//  override def visitIdentifierAssignedToCall(i: Identifier, c: Call): Set[String] = {
-//    if (c.name.startsWith("<operator>")) {
-//      visitIdentifierAssignedToOperator(i, c, c.name)
-//    } else if (symbolTable.contains(c) && isConstructor(c)) {
-//      visitIdentifierAssignedToConstructor(i, c)
-//    } else if (symbolTable.contains(c)) {
-//      visitIdentifierAssignedToCallRetVal(i, c)
-//    } else if (c.argument.headOption.exists(symbolTable.contains)) {
-//      setCallMethodFullNameFromBase(c)
-//      // Repeat this method now that the call has a type
-//      visitIdentifierAssignedToCall(i, c)
-//    } else if (
-//      c.argument.headOption
-//        .exists(_.isCall) && c.argument.head
-//        .asInstanceOf[Call]
-//        .name
-//        .equals("<operator>.scopeResolution") && c.argument.head
-//        .asInstanceOf[Call]
-//        .argument
-//        .lastOption
-//        .exists(symbolTable.contains)
-//    ) {
-//      setCallMethodFullNameFromBaseScopeResolution(c)
-//      // Repeat this method now that the call has a type
-//      visitIdentifierAssignedToCall(i, c)
-//    } else {
-//      // We can try obtain a return type for this call
-//      visitIdentifierAssignedToCallRetVal(i, c)
-//    }
-//  }
-//
-//  protected def setCallMethodFullNameFromBaseScopeResolution(c: Call): Set[String] = {
-//    val recTypes = c.argument.headOption
-//      .map {
-//        case x: Call if x.name.equals("<operator>.scopeResolution") =>
-//          x.argument.lastOption.map(i => symbolTable.get(i)).getOrElse(Set.empty[String])
-//      }
-//      .getOrElse(Set.empty[String])
-//    val callTypes = recTypes.map(_.concat(s"$pathSep${c.name}"))
-//    symbolTable.append(c, callTypes)
-//  }
-//
-//  override protected def visitIdentifierAssignedToTypeRef(i: Identifier, t: TypeRef, rec: Option[String]): Set[String] =
-//    t.typ.referencedTypeDecl
-//      .map(_.fullName.stripSuffix("<meta>"))
-//      .map(td => symbolTable.append(CallAlias(i.name, rec), Set(td)))
-//      .headOption
-//      .getOrElse(super.visitIdentifierAssignedToTypeRef(i, t, rec))
-//
+  override def methodReturnValues(methodFullNames: Seq[String]): Set[String] = {
+    // Check if we have a corresponding member to resolve type
+    val memberTypes = methodFullNames.flatMap { fullName =>
+      val memberName = fullName.split("\\.").lastOption
+      if (memberName.isDefined) {
+        val typeDeclFullName = fullName.stripSuffix(s".${memberName.get}")
+        cpg.typeDecl.fullName(typeDeclFullName).member.nameExact(memberName.get).typeFullName.l
+      } else
+        List.empty
+    }.toSet
+    if (memberTypes.nonEmpty) memberTypes else super.methodReturnValues(methodFullNames)
+  }
 }
--- joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/passes/RubyTypeRecoveryTests.scala
+++ joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/passes/RubyTypeRecoveryTests.scala
@@ -8,6 +8,19 @@ import io.shiftleft.semanticcpg.language.*
 
 import scala.collection.immutable.List
 
+object RubyTypeRecoveryTests {
+  private val GEMFILE =
+    """
+      |# frozen_string_literal: true
+      |source "https://rubygems.org"
+      |
+      |ruby "2.6.5"
+      |
+      |gem "sendgrid-ruby"
+      |gem "dummy_logger"
+      |""".stripMargin
+}
+
 class RubyTypeRecoveryTests extends RubyCode2CpgFixture(withPostProcessing = true, withDataFlow = true) {
   "recovering paths for built-in calls" should {
     lazy val cpg = code(
@@ -33,6 +46,237 @@ class RubyTypeRecoveryTests extends RubyCode2CpgFixture(withPostProcessing = tru
     "present the declared method name when a built-in with the same name is used in the same compilation unit" in {
       val List(absCall) = cpg.call("sleep").l
       absCall.methodFullName shouldBe "main.rb:<global>::program:sleep"
+    }
+  }
+
+  "Type information for literals" should {
+    val cpg = code("""
+        |def func
+        | a = 2
+        | b = "abc"
+        | b
+        |end
+        |
+        |def func2
+        | func
+        |end
+        |
+        |c = func2()
+        |""".stripMargin)
+
+    "propagate function return types" in {
+      inside(cpg.method.name("func2?").l) {
+        case func :: func2 :: Nil =>
+          func.methodReturn.typeFullName shouldBe "__builtin.String"
+          func2.methodReturn.typeFullName shouldBe "__builtin.String"
+        case xs => fail(s"Expected 2 functions, got [${xs.name.mkString(",")}]")
+      }
+    }
+
+    "propagate return type to identifier c" in {
+      inside(cpg.identifier.name("c").l) {
+        case cIdent :: Nil =>
+          cIdent.typeFullName shouldBe "__builtin.String"
+        case xs => fail(s"Expected one identifier for c, got [${xs.name.mkString(",")}]")
+      }
+    }
+  }
+
+  "Type information for constructors" should {
+    val cpg = code("""
+        |class A
+        |end
+        |
+        |def func
+        | d = A.new
+        | d
+        |end
+        |
+        |a = A.new
+        |b = func
+        |""".stripMargin)
+
+    "propagate to identifier" in {
+      inside(cpg.identifier.name("(a|b)").l) {
+        case aIdent :: bIdent :: Nil =>
+          aIdent.typeFullName shouldBe "Test0.rb:<global>::program.A"
+          bIdent.typeFullName shouldBe "Test0.rb:<global>::program.A"
+        case xs => fail(s"Expected one identifier, got [${xs.name.mkString(",")}]")
+      }
+    }
+  }
+
+  "Type information for nodes with external dependency" ignore {
+
+    val cpg = code(
+      """
+        |require "sendgrid-ruby"
+        |
+        |def func
+        |   sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+        |   response = sg.client.mail._('send').post(request_body: data)
+        |end
+        |""".stripMargin,
+      "main.rb"
+    )
+      .moreCode(RubyTypeRecoveryTests.GEMFILE, "Gemfile")
+
+    cpg.method.name("func").dotAst.l.foreach(println)
+
+    "be present in (Case 1)" in {
+      cpg.identifier("sg").lineNumber(5).typeFullName.l shouldBe List("sendgrid-ruby::program.SendGrid.API")
+      cpg.call("client").methodFullName.l shouldBe List("sendgrid-ruby::program.SendGrid.API.client")
+    }
+
+    "be present in (Case 2)" ignore {
+      cpg.call("post").methodFullName.l shouldBe List(
+        "sendgrid-ruby::program.SendGrid.API.client<returnValue>.mail<returnValue>.anonymous<returnValue>.post"
+      )
+    }
+  }
+
+  "recovering module members across modules" ignore {
+    lazy val cpg = code(
+      """
+        |require "dbi"
+        |
+        |module FooModule
+        | x = 1
+        | y = "test"
+        | db = DBI.connect("DBI:Mysql:TESTDB:localhost", "testuser", "test123")
+        |end
+        |
+        |""".stripMargin,
+      "foo.rb"
+    ).moreCode(
+      """
+        |require_relative "./foo.rb"
+        |
+        |z = FooModule::x
+        |z = FooModule::y
+        |
+        |d = FooModule::db
+        |
+        |row = d.select_one("SELECT VERSION()")
+        |
+        |""".stripMargin,
+      "bar.rb"
+    ).cpg
+
+    "resolve 'x' and 'y' locally under foo.rb" in {
+      val Some(x) = cpg.identifier("x").where(_.file.name(".*foo.*")).headOption: @unchecked
+      x.typeFullName shouldBe "__builtin.Integer"
+      val Some(y) = cpg.identifier("y").where(_.file.name(".*foo.*")).headOption: @unchecked
+      y.typeFullName shouldBe "__builtin.String"
+    }
+
+    "resolve 'FooModule.x' and 'FooModule.y' field access primitive types correctly" ignore {
+      val List(z1, z2) = cpg.file
+        .name(".*bar.*")
+        .ast
+        .isIdentifier
+        .name("z")
+        .l
+      z1.typeFullName shouldBe "ANY"
+      z1.dynamicTypeHintFullName shouldBe Seq("__builtin.Integer", "__builtin.String")
+      z2.typeFullName shouldBe "ANY"
+      z2.dynamicTypeHintFullName shouldBe Seq("__builtin.Integer", "__builtin.String")
+    }
+
+    "resolve 'FooModule.d' field access object types correctly" ignore {
+      val Some(d) = cpg.file
+        .name(".*bar.*")
+        .ast
+        .isIdentifier
+        .name("d")
+        .headOption: @unchecked
+      d.typeFullName shouldBe "dbi::program.DBI.connect.<returnValue>"
+      d.dynamicTypeHintFullName shouldBe Seq()
+    }
+
+    "resolve a 'select_one' call indirectly from 'FooModule.d' field access correctly" ignore {
+      val List(d) = cpg.file
+        .name(".*bar.*")
+        .ast
+        .isCall
+        .name("select_one")
+        .l
+      d.methodFullName shouldBe "dbi::program.DBI.connect.<returnValue>.select_one"
+      d.dynamicTypeHintFullName shouldBe Seq()
+      d.callee(NoResolve).isExternal.headOption shouldBe Some(true)
+    }
+
+    // TODO Waiting for Module modelling to be done
+    "resolve correct imports via tag nodes" ignore {
+      val List(foo: ResolvedTypeDecl) =
+        cpg.file(".*foo.rb").ast.isCall.where(_.referencedImports).tag._toEvaluatedImport.toList: @unchecked
+      foo.fullName shouldBe "dbi::program.DBI"
+      val List(bar: ResolvedTypeDecl) =
+        cpg.file(".*bar.rb").ast.isCall.where(_.referencedImports).tag._toEvaluatedImport.toList: @unchecked
+      bar.fullName shouldBe "foo.rb::program.FooModule"
+    }
+
+  }
+
+  "assignment from a call to a identifier inside an imported module using new" ignore {
+    lazy val cpg = code("""
+                          |require 'logger'
+                          |
+                          |log = Logger.new(STDOUT)
+                          |log.error("foo")
+                          |
+                          |""".stripMargin).cpg
+
+    "resolve correct imports via tag nodes" in {
+      val List(logging: ResolvedMethod, _) =
+        cpg.call.where(_.referencedImports).tag._toEvaluatedImport.toList: @unchecked
+      logging.fullName shouldBe s"logger::program.Logger.${XDefines.ConstructorMethodName}"
+    }
+
+    "provide a dummy type" in {
+      val Some(log) = cpg.identifier("log").headOption: @unchecked
+      log.typeFullName shouldBe "logger::program.Logger"
+      val List(errorCall) = cpg.call("error").l
+      errorCall.methodFullName shouldBe "logger::program.Logger.error"
+    }
+  }
+
+  "assignment from a call to a identifier inside an imported module using methodCall" ignore {
+    lazy val cpg = code("""
+                          |require 'stripe'
+                          |
+                          |customer = Stripe::Customer.create
+                          |
+                          |""".stripMargin).cpg
+
+    "resolved the type of call" in {
+      val Some(create) = cpg.call("create").headOption: @unchecked
+      create.methodFullName shouldBe "stripe::program.Stripe.Customer.create"
+    }
+
+    "resolved the type of identifier" in {
+      val Some(customer) = cpg.identifier("customer").headOption: @unchecked
+      customer.typeFullName shouldBe "stripe::program.Stripe.Customer.create.<returnValue>"
+    }
+  }
+
+  "recovery of type for call having a method with same name" ignore {
+    lazy val cpg = code("""
+                          |require "dbi"
+                          |
+                          |def connect
+                          |  puts "I am here"
+                          |end
+                          |
+                          |d = DBI.connect("DBI:Mysql:TESTDB:localhost", "testuser", "test123")
+                          |""".stripMargin)
+
+    "have a correct type for call `connect`" in {
+      cpg.call("connect").methodFullName.l shouldBe List("dbi::program.DBI.connect")
+    }
+
+    "have a correct type for identifier `d`" in {
+      cpg.identifier("d").typeFullName.l shouldBe List("dbi::program.DBI.connect.<returnValue>")
     }
   }
 }
