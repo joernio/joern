b7fb5fddc587678f26a925e5fe7d5b88903e0d7d
Karan Batavia <karan.batavia@privado.ai>
2024-05-28 17:19:27 +0530

    remove debug logs

--- joern-cli/frontends/javasrc2cpg/src/main/scala/io/joern/javasrc2cpg/astcreation/declarations/AstForMethodsCreator.scala
+++ joern-cli/frontends/javasrc2cpg/src/main/scala/io/joern/javasrc2cpg/astcreation/declarations/AstForMethodsCreator.scala
@@ -45,8 +45,7 @@ import io.joern.javasrc2cpg.astcreation.declarations.AstForMethodsCreator.Partia
 
 private[declarations] trait AstForMethodsCreator { this: AstCreator =>
   def astForMethod(methodDeclaration: MethodDeclaration): Ast = {
-    val methodNode = createPartialMethod(methodDeclaration)
-    println("Some method")
+    val methodNode     = createPartialMethod(methodDeclaration)
     val typeParameters = getIdentifiersForTypeParameters(methodDeclaration)
 
     val maybeResolved      = tryWithSafeStackOverflow(methodDeclaration.resolve())
