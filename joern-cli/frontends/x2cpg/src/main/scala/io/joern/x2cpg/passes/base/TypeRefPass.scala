ed93be53fbf3d7123f70e381f27c58ea95cd9c6f
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2024-05-29 14:25:34 +0200

    Hotfix for broken grouping of parts as input to parallel passes
    Just some fixed MAX_BATCH_SIZE until we come up with something better.
    
    Fixes: https://github.com/joernio/joern/issues/4611

--- joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/passes/base/TypeRefPass.scala
+++ joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/passes/base/TypeRefPass.scala
@@ -8,12 +8,13 @@ import overflowdb.Node
 import overflowdb.traversal.*
 
 class TypeRefPass(cpg: Cpg) extends ForkJoinParallelCpgPass[List[Node]](cpg) with LinkingUtil {
-  val srcLabels = List(NodeTypes.TYPE)
+
+  private val srcLabels = List(NodeTypes.TYPE)
 
   def generateParts(): Array[List[Node]] = {
-    val nodes = cpg.graph.nodes(srcLabels*).toList
-    nodes.grouped(getBatchSize(nodes.size)).toArray
+    cpg.graph.nodes(srcLabels*).toList.grouped(MAX_BATCH_SIZE).toArray
   }
+
   def runOnPart(builder: DiffGraphBuilder, part: List[overflowdb.Node]): Unit = {
     linkToSingle(
       cpg = cpg,
