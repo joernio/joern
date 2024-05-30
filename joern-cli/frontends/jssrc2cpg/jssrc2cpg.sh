bd0cf72d7104004d44ebbd0e8c9722d08ebd6565
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2024-05-30 14:34:39 +0200

    Update jssrc2cpg.sh

--- joern-cli/frontends/jssrc2cpg/jssrc2cpg.sh
+++ joern-cli/frontends/jssrc2cpg/jssrc2cpg.sh
@@ -4,6 +4,4 @@ SCRIPT_ABS_PATH=$(readlink -f "$0")
 SCRIPT_ABS_DIR=$(dirname $SCRIPT_ABS_PATH)
 LOG4J2_CONFIG="${SCRIPT_ABS_DIR}/src/main/resources/log4j2.xml"
 
-echo "${LOG4J2_CONFIG}"
-
 "${SCRIPT_ABS_DIR}/target/universal/stage/bin/jssrc2cpg.sh" -Dlog4j2.formatMsgNoLookups=true -Dlog4j.configurationFile="${LOG4J2_CONFIG}" "$@"
