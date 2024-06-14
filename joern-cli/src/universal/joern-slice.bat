32fbcdc8399cc05ce3bf7a7c822321f1a3d48628
Michael Pollmeier <michael@michaelpollmeier.com>
2024-06-14 12:40:46 +0200

    adjust double-quotes in windows starter scripts
    fixes https://github.com/joernio/joern/issues/4661

--- joern-cli/src/universal/joern-slice.bat
+++ joern-cli/src/universal/joern-slice.bat
@@ -3,4 +3,4 @@
 set "SCRIPT_ABS_DIR=%~dp0"
 set "SCRIPT=%SCRIPT_ABS_DIR%bin\joern-slice.bat"
 
-"%SCRIPT%" "-J-XX:+UseG1GC" "-J-XX:CompressedClassSpaceSize=128m" "-Dlog4j.configurationFile=%SCRIPT_ABS_DIR%conf\log4j2.xml" %*
+"%SCRIPT%" -J-XX:+UseG1GC -J-XX:CompressedClassSpaceSize=128m "-Dlog4j.configurationFile=%SCRIPT_ABS_DIR%conf\log4j2.xml" %*
