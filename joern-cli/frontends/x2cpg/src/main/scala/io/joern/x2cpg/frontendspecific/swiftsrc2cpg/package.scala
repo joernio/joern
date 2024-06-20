49b2c71049b053aca6bb7bb089dcf204b0439cc0
Michael Pollmeier <michael@michaelpollmeier.com>
2024-06-20 16:44:07 +0200

    add previously missing swiftsrc2cpg part

--- joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/frontendspecific/swiftsrc2cpg/package.scala
+++ joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/frontendspecific/swiftsrc2cpg/package.scala
@@ -1,5 +1,6 @@
 package io.joern.x2cpg.frontendspecific
 
+import io.joern.x2cpg.passes.callgraph.NaiveCallLinker
 import io.joern.x2cpg.passes.frontend.XTypeRecoveryConfig
 import io.shiftleft.codepropertygraph.generated.Cpg
 import io.shiftleft.passes.CpgPassBase
@@ -7,6 +8,10 @@ import io.shiftleft.passes.CpgPassBase
 package object swiftsrc2cpg {
 
   def postProcessingPasses(cpg: Cpg, typeRecoveryConfig: XTypeRecoveryConfig): List[CpgPassBase] = {
-    ???
+    List(new SwiftInheritanceNamePass(cpg), new ConstClosurePass(cpg)) ++
+      new SwiftTypeRecoveryPassGenerator(cpg, typeRecoveryConfig).generate() ++ List(
+        new SwiftTypeHintCallLinker(cpg),
+        new NaiveCallLinker(cpg)
+      )
   }
 }
