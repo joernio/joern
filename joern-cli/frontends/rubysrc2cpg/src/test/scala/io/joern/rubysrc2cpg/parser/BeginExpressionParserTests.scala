31b3d23d393845a680634a24f40a8088439118f6
Andrei Dreyer <andrei@whirlylabs.com>
2024-06-28 11:21:28 +0200

    [ruby] Rename parser test file

--- joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/parser/BeginExpressionParserTests.scala
+++ joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/parser/BeginExpressionParserTests.scala
@@ -3,7 +3,7 @@ package io.joern.rubysrc2cpg.parser
 import io.joern.rubysrc2cpg.testfixtures.RubyParserFixture
 import org.scalatest.matchers.should.Matchers
 
-class BeginExpressionTests extends RubyParserFixture with Matchers {
+class BeginExpressionParserTests extends RubyParserFixture with Matchers {
   "Begin expression" in {
     test("""begin
         |1/0
