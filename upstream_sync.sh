3b0b94acbe691183d87777dccaeec64e1a146b4c
Karan Batavia <karan.batavia@privado.ai>
2024-06-28 16:11:56 +0530

    force push

--- upstream_sync.sh
+++ upstream_sync.sh
@@ -35,8 +35,7 @@ git fetch upstream
 git checkout backup_sync
 git checkout -b "$MODIFIED_BRANCH"
 git pull upstream master --no-ff
-git pull origin "$MODIFIED_BRANCH"
-git push origin "$MODIFIED_BRANCH"
+git push origin "$MODIFIED_BRANCH" --force
 
 if [ "$PUBLISH" = true ]; then
   git checkout master
