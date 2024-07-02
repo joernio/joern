3b547e48b86a2a7111adbc25dfd0ea8c9e528244
Andrei Dreyer <andrei@whirlylabs.com>
2024-07-02 11:03:34 +0200

    [ruby] Move hash test out of fixme

--- joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/parser/HashLiteralParserTests.scala
+++ joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/parser/HashLiteralParserTests.scala
@@ -4,15 +4,11 @@ import io.joern.rubysrc2cpg.testfixtures.RubyParserFixture
 import org.scalatest.matchers.should.Matchers
 
 class HashLiteralParserTests extends RubyParserFixture with Matchers {
-  "fix tests" in {
-    // TODO: Syntax error
-    test("{**group_by_type(some)}")
-  }
-
   "hash-literal" in {
     test("{ }")
     test("{**x}")
     test("{**x, **y}")
     test("{**x, y => 1, **z}")
+    test("{**group_by_type(some)}")
   }
 }
