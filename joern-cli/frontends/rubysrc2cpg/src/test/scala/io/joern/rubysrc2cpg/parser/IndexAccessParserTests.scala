65e8cb41aff11489cd4ea1503b2b3147e1fa3849
Andrei Dreyer <andrei@whirlylabs.com>
2024-07-03 09:29:00 +0200

    [ruby] Added last parser tests from querying folder

--- joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/parser/IndexAccessParserTests.scala
+++ joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/parser/IndexAccessParserTests.scala
@@ -3,9 +3,9 @@ package io.joern.rubysrc2cpg.parser
 import io.joern.rubysrc2cpg.testfixtures.RubyParserFixture
 import org.scalatest.matchers.should.Matchers
 
-class FieldAccessParserTests extends RubyParserFixture with Matchers {
-  "Normal field access" in {
-    test("x.y")
-    test("self.x")
+class IndexAccessParserTests extends RubyParserFixture with Matchers {
+  "Index access" in {
+    test("a[1]")
+    test("a[1,2]")
   }
 }
--- joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/parser/RangeParserTests.scala
+++ joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/parser/RangeParserTests.scala
@@ -3,9 +3,8 @@ package io.joern.rubysrc2cpg.parser
 import io.joern.rubysrc2cpg.testfixtures.RubyParserFixture
 import org.scalatest.matchers.should.Matchers
 
-class FieldAccessParserTests extends RubyParserFixture with Matchers {
-  "Normal field access" in {
-    test("x.y")
-    test("self.x")
+class RangeParserTests extends RubyParserFixture with Matchers {
+  "Range Operator" in {
+    test("1..2")
   }
 }
