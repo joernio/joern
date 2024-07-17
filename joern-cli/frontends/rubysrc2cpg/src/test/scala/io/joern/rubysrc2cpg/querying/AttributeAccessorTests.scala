16aa2daa04885d9e45908594d5925f4769abade1
David Baker Effendi <dave@whirlylabs.com>
2024-07-17 12:27:32 +0200

    Review changes

--- joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/querying/AttributeAccessorTests.scala
+++ joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/querying/AttributeAccessorTests.scala
@@ -1,13 +1,14 @@
 package io.joern.rubysrc2cpg.querying
 
+import io.joern.x2cpg.Defines
 import io.joern.rubysrc2cpg.testfixtures.RubyCode2CpgFixture
-import io.shiftleft.codepropertygraph.generated.Operators
+import io.shiftleft.codepropertygraph.generated.{Operators, DispatchTypes}
 import io.shiftleft.codepropertygraph.generated.nodes.{Call, FieldIdentifier, Identifier}
 import io.shiftleft.semanticcpg.language.*
 
-class SetterTests extends RubyCode2CpgFixture {
+class AttributeAccessorTests extends RubyCode2CpgFixture {
 
-  "`x.y=1` is approximated by a `x.y =` assigment with argument `1`" in {
+  "`x.y=1` is approximated by a `x.y =` assignment with argument `1`" in {
     val cpg = code("""x = Foo.new
                      |x.y = 1
                      |""".stripMargin)
@@ -19,6 +20,8 @@ class SetterTests extends RubyCode2CpgFixture {
         val fieldTarget = xyAssign.target.asInstanceOf[Call]
         fieldTarget.code shouldBe "x.@y"
         fieldTarget.name shouldBe Operators.fieldAccess
+        fieldTarget.methodFullName shouldBe Operators.fieldAccess
+        fieldTarget.dispatchType shouldBe DispatchTypes.STATIC_DISPATCH
 
         inside(fieldTarget.argument.l) {
           case (base: Identifier) :: (field: FieldIdentifier) :: Nil =>
@@ -26,6 +29,21 @@ class SetterTests extends RubyCode2CpgFixture {
             field.canonicalName shouldBe "@y"
           case xs => fail("Expected field access to have two targets")
         }
+      case xs => fail("Expected a single assignment to the literal `1`")
+    }
+  }
+
+  "`x.y` is represented by a CALL `x.y`" in {
+    val cpg = code(
+      """x = Foo.new
+        |x.y
+        |""".stripMargin)
+    inside(cpg.call.nameExact("y").l) {
+      case xyCall :: Nil =>
+        xyCall.lineNumber shouldBe Some(2)
+        xyCall.code shouldBe "x.y"
+        xyCall.methodFullName shouldBe Defines.DynamicCallUnknownFullName
+        xyCall.dispatchType shouldBe DispatchTypes.DYNAMIC_DISPATCH
       case xs => fail("Expected a single assignment to the literal `1`")
     }
   }
