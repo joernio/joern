737111b48329c6348b587050c815265446a0cf03
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2024-07-18 08:37:08 +0200

    [c2cpg] MethodRefs from identifier also for method decls

--- joern-cli/frontends/c2cpg/src/main/scala/io/joern/c2cpg/astcreation/AstForPrimitivesCreator.scala
+++ joern-cli/frontends/c2cpg/src/main/scala/io/joern/c2cpg/astcreation/AstForPrimitivesCreator.scala
@@ -1,11 +1,16 @@
 package io.joern.c2cpg.astcreation
 
-import io.joern.x2cpg.{Ast, ValidationMode, Defines as X2CpgDefines}
+import io.joern.x2cpg.Ast
+import io.joern.x2cpg.ValidationMode
+import io.joern.x2cpg.Defines as X2CpgDefines
+import io.shiftleft.codepropertygraph.generated.DispatchTypes
+import io.shiftleft.codepropertygraph.generated.Operators
 import io.shiftleft.codepropertygraph.generated.nodes.NewMethodRef
-import io.shiftleft.codepropertygraph.generated.{DispatchTypes, Operators}
 import org.eclipse.cdt.core.dom.ast.*
 import org.eclipse.cdt.internal.core.dom.parser.c.ICInternalBinding
-import org.eclipse.cdt.internal.core.dom.parser.cpp.{CPPASTQualifiedName, ICPPInternalBinding}
+import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTQualifiedName
+import org.eclipse.cdt.internal.core.dom.parser.cpp.ICPPInternalBinding
+import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTFunctionDeclarator
 import org.eclipse.cdt.internal.core.model.ASTStringUtil
 
 trait AstForPrimitivesCreator(implicit withSchemaValidation: ValidationMode) { this: AstCreator =>
@@ -20,15 +25,21 @@ trait AstForPrimitivesCreator(implicit withSchemaValidation: ValidationMode) { t
 
   private def namesForBinding(binding: ICInternalBinding | ICPPInternalBinding): (Option[String], Option[String]) = {
     val definition = binding match {
-      // sadly, there is no common interface defining .getDefinition
-      case b: ICInternalBinding   => b.getDefinition.asInstanceOf[IASTFunctionDeclarator]
-      case b: ICPPInternalBinding => b.getDefinition.asInstanceOf[IASTFunctionDeclarator]
+      // sadly, there is no common interface
+      case b: ICInternalBinding if b.getDefinition.isInstanceOf[IASTFunctionDeclarator] =>
+        Some(b.getDefinition.asInstanceOf[IASTFunctionDeclarator])
+      case b: ICPPInternalBinding if b.getDefinition.isInstanceOf[IASTFunctionDeclarator] =>
+        Some(b.getDefinition.asInstanceOf[IASTFunctionDeclarator])
+      case b: ICInternalBinding   => b.getDeclarations.find(_.isInstanceOf[IASTFunctionDeclarator])
+      case b: ICPPInternalBinding => b.getDeclarations.find(_.isInstanceOf[IASTFunctionDeclarator])
+      case null                   => None
     }
-    val typeFullName = definition.getParent match {
-      case d: IASTFunctionDefinition => Some(typeForDeclSpecifier(d.getDeclSpecifier))
-      case _                         => None
+    val typeFullName = definition.map(_.getParent) match {
+      case Some(d: IASTFunctionDefinition) => Some(typeForDeclSpecifier(d.getDeclSpecifier))
+      case Some(d: IASTSimpleDeclaration)  => Some(typeForDeclSpecifier(d.getDeclSpecifier))
+      case _                               => None
     }
-    (Some(this.fullName(definition)), typeFullName)
+    (definition.map(fullName), typeFullName)
   }
 
   private def maybeMethodRefForIdentifier(ident: IASTNode): Option[NewMethodRef] = {
@@ -38,7 +49,15 @@ trait AstForPrimitivesCreator(implicit withSchemaValidation: ValidationMode) { t
         val (mayBeFullName, mayBeTypeFullName) = id.getName.getBinding match {
           case binding: ICInternalBinding if binding.getDefinition.isInstanceOf[IASTFunctionDeclarator] =>
             namesForBinding(binding)
+          case binding: ICInternalBinding
+              if binding.getDeclarations != null &&
+                binding.getDeclarations.exists(_.isInstanceOf[IASTFunctionDeclarator]) =>
+            namesForBinding(binding)
           case binding: ICPPInternalBinding if binding.getDefinition.isInstanceOf[IASTFunctionDeclarator] =>
+            namesForBinding(binding)
+          case binding: ICPPInternalBinding
+              if binding.getDeclarations != null &&
+                binding.getDeclarations.exists(_.isInstanceOf[CPPASTFunctionDeclarator]) =>
             namesForBinding(binding)
           case _ => (None, None)
         }
