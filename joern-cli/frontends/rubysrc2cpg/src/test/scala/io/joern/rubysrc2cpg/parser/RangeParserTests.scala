c170e826362a9e27af7c4b5242cdcad95e32a459
Andrei Dreyer <andrei@whirlylabs.com>
2024-07-26 15:15:10 +0200

    [ruby] Range, regex, require parser tests moved

--- joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/parser/RangeParserTests.scala
+++ joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/parser/RangeParserTests.scala
@@ -3,7 +3,7 @@ package io.joern.rubysrc2cpg.parser
 import io.joern.rubysrc2cpg.testfixtures.RubyParserFixture
 import org.scalatest.matchers.should.Matchers
 
-class RangeParserTests extends RubyParserFixture with Matchers {
+class RangeParserTests extends RubyParserFixture(newMatch = true) with Matchers {
   "Range Operator" in {
     test("1..2")
   }
