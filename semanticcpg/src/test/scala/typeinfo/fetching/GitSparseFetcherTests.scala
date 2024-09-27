37745eb18ec119fa13b37daf8d4bc5b16f7da3d8
flandini <mflanders@qwiet.ai>
2024-09-27 13:07:58 -0500

    close GitSparseFetcher in tests

--- semanticcpg/src/test/scala/typeinfo/fetching/GitSparseFetcherTests.scala
+++ semanticcpg/src/test/scala/typeinfo/fetching/GitSparseFetcherTests.scala
@@ -4,14 +4,16 @@ import io.shiftleft.semanticcpg.typeinfo._
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
+import scala.util.Using
+
 class GitSparseFetcherTests extends AnyWordSpec with Matchers {
   "getVersions" should {
     "return all versions for a package" in {
-      val fetcher = GitSparseFetcher()
-      val versions = fetcher.getVersions(PackageIdentifier(LanguageFrontend.Java, "ion-java"))
-      
-      versions.isSuccess shouldEqual true
-      versions.get.sorted shouldEqual List("v1.0.0", "v1.11.9").sorted
+      Using(GitSparseFetcher()) { fetcher =>
+        val versions = fetcher.getVersions(PackageIdentifier(LanguageFrontend.Java, "ion-java"))
+        versions.isSuccess shouldEqual true
+        versions.get.sorted shouldEqual List("v1.0.0", "v1.11.9").sorted
+      }
     }
   }
 }
