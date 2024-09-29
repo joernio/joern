f5dda2125ce5250121cc209cd8eb0d6ef6d0b26d
Xavier Pinho <mail@xavierp.org>
2024-09-29 18:40:31 +0100

    [dataflowengineoss] composable semantics (#4974)

--- dataflowengineoss/src/main/scala/io/joern/dataflowengineoss/semanticsloader/Semantics.scala
+++ dataflowengineoss/src/main/scala/io/joern/dataflowengineoss/semanticsloader/Semantics.scala
@@ -2,22 +2,86 @@ package io.joern.dataflowengineoss.semanticsloader
 
 import io.shiftleft.codepropertygraph.generated.Cpg
 import io.shiftleft.codepropertygraph.generated.nodes.Method
+import io.shiftleft.semanticcpg.language.*
 
 trait Semantics {
 
   /** Useful for `Semantics` that benefit from having some kind of internal state tailored to the current CPG.
     */
-  def initialize(cpg: Cpg): Unit
+  def initialize(cpg: Cpg): Unit = {}
 
   def forMethod(method: Method): Option[FlowSemantic]
+
+  /** Builds a new `Semantics` whose `forMethod` behaviour first lookups in `other` and only if it fails (i.e. returns
+    * `None`) lookups in the current one.
+    */
+  def after(other: Semantics): Semantics = Semantics.compose(this, other)
 }
 
-/** The empty Semantics */
+object Semantics {
+
+  private def compose(first: Semantics, second: Semantics): Semantics = new Semantics {
+
+    override def initialize(cpg: Cpg): Unit = {
+      second.initialize(cpg)
+      first.initialize(cpg)
+    }
+
+    override def forMethod(method: Method): Option[FlowSemantic] =
+      second.forMethod(method).orElse { first.forMethod(method) }
+  }
+}
+
+/** The empty Semantics, whose `forMethod` always fails, i.e. the identity under `Semantics.after`. */
 object NoSemantics extends Semantics {
 
-  override def initialize(cpg: Cpg): Unit = {}
-
   override def forMethod(method: Method): Option[FlowSemantic] = None
+}
+
+/** The nil Semantics, whose `forMethod` always succeeds but returns the empty (nil) mapping. */
+object NilSemantics {
+
+  /** Builds a universal nil semantics. Beware this is right-absorbing under `Semantics.after`. */
+  def apply(): Semantics = new Semantics {
+    override def forMethod(method: Method): Option[FlowSemantic] = Some(FlowSemantic(method.fullName, List.empty))
+  }
+
+  /** Extensionally builds a nil semantics. */
+  def where(methodFullNames: List[String], regex: Boolean = false): Semantics =
+    FullNameSemantics.fromList(methodFullNames.map {
+      FlowSemantic(_, List.empty, regex)
+    })
+
+  /** Intensionally builds a nil semantics. */
+  def where(predicate: Method => Boolean): Semantics = new Semantics {
+    override def forMethod(method: Method): Option[FlowSemantic] = Option.when(predicate(method)) {
+      FlowSemantic(method.fullName, List.empty)
+    }
+  }
+}
+
+/** Semantics whose mappings are: 0->0, PassThroughMapping. */
+object NoCrossTaintSemantics {
+
+  /** Builds a universal no-cross-taint semantics. Beware this is right-absorbing under `Semantics.after`. */
+  def apply(): Semantics = new Semantics {
+    override def forMethod(method: Method): Option[FlowSemantic] = Some(
+      FlowSemantic(method.fullName, List(FlowMapping(0, 0), PassThroughMapping))
+    )
+  }
+
+  /** Extensionally builds a no-cross-taint semantics. */
+  def where(methodFullNames: List[String], regex: Boolean = false): Semantics =
+    FullNameSemantics.fromList(methodFullNames.map {
+      FlowSemantic(_, List(FlowMapping(0, 0), PassThroughMapping), regex)
+    })
+
+  /** Intensionally builds a no-cross-taint semantics. */
+  def where(predicate: Method => Boolean): Semantics = new Semantics {
+    override def forMethod(method: Method): Option[FlowSemantic] = Option.when(predicate(method)) {
+      FlowSemantic(method.fullName, List(FlowMapping(0, 0), PassThroughMapping))
+    }
+  }
 }
 
 case class FlowSemantic(methodFullName: String, mappings: List[FlowPath] = List.empty, regex: Boolean = false)
