e46c8b83ee6c715b670c0feaddc10e1ad0d3d91c
flandini <mflanders@qwiet.ai>
2024-10-01 15:46:57 -0500

    reword doc comment for abstract class Version

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/typeinfo/Version.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/typeinfo/Version.scala
@@ -1,9 +1,10 @@
 package io.shiftleft.semanticcpg.typeinfo
 
-/** Languages should override this providing conversions from version strings to strings usable by the fetcher. Should
-  * also provide a compare function that returns -1 for this < other, 0 this == other, 1 otherwise. The compare method
-  * is used to resolve dependencies with <,=,>,etc constraints.
-  */
+/** This should be overridden for each version-format where each deriving class provides functionality to convert
+ * between literal version strings to strings usable by the fetcher. Should also provide a compare function that 
+ * returns -1 for this < other, 0 this == other, 1 otherwise. The compare method is used to resolve dependencies 
+ * with <,=,>,etc constraints.
+ */
 abstract class Version(str: String) {
   def compare(other: Version): Int
   def toFetcherStr: String
