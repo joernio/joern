713d4515dfd8a4005d97b137759375a035dc39d5
flandini <mflanders@qwiet.ai>
2024-10-02 14:08:52 -0500

    better typing of path manipulation during fetching

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/typeinfo/fetching/Fetcher.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/typeinfo/fetching/Fetcher.scala
@@ -11,35 +11,42 @@ import scala.util.Try
  * versioned package, there is metadata for this specific version and package which stores: direct dependencies 
  * (see DirectDependency class), transitive dependencies (see TransitiveDependency class), */
 abstract class Fetcher extends AutoCloseable {
+    protected final case class FetcherResult(path: ServerPath, data: InputStream)
+
+    /** Much of fetching is building paths to resources and converting between different types of paths: paths that
+     * generic fetching understands (e.g., path to version info or dependency files), paths that a specific fetcher
+     * understands (e.g., git repo paths), and filesystem paths. To avoid mixing incompatible Path instances, instances
+     * of ServerPath are given to abstract methods that deriving types should implement; these deriving types can use
+     * ServerPaths as they are or convert between other fetcher-specific Path types. */
+    protected final case class ServerPath(path: Path) {
+        def getVersionInfoPath: ServerPath = {
+            ServerPath(path.resolve("metadata").resolve("versions.ion"))
+        }
+        override def toString: String = path.toString
+    }
+    protected object ServerPath {
+        def build(pid: PackageIdentifier, version: Option[Version] = None): ServerPath = {
+            val packageDirPath = Paths.get(pid.platform.toString).resolve(pid.name)
+            val finalPath = version match
+                case None => packageDirPath
+                case Some(version) => packageDirPath.resolve(version.toFetcherStr)
+            ServerPath(finalPath)
+        }
+    }
+    
     def fetchVersionsInfo(pid: PackageIdentifier): InputStream = {
-        val versionsInfoPath = appendVersionInfoFile(buildServerPath(pid))
-        val downloadResults = downloadFiles(List(versionsInfoPath))
+        val infoFilePath = ServerPath.build(pid).getVersionInfoPath
+        val downloadResults = downloadFiles(List(infoFilePath))
         downloadResults.head.data
     }
     
     /** Returns a map from type name -> an input stream to read data about this type. The type name keys are type
-     * short names, the NAME property of a TYPE_DECL in a CPG.
-     * @throws */
+     * short names, the NAME property of a TYPE_DECL in a CPG. */
     def fetchAllTypeData(pid: PackageIdentifier, version: Version): Map[String, InputStream] = throw new NotImplementedError()
     
     def fetchDirectDependencies(pid: PackageIdentifier, version: Version): InputStream = throw new NotImplementedError()
     
     def fetchTransitiveDependencies(pid: PackageIdentifier, version: Version): InputStream = throw new NotImplementedError()
     
-    protected final case class DownloadResult(path: Path, data: InputStream)
-    
-    protected def downloadFiles(path: List[Path]): List[DownloadResult]
-
-    override def close(): Unit = ()
-    
-    private def buildServerPath(pid: PackageIdentifier, version: Option[Version] = None): Path = {
-        val packageDirPath = Paths.get(pid.platform.toString).resolve(pid.name)
-        version match
-            case None => packageDirPath
-            case Some(version) => packageDirPath.resolve(version.toFetcherStr)
-    }
-    
-    private def appendVersionInfoFile(packageDirPath: Path): Path = {
-        packageDirPath.resolve("metadata").resolve("versions.ion")
-    }
+    protected def downloadFiles(path: List[ServerPath]): List[FetcherResult]
 }
--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/typeinfo/fetching/GitSparseFetcher.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/typeinfo/fetching/GitSparseFetcher.scala
@@ -4,18 +4,42 @@ import io.shiftleft.semanticcpg.typeinfo.LanguagePlatform.JVM
 import io.shiftleft.semanticcpg.typeinfo.dependencies.DirectDependency
 import io.shiftleft.semanticcpg.typeinfo.loading.DependencyIonLoader
 import io.shiftleft.semanticcpg.typeinfo.{LanguagePlatform, PackageIdentifier, Version}
-import replpp.shaded.os.SubProcess.InputStream
 
 import scala.jdk.CollectionConverters.*
 import java.io.File
 import java.nio.file.{Files, Path, Paths}
 import java.lang.ProcessBuilder
 import java.util.Comparator
+import java.io.InputStream
 import scala.util.{Failure, Success, Try, Using}
 
-class GitSparseFetcher(repoUrl: String = "git@github.com:flandini/typeinfo.git", gitRef: String = "main") extends Fetcher {
+final class GitSparseFetcher(repoUrl: String = "git@github.com:flandini/typeinfo.git", gitRef: String = "main") extends Fetcher {
   private var firstDownload: Boolean = true
   private lazy val tmpDir: Path      = Files.createTempDirectory("typeinfo-")
+
+  /** The repo stores all data in the data/ dir. Other top-level dirs and files are used for maintaining the type info
+   * storage repo. */
+  private class GitPath(serverPath: ServerPath) {
+    val path: Path = Paths.get("data").resolve(serverPath.path)
+
+    def getParent: GitPath = GitPath(ServerPath(serverPath.path.getParent))
+    
+    override def toString: String = path.toString
+
+    override def equals(other: Any): Boolean = {
+      other match
+        case o: GitPath => path == o.path
+        case _ => false
+    }
+  }
+  
+  private class FileSystemPath(gitPath: GitPath) {
+    val path: Path = tmpDir.resolve(gitPath.path)
+    
+    def getInputStream(): InputStream = Files.newInputStream(path)
+
+    override def toString: String = path.toString
+  }
 //
 //  def getVersions(pid: PackageIdentifier): Try[List[String]] = {
 //    val depsDir = buildPackageDepsDirPath(pid)
@@ -47,45 +71,25 @@ class GitSparseFetcher(repoUrl: String = "git@github.com:flandini/typeinfo.git",
       fileStream.sorted(Comparator.reverseOrder()).map(_.toFile).forEach(_.delete())
     }
   }
-
-  /** "data/java/ion-java/" -> "/tmp/typeinfo-1237asd/data/java/ion-java/" */
-  private def buildFileSystemPath(serverPath: Path): Path = tmpDir.resolve(serverPath)
-
-  /** "v1.0.0" -> "v1.0.0.ion" */
-  private def getDepsIonFileName(version: Version): String = version.toFetcherStr + ".ion"
-
-  /** "ion-java" -> "data/" + "java/" + "ion-java/" + "deps/" */
-  private def buildPackageDepsDirPath(pid: PackageIdentifier): Path = {
-    buildPackageDirPath(pid).resolve("deps")
-  }
-
-  /** e.g., "ion-java" -> "data/" + "java/" + "ion-java/" */
-  private def buildPackageDirPath(pid: PackageIdentifier): Path = {
-    Paths.get("data", pid.platform.toString, pid.name)
-  }
-
-  /** e.g., "java.io.File", v1.0.0 -> "data/" + "java/" + "java */
-  private def buildPackagePath(pid: PackageIdentifier, version: Version): Path = {
-    buildPackageDirPath(pid).resolve(version.toFetcherStr)
-  }
   
   /** 1. git sparse checkout uses directories paths, not file paths, so first, chop off the filename of each path
    * 2. download each directory path
    * 3. concat the temp directory + data/ + path (including file name) provided by fetcher to map each
    *    fetcher Path to a file system Path
    * 4. Open input streams to each file on the file system and return these as download results */
-  override protected def downloadFiles(paths: List[Path]): List[DownloadResult] = {
-    val dirPaths = paths.map(path => prependDataDir(path.getParent())).distinct
-    downloadDirs(dirPaths)
-    val inputStreams = paths
-      .map(convertServerPathToFileSysPath)
-      .map(Files.newInputStream(_))
-    paths
-      .zip(inputStreams)
-      .map((path, inputStream) => DownloadResult(path, inputStream))
+  override protected def downloadFiles(paths: List[ServerPath]): List[FetcherResult] = {
+    val gitPaths = paths.map(GitPath(_))
+    val gitDirPaths = gitPaths.map(_.getParent).distinct
+    
+    downloadDirs(gitDirPaths)
+    
+    val fsPaths = gitPaths.map(FileSystemPath(_))
+    val inputStreams = fsPaths.map(_.getInputStream())
+    
+    paths.zip(inputStreams).map((path, inputStream) => FetcherResult(path, inputStream))
   }
 
-  private def downloadDirs(dirPaths: List[Path]): Unit = {
+  private def downloadDirs(dirPaths: List[GitPath]): Unit = {
     if (firstDownload) {
       firstDownload = false
       if (sparseClone() != 0) {
@@ -111,7 +115,6 @@ class GitSparseFetcher(repoUrl: String = "git@github.com:flandini/typeinfo.git",
     Paths.get("data").resolve(path)
   }
   
-  /** The repo stores all data in the data/ dir */
   private def convertServerPathToFileSysPath(fetcherPath: Path): Path = {
     tmpDir.resolve(prependDataDir(fetcherPath))
   }
@@ -126,7 +129,7 @@ class GitSparseFetcher(repoUrl: String = "git@github.com:flandini/typeinfo.git",
     makeProcess(tmpDir, "git", "sparse-checkout", "set", "--cone").start().waitFor()
   }
 
-  private def setInitPathFilter(paths: List[Path]): Int = {
+  private def setInitPathFilter(paths: List[GitPath]): Int = {
     val args = Seq("git", "sparse-checkout", "set") ++ paths.toSeq.map(_.toString)
     makeProcess(tmpDir, args*).start().waitFor()
   }
@@ -135,7 +138,7 @@ class GitSparseFetcher(repoUrl: String = "git@github.com:flandini/typeinfo.git",
     makeProcess(tmpDir, "git", "checkout", gitRef).start().waitFor()
   }
 
-  private def addPathFiltersAndDownload(paths: List[Path]): Int = {
+  private def addPathFiltersAndDownload(paths: List[GitPath]): Int = {
     val args = Seq("git", "sparse-checkout", "add") ++ paths.toSeq.map(_.toString)
     makeProcess(tmpDir, args*).start().waitFor()
   }
