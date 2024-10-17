d46b2ccada3d6ae6c625b3c0a1a430c100d35087
flandini <mflanders@qwiet.ai>
2024-10-17 17:35:00 -0500

    add TODO

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/typeinfo/writing/Writer.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/typeinfo/writing/Writer.scala
@@ -3,6 +3,7 @@ package io.shiftleft.semanticcpg.typeinfo
 import java.io.OutputStream
 import scala.util.Try
 
+/** TODO: will change to match the current TypeStorageLoader */
 trait BytesWriter[InputTy] {
   def write(ty: InputTy): Array[Byte]
 }
