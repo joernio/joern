df819e6f9abd94c388cef2fd1d124f13091f26f1
flandini <mflanders@qwiet.ai>
2024-10-18 10:19:16 -0500

    adds TestMockFetcher for use in MavenDependencyResolverTests

--- semanticcpg/src/test/scala/typeinfo/dependencies/MavenDependencyResolverTests.scala
+++ semanticcpg/src/test/scala/typeinfo/dependencies/MavenDependencyResolverTests.scala
@@ -4,7 +4,7 @@ import io.shiftleft.semanticcpg.typeinfo.LanguagePlatform.JVM
 import io.shiftleft.semanticcpg.typeinfo.PackageIdentifier
 import io.shiftleft.semanticcpg.typeinfo.version.{SemVer2, Version}
 import io.shiftleft.semanticcpg.typeinfo.dependencies.MavenDependencyResolver
-import io.shiftleft.semanticcpg.typeinfo.fetching.GitSparseFetcher
+import io.shiftleft.semanticcpg.typeinfo.fetching.{GitSparseFetcher, TestMockFetcher}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -13,13 +13,49 @@ import scala.concurrent.duration.*
 import scala.util.Using
 
 class MavenDependencyResolverTests extends AnyWordSpec with Matchers {
+  private val mockFetcherTimeOut = 10.seconds
+  private val gitSparseFetcherTimeOut = 60.seconds
+  
   "resolution" should {
+    "handle simple constraints" in {
+      val mockFetcher = TestMockFetcher()
+        .addFiles(
+          ("jvm/package1/metadata/metadata.ion", 
+            """{ VERSIONS: ["1.0.0", "1.0.0-rc2", "2.0.0"]}"""),
+          ("jvm/package2/metadata/metadata.ion", 
+            """{ VERSIONS: ["1.0.0"]}"""),
+          ("jvm/package2/dependencies/1.0.0/direct.ion", 
+            """[ { NAME: "package1", VERSION_CONSTRAINT: "<2.0.0 && >1.0.0-rc2" }]"""),
+          ("jvm/package1/dependencies/2.0.0/direct.ion", 
+            """[]"""),
+          ("jvm/package1/dependencies/1.0.0-rc2/direct.ion",
+            """[]"""),
+          ("jvm/package1/dependencies/1.0.0/direct.ion",
+            """[]""")
+        )
+      val topLevelPackage = PackageIdentifier(JVM, "package2")
+      val topLevelPackageVersion = SemVer2("1.0.0")
+      
+      val dependenciesFuture = MavenDependencyResolver.resolveDependencies(
+        mockFetcher, 
+        topLevelPackage, 
+        topLevelPackageVersion)
+      val dependencies = Await.result(dependenciesFuture, mockFetcherTimeOut)
+      
+      val expectedDependencies = List(
+        TransitiveDependency("package2", topLevelPackageVersion),
+        TransitiveDependency("package1", SemVer2("1.0.0"))
+      )
+      
+      dependencies shouldEqual expectedDependencies
+    }
+    
     "get all expected dependencies for test data in test repo" in {
       Using.resource(GitSparseFetcher()) { fetcher =>
         val amazonIonPid       = PackageIdentifier(JVM, "ion-java")
         val version            = SemVer2("1.11.9")
         val dependenciesFuture = MavenDependencyResolver.resolveDependencies(fetcher, amazonIonPid, version)
-        val dependencies       = Await.result(dependenciesFuture, 60.seconds)
+        val dependencies       = Await.result(dependenciesFuture, gitSparseFetcherTimeOut)
         val expectedDependencies =
           List(TransitiveDependency(amazonIonPid.name, version), TransitiveDependency("java.lang", SemVer2("8.0.0")))
         dependencies shouldEqual expectedDependencies
---
+++ semanticcpg/src/test/scala/typeinfo/fetching/TestMockFetcher.scala
@@ -0,0 +1,32 @@
+package io.shiftleft.semanticcpg.typeinfo.fetching
+
+import io.shiftleft.semanticcpg.typeinfo.fetching._
+
+/** For use in testing, this is a fetcher that is just an immutable wrapper around an
+ * immutable `Map[String, Array[Byte]]` where the String is a server path like
+ * `/jvm/$PACKAGE_NAME/$VERSION/$TYPE_NAME.ion` */
+class TestMockFetcher private (private val store: Map[String, Array[Byte]]) extends Fetcher {
+  def this() = this(Map())
+  
+  /** First item of each tuple is string of ServerPath and second item is file contents
+   * as a string */
+  def addFiles(files: (String, String)*): TestMockFetcher = {
+    val withByteContent = files.map((fileName, content) => (fileName, content.getBytes("UTF-8")))
+    TestMockFetcher(store ++ withByteContent)
+  }
+  
+  override def close(): Unit = ()
+
+  override protected def downloadFiles(paths: List[ServerPath]): List[FetcherResult] = {
+    paths.foreach(validateFileInStore)
+    paths.map(serverPath => FetcherResult(serverPath, store(serverPath.toString)))
+  }
+  
+  private def validateFileInStore(path: ServerPath): Unit = {
+    if (!store.contains(path.toString)) {
+      val storeContents = store.mkString("->")
+      val errMsg = s"File with server path $path is not in TestMockFetcher store $storeContents"
+      throw new IllegalArgumentException(errMsg)
+    }
+  }
+}
