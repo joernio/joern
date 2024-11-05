880564cfc81f1b419a6829dc9b06637103bc50e8
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2024-11-05 10:22:59 +0100

    [c2cpg] Added test for compile commands json with broken file path (#5054)

--- joern-cli/frontends/c2cpg/src/test/scala/io/joern/c2cpg/io/JSONCompilationDatabaseParserTests.scala
+++ joern-cli/frontends/c2cpg/src/test/scala/io/joern/c2cpg/io/JSONCompilationDatabaseParserTests.scala
@@ -13,37 +13,6 @@ import java.nio.file.Paths
 
 class JSONCompilationDatabaseParserTests extends AnyWordSpec with Matchers {
 
-  "Parsing a simple compile_commands.json" should {
-    "generate a proper list of CommandObjects" in {
-      val content =
-        """
-          |[
-          |  { "directory": "/home/user/llvm/build",
-          |    "arguments": ["/usr/bin/clang++", "-I/usr/include", "-I./include", "-DSOMEDEFA=With spaces, quotes and \\-es.", "-c", "-o", "file.o", "file.cc"],
-          |    "file": "file.cc" },
-          |  { "directory": "/home/user/llvm/build",
-          |    "command": "/usr/bin/clang++ -I/home/user/project/includes -DSOMEDEFB=\"With spaces, quotes and \\-es.\" -DSOMEDEFC -c -o file.o file.cc",
-          |    "file": "file2.cc" }
-          |]""".stripMargin
-
-      File.usingTemporaryFile("compile_commands.json") { commandJsonFile =>
-        commandJsonFile.writeText(content)
-
-        val commandObjects = JSONCompilationDatabaseParser.parse(commandJsonFile.pathAsString)
-        commandObjects.map(_.compiledFile()) shouldBe List(
-          Paths.get("/home/user/llvm/build/file.cc").toString,
-          Paths.get("/home/user/llvm/build/file2.cc").toString
-        )
-        commandObjects.flatMap(_.defines()) shouldBe List(
-          ("SOMEDEFA", "With spaces, quotes and \\-es."),
-          ("SOMEDEFB", "\"With spaces, quotes and \\-es.\""),
-          ("SOMEDEFC", "")
-        )
-        commandObjects.flatMap(_.includes()) shouldBe List("/usr/include", "./include", "/home/user/project/includes")
-      }
-    }
-  }
-
   private def newProjectUnderTest(): File = {
     val dir = File.newTemporaryDirectory("c2cpgJSONCompilationDatabaseParserTests")
 
@@ -75,19 +44,84 @@ class JSONCompilationDatabaseParserTests extends AnyWordSpec with Matchers {
 
     val compilerCommands = dir / "compile_commands.json"
     compilerCommands.createIfNotExists(createParents = true)
-    val content = s"""
-       |[
-       |  { "directory": "${dir.pathAsString}",
-       |    "arguments": ["/usr/bin/clang++", "-Irelative", "-DSOMEDEFA=With spaces, quotes and \\-es.", "-c", "-o", "fileA.o", "fileA.cc"],
-       |    "file": "fileA.c" },
-       |  { "directory": ".",
-       |    "arguments": ["/usr/bin/clang++", "-Irelative", "-DSOMEDEFB=With spaces, quotes and \\-es.", "-c", "-o", "fileB.o", "fileB.cc"],
-       |    "file": "${fileB.pathAsString}" }
-       |]""".stripMargin.replace("\\", "\\\\") // escape for tests under Windows
+    val content =
+      s"""
+         |[
+         |  { "directory": "${dir.pathAsString}",
+         |    "arguments": ["/usr/bin/clang++", "-Irelative", "-DSOMEDEFA=With spaces, quotes and \\-es.", "-c", "-o", "fileA.o", "fileA.cc"],
+         |    "file": "fileA.c" },
+         |  { "directory": ".",
+         |    "arguments": ["/usr/bin/clang++", "-Irelative", "-DSOMEDEFB=With spaces, quotes and \\-es.", "-c", "-o", "fileB.o", "fileB.cc"],
+         |    "file": "${fileB.pathAsString}" }
+         |]""".stripMargin.replace("\\", "\\\\") // escape for tests under Windows
     compilerCommands.writeText(content)
     compilerCommands.deleteOnExit()
 
     dir.deleteOnExit()
+  }
+
+  private def newBrokenProjectUnderTest(): File = {
+    val dir = File.newTemporaryDirectory("c2cpgJSONCompilationDatabaseParserTests")
+
+    val mainText =
+      """
+        |int main(int argc, char *argv[]) {
+        |  print("Hello World!");
+        |}
+        |""".stripMargin
+
+    val fileA = dir / "fileA.c"
+    fileA.createIfNotExists(createParents = true)
+    fileA.writeText(mainText)
+    fileA.deleteOnExit()
+
+    val compilerCommands = dir / "compile_commands.json"
+    compilerCommands.createIfNotExists(createParents = true)
+    val content =
+      s"""
+         |[
+         |  { "directory": "${dir.pathAsString}",
+         |    "arguments": ["/usr/bin/clang++", "-Irelative", "-DSOMEDEFA=With spaces, quotes and \\-es.", "-c", "-o", "fileA.o", "fileA.cc"],
+         |    "file": "fileA.c" },
+         |  { "directory": "/does/not/exist",
+         |    "arguments": ["/usr/bin/clang++", "-c", "-o", "fileB.o", "name.cpp"],
+         |    "file": "name.cpp" }
+         |]""".stripMargin.replace("\\", "\\\\") // escape for tests under Windows
+    compilerCommands.writeText(content)
+    compilerCommands.deleteOnExit()
+
+    dir.deleteOnExit()
+  }
+
+  "Parsing a simple compile_commands.json" should {
+    "generate a proper list of CommandObjects" in {
+      val content =
+        """
+          |[
+          |  { "directory": "/home/user/llvm/build",
+          |    "arguments": ["/usr/bin/clang++", "-I/usr/include", "-I./include", "-DSOMEDEFA=With spaces, quotes and \\-es.", "-c", "-o", "file.o", "file.cc"],
+          |    "file": "file.cc" },
+          |  { "directory": "/home/user/llvm/build",
+          |    "command": "/usr/bin/clang++ -I/home/user/project/includes -DSOMEDEFB=\"With spaces, quotes and \\-es.\" -DSOMEDEFC -c -o file.o file.cc",
+          |    "file": "file2.cc" }
+          |]""".stripMargin
+
+      File.usingTemporaryFile("compile_commands.json") { commandJsonFile =>
+        commandJsonFile.writeText(content)
+
+        val commandObjects = JSONCompilationDatabaseParser.parse(commandJsonFile.pathAsString)
+        commandObjects.map(_.compiledFile()) shouldBe List(
+          Paths.get("/home/user/llvm/build/file.cc").toString,
+          Paths.get("/home/user/llvm/build/file2.cc").toString
+        )
+        commandObjects.flatMap(_.defines()) shouldBe List(
+          ("SOMEDEFA", "With spaces, quotes and \\-es."),
+          ("SOMEDEFB", "\"With spaces, quotes and \\-es.\""),
+          ("SOMEDEFC", "")
+        )
+        commandObjects.flatMap(_.includes()) shouldBe List("/usr/include", "./include", "/home/user/project/includes")
+      }
+    }
   }
 
   "Using a simple compile_commands.json" should {
@@ -109,6 +143,22 @@ class JSONCompilationDatabaseParserTests extends AnyWordSpec with Matchers {
         // fileC.c is ignored because it is not listed in the compile_commands.json
       )
       cpg.method.nameNot("<global>").name.sorted.l shouldBe List("foo", "main", "main")
+    }
+
+    "handle broken file paths" in {
+      val cpgOutFile = File.newTemporaryFile("c2cpg.bin")
+      cpgOutFile.deleteOnExit()
+      val projectUnderTest = newBrokenProjectUnderTest()
+      val input            = projectUnderTest.path.toAbsolutePath.toString
+      val output           = cpgOutFile.toString
+      val config = Config()
+        .withInputPath(input)
+        .withOutputPath(output)
+        .withCompilationDatabase((File(input) / "compile_commands.json").pathAsString)
+      val c2cpg = new C2Cpg()
+      val cpg   = c2cpg.createCpg(config).get
+      cpg.file.nameNot(FileTraversal.UNKNOWN, "<includes>").name.l shouldBe List("fileA.c")
+      cpg.method.nameNot("<global>").name.l shouldBe List("main")
     }
   }
 }
