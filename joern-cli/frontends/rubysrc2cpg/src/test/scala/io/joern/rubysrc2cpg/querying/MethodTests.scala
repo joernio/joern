3225661d5fa36441532990e3f87820c6ebf71022
Andrei Dreyer <andrei@whirlylabs.com>
2024-11-20 14:32:25 +0200

    Fixed warning on calling function that shadows a reserved keyword

--- joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/querying/MethodTests.scala
+++ joern-cli/frontends/rubysrc2cpg/src/test/scala/io/joern/rubysrc2cpg/querying/MethodTests.scala
@@ -1064,4 +1064,24 @@ class MethodTests extends RubyCode2CpgFixture {
       case xs => fail(s"Expected one return, got [${xs.code.mkString(",")}]")
     }
   }
+
+  "Method call with same name as reserved keyword" in {
+    val cpg = code("""
+        |    def public
+        |      list.sort_by(&:position).filter_map { |category| category.slug if category.visible_to_public? }
+        |    end
+        |
+        |    def notifiable
+        |      public
+        |    end
+        |""".stripMargin)
+
+    inside(cpg.method.name("notifiable").body.astChildren.isReturn.astChildren.isCall.name("public").l) {
+      case publicCall :: Nil =>
+        publicCall.code shouldBe "public"
+
+        val List(selfArg) = publicCall.argument.l
+      case xs => fail(s"Expected one call, got ${xs.code.mkString(",")}")
+    }
+  }
 }
