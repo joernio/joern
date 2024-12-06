e313500d15eee37e9ebb73caae9a522f732bb14f
Johannes Coetzee <jcoetzee@qwiet.ai>
2024-12-06 15:23:18 +0100

    Fix javasrc annotation resolution

--- joern-cli/frontends/javasrc2cpg/src/main/scala/io/joern/javasrc2cpg/astcreation/declarations/AstForTypeDeclsCreator.scala
+++ joern-cli/frontends/javasrc2cpg/src/main/scala/io/joern/javasrc2cpg/astcreation/declarations/AstForTypeDeclsCreator.scala
@@ -458,8 +458,12 @@ private[declarations] trait AstForTypeDeclsCreator { this: AstCreator =>
   }
 
   private[declarations] def astForAnnotationExpr(annotationExpr: AnnotationExpr): Ast = {
-    val fallbackType = s"${Defines.UnresolvedNamespace}.${annotationExpr.getNameAsString}"
-    val fullName     = expressionReturnTypeFullName(annotationExpr).getOrElse(fallbackType)
+    lazy val typ = tryWithSafeStackOverflow(annotationExpr.resolve()).toOption
+
+    val fullName = scope
+      .lookupType(annotationExpr.getNameAsString, false)
+      .orElse(tryWithSafeStackOverflow(annotationExpr.resolve()).toOption.flatMap(typeInfoCalc.fullName))
+      .getOrElse(defaultTypeFallback(annotationExpr.getNameAsString))
     typeInfoCalc.registerType(fullName)
     val code = annotationExpr.toString
     val name = annotationExpr.getName.getIdentifier
