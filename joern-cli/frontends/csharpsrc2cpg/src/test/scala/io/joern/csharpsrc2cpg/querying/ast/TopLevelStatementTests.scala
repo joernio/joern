1941026006b40f94ae94e6e1df6782cf7a7a5af9
Xavier Pinho <mail@xavierp.org>
2024-12-17 20:19:38 +0000

    [c#] support for top-level statements

---
+++ joern-cli/frontends/csharpsrc2cpg/src/test/scala/io/joern/csharpsrc2cpg/querying/ast/TopLevelStatementTests.scala
@@ -0,0 +1,46 @@
+package io.joern.csharpsrc2cpg.querying.ast
+
+import io.joern.csharpsrc2cpg.testfixtures.CSharpCode2CpgFixture
+import io.shiftleft.semanticcpg.language.*
+
+class TopLevelStatementTests extends CSharpCode2CpgFixture {
+
+  "WriteLine as a top-level statement should be found inside a fictitious method" in {
+    val cpg = code("""
+        |using System;
+        |Console.WriteLine("Foo");
+        |""".stripMargin)
+
+    inside(cpg.call("WriteLine").method.l) {
+      case method :: Nil =>
+        method.fullName shouldBe "Program.<Main>$"
+        method.signature shouldBe "System.Void(System.String[])"
+        method.typeDecl.l shouldBe cpg.typeDecl("Program").l
+      case xs => fail(s"Expected a method above WriteLine, but found $xs")
+    }
+  }
+
+  "free-variable `args` is in fact a method parameter" in {
+    val cpg = code("""
+        |System.Console.WriteLine(args);
+        |""".stripMargin)
+    inside(cpg.parameter("args").l) {
+      case args :: Nil =>
+        args.typeFullName shouldBe "System.String[]"
+        args.method.fullName shouldBe "Program.<Main>$"
+      case xs => fail(s"Expected single parameter named `args`, but found $xs")
+    }
+  }
+
+  "class declaration defined after top-level statements is present" in {
+    val cpg = code("""
+        |System.Console.WriteLine(args);
+        |class XYZ
+        |{
+        |}""".stripMargin)
+    inside(cpg.typeDecl("XYZ").l) {
+      case xyz :: Nil => xyz.fullName shouldBe "XYZ"
+      case xs         => fail(s"Expected single TYPE_DECL named `XYZ`, but found $xs")
+    }
+  }
+}
