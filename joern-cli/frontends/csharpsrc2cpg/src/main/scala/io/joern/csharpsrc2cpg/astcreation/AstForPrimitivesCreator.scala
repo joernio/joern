7fc450c4a7d14b38fb0bfa6e1dd50de1460a7cb7
Xavier Pinho <mail@xavierp.org>
2024-12-31 15:23:05 +0000

    [c#] fix member .code

--- joern-cli/frontends/csharpsrc2cpg/src/main/scala/io/joern/csharpsrc2cpg/astcreation/AstForPrimitivesCreator.scala
+++ joern-cli/frontends/csharpsrc2cpg/src/main/scala/io/joern/csharpsrc2cpg/astcreation/AstForPrimitivesCreator.scala
@@ -23,7 +23,7 @@ trait AstForPrimitivesCreator(implicit withSchemaValidation: ValidationMode) { t
             case Some(field) if field.node.node != DotNetJsonAst.VariableDeclarator =>
               astForFieldIdentifier(typeFullName, identifierName, field)
             case Some(field) =>
-              Ast(identifierNode(ident, identifierName, ident.code, field.typeFullName))
+              Ast(identifierNode(ident, identifierName, identifierName, field.typeFullName))
             case None =>
               // Check for static type reference
               scope.tryResolveTypeReference(identifierName) match {
