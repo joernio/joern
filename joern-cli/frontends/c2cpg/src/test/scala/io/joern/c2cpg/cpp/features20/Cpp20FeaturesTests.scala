99cecc8385b1f68c4c178d98c636ce764ab98df0
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2025-01-21 09:34:36 +0100

    [c2cpg] More C++20 features (#5238)

--- joern-cli/frontends/c2cpg/src/test/scala/io/joern/c2cpg/cpp/features20/Cpp20FeaturesTests.scala
+++ joern-cli/frontends/c2cpg/src/test/scala/io/joern/c2cpg/cpp/features20/Cpp20FeaturesTests.scala
@@ -2,26 +2,20 @@ package io.joern.c2cpg.cpp.features20
 
 import io.joern.c2cpg.parser.FileDefaults
 import io.joern.c2cpg.testfixtures.AstC2CpgSuite
+import io.shiftleft.codepropertygraph.generated.ControlStructureTypes
 import io.shiftleft.semanticcpg.language.*
 
 class Cpp20FeaturesTests extends AstC2CpgSuite(fileSuffix = FileDefaults.CppExt) {
 
   "C++20 feature support" should {
 
-    "handle coroutines" ignore {
+    "handle coroutines" in {
       val cpg = code("""
           |generator<int> range(int start, int end) {
           |  while (start < end) {
           |    co_yield start;
           |    start++;
           |  }
-          |
-          |  // Implicit co_return at the end of this function:
-          |  // co_return;
-          |}
-          |
-          |for (int n : range(0, 10)) {
-          |  std::cout << n << std::endl;
           |}
           |
           |task<void> echo(socket s) {
@@ -29,15 +23,34 @@ class Cpp20FeaturesTests extends AstC2CpgSuite(fileSuffix = FileDefaults.CppExt)
           |    auto data = co_await s.async_read();
           |    co_await async_write(s, data);
           |  }
-          |
-          |  // Implicit co_return at the end of this function:
-          |  // co_return;
           |}
           |""".stripMargin)
-      ???
+      cpg.method
+        .nameExact("range")
+        .controlStructure
+        .astChildren
+        .isBlock
+        .astChildren
+        .isCall
+        .nameExact("<operator>.yield")
+        .size shouldBe 1
+
+      pendingUntilFixed {
+        // `auto data = co_await s.async_read();` can not be parsed yet.
+        // Hence, this co_await call is missing.
+        cpg.method
+          .nameExact("echo")
+          .controlStructure
+          .astChildren
+          .isBlock
+          .astChildren
+          .isCall
+          .nameExact("<operator>.await")
+          .size shouldBe 2
+      }
     }
 
-    "handle concepts" ignore {
+    "handle concepts" in {
       val cpg = code("""
           |// `T` is not limited by any constraints.
           |template <typename T>
@@ -55,53 +68,51 @@ class Cpp20FeaturesTests extends AstC2CpgSuite(fileSuffix = FileDefaults.CppExt)
           |// Forms for function parameters:
           |// `T` is a constrained type template parameter.
           |template <my_concept T>
-          |void f(T v);
+          |void f1(T v);
           |
           |// `T` is a constrained type template parameter.
           |template <typename T>
           |  requires my_concept<T>
-          |void f(T v);
+          |void f2(T v);
           |
           |// `T` is a constrained type template parameter.
           |template <typename T>
-          |void f(T v) requires my_concept<T>;
+          |void f3(T v) requires my_concept<T>;
           |
           |// `v` is a constrained deduced parameter.
-          |void f(my_concept auto v);
+          |void f4(my_concept auto v);
           |
           |// `v` is a constrained non-type template parameter.
           |template <my_concept auto v>
-          |void g();
+          |void f5();
           |
-          |// Forms for auto-deduced variables:
-          |// `foo` is a constrained auto-deduced value.
-          |my_concept auto foo = ...;
-          |
-          |// Forms for lambdas:
-          |// `T` is a constrained type template parameter.
-          |auto f = []<my_concept T> (T v) {
-          |  // ...
-          |};
-          |// `T` is a constrained type template parameter.
-          |auto f = []<typename T> requires my_concept<T> (T v) {
-          |  // ...
-          |};
-          |// `T` is a constrained type template parameter.
-          |auto f = []<typename T> (T v) requires my_concept<T> {
-          |  // ...
-          |};
-          |// `v` is a constrained deduced parameter.
-          |auto f = [](my_concept auto v) {
-          |  // ...
-          |};
-          |// `v` is a constrained non-type template parameter.
-          |auto g = []<my_concept auto v> () {
-          |  // ...
-          |};
+          |void foo() {
+          |  // Forms for lambdas:
+          |  // `T` is a constrained type template parameter.
+          |  auto l1 = []<my_concept T> (T v) {
+          |    // ...
+          |  };
+          |  // `T` is a constrained type template parameter.
+          |  auto l2 = []<typename T> requires my_concept<T> (T v) {
+          |    // ...
+          |  };
+          |  // `T` is a constrained type template parameter.
+          |  auto l3 = []<typename T> (T v) requires my_concept<T> {
+          |    // ...
+          |  };
+          |  // `v` is a constrained deduced parameter.
+          |  auto l4 = [](my_concept auto v) {
+          |    // ...
+          |  };
+          |  // `v` is a constrained non-type template parameter.
+          |  auto l5 = []<my_concept auto v> () {
+          |    // ...
+          |  };
+          |}
           |
           |template <typename T>
           |  requires my_concept<T> // `requires` clause.
-          |void f(T);
+          |void f6(T);
           |
           |template <typename T>
           |concept callable = requires (T f) { f(); }; // `requires` expression.
@@ -113,28 +124,58 @@ class Cpp20FeaturesTests extends AstC2CpgSuite(fileSuffix = FileDefaults.CppExt)
           |}
           |
           |template <typename T>
-          |concept C = requires(T x) {
+          |concept C1 = requires(T x) {
           |  {*x} -> std::convertible_to<typename T::inner>; // the type of the expression `*x` is convertible to `T::inner`
           |  {x + 1} -> std::same_as<int>; // the expression `x + 1` satisfies `std::same_as<decltype((x + 1))>`
           |  {x * 1} -> std::convertible_to<T>; // the type of the expression `x * 1` is convertible to `T`
           |};
           |
           |template <typename T>
-          |concept C = requires(T x) {
+          |concept C2 = requires(T x) {
           |  requires std::same_as<sizeof(x), size_t>;
           |};
           |""".stripMargin)
-      ???
+      // we can't express concepts withing the CPG but parsing constructs using them should not be hindered
+      // sadly, at some places (e.g., for parameters) it fails parsing
+      cpg.method.nameNot("<global>").fullName.sorted.l shouldBe List(
+        "add:T(T,T)",
+        "f1:void(T)",
+        "f2:void(T)",
+        "f3:void(T)",
+        "f6:void(T)",
+        "foo:void()",
+        "requires:requires(T)"
+      )
+      pendingUntilFixed {
+        cpg.method.nameNot("<global>").fullName.sorted.l shouldBe List(
+          "add:T(T,T)",
+          "f1:void(T)",
+          "f2:void(T)",
+          "f3:void(T)",
+          "f4:void(ANY)",
+          "f5:void()",
+          "f6:void(T)",
+          // remaining lambdas ...
+          "foo:void()"
+        )
+      }
     }
 
-    "handle three-way comparison" ignore {
+    "handle three-way comparison" in {
       val cpg = code("""
-          |bool x = 1 <=> 2;
+          |bool foo() {
+          |  bool x = 1 <=> 2;
+          |  return x;
+          |}
           |""".stripMargin)
-      ???
+      // three-way comparison operator can not be parsed at the moment
+      pendingUntilFixed {
+        cpg.assignment.code.l shouldBe List("bool x = 1 <=> 2")
+        // TODO: test for children AST elements
+      }
     }
 
-    "handle designated initializers" ignore {
+    "handle designated initializers" in {
       val cpg = code("""
           |struct A {
           |  int x;
@@ -142,18 +183,31 @@ class Cpp20FeaturesTests extends AstC2CpgSuite(fileSuffix = FileDefaults.CppExt)
           |  int z = 123;
           |};
           |
-          |A a {.x = 1, .z = 2}; // a.x == 1, a.y == 0, a.z == 2
+          |void foo() {
+          |  A a {.x = 1, .z = 2}; // a.x == 1, a.y == 0, a.z == 2
+          |}
           |""".stripMargin)
-      ???
+      cpg.assignment.code.sorted.l shouldBe List("a.x = 1", "a.z = 2", "z = 123")
+      pendingUntilFixed {
+        // CDT failed to assign the type A to a here so we are not
+        // able to identify all struct fields. Hence, no a.y = 0 assignment yet.
+        cpg.assignment.code.sorted.l shouldBe List("a.x = 1", "a.y = 0", "a.z = 2", "z = 123")
+      }
     }
 
-    "handle template syntax for lambdas" ignore {
+    "handle template syntax for lambdas" in {
       val cpg = code("""
-          |auto f = []<typename T>(std::vector<T> v) {
-          |  // ...
-          |};
+          |void foo() {
+          |  auto f = []<typename T>(std::vector<T> v) {
+          |    // ...
+          |  };
+          |}
           |""".stripMargin)
-      ???
+      cpg.method.nameNot("<global>").fullName.sorted.l shouldBe List("foo:void()")
+      pendingUntilFixed {
+        // []<typename T>(std::vector<T> v) { ... } can not be parsed by CDT at the moment
+        cpg.method.nameNot("<global>").fullName.sorted.l shouldBe List("foo:void()", "<lambda>0")
+      }
     }
 
     "handle range-based for loop with initializer" ignore {
@@ -226,7 +280,7 @@ class Cpp20FeaturesTests extends AstC2CpgSuite(fileSuffix = FileDefaults.CppExt)
       ???
     }
 
-    "handle constexpr virtual functions" ignore {
+    "handle constexpr virtual functions" in {
       val cpg = code("""
           |struct X1 {
           |  virtual int f() const = 0;
@@ -244,13 +298,22 @@ class Cpp20FeaturesTests extends AstC2CpgSuite(fileSuffix = FileDefaults.CppExt)
           |  constexpr virtual int f() const { return 4; }
           |};
           |
-          |constexpr X4 x4;
-          |x4.f(); // == 4
+          |void foo() {
+          |  constexpr X4 x4;
+          |  x4.f(); // == 4
+          |}
           |""".stripMargin)
-      ???
+      cpg.method.nameNot("<global>").fullName.sorted.l shouldBe List(
+        "X1.f:int()",
+        "X2.f:int()",
+        "X3.f:int()",
+        "X4.f:int()",
+        "foo:void()"
+      )
+      cpg.method.nameExact("foo").local.typeFullName.l shouldBe List("X4")
     }
 
-    "handle explicit(bool)" ignore {
+    "handle explicit(bool)" in {
       val cpg = code("""
           |struct foo {
           |  // Specify non-integral types (strings, floats, etc.) require explicit construction.
@@ -258,27 +321,35 @@ class Cpp20FeaturesTests extends AstC2CpgSuite(fileSuffix = FileDefaults.CppExt)
           |  explicit(!std::is_integral_v<T>) foo(T) {}
           |};
           |
-          |foo a = 123;
-          |foo c {"123"};
+          |void foo() {
+          |  foo a = 123;
+          |  foo c {"123"};
+          |}
           |""".stripMargin)
-      ???
+      cpg.method.nameExact("foo").local.typeFullName.distinct.l shouldBe List("foo")
+      pendingUntilFixed {
+        // CDT can not parse explicit(bool) yet
+        cpg.method.nameNot("<global>").fullName.sorted.l shouldBe List("A.foo:T(T)", "foo:void()")
+      }
     }
 
-    "handle immediate functions" ignore {
+    "handle immediate functions" in {
       val cpg = code("""
           |consteval int sqr(int n) {
           |  return n * n;
           |}
           |
-          |constexpr int r = sqr(100); // OK
-          |int x = 100;
-          |int r2 = sqr(x); // ERROR: the value of 'x' is not usable in a constant expression
-          |                 // OK if `sqr` were a `constexpr` function
+          |void foo() {
+          |  constexpr int r = sqr(100);
+          |}
           |""".stripMargin)
-      ???
+      cpg.method.nameNot("<global>").fullName.sorted.l shouldBe List("foo:void()", "sqr:int(int)")
+      val List(rLocal) = cpg.method.nameExact("foo").local.l
+      rLocal.typeFullName shouldBe "int"
+      rLocal.code shouldBe "constexpr int r"
     }
 
-    "handle using enum" ignore {
+    "handle using enum" in {
       val cpg = code("""
           |enum class rgba_color_channel { red, green, blue, alpha };
           |
@@ -292,55 +363,92 @@ class Cpp20FeaturesTests extends AstC2CpgSuite(fileSuffix = FileDefaults.CppExt)
           |  }
           |}
           |""".stripMargin)
-      ???
+      val List(switchBlock) = cpg.method
+        .nameExact("to_string")
+        .controlStructure
+        .controlStructureTypeExact(ControlStructureTypes.SWITCH)
+        .astChildren
+        .isBlock
+        .l
+      switchBlock._jumpTargetViaAstOut.code.l shouldBe List("case red:", "case green:", "case blue:", "case alpha:")
+      pendingUntilFixed {
+        // using clause is not parsed by CDT at all yet
+        switchBlock._jumpTargetViaAstOut.astChildren.isCall.code.l shouldBe List(
+          "rgba_color_channel.red",
+          "rgba_color_channel.green",
+          "rgba_color_channel.blue",
+          "rgba_color_channel.alpha"
+        )
+      }
     }
 
-    "handle lambda capture of parameter pack" ignore {
+    "handle lambda capture of parameter pack" in {
       val cpg = code("""
           |template <typename... Args>
           |auto f1(Args&&... args){
           |    // BY VALUE:
-          |    return [...args = std::forward<Args>(args)] {
-          |        // ...
-          |    };
+          |    return [...args = std::forward<Args>(args)] {};
           |}
           |
           |template <typename... Args>
           |auto f2(Args&&... args){
           |    // BY REFERENCE:
-          |    return [&...args = std::forward<Args>(args)] {
-          |        // ...
-          |    };
+          |    return [&...args = std::forward<Args>(args)] {};
           |}
           |""".stripMargin)
-      ???
+      cpg.method.nameNot("<global>").fullName.sorted.l shouldBe List("f1:ANY(Args&&)", "f2:ANY(Args&&)")
+      cpg.method.nameNot("<global>").signature.sorted.l shouldBe List("ANY(Args&&)", "ANY(Args&&)")
+      pendingUntilFixed {
+        // the actual return value (i.e., the lambda defined at the return) can not be parsed by CDT
+        cpg.method.nameExact("f1").ast.isReturn.astChildren.isMethodRef.code.l shouldBe List(
+          "[...args = std::forward<Args>(args)] {};"
+        )
+        cpg.method.nameExact("f2").ast.isReturn.astChildren.isMethodRef.code.l shouldBe List(
+          "[&...args = std::forward<Args>(args)] {};"
+        )
+      }
     }
 
-    "handle char8_t" ignore {
+    "handle char8_t" in {
       val cpg = code("""
-          |char8_t utf8_str[] = u8"\\u0123";
+          |char8_t utf8_str[] = u8"\u0123";
           |""".stripMargin)
-      ???
+      val List(assignmentCall) = cpg.call.l
+      assignmentCall.code shouldBe """utf8_str[] = u8"\u0123""""
+      val List(utf8_str) = assignmentCall.argument.isIdentifier.l
+      utf8_str.name shouldBe "utf8_str"
+      utf8_str.typeFullName shouldBe "char8_t[6]"
+      val List(u8) = assignmentCall.argument.isLiteral.l
+      u8.code shouldBe """u8"\u0123""""
+      u8.typeFullName shouldBe "char[6]"
     }
 
-    "handle constinit" ignore {
+    "handle constinit" in {
       val cpg = code("""
-          |const char* g() { return "dynamic initialization"; }
           |constexpr const char* f(bool p) { return p ? "constant initializer" : g(); }
           |
-          |constinit const char* c = f(true); // OK
-          |constinit const char* d = g(false); // ERROR: `g` is not constexpr, so `d` cannot be evaluated at compile-time.
+          |void foo() {
+          |  constinit const char *c = f(true);
+          |}
           |""".stripMargin)
-      ???
+      cpg.method.nameNot("<global>").fullName.sorted.l shouldBe List("f:char*(bool)", "foo:void()")
+      val List(cLocal) = cpg.method.nameExact("foo").local.l
+      cLocal.typeFullName shouldBe "char*"
+      cLocal.code shouldBe "const char *c" // constinit keyword is not parsed by CDT
     }
 
-    "handle __VA_OPT__" ignore {
+    "handle __VA_OPT__" in {
       val cpg = code("""
           |#define F(...) f(0 __VA_OPT__(,) __VA_ARGS__)
-          |F(a, b, c) // replaced by f(0, a, b, c)
-          |F()        // replaced by f(0)
+          |void foo() {
+          |  F(a, b, c); // replaced by f(0, a, b, c)
+          |  F();        // replaced by f(0)
+          |}
           |""".stripMargin)
-      ???
+      pendingUntilFixed {
+        // Impossible to test without C++ system headers for __VA_OPT__ and __VA_ARGS__ definitions
+        cpg.call.code.l shouldBe List("f(a, b, c)", "f()")
+      }
     }
 
   }
