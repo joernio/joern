95fb3463c015f5bd8955041c539ce094b0d1f26c
David Baker Effendi <dave@whirlylabs.com>
2025-01-27 13:11:37 +0200

    Update tests/finding-to-sarif-test.sh
    Co-authored-by: Michael Pollmeier <michael@michaelpollmeier.com>

--- tests/finding-to-sarif-test.sh
+++ tests/finding-to-sarif-test.sh
@@ -6,10 +6,9 @@ SCRIPT_ABS_PATH=$(readlink -f "$0")
 JOERN_TESTS_DIR=$(dirname "$SCRIPT_ABS_PATH")
 JOERN="$JOERN_TESTS_DIR"/..
 
+mkdir -p /tmp/sarif
 ./joern-scan "$JOERN_TESTS_DIR/code/sarif-test" --store
 ./joern --script "$JOERN_TESTS_DIR/test-sarif.sc" --param cpgFile="$JOERN/workspace/sarif-test/cpg.bin" --param outFile="/tmp/sarif/test.sarif"
-
-mkdir -p /tmp/sarif
 exit_code=$(curl -s -X POST \
     -F "postedFiles=@/tmp/sarif/test.sarif;type=application/octet-stream" \
     https://sarifweb.azurewebsites.net/Validation/ValidateFiles | jq -r '.exitCode')
