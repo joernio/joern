86a2e9bac88dddbcea374441c36f642ac7e7c7f6
Andrei Dreyer <andrei@whirlylabs.com>
2025-03-18 08:46:44 +0200

    move delimiters into companion object, replace substring with StringUtils.strip

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/LiteralMethods.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/nodemethods/LiteralMethods.scala
@@ -5,43 +5,40 @@ import io.shiftleft.codepropertygraph.generated.Languages
 import io.shiftleft.codepropertygraph.generated.nodes.{Literal, NewLocation}
 import io.shiftleft.semanticcpg.NodeExtension
 import io.shiftleft.semanticcpg.language.*
+import io.shiftleft.semanticcpg.language.nodemethods.LiteralMethods.delimiters
+import org.apache.commons.lang3.StringUtils
 
 class LiteralMethods(val literal: Literal) extends AnyVal with NodeExtension with HasLocation {
   def innerText: Option[String] = {
-    val delimiters = Cpg(literal.graph).metaData.language.headOption match {
-      case Some(Languages.JAVASRC | Languages.JAVA | Languages.KOTLIN | Languages.SWIFTSRC) =>
-        "\"\"\"" :: "\"" :: Nil
-      case Some(Languages.C | Languages.NEWC | Languages.PHP) =>
-        "\"" :: "'" :: Nil
-      case Some(Languages.JAVASCRIPT | Languages.JSSRC) =>
-        "\"" :: "'" :: "`" :: Nil
-      case Some(Languages.GOLANG) =>
-        "\"" :: "`" :: Nil
-      case Some(Languages.CSHARP | Languages.CSHARPSRC) =>
-        "\"" :: Nil
-      case Some(Languages.RUBYSRC) =>
-        "\"" :: "'" :: Nil
-      case Some(Languages.PYTHON | Languages.PYTHONSRC) =>
-        "\"\"\"" :: "'''" :: "\"" :: "'" :: Nil
-      case _ =>
-        "\"" :: Nil
-    }
-
-    delimiters
+    delimiters(literal)
       .filter(literal.code.startsWith(_))
-      .map { delimiter =>
-        val start =
-          if (delimiter == "\"\"\"" || delimiter == "'''") then literal.code.indexOf(delimiter) + 3
-          else literal.code.indexOf(delimiter) + 1
-        val end = literal.code.lastIndexOf(delimiter)
-
-        literal.code.substring(start, end)
-      }
+      .map(StringUtils.strip(literal.code, _))
       .headOption
   }
 
   override def location: NewLocation = {
     LocationCreator(literal, literal.code, literal.label, literal.lineNumber, literal.method)
 
+  }
+}
+
+object LiteralMethods {
+  def delimiters(literal: Literal): List[String] = Cpg(literal.graph).metaData.language.headOption match {
+    case Some(Languages.JAVASRC | Languages.JAVA | Languages.KOTLIN | Languages.SWIFTSRC) =>
+      "\"\"\"" :: "\"" :: Nil
+    case Some(Languages.C | Languages.NEWC | Languages.PHP) =>
+      "\"" :: "'" :: Nil
+    case Some(Languages.JAVASCRIPT | Languages.JSSRC) =>
+      "\"" :: "'" :: "`" :: Nil
+    case Some(Languages.GOLANG) =>
+      "\"" :: "`" :: Nil
+    case Some(Languages.CSHARP | Languages.CSHARPSRC) =>
+      "\"" :: Nil
+    case Some(Languages.RUBYSRC) =>
+      "\"" :: "'" :: Nil
+    case Some(Languages.PYTHON | Languages.PYTHONSRC) =>
+      "\"\"\"" :: "'''" :: "\"" :: "'" :: Nil
+    case _ =>
+      "\"" :: Nil
   }
 }
