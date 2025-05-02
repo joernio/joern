5c6c0c15d6772e83c9290f592bca457c3edff714
phli <lipenghui315@gmail.com>
2025-05-02 04:41:57 -0400

    [PHP Query] fix argument of magic hash function call (#5464)

--- querydb/src/test/scala/io/joern/scanners/php/MagicHashTests.scala
+++ querydb/src/test/scala/io/joern/scanners/php/MagicHashTests.scala
@@ -32,4 +32,18 @@ class MagicHashTests extends PHPQueryTestSuite(MagicHash) {
       results shouldBe List("$_GET[\"password\"]")
     }
   }
+
+  "The `magic-hash` query" when {
+    "detects Magic Hash (through dedicated hash funcs) in loose comparison " in {
+      val cpg = code("""<?php
+          |$input = $_GET["password"];
+          |$hash = md5($input);
+          |if ($hash == "0e1234") {
+          |};
+          |""".stripMargin)
+      val query   = queryBundle.magicHash()
+      val results = this.findMatchingCalls(cpg, query)
+      results shouldBe List("$_GET[\"password\"]")
+    }
+  }
 }
