13beee02841488fa9fc244f03a8bc2666c75554b
David Baker Effendi <dave@whirlylabs.com>
2025-05-05 13:08:01 +0200

    Added tests for classes, methods, and fields

--- joern-cli/frontends/php2cpg/src/test/scala/io/joern/php2cpg/passes/PhpSummaryPassTests.scala
+++ joern-cli/frontends/php2cpg/src/test/scala/io/joern/php2cpg/passes/PhpSummaryPassTests.scala
@@ -1,6 +1,6 @@
 package io.joern.php2cpg.passes
 
-import io.joern.php2cpg.datastructures.{PhpMethod, PhpProgramSummary}
+import io.joern.php2cpg.datastructures.{PhpField, PhpMethod, PhpProgramSummary}
 import io.joern.php2cpg.parser.PhpParser
 import io.joern.php2cpg.{Config, Php2Cpg}
 import io.shiftleft.semanticcpg.utils.FileUtil
@@ -17,7 +17,7 @@ class PhpSummaryPassTests extends AnyWordSpec with Matchers {
   "pre-parsing a file with a top-level function should provide a summary of that function" in {
     assertAgainstTempFile(
       """<?php
-        |function foo($a, $b): int {}
+        |function foo($a, string $b): int {}
         |""".stripMargin,
       { programSummary =>
         val summary = programSummary.namespaceToType
@@ -25,8 +25,43 @@ class PhpSummaryPassTests extends AnyWordSpec with Matchers {
         val globalType = summary("Test.php:<global>").head
         globalType.name shouldBe "Test.php:<global>"
         globalType.methods should contain(
-          PhpMethod("foo", List(("a", "ANY"), ("b", "ANY")), "int", Some("Test.php:<global>"))
+          PhpMethod("foo", List(("a", "ANY"), ("b", "string")), "int", Some("Test.php:<global>"))
         )
+      }
+    )
+  }
+
+  "pre-parsing a file with a top-level class with a nested function should provide a summary of that class" in {
+    assertAgainstTempFile(
+      """<?php
+        |class Foo {
+        |  final public function foo(int $x): int {
+        |    return 0;
+        |  }
+        |}
+        |""".stripMargin,
+      { programSummary =>
+        val summary = programSummary.namespaceToType
+        summary.size shouldBe 1
+        val fooType = summary("Test.php:<global>").filter(_.name == "Foo").head
+        fooType.methods should contain(PhpMethod("foo", List(("this", "Foo"), ("x", "int")), "int", Some("Foo")))
+        fooType.methods should contain(PhpMethod("__construct", List(("this", "Foo")), "ANY", Some("Foo")))
+      }
+    )
+  }
+
+  "pre-parsing a file with a top-level class with a nested constant should provide a summary of that class" in {
+    assertAgainstTempFile(
+      """<?php
+        |class Foo {
+        |  const B = "B";
+        |}
+        |""".stripMargin,
+      { programSummary =>
+        val summary = programSummary.namespaceToType
+        summary.size shouldBe 1
+        val fooType = summary("Test.php:<global>").filter(_.name == "Foo").head
+        fooType.fields should contain(PhpField("B", "ANY"))
       }
     )
   }
