5be6b1f63b51b7ca24350d646f486083ba7e07b5
Michael Pollmeier <michael@michaelpollmeier.com>
2025-05-14 16:06:34 +0200

    scala 3.6.4

--- joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/utils/ExternalCommand.scala
+++ joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/utils/ExternalCommand.scala
@@ -2,5 +2,5 @@ package io.joern.x2cpg.utils
 
 import io.shiftleft.semanticcpg.utils.ExternalCommandImpl
 
-@Deprecated("Use `io.shiftleft.semanticcpg.utils.ExternalCommand` instead")
+@Deprecated(since = "Use `io.shiftleft.semanticcpg.utils.ExternalCommand` instead")
 object ExternalCommand extends ExternalCommandImpl
