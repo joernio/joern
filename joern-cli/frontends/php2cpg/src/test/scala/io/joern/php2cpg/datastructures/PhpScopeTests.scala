c45bcc7808bc99245654b5f6e5828e60fd3a3c62
David Baker Effendi <dave@whirlylabs.com>
2025-05-16 11:33:49 +0200

    Added test fixture for testing scope objects

---
+++ joern-cli/frontends/php2cpg/src/test/scala/io/joern/php2cpg/datastructures/PhpScopeTests.scala
@@ -0,0 +1,47 @@
+package io.joern.php2cpg.datastructures
+
+import io.joern.php2cpg.passes.SymbolSummaryPass.*
+import io.joern.php2cpg.utils.Scope
+import io.shiftleft.codepropertygraph.generated.nodes.NewImport
+import org.scalatest.Assertion
+import org.scalatest.Assertions.fail
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class PhpScopeTests extends AnyWordSpec with Matchers {
+
+  import PhpScopeTests.*
+
+  "a function in the summary should resolve when imported" in {
+    val scope = scopeFrom(PhpFunction("foo"))
+    scope.imporT("foo")
+    scope.testResolve("foo") {
+      case PhpFunction(name) if name == "foo" => succeed
+      case symbol                             => fail(s"Unable to resolve correct symbol (given $symbol)")
+    }
+  }
+
+}
+
+object PhpScopeTests {
+
+  def scopeFrom(symbols: SymbolSummary*): Scope = {
+    Scope(symbols)(() => "nan")
+  }
+
+  implicit class ScopeExt(scope: Scope) {
+
+    def imporT(path: String): Scope = {
+      scope.useImport(NewImport().importedEntity(path) :: Nil)
+      scope
+    }
+
+    def testResolve(symbol: String)(expectation: SymbolSummary => Assertion): Assertion = {
+      scope.resolveImportedSymbol(symbol) match {
+        case Some(symbol) => expectation(symbol)
+        case None         => fail("Unable to resolve imported symbol")
+      }
+    }
+
+  }
+}
