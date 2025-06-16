ea41d7ae72af0c2448f9b81cdcef511384619979
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2025-06-16 11:49:22 +0200

    another try

--- testDistro.py
+++ testDistro.py
@@ -8,7 +8,7 @@ import tempfile
 import requests
 
 # Prerequisite:
-# > sbt joerncli/stage querydb/createDistribution
+# > sbt stage querydb/createDistribution
 
 def executable_name(executable):
     # Determine script name for Windows vs. Unix
