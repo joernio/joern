904b1d4bace41dedc1b8722116f84334063c7d3f
Johannes Coetzee <jcoetzee@qwiet.ai>
2025-07-10 14:45:10 +0200

    Bump javaparser version (#5578)

--- project/Versions.scala
+++ project/Versions.scala
@@ -19,7 +19,7 @@ object Versions {
   val ghidra                 = "11.4_f331b5bbea-202504251242"
   val gradleTooling          = "8.3"
   val jacksonDatabind        = "2.17.0"
-  val javaParser             = "3.26.2"
+  val javaParser             = "3.27.0"
   val jlhttp                 = "3.1"
   val jRuby                  = "9.4.9.0"
   val json4s                 = "4.0.7"
