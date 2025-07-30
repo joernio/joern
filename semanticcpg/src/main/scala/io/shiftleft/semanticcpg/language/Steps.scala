joernio/joern

commit efc42d024f17b219287320c8636ce2aa5aafbc92

Change .location, NewLocation, and Location to lazily compute location information (#5530)
* Switch to a lazy location implementatation

* Move node methods from cs into joern

* Add filename override option for LocationInfo

* Bump local cpg

* Bump CPG

* Fix slice test expectation

---------

Co-authored-by: flandini <mflanders@qwiet.ai>
Co-authored-by: Johannes Coetzee <jcoetzee@qwiet.ai>

semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/Steps.scala
@@ -83,14 +83,16 @@ object Steps {
   private lazy val nodeSerializer = new CustomSerializer[AbstractNode](implicit format =>
     (
       { case _ => ??? }, // deserializer not required for now
-      { case node: AbstractNode =>
+      { case node: Product =>
         val elementMap = Map.newBuilder[String, Any]
         (0 until node.productArity).foreach { i =>
           val label   = node.productElementName(i)
           val element = node.productElement(i)
           elementMap.addOne(label -> element)
         }
-        elementMap.addOne("_label" -> node.label)
+        if (node.isInstanceOf[AbstractNode]) {
+          elementMap.addOne("_label" -> node.asInstanceOf[AbstractNode].label)
+        }
         if (node.isInstanceOf[StoredNode]) {
           elementMap.addOne("_id" -> node.asInstanceOf[StoredNode].id())
         }

semanticcpg/src/test/scala/io/shiftleft/semanticcpg/language/StepsTest.scala
@@ -126,8 +126,7 @@ class StepsTest extends AnyWordSpec with Matchers {
     }
 
     "operating on NewNode" in {
-      implicit val finder: NodeExtensionFinder = DefaultNodeExtensionFinder
-      def location                             = cpg.method.name("foo").location
+      def location = cpg.method.name("foo").location
 
       location.size shouldBe 1
       val parsedChildren = parse(location.toJson).children

build.sbt
@@ -2,7 +2,7 @@ name                     := "joern"
 ThisBuild / organization := "io.joern"
 ThisBuild / scalaVersion := "3.6.4"
 
-val cpgVersion = "1.7.35"
+val cpgVersion = "1.7.36"
 
 lazy val joerncli          = Projects.joerncli
 lazy val querydb           = Projects.querydb

console/src/main/scala/io/joern/console/scan/package.scala
@@ -100,7 +100,7 @@ package object scan {
 
   /** Print human readable list of findings to standard out.
     */
-  def outputFindings(cpg: Cpg)(implicit finder: NodeExtensionFinder): Unit = {
+  def outputFindings(cpg: Cpg)(implicit locationCreator: LocationCreator): Unit = {
     cpg.finding.sortBy(_.score.toInt).foreach { finding =>
       val evidence = finding.evidence.headOption
         .map { e =>

joern-cli/frontends/javasrc2cpg/src/test/scala/io/joern/javasrc2cpg/querying/CodeDumperTests.scala
@@ -7,9 +7,6 @@ import io.shiftleft.semanticcpg.language.*
 class CodeDumperTests extends JavaSrcCode2CpgFixture {
 
   "a Java source code CPG" should {
-
-    implicit val finder: NodeExtensionFinder = DefaultNodeExtensionFinder
-
     val cpg = code("""
         |public class Foo {
         |

joern-cli/src/main/scala/io/joern/joerncli/JoernScan.scala
@@ -8,7 +8,7 @@ import io.joern.joerncli.JoernScan.getQueriesFromQueryDb
 import io.joern.joerncli.Scan.{allTag, defaultTag}
 import io.joern.joerncli.console.ReplBridge
 import io.shiftleft.codepropertygraph.generated.Languages
-import io.shiftleft.semanticcpg.language.{DefaultNodeExtensionFinder, NodeExtensionFinder}
+import io.shiftleft.semanticcpg.language.{DefaultNodeExtensionFinder, NodeExtensionFinder, locationCreator}
 import io.shiftleft.semanticcpg.layers.{LayerCreator, LayerCreatorContext, LayerCreatorOptions}
 import io.shiftleft.semanticcpg.utils.FileUtil
 import io.shiftleft.semanticcpg.utils.FileUtil.*
@@ -259,8 +259,6 @@ class ScanOptions(var maxCallDepth: Int, var names: Array[String], var tags: Arr
     extends LayerCreatorOptions {}
 
 class Scan(options: ScanOptions)(implicit engineContext: EngineContext) extends LayerCreator {
-  implicit val finder: NodeExtensionFinder = DefaultNodeExtensionFinder
-
   override val overlayName: String = Scan.overlayName
   override val description: String = Scan.description
 

semanticcpg/src/main/scala/io/shiftleft/semanticcpg/codedumper/CodeDumper.scala
@@ -1,7 +1,7 @@
 package io.shiftleft.semanticcpg.codedumper
 
 import io.shiftleft.codepropertygraph.generated.Languages
-import io.shiftleft.codepropertygraph.generated.nodes.{Expression, Local, Method, NewLocation}
+import io.shiftleft.codepropertygraph.generated.nodes.{Expression, Local, Method}
 import io.shiftleft.semanticcpg.language.*
 import io.shiftleft.utils.IOUtils
 import org.slf4j.{Logger, LoggerFactory}
@@ -39,7 +39,7 @@ object CodeDumper {
   /** Dump string representation of code at given `location`.
     */
   def dump(
-    location: NewLocation,
+    location: LocationInfo,
     language: Option[String],
     rootPath: Option[String],
     highlight: Boolean,

semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/HasLocation.scala
@@ -1,7 +1,0 @@
-package io.shiftleft.semanticcpg.language
-
-import io.shiftleft.codepropertygraph.generated.nodes.NewLocation
-
-trait HasLocation extends Any {
-  def location: NewLocation
-}

commit 22174d850ddf1979d06835239211898573ed7557

fix json serializer: define only for nodes, not for all Products
prior to this change:
```
List((1,2,3)).toJson
// [{"head":1,"next":{"head":2,"next":{"head":3,"next":{}}}}]
```

now:
```
List((1,2,3)).toJson
// [1,2,3]
```

It's a regression that was introduced in https://github.com/joernio/joern/pull/5530
I have no idea why Michael Flanders made this change.
This is also adding a test so we catch it early next time round.

semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/Steps.scala
@@ -83,16 +83,14 @@ object Steps {
   private lazy val nodeSerializer = new CustomSerializer[AbstractNode](implicit format =>
     (
       { case _ => ??? }, // deserializer not required for now
-      { case node: Product =>
+      { case node: AbstractNode =>
         val elementMap = Map.newBuilder[String, Any]
         (0 until node.productArity).foreach { i =>
           val label   = node.productElementName(i)
           val element = node.productElement(i)
           elementMap.addOne(label -> element)
         }
-        if (node.isInstanceOf[AbstractNode]) {
-          elementMap.addOne("_label" -> node.asInstanceOf[AbstractNode].label)
-        }
+        elementMap.addOne("_label" -> node.label)
         if (node.isInstanceOf[StoredNode]) {
           elementMap.addOne("_id" -> node.asInstanceOf[StoredNode].id())
         }

semanticcpg/src/test/scala/io/shiftleft/semanticcpg/language/StepsTest.scala
@@ -141,6 +141,10 @@ class StepsTest extends AnyWordSpec with Matchers {
       val parsed = parse(json).children.head // exactly one result for the above query
       parsed shouldBe JString("asignature")
     }
+
+    "operating on regular stdlib classes" in {
+      List(1, 2, 3).toJson shouldBe "[1,2,3]"
+    }
   }
 
   ".p for pretty printing" should {