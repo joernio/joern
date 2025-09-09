8736ca12d86b8b224bd883d83ee20ecd2e9bef00
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2025-09-09 07:52:35 +0200

    [swiftsrc2cpg] Shrink Swift type map when file gets handled (#5646)
    We can safely remove everything for the currently handled file to safe heap space.

--- joern-cli/frontends/swiftsrc2cpg/src/main/scala/io/joern/swiftsrc2cpg/passes/AstCreationPass.scala
+++ joern-cli/frontends/swiftsrc2cpg/src/main/scala/io/joern/swiftsrc2cpg/passes/AstCreationPass.scala
@@ -6,11 +6,7 @@ import io.joern.swiftsrc2cpg.parser.SwiftJsonParser
 import io.joern.swiftsrc2cpg.parser.SwiftJsonParser.ParseResult
 import io.joern.swiftsrc2cpg.utils.AstGenRunner.AstGenRunnerResult
 import io.joern.swiftsrc2cpg.utils.SwiftTypesProvider
-import io.joern.swiftsrc2cpg.utils.SwiftTypesProvider.{
-  MutableSwiftFileLocalTypeMapping,
-  MutableSwiftTypeMapping,
-  SwiftFileLocalTypeMapping
-}
+import io.joern.swiftsrc2cpg.utils.SwiftTypesProvider.SwiftFileLocalTypeMapping
 import io.joern.x2cpg.ValidationMode
 import io.joern.x2cpg.datastructures.Global
 import io.joern.x2cpg.frontendspecific.swiftsrc2cpg.Defines
@@ -21,7 +17,6 @@ import io.shiftleft.utils.IOUtils
 import org.slf4j.{Logger, LoggerFactory}
 
 import java.nio.file.Paths
-import scala.collection.concurrent.TrieMap
 import scala.jdk.CollectionConverters.*
 import scala.util.{Failure, Success, Try}
 
@@ -32,7 +27,7 @@ class AstCreationPass(cpg: Cpg, astGenRunnerResult: AstGenRunnerResult, config: 
   private val logger: Logger = LoggerFactory.getLogger(classOf[AstCreationPass])
 
   private val global  = new Global()
-  private val typeMap = SwiftTypesProvider(config).map(_.retrieveMappings()).getOrElse(Map.empty)
+  private var typeMap = SwiftTypesProvider(config).map(_.retrieveMappings()).getOrElse(Map.empty)
 
   def typesSeen(): List[String] = global.usedTypes.keys().asScala.filterNot(Defines.SwiftTypes.contains).toList
 
@@ -52,13 +47,18 @@ class AstCreationPass(cpg: Cpg, astGenRunnerResult: AstGenRunnerResult, config: 
   }
 
   private def extractFileLocalTypeMap(parseResult: ParseResult): SwiftFileLocalTypeMapping = {
-    val fileLocalTypesMap = typeMap.collectFirst {
-      // special handling for GitHub Windows and MacOS runner
-      // Windows: C:\Users\RUNNER~1\AppData\Local\Temp\hash\Input\file.swift vs. C:\Users\runneradmin\AppData\Local\Temp\hash\Input\file.swift
-      // macOS: /private/var/folders/y6/hash/T/Input/file.swift vs. /var/folders/y6/hash/T/Input/file.swift
-      case (filename, map) if filename.replace("\\", "/").endsWith(parseResult.filename) => map
-    }
-    fileLocalTypesMap.getOrElse(Map.empty)
+    typeMap
+      .collectFirst {
+        // special handling for GitHub Windows and MacOS runner
+        // Windows: C:\Users\RUNNER~1\AppData\Local\Temp\hash\Input\file.swift vs. C:\Users\runneradmin\AppData\Local\Temp\hash\Input\file.swift
+        // macOS: /private/var/folders/y6/hash/T/Input/file.swift vs. /var/folders/y6/hash/T/Input/file.swift
+        case (filename, map) if filename.replace("\\", "/").endsWith(parseResult.filename) => (filename, map)
+      }
+      .map { case (filename, map) =>
+        typeMap = typeMap.removed(filename)
+        map
+      }
+      .getOrElse(Map.empty)
   }
 
   override def runOnPart(diffGraph: DiffGraphBuilder, input: String): Unit = {
