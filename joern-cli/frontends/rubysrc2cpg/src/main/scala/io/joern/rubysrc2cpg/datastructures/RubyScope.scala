9c9bed7bee844219ebc734f7313e18d926c8b64c
Tebogo Selahle <tebogo@whirlylabs.com>
2025-10-17 13:53:38 +0200

    improvement: turn lookupSelfInOuterScope into wrapper

--- joern-cli/frontends/rubysrc2cpg/src/main/scala/io/joern/rubysrc2cpg/datastructures/RubyScope.scala
+++ joern-cli/frontends/rubysrc2cpg/src/main/scala/io/joern/rubysrc2cpg/datastructures/RubyScope.scala
@@ -152,12 +152,7 @@ class RubyScope(summary: RubyProgramSummary, projectRoot: Option[String])
     }
   }
 
-  def lookupSelfInOuterScope: Option[DeclarationNew] = {
-    stack.drop(1).collectFirst {
-      case scopeElement if scopeElement.variables.contains(RubyDefines.Self) =>
-        scopeElement.variables(RubyDefines.Self)
-    }
-  }
+  def lookupSelfInOuterScope: Option[DeclarationNew] = lookupVariableInOuterScope(RubyDefines.Self).headOption
 
   def lookupSelfInCurrentScope: Option[DeclarationNew] = {
     stack.headOption.collectFirst {
