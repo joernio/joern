581336d2920cfa6f485efef8f0e0614b1aa43198
Louis van der Walt <louis@whirlylabs.com>
2026-01-21 09:06:53 +0200

    Deprecate FileUtil.deleteOnExit in swiftsrc2cpg HTTP tests

--- joern-cli/frontends/swiftsrc2cpg/src/test/scala/io/joern/swiftsrc2cpg/io/SwiftSrc2CpgHTTPServerTests.scala
+++ joern-cli/frontends/swiftsrc2cpg/src/test/scala/io/joern/swiftsrc2cpg/io/SwiftSrc2CpgHTTPServerTests.scala
@@ -19,19 +19,18 @@ class SwiftSrc2CpgHTTPServerTests extends AnyWordSpec with Matchers with BeforeA
 
   private var port: Int = -1
 
-  private def newProjectUnderTest(index: Option[Int] = None): Path = {
-    val dir  = Files.createTempDirectory("swiftsrc2cpgTestsHttpTest")
-    val file = dir / "main.swift"
-    file.createWithParentsIfNotExists(createParents = true)
-    val indexStr = index.map(_.toString).getOrElse("")
-    val content = s"""
-                     |func main() {
-                     |  println($indexStr)
-                     |}""".stripMargin
-    Files.writeString(file, content)
-    FileUtil.deleteOnExit(file)
-    FileUtil.deleteOnExit(dir)
-    dir
+  private def newProjectUnderTest[T](index: Option[Int] = None)(f: Path => T): T = {
+    FileUtil.usingTemporaryDirectory("swiftsrc2cpgTestsHttpTest") { dir =>
+      val file = dir / "main.swift"
+      file.createWithParentsIfNotExists(createParents = true)
+      val indexStr = index.map(_.toString).getOrElse("")
+      val content = s"""
+                       |func main() {
+                       |  println($indexStr)
+                       |}""".stripMargin
+      Files.writeString(file, content)
+      f(dir)
+    }
   }
 
   override def beforeAll(): Unit = {
@@ -44,39 +43,41 @@ class SwiftSrc2CpgHTTPServerTests extends AnyWordSpec with Matchers with BeforeA
 
   "Using swiftsrc2cpg in server mode" should {
     "build CPGs correctly (single test)" in {
-      val cpgOutFile = FileUtil.newTemporaryFile("swiftsrc2cpg.bin")
-      FileUtil.deleteOnExit(cpgOutFile)
-      val projectUnderTest = newProjectUnderTest()
-      val input            = projectUnderTest.toAbsolutePath.toString
-      val output           = cpgOutFile.toString
-      val client           = FrontendHTTPClient(port)
-      val req              = client.buildRequest(Array(s"input=$input", s"output=$output"))
-      client.sendRequest(req) match {
-        case Failure(exception) => fail(exception.getMessage)
-        case Success(out) =>
-          out shouldBe output
-          val cpg = CpgLoader.load(output)
-          cpg.method.name.l should contain("main")
-          cpg.call.code.l should contain("println()")
+      FileUtil.usingTemporaryFile("swiftsrc2cpg", ".bin") { cpgOutFile =>
+        newProjectUnderTest() { projectUnderTest =>
+          val input  = projectUnderTest.toAbsolutePath.toString
+          val output = cpgOutFile.toString
+          val client = FrontendHTTPClient(port)
+          val req    = client.buildRequest(Array(s"input=$input", s"output=$output"))
+          client.sendRequest(req) match {
+            case Failure(exception) => fail(exception.getMessage)
+            case Success(out) =>
+              out shouldBe output
+              val cpg = CpgLoader.load(output)
+              cpg.method.name.l should contain("main")
+              cpg.call.code.l should contain("println()")
+          }
+        }
       }
     }
 
     "build CPGs correctly (multi-threaded test)" in {
       (0 until 10).par.foreach { index =>
-        val cpgOutFile = FileUtil.newTemporaryFile("swiftsrc2cpg.bin")
-        FileUtil.deleteOnExit(cpgOutFile)
-        val projectUnderTest = newProjectUnderTest(Some(index))
-        val input            = projectUnderTest.toAbsolutePath.toString
-        val output           = cpgOutFile.toString
-        val client           = FrontendHTTPClient(port)
-        val req              = client.buildRequest(Array(s"input=$input", s"output=$output"))
-        client.sendRequest(req) match {
-          case Failure(exception) => fail(exception.getMessage)
-          case Success(out) =>
-            out shouldBe output
-            val cpg = CpgLoader.load(output)
-            cpg.method.name.l should contain("main")
-            cpg.call.code.l should contain(s"println($index)")
+        FileUtil.usingTemporaryFile("swiftsrc2cpg", ".bin") { cpgOutFile =>
+          newProjectUnderTest(Some(index)) { projectUnderTest =>
+            val input  = projectUnderTest.toAbsolutePath.toString
+            val output = cpgOutFile.toString
+            val client = FrontendHTTPClient(port)
+            val req    = client.buildRequest(Array(s"input=$input", s"output=$output"))
+            client.sendRequest(req) match {
+              case Failure(exception) => fail(exception.getMessage)
+              case Success(out) =>
+                out shouldBe output
+                val cpg = CpgLoader.load(output)
+                cpg.method.name.l should contain("main")
+                cpg.call.code.l should contain(s"println($index)")
+            }
+          }
         }
       }
     }
