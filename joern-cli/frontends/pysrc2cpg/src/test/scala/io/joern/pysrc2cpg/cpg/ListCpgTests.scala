6c8f27f8fdc72efd6b02d4dbc05b3237c8ebc478
Markus Lottmann <markus@shiftleft.io>
2026-02-02 15:48:34 +0100

    [pysrc2cpg] Add list expression tests. (#5792)

--- joern-cli/frontends/pysrc2cpg/src/test/scala/io/joern/pysrc2cpg/cpg/ListCpgTests.scala
+++ joern-cli/frontends/pysrc2cpg/src/test/scala/io/joern/pysrc2cpg/cpg/ListCpgTests.scala
@@ -1,15 +1,38 @@
 package io.joern.pysrc2cpg.cpg
 
 import io.joern.pysrc2cpg.testfixtures.Py2CpgTestContext
+import io.shiftleft.codepropertygraph.generated.DispatchTypes
 import io.shiftleft.semanticcpg.language.*
+import org.scalatest.Inside.*
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.should.Matchers
 
 class ListCpgTests extends AnyFreeSpec with Matchers {
   lazy val cpg = Py2CpgTestContext.buildCpg("""[1,2,3]""".stripMargin)
 
-  "foo" ignore {
-    // TODO
+  "test list expression node properties" in {
+    inside(cpg.call.methodFullName("<operator>.listLiteral").l) { case call :: Nil =>
+      call.code shouldBe "[1, 2, 3]"
+      call.dispatchType shouldBe DispatchTypes.STATIC_DISPATCH
+      call.lineNumber shouldBe Some(1)
+      call.columnNumber shouldBe Some(1)
+    }
+  }
+
+  "test list expression arguments" in {
+    inside(cpg.call.methodFullName("<operator>.listLiteral").l) { case call :: Nil =>
+      val arg1 = call.argument(1)
+      arg1.code shouldBe "1"
+      arg1.astParent shouldBe call
+
+      val arg2 = call.argument(2)
+      arg2.code shouldBe "2"
+      arg2.astParent shouldBe call
+
+      val arg3 = call.argument(3)
+      arg3.code shouldBe "3"
+      arg3.astParent shouldBe call
+    }
   }
 
 }
