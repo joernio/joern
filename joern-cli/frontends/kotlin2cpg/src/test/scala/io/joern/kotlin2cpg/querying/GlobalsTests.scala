c4dfaafe802324942da7c96e247f404b3521b6db
Louis van der Walt <louis@whirlylabs.com>
2026-03-13 09:29:31 +0200

    [kotlin2cpg] Global-captures-non-main

--- joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/querying/GlobalsTests.scala
+++ joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/querying/GlobalsTests.scala
@@ -12,10 +12,24 @@ class GlobalsTests extends KotlinCode2CpgFixture(withOssDataflow = false) {
         |}
         |""".stripMargin)
 
-    "should contain a LOCAL node for `AGLOBAL` with the correct props set" in {
-      val List(a) = cpg.local.nameExact("AGLOBAL").l
-      a.code shouldBe "AGLOBAL"
-      a.typeFullName shouldBe "java.lang.String"
+    "should contain a LOCAL node for `AGLOBAL` in <global> with the correct props set" in {
+      val List(globalMethod) = cpg.method.nameExact("<global>").l
+      val List(aGlobal)      = globalMethod.local.nameExact("AGLOBAL").l
+      aGlobal.code shouldBe "AGLOBAL"
+      aGlobal.typeFullName shouldBe "java.lang.String"
+      aGlobal.closureBindingId shouldBe None
+    }
+
+    "should contain a captured LOCAL node for `AGLOBAL` in `f1`" in {
+      val List(f1Local) = cpg.method.nameExact("f1").local.nameExact("AGLOBAL").l
+      f1Local.typeFullName shouldBe "java.lang.String"
+      f1Local.closureBindingId should not be None
+    }
+
+    "should have `AGLOBAL` identifier in `f1` referencing the local in `f1`" in {
+      val List(idInF1)    = cpg.method.nameExact("f1").ast.isIdentifier.nameExact("AGLOBAL").l
+      val List(localInF1) = cpg.method.nameExact("f1").local.nameExact("AGLOBAL").l
+      idInF1.refsTo.l shouldBe List(localInF1)
     }
   }
 }
