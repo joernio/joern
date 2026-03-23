63f2d48510d5df1541e975ad46bc860989d85e42
Tebogo Selahle <tn.selahle@gmail.com>
2026-03-23 10:00:07 +0200

    chore(temp): demonstrate usage

--- joern-cli/frontends/javasrc2cpg/src/test/scala/io/joern/javasrc2cpg/testfixtures/JavaSrcCodeToCpgFixture.scala
+++ joern-cli/frontends/javasrc2cpg/src/test/scala/io/joern/javasrc2cpg/testfixtures/JavaSrcCodeToCpgFixture.scala
@@ -37,7 +37,7 @@ trait JavaSrcFrontend extends LanguageFrontend {
       .getOrElse(JavaSrc2Cpg.DefaultConfig.withDelombokMode("no-delombok"))
       .withCacheJdkTypeSolver(true)
     val res = new JavaSrc2Cpg().createCpg(config.withInputPath(sourceCodeFile.getAbsolutePath)).get
-    new PostFrontendValidator(res, false).run()
+    new PostFrontendValidator(res, true, validationLevel = 0).run()
     res
   }
 }
