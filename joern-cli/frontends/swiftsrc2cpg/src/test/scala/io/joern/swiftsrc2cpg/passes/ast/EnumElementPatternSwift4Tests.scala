48d001a54f3b3b0cf24c75f964d21cff59cd505e
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2026-05-19 11:04:27 +0200

    [swiftsrc2cpg] Enable EnumElementPatternSwift4Tests with switch/throw/typeDecl assertions

--- joern-cli/frontends/swiftsrc2cpg/src/test/scala/io/joern/swiftsrc2cpg/passes/ast/EnumElementPatternSwift4Tests.scala
+++ joern-cli/frontends/swiftsrc2cpg/src/test/scala/io/joern/swiftsrc2cpg/passes/ast/EnumElementPatternSwift4Tests.scala
@@ -3,12 +3,14 @@
 package io.joern.swiftsrc2cpg.passes.ast
 
 import io.joern.swiftsrc2cpg.testfixtures.SwiftSrc2CpgSuite
+import io.shiftleft.codepropertygraph.generated.ControlStructureTypes
+import io.shiftleft.semanticcpg.language.*
 
 class EnumElementPatternSwift4Tests extends SwiftSrc2CpgSuite {
 
   "EnumElementPatternSwift4Tests" should {
 
-    "testEnumElementPatternSwift42" ignore {
+    "testEnumElementPatternSwift42" in {
       val cpg = code("""
         |enum E {
         |  case A, B, C, D
@@ -24,10 +26,15 @@ class EnumElementPatternSwift4Tests extends SwiftSrc2CpgSuite {
         |  }
         |}
         |""".stripMargin)
-      ???
+      val enumEs = cpg.typeDecl.nameExact("E").fullName.l
+      enumEs should contain("Test0.swift:<global>.E")
+      val List(testE) = cpg.method.nameExact("testE").l
+      testE.fullName shouldBe "Test0.swift:<global>.E.testE:(e:E)->ANY"
+      val List(switchStruct) = testE.controlStructure.controlStructureType(ControlStructureTypes.SWITCH).l
+      switchStruct.code should startWith("switch e")
     }
 
-    "testEnumElementPatternSwift43" ignore {
+    "testEnumElementPatternSwift43" in {
       val cpg = code("""
         |func testE(e: E) {
         |  switch e {
@@ -49,20 +56,27 @@ class EnumElementPatternSwift4Tests extends SwiftSrc2CpgSuite {
         |  else { return }
         |}
         |""".stripMargin)
-      ???
+      val List(testE) = cpg.method.nameExact("testE").l
+      testE.fullName shouldBe "Test0.swift:<global>.testE:(e:E)->ANY"
+      val List(switchStruct) = testE.controlStructure.controlStructureType(ControlStructureTypes.SWITCH).l
+      switchStruct.code should startWith("switch e")
+      val guardIfs = testE.controlStructure.controlStructureType(ControlStructureTypes.IF).l
+      guardIfs.code.l.exists(_.startsWith("guard")) shouldBe true
     }
 
-    "testEnumElementPatternSwift44" ignore {
+    "testEnumElementPatternSwift44" in {
       val cpg = code("""
         |extension E : Error {}
         |      func canThrow() throws {
         |  throw E.A
         |}
         |""".stripMargin)
-      ???
+      val List(canThrow) = cpg.method.nameExact("canThrow").l
+      canThrow.fullName shouldBe "Test0.swift:<global>.canThrow:()->ANY"
+      cpg.call.code.l should contain("E.A")
     }
 
-    "testEnumElementPatternSwift45" ignore {
+    "testEnumElementPatternSwift45" in {
       val cpg = code("""
         |do {
         |  try canThrow()
@@ -72,7 +86,9 @@ class EnumElementPatternSwift4Tests extends SwiftSrc2CpgSuite {
         |  let _: () = payload
         |}
         |""".stripMargin)
-      ???
+      val List(tryStruct) = cpg.controlStructure.controlStructureType(ControlStructureTypes.TRY).l
+      tryStruct.code should startWith("do {")
+      cpg.call.code.l should contain("canThrow()")
     }
 
   }
