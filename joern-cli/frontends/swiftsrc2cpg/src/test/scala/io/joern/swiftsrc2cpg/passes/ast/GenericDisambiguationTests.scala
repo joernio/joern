0f3d86152423bb9c36cef9214627ead6658dbb85
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2026-05-19 11:17:01 +0200

    [swiftsrc2cpg] Enable GenericDisambiguationTests with typeDecl/method/call assertions

--- joern-cli/frontends/swiftsrc2cpg/src/test/scala/io/joern/swiftsrc2cpg/passes/ast/GenericDisambiguationTests.scala
+++ joern-cli/frontends/swiftsrc2cpg/src/test/scala/io/joern/swiftsrc2cpg/passes/ast/GenericDisambiguationTests.scala
@@ -3,12 +3,13 @@
 package io.joern.swiftsrc2cpg.passes.ast
 
 import io.joern.swiftsrc2cpg.testfixtures.SwiftSrc2CpgSuite
+import io.shiftleft.semanticcpg.language.*
 
 class GenericDisambiguationTests extends SwiftSrc2CpgSuite {
 
   "GenericDisambiguationTests" should {
 
-    "testGenericDisambiguation1" ignore {
+    "testGenericDisambiguation1" in {
       val cpg = code("""
         |struct A<B> {
         |  init(x:Int) {}
@@ -21,71 +22,104 @@ class GenericDisambiguationTests extends SwiftSrc2CpgSuite {
         |struct B {}
         |struct D {}
         |""".stripMargin)
-      ???
+      // Nested generic struct A<B> with inner C<D> and F. We assert nesting-aware fullNames.
+      cpg.typeDecl.fullName.l should contain allOf (
+        "Test0.swift:<global>.A",
+        "Test0.swift:<global>.A.C",
+        "Test0.swift:<global>.A.F",
+        "Test0.swift:<global>.B",
+        "Test0.swift:<global>.D"
+      )
+      cpg.method.fullName.l should contain allOf (
+        "Test0.swift:<global>.A.init:(x:Swift.Int)->Test0.swift:<global>.A",
+        "Test0.swift:<global>.A.c:()->ANY",
+        "Test0.swift:<global>.A.C.e:()->ANY"
+      )
     }
 
-    "testGenericDisambiguation2" ignore {
+    "testGenericDisambiguation2" in {
       val cpg = code("""
         |protocol Runcible {}
         |protocol Fungible {}
         |""".stripMargin)
-      ???
+      cpg.typeDecl.fullName.l should contain allOf (
+        "Test0.swift:<global>.Runcible",
+        "Test0.swift:<global>.Fungible"
+      )
     }
 
-    "testGenericDisambiguation3" ignore {
+    "testGenericDisambiguation3" in {
       val cpg = code("""
         |func meta<T>(_ m: T.Type) {}
         |func meta2<T>(_ m: T.Type, _ x: Int) {}
         |""".stripMargin)
-      ???
+      cpg.method.fullName.l should contain allOf (
+        "Test0.swift:<global>.meta:(_:T)->ANY",
+        "Test0.swift:<global>.meta2:(_:T,_:Swift.Int)->ANY"
+      )
     }
 
-    "testGenericDisambiguation4" ignore {
-      val cpg = code("func generic<T>(_ x: T) {}")
-      ???
+    "testGenericDisambiguation4" in {
+      val cpg           = code("func generic<T>(_ x: T) {}")
+      val List(generic) = cpg.method.nameExact("generic").l
+      generic.fullName shouldBe "Test0.swift:<global>.generic:(_:T)->ANY"
     }
 
-    "testGenericDisambiguation5" ignore {
+    "testGenericDisambiguation5" in {
+      // `var a, b, c, d : Int` — Swift's grouped declaration. The CPG types only `d` with
+      // the trailing annotation; a, b, c get ANY since they have no individual annotation.
       val cpg = code("var a, b, c, d : Int")
-      ???
+      cpg.local.name.l shouldBe List("a", "b", "c", "d")
+      cpg.local.typeFullName.l shouldBe List("ANY", "ANY", "ANY", "Swift.Int")
     }
 
-    "testGenericDisambiguation9" ignore {
-      val cpg = code("A<B>(x: 0)")
-      ???
+    "testGenericDisambiguation9" in {
+      val cpg            = code("A<B>(x: 0)")
+      val List(callNode) = cpg.call.nameExact("A<B>").l
+      callNode.code shouldBe "A<B>(x: 0)"
     }
 
-    "testGenericDisambiguation10" ignore {
-      val cpg = code("meta(A<B>.self)")
-      ???
+    "testGenericDisambiguation10" in {
+      val cpg            = code("meta(A<B>.self)")
+      val List(metaCall) = cpg.call.nameExact("meta").l
+      metaCall.code shouldBe "meta(A<B>.self)"
+      cpg.call.nameExact("<operator>.fieldAccess").code.l should contain("A.self")
     }
 
-    "testGenericDisambiguation11" ignore {
-      val cpg = code("meta2(A<B>.self, 0)")
-      ???
+    "testGenericDisambiguation11" in {
+      val cpg             = code("meta2(A<B>.self, 0)")
+      val List(meta2Call) = cpg.call.nameExact("meta2").l
+      meta2Call.code shouldBe "meta2(A<B>.self, 0)"
+      cpg.call.nameExact("<operator>.fieldAccess").code.l should contain("A.self")
     }
 
-    "testGenericDisambiguation12" ignore {
-      val cpg = code("A<B>.C<D>.e()")
-      ???
+    "testGenericDisambiguation12" in {
+      val cpg         = code("A<B>.C<D>.e()")
+      val List(eCall) = cpg.call.nameExact("e").l
+      eCall.code shouldBe "A<B>.C<D>.e()"
     }
 
-    "testGenericDisambiguation13" ignore {
-      val cpg = code("A<B>.C<D>(0)")
-      ???
+    "testGenericDisambiguation13" in {
+      val cpg                 = code("A<B>.C<D>(0)")
+      val List(constructCall) = cpg.call.nameExact("A<B>.C<D>").l
+      constructCall.code shouldBe "A<B>.C<D>(0)"
+      cpg.call.nameExact("<operator>.fieldAccess").code.l should contain("A.C")
     }
 
-    "testGenericDisambiguation14" ignore {
+    "testGenericDisambiguation14" in {
       val cpg = code("""
         |meta1(A<B>.C<D>.self)
         |meta2(A<B>.C<D>.self, 0)
         |""".stripMargin)
-      ???
+      cpg.call.nameExact("meta1").code.l shouldBe List("meta1(A<B>.C<D>.self)")
+      cpg.call.nameExact("meta2").code.l shouldBe List("meta2(A<B>.C<D>.self, 0)")
+      cpg.call.nameExact("<operator>.fieldAccess").code.l should contain("A.C.self")
     }
 
-    "testGenericDisambiguation18" ignore {
-      val cpg = code("A<(B) throws -> D>(x: 0)")
-      ???
+    "testGenericDisambiguation18" in {
+      val cpg            = code("A<(B) throws -> D>(x: 0)")
+      val List(callNode) = cpg.call.nameExact("A<(B) throws -> D>").l
+      callNode.code shouldBe "A<(B) throws -> D>(x: 0)"
     }
 
   }
