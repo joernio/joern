debebc3a56d2c73d9de8a3eae1b793c4a3bc9faf
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2026-05-19 09:25:48 +0200

    [swiftsrc2cpg] Enable PatternTests for non-binding if/switch patterns

--- joern-cli/frontends/swiftsrc2cpg/src/test/scala/io/joern/swiftsrc2cpg/passes/ast/PatternTests.scala
+++ joern-cli/frontends/swiftsrc2cpg/src/test/scala/io/joern/swiftsrc2cpg/passes/ast/PatternTests.scala
@@ -1,6 +1,8 @@
 package io.joern.swiftsrc2cpg.passes.ast
 
 import io.joern.swiftsrc2cpg.testfixtures.SwiftSrc2CpgSuite
+import io.shiftleft.codepropertygraph.generated.ControlStructureTypes
+import io.shiftleft.semanticcpg.language.*
 
 class PatternTests extends SwiftSrc2CpgSuite {
 
@@ -21,27 +23,33 @@ class PatternTests extends SwiftSrc2CpgSuite {
       ???
     }
 
-    "testNonBinding3" ignore {
-      val cpg = code("if case let (y[0], z) = x {}")
-      ???
+    // testNonBinding3-6 contain invalid Swift patterns (subscript inside binding). The parser
+    // recovers and we still emit the surrounding IF/SWITCH control structure. Assert on that.
+
+    "testNonBinding3" in {
+      val cpg          = code("if case let (y[0], z) = x {}")
+      val List(ifNode) = cpg.controlStructure.controlStructureType(ControlStructureTypes.IF).l
+      ifNode.code should startWith("if case let")
     }
 
-    "testNonBinding4" ignore {
+    "testNonBinding4" in {
       val cpg = code("""
         |switch x {
         |  case let (y[0], z):
         |    z
         |}
         |""".stripMargin)
-      ???
+      val List(switchNode) = cpg.controlStructure.controlStructureType(ControlStructureTypes.SWITCH).l
+      switchNode.code should startWith("switch x")
     }
 
-    "testNonBinding5" ignore {
-      val cpg = code("if case let y[z] = x {}")
-      ???
+    "testNonBinding5" in {
+      val cpg          = code("if case let y[z] = x {}")
+      val List(ifNode) = cpg.controlStructure.controlStructureType(ControlStructureTypes.IF).l
+      ifNode.code should startWith("if case let")
     }
 
-    "testNonBinding6" ignore {
+    "testNonBinding6" in {
       val cpg = code("""
         |switch 0 {
         |  case let y[z]:
@@ -52,7 +60,8 @@ class PatternTests extends SwiftSrc2CpgSuite {
         |     0
         |}
         |""".stripMargin)
-      ???
+      val List(switchNode) = cpg.controlStructure.controlStructureType(ControlStructureTypes.SWITCH).l
+      switchNode.code should startWith("switch 0")
     }
 
   }
