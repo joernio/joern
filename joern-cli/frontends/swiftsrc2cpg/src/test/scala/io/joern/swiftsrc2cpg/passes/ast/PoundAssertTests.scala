8b329475e99a30e5d99bd8dcab5b4a7c64aa4e8c
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2026-05-19 10:54:45 +0200

    [swiftsrc2cpg] Enable PoundAssertTests with assert call/literal-argument assertions

--- joern-cli/frontends/swiftsrc2cpg/src/test/scala/io/joern/swiftsrc2cpg/passes/ast/PoundAssertTests.scala
+++ joern-cli/frontends/swiftsrc2cpg/src/test/scala/io/joern/swiftsrc2cpg/passes/ast/PoundAssertTests.scala
@@ -3,37 +3,48 @@
 package io.joern.swiftsrc2cpg.passes.ast
 
 import io.joern.swiftsrc2cpg.testfixtures.SwiftSrc2CpgSuite
+import io.shiftleft.semanticcpg.language.*
 
 class PoundAssertTests extends SwiftSrc2CpgSuite {
 
   "PoundAssertTests" should {
 
-    "testPoundAssert1" ignore {
-      val cpg = code("#assert(true, 123)")
-      ???
+    "testPoundAssert1" in {
+      val cpg              = code("#assert(true, 123)")
+      val List(assertCall) = cpg.call.nameExact("assert").l
+      assertCall.code shouldBe "#assert(true, 123)"
+      assertCall.ast.isLiteral.code.l shouldBe List("true", "123")
     }
 
-    "testPoundAssert2" ignore {
-      val cpg = code(""" #assert(true, "error \(1) message")""")
-      ???
+    "testPoundAssert2" in {
+      val cpg              = code(""" #assert(true, "error \(1) message")""")
+      val List(assertCall) = cpg.call.nameExact("assert").l
+      assertCall.code shouldBe "#assert(true, \"error \\(1) message\")"
+      assertCall.ast.isLiteral.code.l should contain("true")
     }
 
-    "testPoundAssert5" ignore {
+    "testPoundAssert5" in {
       val cpg = code("""
         |func unbalanced1() {
         |  #assert(true)
         |}
         |""".stripMargin)
-      ???
+      val List(unbalanced1) = cpg.method.nameExact("unbalanced1").l
+      val List(assertCall)  = unbalanced1.call.nameExact("assert").l
+      assertCall.code shouldBe "#assert(true)"
+      assertCall.ast.isLiteral.code.l shouldBe List("true")
     }
 
-    "testPoundAssert6" ignore {
+    "testPoundAssert6" in {
       val cpg = code("""
         |func unbalanced2() {
         |  #assert(true, "hello world")
         |}
         |""".stripMargin)
-      ???
+      val List(unbalanced2) = cpg.method.nameExact("unbalanced2").l
+      val List(assertCall)  = unbalanced2.call.nameExact("assert").l
+      assertCall.code shouldBe "#assert(true, \"hello world\")"
+      assertCall.ast.isLiteral.code.l shouldBe List("true", "\"hello world\"")
     }
 
   }
