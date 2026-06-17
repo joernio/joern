1c8e33b5f51204ba89390f1729d64f64fb06416e
Markus Lottmann <markus.lottmann@mlbox.net>
2026-06-17 12:42:06 +0200

    Make JoernRunfilesLocator private to `io.joern`.

--- joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/utils/JoernRunfilesLocator.scala
+++ joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/utils/JoernRunfilesLocator.scala
@@ -4,7 +4,9 @@ import scala.jdk.OptionConverters.RichOptional
 
 // Use this class inside Joern repository and not in repositories
 // which bring in the Joern repository as dependency.
-object JoernRunfilesLocator {
+// Restricting it to io.joern package does not fully enforce this.
+// But it will catch most cases of unintended misuse.
+private[joern] object JoernRunfilesLocator {
   def resolve(path: String): Option[String] = {
     JoernRunfilesLocatorJava.resolve(path).toScala
   }
