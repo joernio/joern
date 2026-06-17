ccf72fef01334eb884becb8679e66035378bae2b
Markus Lottmann <markus.lottmann@mlbox.net>
2026-06-17 12:37:44 +0200

    Bring in support for rubysrc2cpg.
    Need to do small modifications in the loading of external type stubs as
    the old code was overly complicated and did not allow inserting the
    resource lookup for the Bazel build.

--- joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/typestub/TypeStubUtil.scala
+++ joern-cli/frontends/x2cpg/src/main/scala/io/joern/x2cpg/typestub/TypeStubUtil.scala
@@ -5,13 +5,11 @@ import java.nio.file.{Path, Paths}
 object TypeStubUtil {
 
   /** Obtains the type stub dir for this frontend.
-    * @param metaData
-    *   meta data describing the loaded type stubs.
     * @return
     *   the directory where type stubs are.
     */
-  def typeStubDir(implicit metaData: TypeStubMetaData): Path = {
-    val dir        = metaData.packagePath.toString
+  def typeStubDir(): Path = {
+    val dir        = getClass.getProtectionDomain.getCodeSource.getLocation.toString
     val indexOfLib = dir.lastIndexOf("lib")
     val fixedDir = if (indexOfLib != -1) {
       new java.io.File(dir.substring("file:".length, indexOfLib)).toString
