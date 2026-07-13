6b205a5426f8013514b48a1f1ec550b4cdb3fe41
Max Leuthäuser <1417198+max-leuthaeuser@users.noreply.github.com>
2026-07-13 18:17:31 +0200

    [pysrc2cpg] Improve `del` representation in conjunction with lists and tuples
    Ref: QT-1191

--- joern-cli/frontends/pysrc2cpg/src/test/scala/io/joern/pysrc2cpg/cpg/DeleteCpgTests.scala
+++ joern-cli/frontends/pysrc2cpg/src/test/scala/io/joern/pysrc2cpg/cpg/DeleteCpgTests.scala
@@ -52,15 +52,111 @@ class DeleteCpgTests extends PySrc2CpgFixture with Matchers {
     }
   }
 
-  "delete of list should be represented correctly" in {
-    val cpg      = code("""del [x, y]""".stripMargin)
-    val callNode = cpg.call.methodFullNameExact("<operator>.delete").head
-    callNode.dispatchType shouldBe DispatchTypes.STATIC_DISPATCH
-    callNode.code shouldBe "del [x, y]"
-    callNode.lineNumber shouldBe Some(1)
+  "delete of list" should {
+    val cpg = code("""del [a, b]""".stripMargin)
 
-    val listArgument = callNode.argument(1)
-    listArgument.code shouldBe "[x, y]"
+    "have a single delete call node with correct properties" in {
+      val callNode = cpg.call.methodFullNameExact("<operator>.delete").loneElement
+      callNode.dispatchType shouldBe DispatchTypes.STATIC_DISPATCH
+      callNode.code shouldBe "del [a, b]"
+      callNode.lineNumber shouldBe Some(1)
+    }
+
+    "flatten list elements as direct arguments" in {
+      val deleteCall = cpg.call.methodFullNameExact("<operator>.delete").loneElement
+      deleteCall.argument.size shouldBe 2
+      deleteCall.argument.argumentIndex(1).isIdentifier.loneElement.code shouldBe "a"
+      deleteCall.argument.argumentIndex(2).isIdentifier.loneElement.code shouldBe "b"
+    }
+  }
+
+  "delete of tuple" should {
+    val cpg = code("""del (a, b)""".stripMargin)
+
+    "have a single delete call node with correct properties" in {
+      val callNode = cpg.call.methodFullNameExact("<operator>.delete").loneElement
+      callNode.dispatchType shouldBe DispatchTypes.STATIC_DISPATCH
+      callNode.code shouldBe "del (a, b)"
+      callNode.lineNumber shouldBe Some(1)
+    }
+
+    "flatten tuple elements as direct arguments" in {
+      val deleteCall = cpg.call.methodFullNameExact("<operator>.delete").loneElement
+      deleteCall.argument.size shouldBe 2
+      deleteCall.argument.argumentIndex(1).isIdentifier.loneElement.code shouldBe "a"
+      deleteCall.argument.argumentIndex(2).isIdentifier.loneElement.code shouldBe "b"
+    }
+  }
+
+  "delete of single-element tuple" should {
+    val cpg = code("""del (a,)""".stripMargin)
+
+    "have a single delete call node with correct properties" in {
+      val callNode = cpg.call.methodFullNameExact("<operator>.delete").loneElement
+      callNode.dispatchType shouldBe DispatchTypes.STATIC_DISPATCH
+      callNode.code shouldBe "del (a,)"
+      callNode.lineNumber shouldBe Some(1)
+    }
+
+    "have a single identifier argument" in {
+      val deleteCall = cpg.call.methodFullNameExact("<operator>.delete").loneElement
+      deleteCall.argument.size shouldBe 1
+      deleteCall.argument.argumentIndex(1).isIdentifier.loneElement.code shouldBe "a"
+    }
+  }
+
+  "delete of mixed plain and grouped targets" should {
+    val cpg = code("""del a, [b, c]""".stripMargin)
+
+    "have a single delete call node with correct properties" in {
+      val callNode = cpg.call.methodFullNameExact("<operator>.delete").loneElement
+      callNode.dispatchType shouldBe DispatchTypes.STATIC_DISPATCH
+      callNode.code shouldBe "del a, [b, c]"
+      callNode.lineNumber shouldBe Some(1)
+    }
+
+    "flatten all targets as direct arguments in order" in {
+      val deleteCall = cpg.call.methodFullNameExact("<operator>.delete").loneElement
+      deleteCall.argument.size shouldBe 3
+      deleteCall.argument.argumentIndex(1).isIdentifier.loneElement.code shouldBe "a"
+      deleteCall.argument.argumentIndex(2).isIdentifier.loneElement.code shouldBe "b"
+      deleteCall.argument.argumentIndex(3).isIdentifier.loneElement.code shouldBe "c"
+    }
+  }
+
+  "delete of non-name targets inside a list" should {
+    val cpg = code("""del [a.b, c[0]]""".stripMargin)
+
+    "have a single delete call node with correct properties" in {
+      val callNode = cpg.call.methodFullNameExact("<operator>.delete").loneElement
+      callNode.dispatchType shouldBe DispatchTypes.STATIC_DISPATCH
+      callNode.code shouldBe "del [a.b, c[0]]"
+      callNode.lineNumber shouldBe Some(1)
+    }
+
+    "flatten attribute and subscript targets as direct arguments" in {
+      val deleteCall = cpg.call.methodFullNameExact("<operator>.delete").loneElement
+      deleteCall.argument.size shouldBe 2
+      deleteCall.argument.argumentIndex(1).head.code shouldBe "a.b"
+      deleteCall.argument.argumentIndex(2).head.code shouldBe "c[0]"
+    }
+  }
+
+  "delete of arbitrarily nested list/tuple" should {
+    val cpg = code("""del [[a, [b]], (c,)]""".stripMargin)
+
+    "have a single delete call node with correct code" in {
+      val callNode = cpg.call.methodFullNameExact("<operator>.delete").loneElement
+      callNode.code shouldBe "del [[a, [b]], (c,)]"
+    }
+
+    "flatten all nested elements as direct arguments in order" in {
+      val deleteCall = cpg.call.methodFullNameExact("<operator>.delete").loneElement
+      deleteCall.argument.size shouldBe 3
+      deleteCall.argument.argumentIndex(1).isIdentifier.loneElement.code shouldBe "a"
+      deleteCall.argument.argumentIndex(2).isIdentifier.loneElement.code shouldBe "b"
+      deleteCall.argument.argumentIndex(3).isIdentifier.loneElement.code shouldBe "c"
+    }
   }
 
 }
