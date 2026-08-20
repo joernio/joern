9ec54a9387c1cb7e9dece86b35c03827c0c8ef4e
Xavier Pinho <mail@xavierp.org>
2026-08-20 07:00:01 +0100

    [rust2cpg] support tuple variant enum declarations.

--- joern-cli/frontends/rust2cpg/src/test/scala/io/joern/rust2cpg/passes/ast/EnumTests.scala
+++ joern-cli/frontends/rust2cpg/src/test/scala/io/joern/rust2cpg/passes/ast/EnumTests.scala
@@ -1,7 +1,8 @@
 package io.joern.rust2cpg.passes.ast
 
 import io.joern.rust2cpg.testfixtures.Rust2CpgSuite
-import io.shiftleft.codepropertygraph.generated.ModifierTypes
+import io.shiftleft.codepropertygraph.generated.{ModifierTypes, Operators}
+import io.shiftleft.codepropertygraph.generated.nodes.*
 import io.shiftleft.semanticcpg.language.*
 
 class EnumTests extends Rust2CpgSuite(noSysRoot = true) {
@@ -82,6 +83,115 @@ class EnumTests extends Rust2CpgSuite(noSysRoot = true) {
       inside(cpg.typeDecl.nameExact("Named").method.body.astChildren.isCall.l) { case assign :: Nil =>
         // TODO: pending change to self.name = name, once we remove `&` to <init> calls.
         assign.code shouldBe "(*self).name = name"
+      }
+    }
+  }
+
+  "tuple variant" should {
+    val cpg = code("""
+        |enum Color { Rgb(i32, bool) }
+        |fn main() {
+        |  let c = Color::Rgb(1, true);
+        |}
+        |""".stripMargin)
+
+    "have correct fullName" in {
+      cpg.typeDecl.nameExact("Rgb").fullName.l shouldBe List("rust2cpgtest::Color::Rgb")
+    }
+
+    "have correct inheritsFrom" in {
+      cpg.typeDecl
+        .fullNameExact("rust2cpgtest::Color::Rgb")
+        .inheritsFromTypeFullName
+        .l shouldBe List("rust2cpgtest::Color")
+    }
+
+    "have correct members" in {
+      inside(cpg.typeDecl.nameExact("Rgb").member.l) { case zero :: one :: Nil =>
+        zero.name shouldBe "0"
+        zero.code shouldBe "i32"
+        zero.typeFullName shouldBe "i32"
+
+        one.name shouldBe "1"
+        one.code shouldBe "bool"
+        one.typeFullName shouldBe "bool"
+      }
+    }
+
+    "have correct constructor" in {
+      inside(cpg.typeDecl.nameExact("Rgb").method.l) { case init :: Nil =>
+        init.name shouldBe "<init>"
+        init.fullName shouldBe "rust2cpgtest::Color::Rgb::<init>"
+        init.modifier.modifierType.l shouldBe List(ModifierTypes.CONSTRUCTOR)
+        init.methodReturn.typeFullName shouldBe "()"
+      }
+    }
+
+    "have correct constructor parameters" in {
+      inside(cpg.typeDecl.nameExact("Rgb").method.parameter.sortBy(_.index).l) { case self :: zero :: one :: Nil =>
+        self.name shouldBe "self"
+        self.index shouldBe 0
+        self.typeFullName shouldBe "rust2cpgtest::Color::Rgb"
+
+        zero.name shouldBe "0"
+        zero.index shouldBe 1
+        zero.typeFullName shouldBe "i32"
+
+        one.name shouldBe "1"
+        one.index shouldBe 2
+        one.typeFullName shouldBe "bool"
+      }
+    }
+
+    "have correct field assignments" in {
+      inside(cpg.typeDecl.nameExact("Rgb").method.body.astChildren.assignment.l) {
+        case assignZero :: assignOne :: Nil =>
+          // TODO: pending change to self.0 = 0, once we remove `&` to <init> calls.
+          assignZero.code shouldBe "(*self).0 = 0"
+          assignOne.code shouldBe "(*self).1 = 1"
+      }
+    }
+
+    "have correct ctor wrapper" in {
+      inside(cpg.method.nameExact("Rgb").l) { case ctor :: Nil =>
+        ctor.fullName shouldBe "rust2cpgtest::Color::Rgb"
+        ctor.modifier shouldBe empty
+        ctor.methodReturn.typeFullName shouldBe "rust2cpgtest::Color"
+      }
+    }
+
+    "have correct ctor wrapper parameters" in {
+      inside(cpg.method.nameExact("Rgb").parameter.sortBy(_.index).l) { case zero :: one :: Nil =>
+        zero.name shouldBe "0"
+        zero.index shouldBe 1
+        zero.typeFullName shouldBe "i32"
+
+        one.name shouldBe "1"
+        one.index shouldBe 2
+        one.typeFullName shouldBe "bool"
+      }
+    }
+
+    "have correct ctor wrapper body" in {
+      inside(cpg.method.nameExact("Rgb").body.astChildren.isCall.l) { case allocAssign :: initCall :: Nil =>
+        allocAssign.code shouldBe s"<tmp>0 = ${Operators.alloc}"
+        initCall.name shouldBe "<init>"
+        initCall.methodFullName shouldBe "rust2cpgtest::Color::Rgb::<init>"
+
+        // TODO: pending change to <init>(tmp0, 0, 1), once we remove `&` to <init> calls.
+        initCall.code shouldBe "Rgb::<init>(&<tmp>0, 0, 1)"
+        inside(initCall.argument.sortBy(_.argumentIndex).l) {
+          case (addressOf: Call) :: (zero: Identifier) :: (one: Identifier) :: Nil =>
+            addressOf.code shouldBe "&<tmp>0"
+            addressOf.argumentIndex shouldBe 0
+            addressOf.typeFullName shouldBe "&rust2cpgtest::Color::Rgb"
+
+            zero.name shouldBe "0"
+            zero.typeFullName shouldBe "i32"
+
+            one.name shouldBe "1"
+            one.typeFullName shouldBe "bool"
+        }
       }
     }
   }
