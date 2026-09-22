cd2921ad22f48452c6ab671913432cc10bc5e951
Malte Kraus <malte.kraus@harness.io>
2026-09-22 13:49:27 +0200

    use new steps in deprecated steps

--- semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/nodemethods/OpAstNodeMethods.scala
+++ semanticcpg/src/main/scala/io/shiftleft/semanticcpg/language/operatorextension/nodemethods/OpAstNodeMethods.scala
@@ -9,45 +9,41 @@ class OpAstNodeMethods[A <: AstNode](val node: A) extends AnyVal {
   @deprecated(
     "this step is very imprecise, recursively checking for matching AST children. It may return more than you intended. Consider call.isAssignment instead."
   )
-  def assignment: Iterator[OpNodes.Assignment] = astDown(allAssignmentTypes).cast[OpNodes.Assignment]
+  def assignment: Iterator[OpNodes.Assignment] = node.ast.isCall.isAssignment
 
   @deprecated(
     "this step is very imprecise, recursively checking for matching AST nodes. It may return more than you intended. Consider call.isArithmetic instead."
   )
-  def arithmetic: Iterator[OpNodes.Arithmetic] = astDown(allArithmeticTypes).cast[OpNodes.Arithmetic]
+  def arithmetic: Iterator[OpNodes.Arithmetic] = node.ast.isCall.isArithmetic
 
   @deprecated(
     "this step is very imprecise, recursively checking for matching AST nodes. It may return more than you intended. Consider call.isArrayAccess instead."
   )
-  def arrayAccess: Iterator[OpNodes.ArrayAccess] = astDown(allArrayAccessTypes).cast[OpNodes.ArrayAccess]
+  def arrayAccess: Iterator[OpNodes.ArrayAccess] = node.ast.isCall.isArrayAccess
 
   @deprecated(
     "this step is very imprecise, recursively checking for matching AST nodes. It may return more than you intended. Consider call.isFieldAccess instead."
   )
-  def fieldAccess: Iterator[OpNodes.FieldAccess] = astDown(allFieldAccessTypes).cast[OpNodes.FieldAccess]
-
-  private def astDown(callNames: Seq[String]): Iterator[Call] = node.ast.isCall.nameExact(callNames*)
+  def fieldAccess: Iterator[OpNodes.FieldAccess] = node.ast.isCall.isFieldAccess
 
   @deprecated(
     "this step is very imprecise, recursively checking for matching AST ancestors. It may return more than you intended. Consider call.isAssignment instead."
   )
-  def inAssignment: Iterator[OpNodes.Assignment] = astUp(allAssignmentTypes).cast[OpNodes.Assignment]
+  def inAssignment: Iterator[OpNodes.Assignment] = node.inAstMinusLeaf.isCall.isAssignment
 
   @deprecated(
     "this step is very imprecise, recursively checking for matching AST ancestors. It may return more than you intended. Consider call.isArithmetic instead."
   )
-  def inArithmetic: Iterator[OpNodes.Arithmetic] = astUp(allArithmeticTypes).cast[OpNodes.Arithmetic]
+  def inArithmetic: Iterator[OpNodes.Arithmetic] = node.inAstMinusLeaf.isCall.isArithmetic
 
   @deprecated(
     "this step is very imprecise, recursively checking for matching AST ancestors. It may return more than you intended. Consider call.isArrayAccess instead."
   )
-  def inArrayAccess: Iterator[OpNodes.ArrayAccess] = astUp(allArrayAccessTypes).cast[OpNodes.ArrayAccess]
+  def inArrayAccess: Iterator[OpNodes.ArrayAccess] = node.inAstMinusLeaf.isCall.isArrayAccess
 
   @deprecated(
     "this step is very imprecise, recursively checking for matching AST ancestors. It may return more than you intended. Consider call.isFieldAccess instead."
   )
-  def inFieldAccess: Iterator[OpNodes.FieldAccess] = astUp(allFieldAccessTypes).cast[OpNodes.FieldAccess]
-
-  private def astUp(strings: Seq[String]): Iterator[Call] = node.inAstMinusLeaf.isCall.nameExact(strings*)
+  def inFieldAccess: Iterator[OpNodes.FieldAccess] = node.inAstMinusLeaf.isCall.isFieldAccess
 
 }
