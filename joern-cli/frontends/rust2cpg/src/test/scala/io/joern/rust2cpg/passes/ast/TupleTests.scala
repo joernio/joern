3b3df65852cebc5685df882d8616642c63e27692
Xavier Pinho <mail@xavierp.org>
2026-09-22 10:56:08 +0100

    [rust2cpg] re-use RustOperators.tupleLiteral inside TupleTests.

--- joern-cli/frontends/rust2cpg/src/test/scala/io/joern/rust2cpg/passes/ast/TupleTests.scala
+++ joern-cli/frontends/rust2cpg/src/test/scala/io/joern/rust2cpg/passes/ast/TupleTests.scala
@@ -1,5 +1,6 @@
 package io.joern.rust2cpg.passes.ast
 
+import io.joern.rust2cpg.astcreation.RustOperators
 import io.joern.rust2cpg.testfixtures.Rust2CpgSuite
 import io.joern.x2cpg.Defines
 import io.shiftleft.codepropertygraph.generated.{DispatchTypes, Operators}
@@ -7,8 +8,6 @@ import io.shiftleft.codepropertygraph.generated.nodes.*
 import io.shiftleft.semanticcpg.language.*
 
 class TupleTests extends Rust2CpgSuite(noSysRoot = true) {
-
-  private val tupleLiteral = "<operator>.tupleLiteral"
 
   "a 2-tuple literal" should {
     val cpg = code("""
@@ -18,19 +17,19 @@ class TupleTests extends Rust2CpgSuite(noSysRoot = true) {
         |""".stripMargin)
 
     "lower to a tupleLiteral call" in {
-      inside(cpg.call.nameExact(tupleLiteral).l) { case tuple :: Nil =>
+      inside(cpg.call.nameExact(RustOperators.tupleLiteral).l) { case tuple :: Nil =>
         tuple.code shouldBe "(1, 2)"
-        tuple.methodFullName shouldBe tupleLiteral
+        tuple.methodFullName shouldBe RustOperators.tupleLiteral
         tuple.dispatchType shouldBe DispatchTypes.STATIC_DISPATCH
       }
     }
 
     "type the tupleLiteral as (i32, i32)" in {
-      cpg.call.nameExact(tupleLiteral).typeFullName.l shouldBe List("(i32, i32)")
+      cpg.call.nameExact(RustOperators.tupleLiteral).typeFullName.l shouldBe List("(i32, i32)")
     }
 
     "have the i32 literals as arguments" in {
-      inside(cpg.call.nameExact(tupleLiteral).argument.l) { case (a: Literal) :: (b: Literal) :: Nil =>
+      inside(cpg.call.nameExact(RustOperators.tupleLiteral).argument.l) { case (a: Literal) :: (b: Literal) :: Nil =>
         a.code shouldBe "1"
         a.argumentIndex shouldBe 1
         a.typeFullName shouldBe "i32"
@@ -50,25 +49,25 @@ class TupleTests extends Rust2CpgSuite(noSysRoot = true) {
         |""".stripMargin)
 
     "type the tupleLiteral with the element types" in {
-      cpg.call.nameExact(tupleLiteral).typeFullName.l shouldBe List("(i32, &str, bool)")
+      cpg.call.nameExact(RustOperators.tupleLiteral).typeFullName.l shouldBe List("(i32, &str, bool)")
     }
 
     "have the i32 literal as the first argument" in {
-      inside(cpg.call.nameExact(tupleLiteral).argument(1).l) { case (i: Literal) :: Nil =>
+      inside(cpg.call.nameExact(RustOperators.tupleLiteral).argument(1).l) { case (i: Literal) :: Nil =>
         i.code shouldBe "1"
         i.typeFullName shouldBe "i32"
       }
     }
 
     "have the &str literal as the second argument" in {
-      inside(cpg.call.nameExact(tupleLiteral).argument(2).l) { case (s: Literal) :: Nil =>
+      inside(cpg.call.nameExact(RustOperators.tupleLiteral).argument(2).l) { case (s: Literal) :: Nil =>
         s.code shouldBe "\"x\""
         s.typeFullName shouldBe "&str"
       }
     }
 
     "have the bool literal as the third argument" in {
-      inside(cpg.call.nameExact(tupleLiteral).argument(3).l) { case (b: Literal) :: Nil =>
+      inside(cpg.call.nameExact(RustOperators.tupleLiteral).argument(3).l) { case (b: Literal) :: Nil =>
         b.code shouldBe "true"
         b.typeFullName shouldBe "bool"
       }
@@ -83,28 +82,32 @@ class TupleTests extends Rust2CpgSuite(noSysRoot = true) {
         |""".stripMargin)
 
     "lower as two tupleLiteral calls" in {
-      cpg.call.nameExact(tupleLiteral).code.toSet shouldBe Set("(1, 2)", "((1, 2), 3)")
+      cpg.call.nameExact(RustOperators.tupleLiteral).code.toSet shouldBe Set("(1, 2)", "((1, 2), 3)")
     }
 
     "type the outer tupleLiteral as ((i32, i32), i32)" in {
-      cpg.call.nameExact(tupleLiteral).codeExact("((1, 2), 3)").typeFullName.l shouldBe List("((i32, i32), i32)")
+      cpg.call.nameExact(RustOperators.tupleLiteral).codeExact("((1, 2), 3)").typeFullName.l shouldBe List(
+        "((i32, i32), i32)"
+      )
     }
 
     "type the inner tupleLiteral as (i32, i32)" in {
-      cpg.call.nameExact(tupleLiteral).codeExact("(1, 2)").typeFullName.l shouldBe List("(i32, i32)")
+      cpg.call.nameExact(RustOperators.tupleLiteral).codeExact("(1, 2)").typeFullName.l shouldBe List("(i32, i32)")
     }
 
     "have the inner tupleLiteral as the first argument of the outer" in {
-      inside(cpg.call.nameExact(tupleLiteral).codeExact("((1, 2), 3)").argument(1).l) { case (inner: Call) :: Nil =>
-        inner.code shouldBe "(1, 2)"
-        inner.methodFullName shouldBe tupleLiteral
+      inside(cpg.call.nameExact(RustOperators.tupleLiteral).codeExact("((1, 2), 3)").argument(1).l) {
+        case (inner: Call) :: Nil =>
+          inner.code shouldBe "(1, 2)"
+          inner.methodFullName shouldBe RustOperators.tupleLiteral
       }
     }
 
     "have the i32 literal as the second argument of the outer" in {
-      inside(cpg.call.nameExact(tupleLiteral).codeExact("((1, 2), 3)").argument(2).l) { case (rest: Literal) :: Nil =>
-        rest.code shouldBe "3"
-        rest.typeFullName shouldBe "i32"
+      inside(cpg.call.nameExact(RustOperators.tupleLiteral).codeExact("((1, 2), 3)").argument(2).l) {
+        case (rest: Literal) :: Nil =>
+          rest.code shouldBe "3"
+          rest.typeFullName shouldBe "i32"
       }
     }
   }
