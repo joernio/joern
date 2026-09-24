98b80e8bbd72a9fe73d5d44b2207bf8a07f79f2e
Xavier Pinho <mail@xavierp.org>
2026-09-24 07:33:19 +0100

    [rust2cpg] add annotations to fn parameters.

--- joern-cli/frontends/rust2cpg/src/main/scala/io/joern/rust2cpg/astcreation/RustVisitor.scala
+++ joern-cli/frontends/rust2cpg/src/main/scala/io/joern/rust2cpg/astcreation/RustVisitor.scala
@@ -803,6 +803,7 @@ trait RustVisitor(implicit withSchemaValidation: ValidationMode) { this: AstCrea
         case Some(pat) if collectPatternBindings(pat).nonEmpty       => (code(pat), Some(pat))
         case pat                                                     => (contextStack.nextTmpName(), pat)
       }
+      val attributes   = param.attr.map(visitAttr)
       val typeFullName = typeFullNameForParam(param)
       val paramNode    = parameterInNode(
         node = param,
@@ -819,7 +820,7 @@ trait RustVisitor(implicit withSchemaValidation: ValidationMode) { this: AstCrea
         val mkParamIdentAst = () => identifierAst(pat, paramName, paramName, typeFullName)
         createLocalsForBindings(collectPatternBindings(pat)) ++ createAssignmentsForPattern(pat, mkParamIdentAst)
       }
-      (Ast(paramNode), patternAssignmentAsts)
+      (Ast(paramNode).withChildren(attributes), patternAssignmentAsts)
     }.unzip
 
     (selfParamAst ++ paramAsts, paramAssignmentAsts.flatten)
@@ -828,6 +829,7 @@ trait RustVisitor(implicit withSchemaValidation: ValidationMode) { this: AstCrea
   // SelfParam =
   //  Attr* ( ('&' Lifetime?)? 'mut'? Name | 'mut'? Name ':' Type )
   private def visitSelfParam(selfParam: SelfParam): Ast = {
+    val attributes         = selfParam.attr.map(visitAttr)
     val typeFullName       = typeFullNameForSelfParam(selfParam)
     val evaluationStrategy =
       if (selfParam.ampToken.isDefined) EvaluationStrategies.BY_SHARING else EvaluationStrategies.BY_VALUE
@@ -841,7 +843,7 @@ trait RustVisitor(implicit withSchemaValidation: ValidationMode) { this: AstCrea
       typeFullName = typeFullName
     )
     contextStack.declareParameter(paramNode)
-    Ast(paramNode)
+    Ast(paramNode).withChildren(attributes)
   }
 
   // PathExpr =
